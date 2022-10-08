a78b0c9f0d02bb7d0f63b303e30dc2c97ab4f98a
Richelieu <richelieu042@gmail.com>
2022-10-08 10:45:10 +0800

    init

--- src/component/componentKit/demo_test.go
+++ src/component/componentKit/demo_test.go
@@ -1,10 +1,10 @@
 package componentKit
 
 import (
+	"github.com/gin-gonic/gin"
 	"github.com/richelieu42/go-scales/src/core/errorKit"
 	"github.com/richelieu42/go-scales/src/core/pathKit"
 	"github.com/richelieu42/go-scales/src/core/strKit"
-	"github.com/sirupsen/logrus"
 	"testing"
 )
 
@@ -17,8 +17,13 @@ func Test(t *testing.T) {
 		errorKit.Panic("fail to initialize %s, error: %+v", "env", err)
 	}
 
+	// redis组件（可选）
 	if err := InitializeRedisComponent(); err != nil {
 		errorKit.Panic("fail to initialize %s, error: %+v", "redis", err)
+	}
+
+	if err := tmp(); err != nil {
+		errorKit.PanicByError(err)
 	}
 
 	msgProcessor := func(code string, msg string, data interface{}) string {
@@ -28,5 +33,18 @@ func Test(t *testing.T) {
 		errorKit.Panic("fail to initialize %s, error: %+v", "json", err)
 	}
 
-	logrus.Info("----------------------------------")
+	// gin组件（可选）
+	if err := InitializeGinComponent(nil, tmp1); err != nil {
+		errorKit.Panic("fail to initialize %s, error: %+v", "gin", err)
+	}
+}
+
+func tmp() error {
+	// 业务逻辑（读取配置文件...）
+	return nil
+}
+
+func tmp1(engine *gin.Engine) error {
+	// 业务逻辑（绑定路由...）
+	return nil
 }
