5abdc1a4a777f21208059d9cf88f1de60a2cef72
Richelieu <richelieu042@gmail.com>
2022-10-08 18:24:17 +0800

    init

---
+++ src/grpcKit/error.go
@@ -0,0 +1,21 @@
+package grpcKit
+
+import (
+	"github.com/richelieu42/go-scales/src/core/errorKit"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func IsDeadlineExceeded(err error) bool {
+	if err != nil {
+		// 防止多层error嵌套
+		err = errorKit.Cause(err)
+
+		if s, ok := status.FromError(err); ok {
+			if s.Code() == codes.DeadlineExceeded {
+				return true
+			}
+		}
+	}
+	return false
+}
