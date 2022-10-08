9d29d4ebffc1059f304d93eff21f82972e08200f
Richelieu <richelieu042@gmail.com>
2022-10-08 13:06:16 +0800

    init

--- src/component/componentKit/demo_test.go
+++ src/component/componentKit/demo_test.go
@@ -4,7 +4,6 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/richelieu42/go-scales/src/core/errorKit"
 	"github.com/richelieu42/go-scales/src/core/pathKit"
-	"github.com/richelieu42/go-scales/src/core/strKit"
 	"testing"
 )
 
@@ -26,30 +25,32 @@ func Test(t *testing.T) {
 		errorKit.PanicByError(err)
 	}
 
-	// gin组件（可选）
-	recoveryMiddleware := gin.CustomRecovery(func(c *gin.Context, err any) {
-		// TODO: gin处理请求时发生panic的情况，在此处进行相应的处理（比如响应json给前端）
-	})
-	if err := InitializeGinComponent(recoveryMiddleware, business1); err != nil {
-		errorKit.Panic("fail to initialize %s, error: %+v", "gin", err)
-	}
-}
-
-func business() error {
-	// TODO: 业务逻辑（读取业务配置文件、json组件...）
-
 	// json组件（可选）
 	msgProcessor := func(code string, msg string, data interface{}) string {
-		return strKit.Format("[%s] %s", code, msg)
+		// TODO: 额外处理message
+		return msg
 	}
 	if err := InitializeJsonComponent(msgProcessor, ""); err != nil {
 		errorKit.Panic("fail to initialize %s, error: %+v", "json", err)
 	}
 
+	// gin组件（可选）
+	recoveryMiddleware := gin.CustomRecovery(func(c *gin.Context, err any) {
+		// TODO: gin处理请求时发生panic的情况，在此处进行相应的处理（比如响应json给前端）
+	})
+	if err := InitializeGinComponent(recoveryMiddleware, routeBusiness); err != nil {
+		errorKit.Panic("fail to initialize %s, error: %+v", "gin", err)
+	}
+}
+
+func business() error {
+	// TODO: 业务逻辑（读取业务配置文件...）
+
 	return nil
 }
 
-func business1(engine *gin.Engine) error {
+func routeBusiness(engine *gin.Engine) error {
 	// TODO: 业务逻辑（绑定路由...）
+
 	return nil
 }
