974f151a69dea15605edcfe475ae2f4e1d63a9d5
Richelieu <richelieu042@gmail.com>
2022-10-09 16:31:23 +0800

    init

--- test/test1.go
+++ test/test1.go
@@ -8,12 +8,6 @@ import (
 func main() {
 	jsonKit.SetRespProcessor(func(resp *jsonKit.Response) any {
 		return resp
-		//return &CurJsonResponse{
-		//	Code:    resp.Code,
-		//	Message: resp.Message,
-		//	Data:    resp.Data,
-		//}
 	})
-
 	fmt.Println(jsonKit.SealFully(nil, "c", "m", "d"))
 }
