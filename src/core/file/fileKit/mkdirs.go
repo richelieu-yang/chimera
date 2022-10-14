1a3c962e3655b18648db18d64fd2549b01539663
Richelieu <richelieu042@gmail.com>
2022-10-14 15:13:36 +0800

    init

--- src/core/file/fileKit/mkdirs.go
+++ src/core/file/fileKit/mkdirs.go
@@ -12,7 +12,7 @@ PS:
 (1) 如果目录已经存在，将返回nil；
 (2) 如果 传参dirPath 对应的是个已存在的文件，将返回error（"mkdir {xxx}: not a directory"）.
 
-@param dirPath 相对路径 || 绝对路径
+@param dirPaths 目录路径s（相对路径 || 绝对路径）
 
 e.g.
 ("i:/test/test.exe") 	=> 	路径没问题且目录不存在的情况下，会在i盘创建"test"、"test.exe"两个目录
@@ -20,23 +20,36 @@ e.g.
 ("")					=>	nil（什么都不会做）
 (".")					=>	nil（什么都不会做）
 */
-func MkDirs(dirPath string) error {
-	if strKit.IsEmpty(dirPath) {
-		return nil
+func MkDirs(dirPaths ...string) (err error) {
+	for _, dirPath := range dirPaths {
+		if strKit.IsNotEmpty(dirPath) {
+			err = os.MkdirAll(dirPath, os.ModePerm)
+			if err != nil {
+				break
+			}
+		}
 	}
-	return os.MkdirAll(dirPath, os.ModePerm)
+	return
 }
 
 // MkParentDirs 为文件的父路径，创建（一级或多级）目录.
 /*
+@param filePaths 文件路径s（相对路径 || 绝对路径）
+
 e.g.
 ("")	=> nil
 (".")	=> nil
 */
-func MkParentDirs(filePath string) error {
-	// Richelieu: 为防止 import cycle
-	//dirPath := pathKit.GetParentDir(filePath)
+func MkParentDirs(filePaths ...string) (err error) {
+	for _, fp := range filePaths {
+		// Richelieu: 为防止 import cycle，不直接使用 pathKit.GetParentDir()
+		//dirPath := pathKit.GetParentDir(fp)
+		dirPath := filepath.Dir(fp)
 
-	dirPath := filepath.Dir(filePath)
-	return MkDirs(dirPath)
+		err = MkDirs(dirPath)
+		if err != nil {
+			break
+		}
+	}
+	return
 }
--- test/test1.go
+++ test/test1.go
@@ -1,64 +1,15 @@
 package main
 
 import (
-	"bufio"
-	"github.com/richelieu42/go-scales/src/core/timeKit"
-	"github.com/richelieu42/go-scales/src/log/logrusKit"
-	"github.com/sirupsen/logrus"
+	"fmt"
 	"os"
-	"time"
 )
 
 func main() {
-	rootPwd := "Cyy7587141200"
-
-	//if t, err := timeKit.CorrectSystemTime(rootPwd); err != nil {
-	//	panic(err)
-	//} else {
-	//	fmt.Println(t)
-	//}
-
-	/*
-		真实的开始结束时间: time0、time1
-		虚假的开始结束时间: timeA、timeB
-	*/
-	var time0, time1, timeA, timeB time.Time
-
-	logrusKit.InitializeByDefault()
-
-	// time0
-	time0 = time.Now()
-	logrus.Infof("time0: [%s].", timeKit.FormatTimeToString(time0, timeKit.EntireFormat))
-
-	// timeA
-	timeA, err := timeKit.ParseStringToTime(string(timeKit.CommonFormat), "2022-10-01 00:00:00.000")
+	dirPath := ""
+	err := os.MkdirAll(dirPath, os.ModePerm)
 	if err != nil {
 		panic(err)
 	}
-	logrus.Infof("timeA: [%s].", timeKit.FormatTimeToString(timeA, timeKit.EntireFormat))
-	if err := timeKit.SetSystemTime(timeA, rootPwd); err != nil {
-		panic(err)
-	}
-	logrus.Infof("System time is set to timeA(%s).", timeKit.FormatTimeToString(timeA, timeKit.EntireFormat))
-
-	// timeB
-	logrus.Info("Please enter text to continue...")
-	scanner := bufio.NewScanner(os.Stdin)
-	for scanner.Scan() {
-		text := scanner.Text()
-		logrus.Infof("input text: [%s].", text)
-		timeB = time.Now()
-		logrus.Infof("timeB: [%s].", timeKit.FormatTimeToString(timeB, timeKit.EntireFormat))
-		break
-	}
-	d := timeB.Sub(timeA)
-	logrus.Infof("timeB.Sub(timeA): [%s].", timeKit.FormatDurationToString(d))
-
-	// time1
-	time1 = time0.Add(d)
-	logrus.Infof("time1: [%s].", timeKit.FormatTimeToString(time1, timeKit.EntireFormat))
-	if err := timeKit.SetSystemTime(time1, rootPwd); err != nil {
-		panic(err)
-	}
-	logrus.Infof("System time is set to time1(%s).", timeKit.FormatTimeToString(time1, timeKit.EntireFormat))
+	fmt.Println("------------------------------")
 }
