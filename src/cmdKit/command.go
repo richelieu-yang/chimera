42d57cadc8b1eb14e64f3d96cc163a7e2089e934
Richelieu <richelieu042@gmail.com>
2022-10-19 20:43:48 +0800

    init

--- src/cmdKit/command.go
+++ src/cmdKit/command.go
@@ -1,12 +1,18 @@
 package cmdKit
 
 import (
+	"github.com/richelieu42/go-scales/src/core/strKit"
 	"os/exec"
+	"regexp"
 )
 
 // Execute 执行命令（会阻塞直到命令结束）
 func Execute(name string, args ...string) ([]byte, error) {
+	/*
+		!!!: exec.Cmd结构体执行时，会处理路径中的空格（e.g. java可执行文件的绝对路径、-Djava.ext.dirs=的路径...）
+	*/
 	cmd := exec.Command(name, args...)
+
 	return cmd.CombinedOutput()
 }
 
@@ -17,4 +23,16 @@ func ExecuteToString(name string, args ...string) (string, error) {
 		return "", err
 	}
 	return string(data), nil
+}
+
+// PolyfillCommandPath 处理命令行中的路径（主要是处理其中的空格）
+/*
+e.g.
+("/root/a b/c  d/java") => `/root/a" "b/c"  "d/java`
+*/
+func PolyfillCommandPath(path string) string {
+	re := regexp.MustCompile("\\s+")
+	return re.ReplaceAllStringFunc(path, func(s string) string {
+		return strKit.Format(`"%s"`, s)
+	})
 }
--- test/test1.go
+++ test/test1.go
@@ -1,28 +1,12 @@
 package main
 
 import (
-	"github.com/richelieu42/go-scales/src/core/file/rotateFileKit"
-	"github.com/sirupsen/logrus"
-	"os/exec"
-	"time"
+	"fmt"
+	"github.com/richelieu42/go-scales/src/cmdKit"
 )
 
 func main() {
-	writer, err := rotateFileKit.NewRotateWriter("/Users/richelieu/Downloads/tmp.log", time.Second*10, time.Second*10*6, true)
-	if err != nil {
-		panic(err)
-	}
-
-	cmd := exec.Command("java", "-jar", "/Users/richelieu/Downloads/scales.jar")
-	cmd.Stdout = writer
-	cmd.Stderr = writer
-	// 执行命令
-	if err := cmd.Start(); err != nil {
-		panic(err)
-	}
-	// 等待命令结束
-	if err := cmd.Wait(); err != nil {
-		panic(err)
-	}
-	logrus.Info("-----------------------")
+	path := "/root/a b/c  d/java"
+	path = cmdKit.PolyfillCommandPath(path)
+	fmt.Println(path)
 }
