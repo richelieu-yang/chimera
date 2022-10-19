9c1e010888dbd12c1c0609b18521d6a563a6aaee
Richelieu <richelieu042@gmail.com>
2022-10-19 21:04:14 +0800

    init

--- src/cmdKit/command.go
+++ src/cmdKit/command.go
@@ -7,7 +7,9 @@ import (
 // Execute 执行命令（会阻塞直到命令结束）
 func Execute(name string, args ...string) ([]byte, error) {
 	/*
-		!!!: exec.Cmd结构体执行时，会处理路径中的空格（e.g. java可执行文件的绝对路径、-Djava.ext.dirs=的路径...）
+		!!!:
+		(1) exec.Cmd结构体执行时，会处理路径中的空格（e.g. java可执行文件的绝对路径、-Djava.ext.dirs=的路径...）
+		(2) 假如自行处理命令行中的路径，反而会导致: 命令执行失败
 	*/
 	cmd := exec.Command(name, args...)
 
