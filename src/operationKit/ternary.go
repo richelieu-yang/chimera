c8fd567bfe0bfd7d223da5d8b3ed36dec6ceed9e
Richelieu <richelieu042@gmail.com>
2022-10-27 10:22:49 +0800

    init

--- src/operationKit/ternary.go
+++ src/operationKit/ternary.go
@@ -6,6 +6,9 @@ PS:
 (1) Golang原生不支持三目运算符，因此只能自己实现.
 (2) ternary adj.三元的，三重的
 
+参考：
+golang三目运算的实现 https://www.cnblogs.com/GetcharZp/p/15172602.html
+
 @param rst0 true 条件下的返回值
 @param rst1 false条件下的返回值
 */
