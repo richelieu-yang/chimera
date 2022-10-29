a4ee8724da1aca54ed366995aba530f67ba3fe0b
Richelieu <richelieu042@gmail.com>
2022-10-29 10:06:20 +0800

    init

--- src/wsKit/error.go
+++ src/wsKit/error.go
@@ -2,8 +2,18 @@ package wsKit
 
 import "github.com/gorilla/websocket"
 
-// IsCloseError 错误判断
-func IsCloseError(err error, codes ...int) bool {
+// IsCloseError 判断是否是 CloseError
+/*
+e.g.
+向已经关闭的Conn读取数据，会返回 CloseError.
+*/
+func IsCloseError(err error) (*websocket.CloseError, bool) {
+	ce, ok := err.(*websocket.CloseError)
+	return ce, ok
+}
+
+// IsExpectedCloseError 错误判断
+func IsExpectedCloseError(err error, codes ...int) bool {
 	return websocket.IsCloseError(err, codes...)
 }
 
