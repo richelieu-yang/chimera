c6a8d81c2c6b3edf145f7805ab77e764083e25d2
Richelieu <richelieu042@gmail.com>
2022-10-31 13:29:53 +0800

    init

--- src/wsKit/initialize.go
+++ src/wsKit/initialize.go
@@ -2,18 +2,29 @@ package wsKit
 
 import (
 	"github.com/gorilla/websocket"
-	"github.com/sirupsen/logrus"
+	"github.com/richelieu42/go-scales/src/core/file/fileKit"
+	"github.com/richelieu42/go-scales/src/core/strKit"
 )
+
+var logDir = "."
 
 // Initialize 初始化
 /*
 @param upgrader	可以为nil
 @param listener	可以为nil
-@param logger	可以为nil，默认直接输出到控制台
+@param logDir1	输出日志的目录
 */
-func Initialize(upgrader *websocket.Upgrader, listener Listener, logger *logrus.Logger) error {
+func Initialize(upgrader *websocket.Upgrader, listener Listener, logDir1 string) error {
 	setUpgrader(upgrader)
 	setListener(listener)
+
+	/* logDir */
+	if strKit.IsNotEmpty(logDir1) {
+		logDir = logDir1
+	}
+	if err := fileKit.MkDirs(logDir); err != nil {
+		return err
+	}
 
 	return nil
 }
