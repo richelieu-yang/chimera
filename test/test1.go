ec9cfeb570b211c5ba9e0f9c35b993ac80ef1287
Richelieu <richelieu042@gmail.com>
2022-11-02 09:13:02 +0800

    init

--- test/test1.go
+++ test/test1.go
@@ -2,47 +2,56 @@ package main
 
 import (
 	"fmt"
-	"github.com/richelieu42/go-scales/src/core/strKit"
+	"regexp"
 )
 
 func main() {
-	fmt.Println(strKit.SubAfterString("abcd", "bc"))
-	fmt.Println(strKit.SubBeforeString("abcd", "bc"))
+	r := regexp.MustCompile(`(?i)^CaSe.*`)
 
-	//str := "https://blog.csdn.net/weixin_44014995/article/details/120332422"
-	//
-	//re := regexp.MustCompile("\\\\")
-	//fmt.Println(re.Match([]byte("\\"))) // true
+	fmt.Println(r.MatchString("case"))  // true
+	fmt.Println(r.MatchString("CASE"))  // true
+	fmt.Println(r.MatchString("CAse"))  // true
+	fmt.Println(r.MatchString("1CAse")) // false
 }
 
-// VerifyReferer 验证 referer
-/*
-@return 验证是否成功 + 验证失败的原因
-*/
-func VerifyReferer(referer string, none bool, blocked bool) (bool, string) {
-	if strKit.IsEmpty(referer) {
-		return none, "none"
-	}
-
-	var prefix string
-	if strKit.StartWith(referer, "http://") {
-		prefix = "http://"
-	} else if strKit.StartWith(referer, "https://") {
-		prefix = "https://"
-	} else {
-		return blocked, "blocked"
-	}
-
-	referer = strKit.RemovePrefixIfExist(referer, prefix)
-
-	i := strKit.Index(referer, "/")
-	if i != -1 {
-		referer = strKit.SubBefore(referer, i)
-	}
-	i = strKit.Index(referer, ":")
-	if i != -1 {
-		referer = strKit.SubBefore(referer, i)
-	}
-
-	return false, ""
-}
+//// VerifyReferer 验证 referer
+///*
+//@return 验证是否成功 + 验证失败的原因
+//*/
+//func VerifyReferer(referer string, none bool, blocked bool, res []*regexp.Regexp) (bool, string) {
+//	if strKit.IsEmpty(referer) {
+//		return none, "none"
+//	}
+//
+//	var prefix string
+//	if strKit.StartWith(referer, "http://") {
+//		prefix = "http://"
+//	} else if strKit.StartWith(referer, "https://") {
+//		prefix = "https://"
+//	} else {
+//		return blocked, "blocked"
+//	}
+//
+//	referer = strKit.RemovePrefixIfExist(referer, prefix)
+//	referer = strKit.SubBeforeString(referer, "/")
+//	// 忽略端口号（有的话）
+//	referer = strKit.SubBeforeString(referer, ":")
+//
+//	for _, re := range res {
+//		if re.MatchString(referer) {
+//			return true, ""
+//		}
+//	}
+//	return false, "no match found"
+//
+//	//i := strKit.Index(referer, "/")
+//	//if i != -1 {
+//	//	referer = strKit.SubBefore(referer, i)
+//	//}
+//	//i = strKit.Index(referer, ":")
+//	//if i != -1 {
+//	//	referer = strKit.SubBefore(referer, i)
+//	//}
+//
+//	return false, ""
+//}
