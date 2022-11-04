de04cf4d0411d6598c2f37d4002b0b542fe7d22a
Richelieu <richelieu042@gmail.com>
2022-11-04 16:50:46 +0800

    init

--- test/test1.go
+++ test/test1.go
@@ -2,54 +2,35 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/richelieu42/go-scales/src/core/sliceKit"
-	"github.com/richelieu42/go-scales/src/core/strKit"
-	"github.com/richelieu42/go-scales/src/core/timeKit"
-	"regexp"
+	"github.com/sirupsen/logrus"
 )
 
 func main() {
-	engine := gin.Default()
+	r := gin.New()
+	r.Use(gin.Logger(), gin.Recovery())
 
-	engine.Any("/test.act", func(ctx *gin.Context) {
-		ctx.String(200, timeKit.FormatCurrentTime())
+	r.Use(func(ctx *gin.Context) {
+		/* 第1个自定义中间件 */
+		logrus.Info("[A] before")
+		ctx.Next()
+		logrus.Info("[A] after")
+	}, func(ctx *gin.Context) {
+		/* 第2个自定义中间件 */
+		logrus.Info("[B] before")
+		ctx.Abort()
+		logrus.Info("[B] after")
+	}, func(ctx *gin.Context) {
+		/* 第3个自定义中间件 */
+		logrus.Info("[C] before")
+		ctx.Next()
+		logrus.Info("[C] after")
 	})
 
-	if err := engine.Run(":80"); err != nil {
+	r.GET("/test", func(c *gin.Context) {
+		logrus.Info("[Handler]")
+		c.String(200, "OK")
+	})
+	if err := r.Run(":80"); err != nil {
 		panic(err)
 	}
-}
-
-// VerifyReferer 验证 referer
-/*
-@return 验证是否通过 + 验证失败的原因
-*/
-func VerifyReferer(referer string, none bool, blocked bool, serverNames []string, regexps []*regexp.Regexp) (bool, string) {
-	if strKit.IsEmpty(referer) {
-		return none, "none"
-	}
-
-	var prefix string
-	if strKit.StartWith(referer, "http://") {
-		prefix = "http://"
-	} else if strKit.StartWith(referer, "https://") {
-		prefix = "https://"
-	} else {
-		return blocked, "blocked"
-	}
-
-	referer = strKit.RemovePrefixIfExist(referer, prefix)
-	referer = strKit.SubBeforeString(referer, "/")
-	// 忽略端口号（有的话）
-	referer = strKit.SubBeforeString(referer, ":")
-
-	if sliceKit.ContainsStringIgnoreCase(serverNames, referer) {
-		return true, ""
-	}
-	for _, re := range regexps {
-		if re.MatchString(referer) {
-			return true, ""
-		}
-	}
-	return false, "no match found"
 }
