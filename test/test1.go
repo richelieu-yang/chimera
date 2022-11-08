6079bdf2b6e76d9152af2936835e239375c24430
Richelieu <richelieu042@gmail.com>
2022-11-08 13:08:13 +0800

    init

--- test/test1.go
+++ test/test1.go
@@ -1,42 +1,14 @@
 package main
 
-import (
-	"github.com/gin-gonic/gin"
-	"github.com/richelieu42/go-scales/src/ginKit"
-	"github.com/sirupsen/logrus"
-)
+import "fmt"
 
 func main() {
-	r := gin.New()
+	var s []interface{}
+	var s1 []interface{}
 
-	middleware, err := ginKit.NewRefererMiddleware(true, false, "", "*.yozo.com")
-	if err != nil {
-		panic(err)
-	}
-	r.Use(gin.Logger(), gin.Recovery(), middleware)
+	s = append(s)
+	fmt.Println(s == nil) // true
 
-	r.Use(func(ctx *gin.Context) {
-		/* 第1个自定义中间件 */
-		logrus.Info("[A] before")
-		ctx.Next()
-		logrus.Info("[A] after")
-	}, func(ctx *gin.Context) {
-		/* 第2个自定义中间件 */
-		logrus.Info("[B] before")
-		ctx.Next()
-		logrus.Info("[B] after")
-	}, func(ctx *gin.Context) {
-		/* 第3个自定义中间件 */
-		logrus.Info("[C] before")
-		ctx.Next()
-		logrus.Info("[C] after")
-	})
-
-	r.GET("/test", func(c *gin.Context) {
-		logrus.Info("[Handler]")
-		c.String(200, "OK")
-	})
-	if err := r.Run(":80"); err != nil {
-		panic(err)
-	}
+	s = append(s, s1...)
+	fmt.Println(s == nil) // true
 }
