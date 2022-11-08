27bd216b9ba1e4028d72c25ef0e83924ca37c37c
Richelieu <richelieu042@gmail.com>
2022-11-08 09:18:59 +0800

    init

---
+++ src/http/refererKit/ginMiddleware.go
@@ -0,0 +1,35 @@
+package refererKit
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+// NewGinRefererMiddlewares
+/*
+@return err == nil的情况下，第一个返回值必定不为nil
+*/
+func NewGinRefererMiddlewares(builders []*RefererVerifierBuilder) ([]gin.HandlerFunc, error) {
+	middlewares := make([]gin.HandlerFunc, 0, len(builders))
+
+	for _, builder := range builders {
+		if builder != nil {
+			verifier, err := builder.Build()
+			if err != nil {
+				return nil, err
+			}
+			middlewares = append(middlewares, func(ctx *gin.Context) {
+				if ok, reason := verifier.VerifyByGinContext(ctx); ok {
+					ctx.Next()
+				} else {
+					// http.StatusUnauthorized: 404
+					ctx.AbortWithStatusJSON(http.StatusUnauthorized, map[string]interface{}{
+						"reason": reason,
+					})
+				}
+			})
+		}
+	}
+	return middlewares, nil
+
+}
