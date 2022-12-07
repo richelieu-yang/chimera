3505a18118ba40abe246cf985375c31eec4390a3
Richelieu <richelieu042@gmail.com>
2022-12-07 14:18:20 +0800

    zapKit

--- test/test1.go
+++ test/test1.go
@@ -1,18 +1,5 @@
 package main
 
-import (
-	"github.com/richelieu42/go-scales/src/log/zapKit"
-	"go.uber.org/zap"
-)
-
 func main() {
-	path := "logs/c/rocketmq_client_go.log"
-	maxFileSize := 1073741824
-	maxFileIndex := 10
 
-	logger, err := zapKit.NewSugaredLogger(path, maxFileSize, maxFileIndex, false, zap.InfoLevel)
-	if err != nil {
-		panic(err)
-	}
-	logger.Infof("[TEST] %s %d", "ccc", 999)
 }
