c001c4bbe0fc65c3f47fc5f71bc363ed494bb1b4
Richelieu <richelieu042@gmail.com>
2022-12-15 10:12:10 +0800

    httpClientKit

--- src/cpmponent_demo.go
+++ src/cpmponent_demo.go
@@ -1,22 +1,17 @@
-package componentKit
+package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/richelieu42/go-scales/src/component/componentKit"
 	"github.com/richelieu42/go-scales/src/core/errorKit"
-	"github.com/richelieu42/go-scales/src/core/pathKit"
 	"github.com/richelieu42/go-scales/src/ginKit"
 	"github.com/richelieu42/go-scales/src/jsonKit"
 	"net/http"
-	"testing"
 	"time"
 )
 
-func Test(t *testing.T) {
-	if err := pathKit.ReviseProjectDirWhenTesting(); err != nil {
-		panic(err)
-	}
-
-	if err := InitializeEnvironment(); err != nil {
+func main() {
+	if err := componentKit.InitializeEnvironment(); err != nil {
 		errorKit.Panic("fail to initialize %s, error: %+v", "env", err)
 	}
 
@@ -26,9 +21,9 @@ func Test(t *testing.T) {
 	}
 
 	// redis组件（可选）
-	//if err := InitializeRedisComponent(); err != nil {
-	//	errorKit.Panic("fail to initialize %s, error: %+v", "redis", err)
-	//}
+	if err := componentKit.InitializeRedisComponent(); err != nil {
+		errorKit.Panic("fail to initialize %s, error: %+v", "redis", err)
+	}
 
 	// json组件（可选）
 	var msgProcessor jsonKit.MessageProcessor = func(code string, msg string, data interface{}) string {
@@ -39,7 +34,7 @@ func Test(t *testing.T) {
 		// TODO: 额外处理响应结构体的key
 		return resp
 	}
-	if err := InitializeJsonComponent("", msgProcessor, respProcessor); err != nil {
+	if err := componentKit.InitializeJsonComponent("", msgProcessor, respProcessor); err != nil {
 		errorKit.Panic("fail to initialize %s, error: %+v", "json", err)
 	}
 
@@ -47,7 +42,7 @@ func Test(t *testing.T) {
 	recoveryMiddleware := gin.CustomRecovery(func(c *gin.Context, err any) {
 		// TODO: gin处理请求时发生panic的情况，在此处进行相应的处理（比如响应json给前端）
 	})
-	if err := InitializeGinComponent(recoveryMiddleware, routeBusiness); err != nil {
+	if err := componentKit.InitializeGinComponent(recoveryMiddleware, routeBusiness); err != nil {
 		errorKit.Panic("fail to initialize %s, error: %+v", "gin", err)
 	}
 }
--- test/test1.go
+++ test/test1.go
@@ -1,5 +1,8 @@
 package main
 
-func main() {
+import "github.com/richelieu42/go-scales/src/component/componentKit"
 
+func main() {
+	client := componentKit.GetRedisClient()
+	client.
 }
