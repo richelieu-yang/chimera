5feecc8af95db1ae0e32d3f72aa5e0d48358c486
Richelieu <richelieu042@gmail.com>
2022-12-15 13:28:24 +0800

    init

--- src/jsonKit/bean.go
+++ src/jsonKit/bean.go
@@ -5,7 +5,7 @@ package jsonKit
 响应给前端的json对象.
 */
 type Response struct {
-	Code    string      `json:"code" example:"0"`
-	Message string      `json:"message" example:"no error"`
+	Code    string      `json:"code"`
+	Message string      `json:"message"`
 	Data    interface{} `json:"data,omitempty"`
 }
