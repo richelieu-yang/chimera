8d265e4812d196ab87aaf614c402cb988489ee25
Richelieu <richelieu042@gmail.com>
2022-12-18 14:20:50 +0800

    init

--- src/core/mapKit/decode.go
+++ src/core/mapKit/decode.go
@@ -2,6 +2,7 @@ package mapKit
 
 import (
 	"github.com/mitchellh/mapstructure"
+	"github.com/richelieu42/go-scales/src/core/pointerKit"
 )
 
 // Decode 将 通用的map[string]interface{} 解码到对应的 Go结构体中 ，或者执行相反的操作。
@@ -11,8 +12,11 @@ PS:
 (2) 字段标签(tag)不同于json，详见: https://mp.weixin.qq.com/s/n_AXYv-p_ev-q23mhpDkOg .
 
 @param input	structure（包括map）
-@param output 	指针，must be a pointer to a map or struct
+@param output 	必须是（map或结构体的）指针
 */
 func Decode(input interface{}, output interface{}) error {
+	if err := pointerKit.AssertPointer(output, "output"); err != nil {
+		return err
+	}
 	return mapstructure.Decode(input, output)
 }
--- test/test1.go
+++ test/test1.go
@@ -2,23 +2,25 @@ package main
 
 import (
 	"fmt"
-	"github.com/richelieu42/go-scales/src/jsonKit"
+	"github.com/mitchellh/mapstructure"
+)
+
+type (
+	User struct {
+		Id   int
+		Name string
+	}
 )
 
 func main() {
-	str, err := jsonKit.MarshalToString("")
-	fmt.Println(str)
-	fmt.Println(err)
+	u := &User{
+		Id:   111,
+		Name: "张三",
+	}
+	m := make(map[string]interface{})
 
-	//m := map[string]interface{}{
-	//	"a": 1,
-	//	"b": 2,
-	//}
-	//fmt.Println(m)
-	//
-	//fmt.Println(mapKit.Remove(m, "b"))
-	//fmt.Println(m)
-	//
-	//fmt.Println(mapKit.Remove(m, "b"))
-	//fmt.Println(m)
+	if err := mapstructure.Decode(u, m); err != nil {
+		panic(err)
+	}
+	fmt.Println(m)
 }
