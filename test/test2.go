255652f284b81025531a34b5615458741b68cb3a
Richelieu <richelieu042@gmail.com>
2022-12-21 15:19:19 +0800

    init

--- test/test2.go
+++ test/test2.go
@@ -1,19 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"github.com/richelieu42/go-scales/src/core/file/fileKit"
-)
-
 func main() {
-	if err := fileKit.MkDirs("."); err != nil {
-		panic(err)
-	}
-	if err := fileKit.MkDirs("./"); err != nil {
-		panic(err)
-	}
-	if err := fileKit.MkDirs("/"); err != nil {
-		panic(err)
-	}
-	fmt.Println("---------------------------------------")
+
 }
