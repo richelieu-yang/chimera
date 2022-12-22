9d7aadc7c3d11e7fe76f900a4b8ba2f521e9e6cc
Richelieu <richelieu042@gmail.com>
2022-12-22 15:08:28 +0800

    init

--- src/copyKit/copy.go
+++ src/copyKit/copy.go
@@ -4,6 +4,7 @@ import (
 	"github.com/jinzhu/copier"
 	"github.com/mohae/deepcopy"
 	"github.com/richelieu42/go-scales/src/core/errorKit"
+	"github.com/richelieu42/go-scales/src/core/interfaceKit"
 )
 
 // DeepCopyStruct 深拷贝结构体
@@ -17,13 +18,21 @@ func DeepCopyStruct(toValue interface{}, fromValue interface{}) error {
 	return copier.Copy(toValue, fromValue)
 }
 
-// DeepCopy 深拷贝，支持：map、结构体...
+// DeepCopy 深拷贝，支持: slice、map、结构体...
 /*
+PS:
+参考: 「Go工具箱」推荐一个非常简单的深拷贝工具：deepcopy https://mp.weixin.qq.com/s/e3bL1i6WT-4MwK-SEpUa6Q；
+
 @param src 指针 || 结构体实例
+@return e.g. (interface{}(nil)) => (nil, nil)
 */
 func DeepCopy[T any](src T) (dest T, err error) {
-	obj := deepcopy.Copy(src)
+	// 处理nil的情况
+	if interfaceKit.IsNil(src) {
+		return
+	}
 
+	obj := deepcopy.Copy(src)
 	if t, ok := obj.(T); ok {
 		dest = t
 		return
--- test/test1.go
+++ test/test1.go
@@ -3,37 +3,50 @@ package main
 import (
 	"fmt"
 	"github.com/richelieu42/go-scales/src/copyKit"
-	"github.com/richelieu42/go-scales/src/jsonKit"
 )
 
 type (
 	Bean struct {
 		Id int
-		U  *User
-	}
-
-	User struct {
-		Name string
 	}
 )
 
 func main() {
-	u := &User{
-		Name: "张三",
-	}
-	b := &Bean{
-		Id: 666,
-		U:  u,
-	}
-	b1, err := copyKit.DeepCopy(b)
+	//var c interface{} = nil
+
+	c1, err := copyKit.DeepCopy(interface{}(nil))
 	if err != nil {
 		panic(err)
 	}
+	fmt.Println(c1)
 
-	fmt.Printf("%+v\n", b)
-	fmt.Printf("%+v\n", b1)
+	//b := &Bean{
+	//	Id: 666,
+	//}
+	//
+	//s := []*Bean{b}
+	//s1, err := copyKit.DeepCopy(s)
+	//if err != nil {
+	//	panic(err)
+	//}
+	//fmt.Printf("%+v\n", s)
+	//fmt.Printf("%+v\n", s1)
+	//
+	//fmt.Println(jsonKit.MarshalToString(s))
+	//fmt.Println(jsonKit.MarshalToString(s1))
+	//s[0].Id = 999
+	//fmt.Println(jsonKit.MarshalToString(s))
+	//fmt.Println(jsonKit.MarshalToString(s1))
 
-	u.Name = "李四"
-	fmt.Println(jsonKit.MarshalToString(b))
-	fmt.Println(jsonKit.MarshalToString(b1))
+	//b1, err := copyKit.DeepCopy(b)
+	//if err != nil {
+	//	panic(err)
+	//}
+	//
+	//fmt.Printf("%+v\n", b)
+	//fmt.Printf("%+v\n", b1)
+	//
+	//u.Name = "李四"
+	//fmt.Println(jsonKit.MarshalToString(b))
+	//fmt.Println(jsonKit.MarshalToString(b1))
 }
