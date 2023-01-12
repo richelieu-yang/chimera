0ae630b8ef4786b75a3d68b621b25f695d37d2d4
Richelieu <richelieu042@gmail.com>
2023-01-12 16:02:10 +0800

    init

--- src/database/redisKit/type_string.go
+++ src/database/redisKit/type_string.go
@@ -16,8 +16,8 @@ import (
 					 		-1(即 redis.KeepTTL): 	保持已经存在的TTL.(1)如果key不存在，则TTL为-1；(2)如果"Redis服务器版本"<6.0，会报错：ERR syntax error.
 @return 第一个返回值代表: 是否设置成功
 */
-func (client Client) Set(key string, value interface{}, expiration time.Duration) (bool, error) {
-	reply, err := client.UC.Set(context.TODO(), key, value, expiration).Result()
+func (client Client) Set(ctx context.Context, key string, value interface{}, expiration time.Duration) (bool, error) {
+	reply, err := client.UC.Set(ctx, key, value, expiration).Result()
 	if err != nil {
 		return false, err
 	}
@@ -28,8 +28,8 @@ func (client Client) Set(key string, value interface{}, expiration time.Duration
 /*
 @return 第一个返回值代表: 是否设置成功
 */
-func (client Client) SetNX(key string, value interface{}, expiration time.Duration) (bool, error) {
-	return client.UC.SetNX(context.TODO(), key, value, expiration).Result()
+func (client Client) SetNX(ctx context.Context, key string, value interface{}, expiration time.Duration) (bool, error) {
+	return client.UC.SetNX(ctx, key, value, expiration).Result()
 }
 
 // Get
@@ -38,8 +38,8 @@ PS:
 (1) 如果当前db中不存在传参key，将返回 ("", redis.Nil)
 (2) 如果对应value的类型不为string，会返回error: WRONGTYPE Operation against a key holding the wrong kind of value
 */
-func (client Client) Get(key string) (string, error) {
-	return client.UC.Get(context.TODO(), key).Result()
+func (client Client) Get(ctx context.Context, key string) (string, error) {
+	return client.UC.Get(ctx, key).Result()
 }
 
 // GetWithoutRedisNil
@@ -49,13 +49,13 @@ PS：
 (2) 如果不关心key是否存在，只关心值，可以调用此方法
 (3) 如果对应value的类型不为string，会返回error:	WRONGTYPE Operation against a key holding the wrong kind of value
 */
-func (client Client) GetWithoutRedisNil(key string) (string, error) {
-	str, err := client.Get(key)
+func (client Client) GetWithoutRedisNil(ctx context.Context, key string) (string, error) {
+	str, err := client.Get(ctx, key)
 	if err != nil {
 		if err != redis.Nil {
 			return "", err
 		}
-		// 当前db中不存在传参key的情况
+		// err == redis.Nil: 当前db中不存在 传参key
 		return "", nil
 	}
 	return str, nil
