86f46d7dfe600fbe9c78dcf22a37f10870a08bca
Richelieu <richelieu042@gmail.com>
2023-01-17 14:08:16 +0800

    init

--- src/database/redisKit/command_sorted_set.go
+++ src/database/redisKit/command_sorted_set.go
@@ -5,57 +5,74 @@ import (
 	"github.com/go-redis/redis/v9"
 )
 
+// ZAdd
 /*
-Redis 有序集合(sorted set): https://www.runoob.com/redis/redis-sorted-sets.html
-*/
-
-// ZAdd [增]向有序集合添加一个或多个成员，或者更新已存在成员的分数
-/*
-@param key 如果在db中不存在的话，会自动创建
+命令说明:	将一个或多个成员元素及其分数值加入到有序集当中。
+			(1) 如果某个成员已经是有序集的成员，那么更新这个成员的分数值，并通过重新插入这个成员元素，来保证该成员在正确的位置上。
+			(2) 分数值可以是 整数值 或 双精度浮点数。
+			(3) 如果有序集合 key 不存在，则创建一个空的有序集并执行 ZADD 操作。
+			(4) 当 key 存在但不是有序集类型时，返回一个错误。
+命令语法:	ZADD KEY_NAME SCORE1 VALUE1.. SCOREN VALUEN
+命令返回值:	被成功添加的新成员的数量，不包括那些被更新的、已经存在的成员。
 */
 func (client *Client) ZAdd(ctx context.Context, key string, members ...redis.Z) (int64, error) {
-	return client.goRedisClient.ZAdd(ctx, key, members...).Result()
+	intCmd := client.goRedisClient.ZAdd(ctx, key, members...)
+	return intCmd.Result()
 }
 
-// ZRem [删]移除有序集合中的一个或多个成员
+// ZRem
 /*
-@return 如果传参key在db中不存在的话，将返回(0, nil)
+命令说明:	移除有序集中的一个或多个成员，不存在的成员将被忽略。(当 key 存在但不是有序集类型时，返回一个错误。)
+命令语法:	ZREM key member [member ...]
+命令返回值:	被成功移除的成员的数量，不包括被忽略的成员。
 */
 func (client *Client) ZRem(ctx context.Context, key string, members ...interface{}) (int64, error) {
-	return client.goRedisClient.ZRem(ctx, key, members...).Result()
+	intCmd := client.goRedisClient.ZRem(ctx, key, members...)
+	return intCmd.Result()
 }
 
-// ZRangeByScore 通过分数返回有序集合指定区间内的成员，分数 从低到高 排序
+// ZRangeByScore
 /*
-具体如何调用可以参考 ZRevRangeByScore.
+命令说明:
+	返回有序集合中指定分数区间的成员列表。有序集成员按分数值递增(从小到大)次序排列。
+	具有相同分数值的成员按字典序来排列(该属性是有序集提供的，不需要额外的计算)。
+	默认情况下，区间的取值使用闭区间 (小于等于或大于等于)，你也可以通过给参数前增加 ( 符号来使用可选的开区间 (小于或大于)。
+命令语法:	ZRANGEBYSCORE key min max [WITHSCORES] [LIMIT offset count]
+命令返回值:	指定区间内，带有分数值(可选)的有序集成员的列表。
 */
 func (client *Client) ZRangeByScore(ctx context.Context, key string, opt *redis.ZRangeBy) ([]string, error) {
-	return client.goRedisClient.ZRangeByScore(ctx, key, opt).Result()
+	stringSliceCmd := client.goRedisClient.ZRangeByScore(ctx, key, opt)
+	return stringSliceCmd.Result()
 }
 
-func (client *Client) ZRangeByScoreWithScores(ctx context.Context, key string, opt *redis.ZRangeBy) ([]redis.Z, error) {
-	return client.goRedisClient.ZRangeByScoreWithScores(ctx, key, opt).Result()
-}
-
-// ZRevRangeByScore 返回有序集中指定分数区间内的成员，分数 从高到低 排序
+// ZRangeByScoreWithScores
 /*
-ZREVRANGEBYSCORE命令的语法: ZREVRANGEBYSCORE key max min [WITHSCORES] [LIMIT offset count]
+详见: ZRangeByScore，命令中有 WITHSCORES.
+*/
+func (client *Client) ZRangeByScoreWithScores(ctx context.Context, key string, opt *redis.ZRangeBy) ([]redis.Z, error) {
+	zSliceCmd := client.goRedisClient.ZRangeByScoreWithScores(ctx, key, opt)
+	return zSliceCmd.Result()
+}
 
-@param key	如果在db中不存在的话，将返回([], nil)（[]string实例的len和cap都为0）.
-@param opt	Max属性: 可以为"+inf"，Min属性: 可以为"-inf".
-			默认情况下，区间的取值使用闭区间 (小于等于或大于等于)，你也可以通过给参数前增加 "("符号 来使用可选的开区间 (小于或大于).
-			Offset属性: 	偏移量，从第几个开始（默认从0开始，即不偏移），但需要注意：设置此属性的值非0时，也要设置Count属性！！！
-			Count属性: 	取几条数据？默认0，即返回所有符合条件的数据.（设置此属性时，可不设置Offset属性）
-
-e.g. ZREVRANGEBYSCORE命令
-ZRANGEBYSCORE zset (1 5		返回所有符合条件 1 < score <= 5 的成员
-ZRANGEBYSCORE zset (5 (10	返回所有符合条件 5 < score < 10 的成员
+// ZRevRangeByScore
+/*
+命令说明:
+	返回有序集中指定分数区间内的所有的成员。有序集成员按分数值递减(从大到小)的次序排列。
+	具有相同分数值的成员按字典序的逆序(reverse lexicographical order )排列。
+	除了成员按分数值递减的次序排列这一点外， ZREVRANGEBYSCORE 命令的其他方面和 ZRANGEBYSCORE 命令一样。
+命令语法:	ZREVRANGEBYSCORE key max min [WITHSCORES] [LIMIT offset count]
+命令返回值:	指定区间内，带有分数值(可选)的有序集成员的列表。
 */
 func (client *Client) ZRevRangeByScore(ctx context.Context, key string, opt *redis.ZRangeBy) ([]string, error) {
-	return client.goRedisClient.ZRevRangeByScore(ctx, key, opt).Result()
+	stringSliceCmd := client.goRedisClient.ZRevRangeByScore(ctx, key, opt)
+	return stringSliceCmd.Result()
 }
 
-// ZRevRangeByScoreWithScores 类似 ZRevRangeByScore，返回值元素中多了score属性
+// ZRevRangeByScoreWithScores
+/*
+详见: ZRevRangeByScore，命令中有 WITHSCORES.
+*/
 func (client *Client) ZRevRangeByScoreWithScores(ctx context.Context, key string, opt *redis.ZRangeBy) ([]redis.Z, error) {
-	return client.goRedisClient.ZRevRangeByScoreWithScores(ctx, key, opt).Result()
+	zSliceCmd := client.goRedisClient.ZRevRangeByScoreWithScores(ctx, key, opt)
+	return zSliceCmd.Result()
 }
