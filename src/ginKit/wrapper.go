080533d072a6be92ab58df0c4f4c175b00eee354
Richelieu <richelieu042@gmail.com>
2023-01-18 10:11:09 +0800

    init

--- src/ginKit/wrapper.go
+++ src/ginKit/wrapper.go
@@ -4,12 +4,13 @@ import "github.com/gin-gonic/gin"
 
 type Handler func(ctx *gin.Context) (*ResponsePackage, error)
 
-// WrapToHandlerFunc 封装为 gin.HandlerFunc 类型
+// WrapToHandlerFunc Handler 类型 => gin.HandlerFunc 类型
 func WrapToHandlerFunc(handler Handler) gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		if handler == nil {
 			return
 		}
+
 		pack, err := handler(ctx)
 		RespondPackageOrError(ctx, pack, err)
 	}
