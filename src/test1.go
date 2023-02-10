5770400a4253dfe33c4006edd0d3de199e0b593b
Richelieu <miro42@163.com>
2023-02-10 13:26:05 +0800

    init

--- src/test1.go
+++ src/test1.go
@@ -1,12 +1,33 @@
 package main
 
-import "fmt"
-
-var (
-	Author    string
-	GoVersion string
+import (
+	"context"
+	"fmt"
+	"github.com/richelieu42/go-scales/src/database/redisKit"
 )
 
 func main() {
-	fmt.Println(666)
+	config := &redisKit.RedisConfig{
+		UserName: "",
+		Password: "",
+		Mode:     redisKit.SingleNodeMode,
+		SingleNodeConfig: &redisKit.SingleNodeConfig{
+			Addr: "127.0.0.1:6379",
+			DB:   10,
+		},
+		MasterSlaverConfig: nil,
+		SentinelConfig:     nil,
+		ClusterConfig:      nil,
+	}
+	client, err := redisKit.NewClient(config)
+	if err != nil {
+		panic(err)
+	}
+
+	pubsub := client.Subscribe(context.TODO(), "")
+	ch := pubsub.Channel()
+	//fmt.Println(client.Ping(context.TODO()))
+	for msg := range ch {
+		fmt.Println(msg.Channel, msg.Payload)
+	}
 }
