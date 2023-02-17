32ee344b7543f2b92040b415ad093ea644e576f8
Richelieu <richelieu042@gmail.com>
2023-02-17 13:10:47 +0800

    init

--- src/test1.go
+++ src/test1.go
@@ -1,11 +1,14 @@
 package main
 
-import (
-	"fmt"
-	"path/filepath"
+type (
+	Bean struct {
+		// Count 剩余可以开档的（加密文档）次数
+		Count int
+		// Extra 用于开档的数据（谁 开哪个 文档）
+		Extra string
+	}
 )
 
 func main() {
-	fmt.Println(filepath.Clean("./1.txt"))      // "1.txt"
-	fmt.Println(filepath.Clean("/root/.././c")) // "/c"
+
 }
