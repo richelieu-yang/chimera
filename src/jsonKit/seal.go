47025083e69173bd486b5ca8055b28990d52219c
Richelieu <richelieu042@gmail.com>
2023-02-17 13:38:01 +0800

    init

--- src/jsonKit/seal.go
+++ src/jsonKit/seal.go
@@ -11,21 +11,21 @@ import (
 /*
 @param args 不传参的情况，值为nil
 */
-func Seal(code string, args ...interface{}) string {
-	return SealWithData(code, nil, args...)
+func Seal(code string, msgArgs ...interface{}) string {
+	return SealWithData(code, nil, msgArgs...)
 }
 
-func SealWithData(code string, data interface{}, args ...interface{}) string {
-	json, _ := SealFully(jsoniter.ConfigDefault, code, "", data, args...)
+func SealWithData(code string, data interface{}, msgArgs ...interface{}) string {
+	json, _ := SealFully(jsoniter.ConfigDefault, code, "", data, msgArgs...)
 	return json
 }
 
-func SealFully(api jsoniter.API, code, message string, data interface{}, args ...interface{}) (json string, err error) {
+func SealFully(api jsoniter.API, code, message string, data interface{}, msgArgs ...interface{}) (json string, err error) {
 	if api == nil {
 		api = jsoniter.ConfigDefault
 	}
 
-	message = getFinalMessage(code, message, data, args...)
+	message = getFinalMessage(code, message, data, msgArgs...)
 	resp := &Response{Code: code, Message: message, Data: data}
 
 	var obj any
@@ -37,12 +37,12 @@ func SealFully(api jsoniter.API, code, message string, data interface{}, args ..
 	return MarshalToStringWithJsoniterApi(api, obj)
 }
 
-func getFinalMessage(code, msg string, data interface{}, args ...interface{}) string {
+func getFinalMessage(code, msg string, data interface{}, msgArgs ...interface{}) string {
 	if strKit.IsEmpty(msg) {
 		msg = msgKit.GetMsg(code)
 	}
-	if strKit.IsNotEmpty(msg) && args != nil {
-		msg = fmt.Sprintf(msg, args...)
+	if strKit.IsNotEmpty(msg) && msgArgs != nil {
+		msg = fmt.Sprintf(msg, msgArgs...)
 	}
 	if msgProcessor != nil {
 		msg = msgProcessor(code, msg, data)
