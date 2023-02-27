610cdb57ecd3fcf8196e015f28ff02a749674fc5
Richelieu <richelieu042@gmail.com>
2023-02-27 13:55:38 +0800

    init

--- src/mq/rocketmq5Kit/verify_endpoint_test.go
+++ src/mq/rocketmq5Kit/verify_endpoint_test.go
@@ -3,7 +3,7 @@ package rocketmq5Kit
 import "testing"
 
 func TestVerifyEndpoint(t *testing.T) {
-	err := VerifyEndpoint("localhost:8081", "test")
+	err := VerifyEndpoint("127.0.0.1:8081", "test")
 	if err != nil {
 		panic(err)
 	}
