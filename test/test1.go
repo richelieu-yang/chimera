8ccc606dbbb1464960d507e941cb11a81b32fc38
Richelieu <richelieu042@gmail.com>
2023-03-10 17:19:21 +0800

    init

--- test/test1.go
+++ test/test1.go
@@ -1,21 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"github.com/richelieu42/go-scales/src/goZeroKit"
-)
-
 func main() {
-	wc, err := goZeroKit.NewSizeLimitRotateRuleWriteCloser("1/2/3/test.log", "-", 1, 10, 3, true)
-	if err != nil {
-		panic(err)
-	}
 
-	for {
-		_, err := wc.Write([]byte("qwdqwdq1wdqwdqwd4684986489649864948qwdqwd强无敌群无多群无多\n"))
-		if err != nil {
-			fmt.Println(err.Error())
-			break
-		}
-	}
 }
