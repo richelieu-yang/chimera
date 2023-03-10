19b860f9c76ddf79cca72b4c8dead8af77089b95
Richelieu <richelieu042@gmail.com>
2023-03-10 14:26:53 +0800

    init

--- src/goroutineKit/id.go
+++ src/goroutineKit/id.go
@@ -6,15 +6,16 @@ import (
 	"strconv"
 )
 
-// GetGoroutineID 获取协程的id
-// DEPRECATED: 在go语言中，谷歌开发者不建议大家获取协程ID，主要是为了GC更好的工作，滥用协程ID会导致GC不能及时回收内存。如果一个第三方库使用了协程ID，那么使用该库的人将会莫名中招。
+// GetGoroutineId 获取协程的id
 /*
+DEPRECATED: 在go语言中，谷歌开发者不建议大家获取协程ID，主要是为了GC更好的工作，滥用协程ID会导致GC不能及时回收内存。如果一个第三方库使用了协程ID，那么使用该库的人将会莫名中招.
+
 golang 获取goroutineID
 	https://studygolang.com/articles/19794
 Golang - 获取协程ID，从此走上一条曲折的不归路
 	https://blog.csdn.net/wyansai/article/details/101004226
 */
-func GetGoroutineID() uint64 {
+func GetGoroutineId() uint64 {
 	b := make([]byte, 64)
 	runtime.Stack(b, false)
 	b = bytes.TrimPrefix(b, []byte("goroutine "))
