4a98ca9fb8777da26f138eef17e266885d6770d5
Richelieu <richelieu042@gmail.com>
2023-03-19 14:55:06 +0800

    init

--- src/core/mapKit/generic.go
+++ src/core/mapKit/generic.go
@@ -1,9 +1,6 @@
 package mapKit
 
 import (
-	"github.com/richelieu42/go-scales/src/core/boolKit"
-	"github.com/richelieu42/go-scales/src/core/intKit"
-	"github.com/richelieu42/go-scales/src/core/strKit"
 	"github.com/samber/lo"
 )
 
@@ -82,42 +79,6 @@ func Remove[K comparable, V any](m map[K]V, key K) (V, bool) {
 */
 func Set[K comparable, V any](m map[K]V, key K, value V) {
 	m[key] = value
-}
-
-// Get
-/*
-@param m 	可以为nil
-@param key	如果不存在对应的值，将返回值类型的零值
-*/
-func Get[K comparable, V any](m map[K]V, key K) V {
-	return m[key]
-}
-
-// GetString
-/*
-@param m 可以为nil
-*/
-func GetString[K comparable, V any](m map[K]V, key K) (string, error) {
-	value := Get(m, key)
-	return strKit.ToStringE(value)
-}
-
-// GetInt
-/*
-@param m 可以为nil
-*/
-func GetInt[K comparable, V any](m map[K]V, key K) (int, error) {
-	value := Get(m, key)
-	return intKit.ToIntE(value)
-}
-
-// GetBool
-/*
-@param m 可以为nil
-*/
-func GetBool[K comparable, V any](m map[K]V, key K) (bool, error) {
-	value := Get(m, key)
-	return boolKit.ToBoolE(value)
 }
 
 // Keys creates an array of the map keys.
