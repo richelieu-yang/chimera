124509cd05203d6cb0a7716a403f18d52309caf0
Richelieu <richelieu042@gmail.com>
2023-03-19 14:39:35 +0800

    init

--- src/core/sliceKit/group.go
+++ src/core/sliceKit/group.go
@@ -7,9 +7,19 @@ import (
 
 // Group 分组
 /*
+PS: 不会修改传参s.
+
 @param s		可以为nil
 @param iteratee	不能为nil
 @return			保底为空的map实例
+
+e.g.
+	s := []int{0, 1, 2, 3, 4, 5}
+	m := sliceKit.Group[int, int](s, func(i int) int {
+		return i % 3
+	})
+	fmt.Println(s) // [0 1 2 3 4 5]
+	fmt.Println(m) // map[0:[0 3] 1:[1 4] 2:[2 5]]
 */
 func Group[T any, U comparable](s []T, iteratee func(item T) U) map[U][]T {
 	return lo.GroupBy(s, iteratee)
--- test/test1.go
+++ test/test1.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/richelieu42/go-scales/src/core/sliceKit"
+)
 
+func main() {
+	s := []int{0, 1, 2, 3, 4, 5}
+	m := sliceKit.Group[int, int](s, func(i int) int {
+		return i % 3
+	})
+	fmt.Println(s) // [0 1 2 3 4 5]
+	fmt.Println(m) // map[0:[0 3] 1:[1 4] 2:[2 5]]
 }
