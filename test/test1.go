3e325e262b9d78010388ffb33a5e1384b1fdd340
Richelieu <richelieu042@gmail.com>
2023-03-19 15:10:45 +0800

    init

--- test/test1.go
+++ test/test1.go
@@ -1,15 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"github.com/richelieu42/go-scales/src/core/sliceKit"
-)
-
 func main() {
-	s0 := []int{1, 2, 3, 4}
-	sum := sliceKit.Reduce[int, int](s0, func(agg int, item int, _ int) int {
-		return agg + item
-	}, 0)
-	fmt.Println(s0)  // [1 2 3 4]
-	fmt.Println(sum) // 10
+
 }
