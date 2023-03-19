1a78375d87a9b96c9ce55f70ba143b6c21db136c
Richelieu <richelieu042@gmail.com>
2023-03-19 13:32:19 +0800

    init

--- test/test1.go
+++ test/test1.go
@@ -1,12 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"github.com/richelieu42/go-scales/src/core/mathKit"
-)
-
 func main() {
-	fmt.Println(mathKit.Clamp(0, -10, 10))
-	fmt.Println(mathKit.Clamp(-42, -10, 10))
-	fmt.Println(mathKit.Clamp(42, -10, 10))
+	
 }
