737bd19aab23d20e4f317054695a08c5f22f2d73
Richelieu <richelieu042@gmail.com>
2023-03-22 18:56:24 +0800

    init

--- test/test2.go
+++ test/test2.go
@@ -1,12 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"github.com/redis/go-redis/v9"
-	"github.com/richelieu42/chimera/src/assertKit"
-)
-
 func main() {
-	err := assertKit.Nil(redis.Nil, "name")
-	fmt.Println(err.Error())
+
 }
