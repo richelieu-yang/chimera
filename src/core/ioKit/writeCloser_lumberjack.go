2b5a96537b4d0c8a1b935155dfb8d83795378c6b
Richelieu <richelieu042@gmail.com>
2023-03-23 13:49:53 +0800

    init

--- src/core/ioKit/writeCloser_lumberjack.go
+++ src/core/ioKit/writeCloser_lumberjack.go
@@ -18,11 +18,16 @@ go语言的日志滚动(rolling)记录器——lumberjack
 	https://zhuanlan.zhihu.com/p/430224518
 
 @param filePath		日志路径，归档日志也会保存在对应目录下（若该值为空，则日志会保存到os.TempDir()目录下，日志文件名为<processname>-lumberjack.log）
-@param maxSize		（单位: MB，默认: 100MB）日志大小到达此值，就开始backup
+					(1) 会尝试创建父级目录；
+					(2) 文件不存在，会自动创建；
+					(3) 文件存在，
+						(3.1) 是文件，内容追加在最后；
+						(3.2) 是目录，返回error.
+@param maxSize		[单位: MB，默认: 100MB] 日志大小到达此值，就开始backup
 @param maxBackups	旧日志保存的最大数量，默认保存所有旧日志文件
-@param maxAge		（单位: days）旧日志保存的最大天数，默认保存所有旧日志文件
-@param localTime	（默认使用UTC时间）是否使用本地时间戳？
-@param compress		（默认: false）对backup的日志是否进行压缩
+@param maxAge		[单位: days] 旧日志保存的最大天数，默认保存所有旧日志文件
+@param localTime	[默认使用UTC时间] 是否使用本地时间戳？
+@param compress		[默认: false] 对backup的日志是否进行压缩
 */
 func NewLumberjackWriteCloser(filePath string, maxSize, maxBackups, maxAge int, localTime, compress bool) (io.WriteCloser, error) {
 	if err := fileKit.MkParentDirs(filePath); err != nil {
--- test/test1.go
+++ test/test1.go
@@ -1,13 +1,23 @@
 package main
 
-import "github.com/natefinch/lumberjack"
+import (
+	"github.com/richelieu42/chimera/src/core/ioKit"
+	"github.com/sirupsen/logrus"
+)
 
 func main() {
-	&lumberjack.Logger{
-		Filename:   "/var/log/myapp/foo.log",
-		MaxSize:    500, // megabytes
-		MaxBackups: 3,
-		MaxAge:     28,   //days
-		Compress:   true, // disabled by default
+	writeCloser, err := ioKit.NewLumberjackWriteCloser("/Users/richelieu/Downloads/a.log", 1, 10, 7, true, false)
+	if err != nil {
+		logrus.Fatal(err)
 	}
+
+	//for {
+	//	_, _ = writeCloser.Write([]byte(idKit.NewULID() + "\r\n"))
+	//	time.Sleep(time.Second)
+	//}
+
+	_, _ = writeCloser.Write([]byte("0\n"))
+	_, _ = writeCloser.Write([]byte("1\n"))
+	_, _ = writeCloser.Write([]byte("2\n"))
+	_, _ = writeCloser.Write([]byte("3"))
 }
