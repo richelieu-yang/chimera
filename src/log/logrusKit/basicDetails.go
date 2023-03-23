22abbff711d89e952fd060b51f52a8ccef2742e8
Richelieu <richelieu042@gmail.com>
2023-03-23 14:11:35 +0800

    init

--- src/log/logrusKit/basicDetails.go
+++ src/log/logrusKit/basicDetails.go
@@ -12,68 +12,68 @@ import (
 
 // PrintBasicDetails 输出服务器的基本信息（以便于甩锅）
 func PrintBasicDetails() {
-	logrus.Infof("[SCALES] ======================================================================================================================")
+	logrus.Infof("[CHIMERA] ======================================================================================================================")
 
-	logrus.Infof("[SCALES, PROCESS] pid: [%d].", runtimeKit.PID)
+	logrus.Infof("[CHIMERA, PROCESS] pid: [%d].", runtimeKit.PID)
 
 	// os
-	logrus.Infof("[SCALES, OS] os: [%s].", osKit.OS)
-	logrus.Infof("[SCALES, OS] arch: [%s].", osKit.ARCH)
-	logrus.Infof("[SCALES, OS] bits: [%d].", osKit.BITS)
+	logrus.Infof("[CHIMERA, OS] os: [%s].", osKit.OS)
+	logrus.Infof("[CHIMERA, OS] arch: [%s].", osKit.ARCH)
+	logrus.Infof("[CHIMERA, OS] bits: [%d].", osKit.BITS)
 	printUniqueOsInfo()
 
 	// golang
-	logrus.Infof("[SCALES, GO] go version: [%s].", runtimeKit.GoVersion)
-	logrus.Infof("[SCALES, GO] GOROOT: [%s].", runtimeKit.GoRoot)
+	logrus.Infof("[CHIMERA, GO] go version: [%s].", runtimeKit.GoVersion)
+	logrus.Infof("[CHIMERA, GO] GOROOT: [%s].", runtimeKit.GoRoot)
 
 	// user
-	logrus.Infof("[SCALES, USER] name: [%s].", userKit.GetName())
-	logrus.Infof("[SCALES, USER] user name: [%s].", userKit.GetUserName())
-	logrus.Infof("[SCALES, PATH] home dir: [%s].", userKit.GetUserHomeDir())
+	logrus.Infof("[CHIMERA, USER] name: [%s].", userKit.GetName())
+	logrus.Infof("[CHIMERA, USER] user name: [%s].", userKit.GetUserName())
+	logrus.Infof("[CHIMERA, PATH] home dir: [%s].", userKit.GetUserHomeDir())
 
 	// path
-	logrus.Infof("[SCALES, PATH] project path: [%s].", pathKit.GetProjectDir())
-	logrus.Infof("[SCALES, PATH] temporary directory: [%s].", pathKit.GetTempDir())
+	logrus.Infof("[CHIMERA, PATH] project path: [%s].", pathKit.GetProjectDir())
+	logrus.Infof("[CHIMERA, PATH] temporary directory: [%s].", pathKit.GetTempDir())
 
 	// time
 	systemTime := timeKit.GetSystemTime()
 	zoneName, zoneOffset := systemTime.Zone()
-	logrus.Infof("[SCALES, TIME] system time: [%v], zone: [%s, %d].", systemTime, zoneName, zoneOffset)
+	logrus.Infof("[CHIMERA, TIME] system time: [%v], zone: [%s, %d].", systemTime, zoneName, zoneOffset)
 	if networkTime, source, err := timeKit.GetNetworkTime(); err != nil {
-		logrus.Warnf("[SCALES, TIME] fail to get network time, error: %v", err)
+		logrus.Warnf("[CHIMERA, TIME] fail to get network time, error: %v", err)
 	} else {
-		logrus.Infof("[SCALES, TIME] network time: [%v], source: [%s].", networkTime, source)
+		logrus.Infof("[CHIMERA, TIME] network time: [%v], source: [%s].", networkTime, source)
 	}
 
 	// ip
 	if ip, err := ipKit.GetOutboundIP(); err != nil {
-		logrus.Warnf("[SCALES, IP] fail to get local ip, error: %v", err)
+		logrus.Warnf("[CHIMERA, IP] fail to get local ip, error: %v", err)
 	} else {
-		logrus.Infof("[SCALES, IP] local ip(for reference only): [%s].", ip)
+		logrus.Infof("[CHIMERA, IP] local ip(for reference only): [%s].", ip)
 
 		// ip2region
 		if region, err := ipKit.GetRegionByIp(ip); err != nil {
-			logrus.Warnf("[SCALES, IP] fail to get region of local ip, error: %v", err)
+			logrus.Warnf("[CHIMERA, IP] fail to get region of local ip, error: %v", err)
 		} else {
-			logrus.Infof("[SCALES, IP] region: [%s].", region)
+			logrus.Infof("[CHIMERA, IP] region: [%s].", region)
 		}
 	}
 
 	// host
 	hostInfo := runtimeKit.GetHostInfo()
-	logrus.Infof("[SCALES, HOST] host name: [%s].", hostInfo.Hostname)
+	logrus.Infof("[CHIMERA, HOST] host name: [%s].", hostInfo.Hostname)
 
 	// cpu
 	if cpuId, err := runtimeKit.GetCpuId(); err != nil {
-		logrus.Warnf("[SCALES, CPU] fail to get cpu id, error: %v", err)
+		logrus.Warnf("[CHIMERA, CPU] fail to get cpu id, error: %v", err)
 	} else {
-		logrus.Infof("[SCALES, CPU] cpu id: [%s].", cpuId)
+		logrus.Infof("[CHIMERA, CPU] cpu id: [%s].", cpuId)
 	}
-	logrus.Infof("[SCALES, CPU] available processors: [%d].", runtimeKit.GetCpuNumber())
+	logrus.Infof("[CHIMERA, CPU] available processors: [%d].", runtimeKit.GetCpuNumber())
 	if cpuPercent, err := runtimeKit.GetCpuPercent(); err != nil {
-		logrus.Warnf("[SCALES, CPU] fail to get cpu percent, error: %v", err)
+		logrus.Warnf("[CHIMERA, CPU] fail to get cpu percent, error: %v", err)
 	} else {
-		logrus.Infof("[SCALES, CPU] usage: [%.2f]%%.", cpuPercent)
+		logrus.Infof("[CHIMERA, CPU] usage: [%.2f]%%.", cpuPercent)
 	}
 
 	// mac
@@ -82,14 +82,14 @@ func PrintBasicDetails() {
 			"error": err.Error(),
 		}).Fatal("fail to get mac addresses")
 	} else {
-		logrus.Infof("[SCALES, MAC] mac addresses: [%v].", macAddresses)
+		logrus.Infof("[CHIMERA, MAC] mac addresses: [%v].", macAddresses)
 	}
 
 	//// disk
 	//if stat, err := runtimeKit.GetDiskStat(); err != nil {
 	//	errorKit.Panic("fail to get disk stat, error:\n%+v", err)
 	//} else {
-	//	logrus.Infof("[SCALES, DISK] disk stat: [%s].", stat.String())
+	//	logrus.Infof("[CHIMERA, DISK] disk stat: [%s].", stat.String())
 	//}
 
 	// memory
@@ -98,15 +98,15 @@ func PrintBasicDetails() {
 			"error": err.Error(),
 		}).Fatal("fail to get memory stat")
 	} else {
-		logrus.Infof("[SCALES, MEMORY] memory stat: [%s].", info)
+		logrus.Infof("[CHIMERA, MEMORY] memory stat: [%s].", info)
 	}
 
 	// docker
 	if dockerIds, err := runtimeKit.GetDockerIdList(); err != nil {
-		logrus.Warnf("[SCALES, DOCKER] Fail to get docker id list, error: %v", err)
+		logrus.Warnf("[CHIMERA, DOCKER] Fail to get docker id list, error: %v", err)
 	} else {
-		logrus.Infof("[SCALES, DOCKER] docker id list: %v.", dockerIds)
+		logrus.Infof("[CHIMERA, DOCKER] docker id list: %v.", dockerIds)
 	}
 
-	logrus.Infof("[SCALES] ======================================================================================================================")
+	logrus.Infof("[CHIMERA] ======================================================================================================================")
 }
