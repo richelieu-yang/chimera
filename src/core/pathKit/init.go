a662060469c698386f710343cd5fb6e910388a3b
Richelieu <richelieu042@gmail.com>
2023-03-23 18:38:28 +0800

    init

--- src/core/pathKit/init.go
+++ src/core/pathKit/init.go
@@ -1,8 +1,6 @@
 package pathKit
 
 import (
-	"github.com/richelieu42/chimera/src/core/file/fileKit"
-	"github.com/richelieu42/chimera/src/core/strKit"
 	"github.com/sirupsen/logrus"
 	"os"
 )
@@ -10,15 +8,8 @@ import (
 func init() {
 	var err error
 
-	// projectDir
 	projectDir, err = os.Getwd()
 	if err != nil {
-		logrus.Panicf("[SCALES, PATH] Funtion(os.Getwd()) fails, error: %v", err)
-	}
-	if strKit.IsEmpty(projectDir) {
-		logrus.Panic("[SCALES, PATH] Variable projectDir is empty.")
-	}
-	if !fileKit.IsDir(projectDir) {
-		logrus.Panicf("[SCALES, PATH] Variable projectDir(%s) isn't a directory.", projectDir)
+		logrus.Fatal(err)
 	}
 }
