9d7e5bdc2f4b5b7a60a9b75d6d8de470490a3e8a
Richelieu <richelieu042@gmail.com>
2023-03-23 13:37:42 +0800

    init

--- src/core/ioKit/writeCloser_lumberjack.go
+++ src/core/ioKit/writeCloser_lumberjack.go
@@ -8,7 +8,7 @@ import (
 // NewLumberjackWriteCloser
 /*
 PS:
-(1) lumberjack: 日志切割组件.
+(1) lumberjack: 日志切割组件.一般情况下，lumberjack配合其他日志库，实现日志的滚动(rolling)记录.
 (2) 根据 MaxBackups、MaxAge 删除过期文件，根据 Compress 决定是否压缩哪些未压缩的旧日志文件。
 
 Golang 语言三方库 lumberjack 日志切割组件怎么使用？
@@ -20,14 +20,16 @@ go语言的日志滚动(rolling)记录器——lumberjack
 @param maxSize		（单位: MB，默认: 100MB）日志大小到达此值，就开始backup
 @param maxBackups	旧日志保存的最大数量，默认保存所有旧日志文件
 @param maxAge		（单位: days）旧日志保存的最大天数，默认保存所有旧日志文件
+@param localTime	（默认使用UTC时间）是否使用本地时间戳？
 @param compress		（默认: false）对backup的日志是否进行压缩
 */
-func NewLumberjackWriteCloser(filePath string, maxSize, maxBackups, maxAge int, compress bool) io.WriteCloser {
+func NewLumberjackWriteCloser(filePath string, maxSize, maxBackups, maxAge int, localTime, compress bool) io.WriteCloser {
 	return &lumberjack.Logger{
 		Filename:   filePath,
 		MaxSize:    maxSize,
 		MaxBackups: maxBackups,
 		MaxAge:     maxAge,
+		LocalTime:  localTime,
 		Compress:   compress,
 	}
 }
