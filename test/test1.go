8f4d3909bf5a808454fe664fc6baa28fa9452760
Richelieu <richelieu042@gmail.com>
2023-03-23 17:24:00 +0800

    init

--- test/test1.go
+++ test/test1.go
@@ -1,31 +1,8 @@
 package main
 
-import (
-	"fmt"
-	"github.com/richelieu42/chimera/src/core/strKit"
-	"github.com/sirupsen/logrus"
-	"runtime"
-	"strings"
-)
+import "github.com/sirupsen/logrus"
 
 func main() {
-	l := logrus.New()
-	l.SetReportCaller(true)
-	l.Formatter = &logrus.JSONFormatter{
-		CallerPrettyfier: func(f *runtime.Frame) (funcName string, fileName string) {
-			s := strings.Split(f.Function, ".")
-			funcName = s[len(s)-1]
-
-			s1 := strKit.Split(f.File, "/")
-			length := len(s1)
-			if length >= 2 {
-				fileName = fmt.Sprintf("%s/%s:%d", s1[length-2], s1[length-1], f.Line)
-			} else {
-				fileName = fmt.Sprintf("%s:%d", f.File, f.Line)
-			}
-
-			return funcName, fileName
-		},
-	}
-	l.Info("测试")
+	logrus.SetLevel(logrus.DebugLevel)
+	logrus.Fatal("ccc")
 }
