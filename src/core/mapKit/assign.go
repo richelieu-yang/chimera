756d82b76098e777f76e0c70d5b28e0404202b69
Richelieu <richelieu042@gmail.com>
2023-03-23 19:57:46 +0800

    init

--- src/core/mapKit/assign.go
+++ src/core/mapKit/assign.go
@@ -6,6 +6,8 @@ import (
 
 // Merge 合并多个map实例（合并到最左边的map实例中）
 /*
+PS: 存在key相同的情况，以最右边的为准.
+
 @param maps 可以为nil
 @return 保底为空的map实例（不为nil）
 
@@ -15,6 +17,14 @@ e.g.
 		map[string]int{"b": 3, "c": 4},
 	)
 	fmt.Println(m) // map[a:1 b:3 c:4]
+
+e.g.1
+	m := mapKit.Merge[string, int](
+		map[string]int{"a": 1, "b": 2},
+		map[string]int{"b": 3, "c": 4},
+		map[string]int{"b": 5},
+	)
+	fmt.Println(m) // map[a:1 b:5 c:4]
 */
 func Merge[K comparable, V any](maps ...map[K]V) map[K]V {
 	return lo.Assign(maps...)
--- test/test1.go
+++ test/test1.go
@@ -9,6 +9,7 @@ func main() {
 	m := mapKit.Merge[string, int](
 		map[string]int{"a": 1, "b": 2},
 		map[string]int{"b": 3, "c": 4},
+		map[string]int{"b": 5},
 	)
-	fmt.Println(m) // map[a:1 b:3 c:4]
+	fmt.Println(m) // map[a:1 b:5 c:4]
 }
