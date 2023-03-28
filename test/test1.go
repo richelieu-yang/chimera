42ebabfae64c9e8f90ba5d16026a0da3b736821c
Richelieu <richelieu042@gmail.com>
2023-03-28 16:26:20 +0800

    init

--- test/test1.go
+++ test/test1.go
@@ -1,12 +1,5 @@
 package main
 
-import (
-	"github.com/richelieu42/chimera/src/gozeroKit"
-	"github.com/zeromicro/go-zero/core/logx"
-)
-
 func main() {
-	logx.Info("---")
-	gozeroKit.SetLogrusWriter(nil)
-	logx.Info("+++")
+
 }
