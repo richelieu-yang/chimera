911a32a2099bf7dd9a2116cf5af42013810bf869
Richelieu <richelieu042@gmail.com>
2023-03-28 10:30:00 +0800

    init

--- test/test1.go
+++ test/test1.go
@@ -1,21 +1,12 @@
 package main
 
 import (
-	"github.com/richelieu42/chimera/src/log/logrusKit"
-	"github.com/sirupsen/logrus"
+	"github.com/richelieu42/chimera/src/gozeroKit"
 	"github.com/zeromicro/go-zero/core/logx"
-	"github.com/zeromicro/zero-contrib/logx/logrusx"
 )
 
 func main() {
 	logx.Info("---")
-
-	writer := logrusx.NewLogrusWriter(func(logger *logrus.Logger) {
-		formatter := logrusKit.NewTextFormatter("")
-		logger.SetFormatter(formatter)
-		//logger.SetFormatter(&logrus.TextFormatter{})
-	})
-	logx.SetWriter(writer)
-
+	gozeroKit.SetLogrusWriter(nil)
 	logx.Info("+++")
 }
