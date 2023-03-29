88374915659affa6f279fc7d061eeac3567aa877
Richelieu <richelieu042@gmail.com>
2023-03-29 17:04:35 +0800

    init

--- src/confKit/load.go
+++ src/confKit/load.go
@@ -7,7 +7,9 @@ import (
 
 // MustLoad 读取配置文件并反序列化为 指定结构体指针ptr
 /*
-!!!: 结构体可以参考 go-zero/rest/config.go中的RestConf，可以通过tag控制配置的值（默认值default、范围range、可选optional...）
+!!!:
+(1) 结构体可以参考 go-zero/rest/config.go中的RestConf，可以通过tag控制配置的值（默认值default、范围range、可选optional...）
+(2) 值为数组时要注意（要么有值，要么全注释掉），以免返回error.
 
 @param path	配置文件的路径（推荐使用.yaml）
 @param ptr	[不能为nil] 结构体实例的指针
