aedc3eff12ea1468350918b2cd2c91d96e4334f4
Richelieu <richelieu042@gmail.com>
2023-03-30 17:08:29 +0800

    init

--- src/mq/pulsarKit/setup_test.go
+++ src/mq/pulsarKit/setup_test.go
@@ -8,6 +8,7 @@ import (
 func TestSetUp(t *testing.T) {
 	pulsarConfig := &Config{
 		Addresses: []string{"192.168.80.27:6650", "192.168.80.42:6650", "192.168.80.43:6650"},
+		//Addresses: []string{"172.18.21.50:6650"},
 		VerifyConfig: &VerifyConfig{
 			Topic: "test",
 			Print: true,
--- test/test1.go
+++ test/test1.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
+)
 
+func main() {
+	router := gin.New()
+	router.GET("/ws/connect", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "success",
+		})
+	})
+	// 可以直接用
+	if err := router.RunTLS("0.0.0.0:8888", "ssl.pem", "ssl.key"); err != nil {
+		logrus.Panic(err)
+	}
+	//server := &http.Server{Addr: "0.0.0.0:10679", Handler: router}
+	//_ = server.ListenAndServeTLS("./certs/server.cer", "./certs/server.key")
 }
