ac5d73c1236b19ba2b3f341c7528388e6d40f795
Richelieu <richelieu042@gmail.com>
2023-04-03 13:46:45 +0800

    init

--- test/test1.go
+++ test/test1.go
@@ -1,22 +1,4 @@
 package main
 
-import (
-	"github.com/richelieu42/chimera/src/confKit"
-	"github.com/richelieu42/chimera/src/ginKit"
-	"github.com/sirupsen/logrus"
-)
-
-type (
-	Config struct {
-		Gin *ginKit.Config
-	}
-)
-
 func main() {
-	config := &Config{}
-	confKit.MustLoad("chimera-lib/env.yaml", config)
-
-	ginConfig := config.Gin
-
-	logrus.Info(ginConfig)
 }
