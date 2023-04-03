9b8975f60969b1f6391ad0b3acbecdde7d7e4fac
Richelieu <richelieu042@gmail.com>
2023-04-03 16:28:38 +0800

    init

--- test/test1.go
+++ test/test1.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"fmt"
-	"github.com/richelieu42/chimera/src/core/sliceKit"
 )
 
 func main() {
-	s := []int{0, 1, 2}
-	s1 := sliceKit.Intercept(s, len(s), len(s))
-	fmt.Println(s1)        // []
-	fmt.Println(s1 != nil) // true
+	var s []int = []int{}
+	s1 := s[0:0]
+
+	fmt.Println(s1)
+	fmt.Println(s1 != nil)
+
+	//s1 := sliceKit.Intercept(s, len(s), len(s))
+	//fmt.Println(s1)        // []
+	//fmt.Println(s1 != nil) // true
 }
