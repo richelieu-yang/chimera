b3208277c3f409fa0be7b078971858b285bb83ae
Richelieu <richelieu042@gmail.com>
2023-04-10 17:26:49 +0800

    init

--- src/web/ginKit/parameter.go
+++ src/web/ginKit/parameter.go
@@ -18,26 +18,25 @@ func ObtainGetParam(ctx *gin.Context, key string) string {
 
 func ObtainPostParam(ctx *gin.Context, key string, safeArgs ...bool) string {
 	safe := sliceKit.GetFirstItemWithDefault(false, safeArgs...)
-
 	if safe {
-		// Golang中比较坑的一点：如果不通过此方法获取post参数，如果涉及转发请求，接收方取参数会有问题.
+		/* Golang中比较坑的一点：如果不通过此方法获取post参数，如果涉及转发请求，接收方取参数会有问题. */
+		// (1) 重构body
 		req := ctx.Request
-
-		// 重构body
 		var bodyBytes []byte
 		if req.Body != nil {
 			bodyBytes, _ = io.ReadAll(req.Body)
 		}
 		reader := strings.NewReader(string(bodyBytes))
-		req.Body = &httpKit.Repeat{Reader: reader, Offset: 0}
-
-		str := ctx.PostForm(key)
-
-		// 重置body（否则转发请求会失败）
-		req.Body.(*httpKit.Repeat).Reset()
-
-		return str
+		req.Body = &httpKit.Repeat{
+			Reader: reader,
+			Offset: 0,
+		}
+		defer func() {
+			// (2) 重置body（否则转发请求会失败）
+			req.Body.(*httpKit.Repeat).Reset()
+		}()
 	}
+
 	return ctx.PostForm(key)
 }
 
@@ -75,14 +74,45 @@ func ObtainInt64Param(ctx *gin.Context, key string, safeArgs ...bool) (int64, er
 
 func ObtainFloat32Param(ctx *gin.Context, key string, safeArgs ...bool) (float32, error) {
 	value := ObtainParam(ctx, key, safeArgs...)
-	return floatKit.ToFloat64()
+	return floatKit.ToFloat32E(value)
 }
 
-// GetFormFileContent form请求，根据 传参key 获取文件的字节流
+func ObtainFloat64Param(ctx *gin.Context, key string, safeArgs ...bool) (float64, error) {
+	value := ObtainParam(ctx, key, safeArgs...)
+	return floatKit.ToFloat64E(value)
+}
+
+// ObtainFormFileContent form请求，根据 传参key 获取文件的字节流
 /*
-@return 分别为：文件内容、文件名、错误
+TODO: 待验证：form表单上传文件，传参safeArgs是否有必要?
+
+@return 文件内容 + 文件名 + 错误
 */
-func GetFormFileContent(ctx *gin.Context, key string) ([]byte, string, error) {
+func ObtainFormFileContent(ctx *gin.Context, key string, safeArgs ...bool) ([]byte, string, error) {
+	safe := sliceKit.GetFirstItemWithDefault(false, safeArgs...)
+	if safe {
+		/* Golang中比较坑的一点：如果不通过此方法获取post参数，如果涉及转发请求，接收方取参数会有问题. */
+		// (1) 重构body
+		req := ctx.Request
+		var bodyBytes []byte
+		if req.Body != nil {
+			var err error
+			bodyBytes, err = io.ReadAll(req.Body)
+			if err != nil {
+				return nil, "", err
+			}
+		}
+		reader := strings.NewReader(string(bodyBytes))
+		req.Body = &httpKit.Repeat{
+			Reader: reader,
+			Offset: 0,
+		}
+		defer func() {
+			// (2) 重置body（否则转发请求会失败）
+			req.Body.(*httpKit.Repeat).Reset()
+		}()
+	}
+
 	fileHeader, err := ctx.FormFile(key)
 	if err != nil {
 		return nil, "", err
