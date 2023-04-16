8298fcd73e27ec3d7cbb7293bd2419b9aa7938d5
Richelieu <richelieu042@gmail.com>
2023-04-16 13:59:06 +0800

    init

--- test/test1.go
+++ test/test1.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"github.com/panjf2000/ants/v2"
 	"github.com/richelieu42/chimera/v2/src/core/ioKit"
 	"github.com/richelieu42/chimera/v2/src/log/logrusKit"
 	"github.com/sirupsen/logrus"
 )
 
 func main() {
+	ants.Release
+
 	logger, err := logrusKit.NewLumberjackLogger([]ioKit.LumberjackOption{
 		ioKit.WithFilePath("a.log"),
 		ioKit.WithConsole(true),
