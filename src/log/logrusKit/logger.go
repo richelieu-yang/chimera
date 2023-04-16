f0d44cbc39c2bee0c9a69bc193c1824b9061c192
Richelieu <richelieu042@gmail.com>
2023-04-16 10:11:26 +0800

    init

--- src/log/logrusKit/logger.go
+++ src/log/logrusKit/logger.go
@@ -1,12 +1,81 @@
 package logrusKit
 
 import (
-	"github.com/richelieu42/chimera/v2/src/core/file/fileKit"
 	"github.com/richelieu42/chimera/v2/src/core/ioKit"
 	"github.com/sirupsen/logrus"
 	"io"
-	"os"
 )
+
+type (
+	loggerOptions struct {
+		formatter    logrus.Formatter
+		reportCaller bool
+		level        logrus.Level
+		output       io.Writer
+	}
+
+	LoggerOption func(opts *loggerOptions)
+)
+
+func WithFormatter(formatter logrus.Formatter) LoggerOption {
+	return func(opts *loggerOptions) {
+		opts.formatter = formatter
+	}
+}
+
+func WithReportCaller(reportCaller bool) LoggerOption {
+	return func(opts *loggerOptions) {
+		opts.reportCaller = reportCaller
+	}
+}
+
+func WithLevel(level logrus.Level) LoggerOption {
+	return func(opts *loggerOptions) {
+		opts.level = level
+	}
+}
+
+func WithOutput(output io.Writer) LoggerOption {
+	return func(opts *loggerOptions) {
+		opts.output = output
+	}
+}
+
+func loadOptions(options ...LoggerOption) *loggerOptions {
+	opts := &loggerOptions{
+		formatter:    nil,
+		reportCaller: true,
+		level:        logrus.DebugLevel,
+		output:       nil,
+	}
+	for _, option := range options {
+		option(opts)
+	}
+
+	/* 容错，以防调用方瞎搞 */
+	if opts.formatter == nil {
+		opts.formatter = DefaultTextFormatter
+	}
+
+	return opts
+}
+
+// NewBasicLogger
+/*
+@param options 可以什么都不传
+*/
+func NewBasicLogger(options ...LoggerOption) *logrus.Logger {
+	opts := loadOptions(options...)
+
+	logger := logrus.New()
+	logger.SetFormatter(opts.formatter)
+	logger.SetReportCaller(opts.reportCaller)
+	logger.SetLevel(opts.level)
+	if opts.output != nil {
+		logger.SetOutput(opts.output)
+	}
+	return logger
+}
 
 // DisposeLogger 释放资源（主要针对文件日志）
 func DisposeLogger(logger *logrus.Logger) error {
@@ -14,40 +83,4 @@ func DisposeLogger(logger *logrus.Logger) error {
 		return nil
 	}
 	return ioKit.TryToClose(logger.Out)
-}
-
-// NewFileLogger
-/*
-PS: 如果 logger.Out 被释放后继续调用 logger 进行输出，会失败（e.g. 控制台os.Stderr有输出: Failed to write to log, write /Users/richelieu/Downloads/a.txt: file already closed）.
-
-@param filePath			内部会做处理:
-						(1) 会尝试创建父级目录
-						(2) 文件不存在，会自动创建
-						(3) 文件存在：是个文件，追加在最后；是个目录，返回error
-@param formatter 		可以为nil，此时将采用默认值
-@param toConsoleFlag 	true: 输出到日志文件的同时，也输出到控制台; false: 只输出到文件日志
-*/
-func NewFileLogger(filePath string, formatter logrus.Formatter, level logrus.Level, toConsoleFlag bool) (*logrus.Logger, error) {
-	if err := fileKit.MkParentDirs(filePath); err != nil {
-		return nil, err
-	}
-	if err := fileKit.AssertNotExistOrIsFile(filePath); err != nil {
-		return nil, err
-	}
-
-	writeCloser, err := os.OpenFile(filePath, os.O_RDWR|os.O_APPEND|os.O_CREATE, 0666)
-	if err != nil {
-		return nil, err
-	}
-	return newFileLogger(formatter, level, writeCloser, toConsoleFlag), nil
-}
-
-// newFileLogger 复用代码
-func newFileLogger(formatter logrus.Formatter, level logrus.Level, writeCloser io.WriteCloser, toConsole bool) *logrus.Logger {
-	logger := NewBasicLogger(WithLevel(level), WithFormatter(formatter))
-	if toConsole {
-		writeCloser = ioKit.MultiWriteCloser(writeCloser, ioKit.NopCloserToWriter(os.Stdout))
-	}
-	logger.SetOutput(writeCloser)
-	return logger
 }
