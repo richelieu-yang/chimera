fde425cd263a5f783a8435164d0058cfce5907ad
Richelieu <richelieu042@gmail.com>
2023-04-17 10:17:04 +0800

    init

--- src/core/osKit/signal.go
+++ src/core/osKit/signal.go
@@ -9,14 +9,20 @@ import (
 
 // MonitorExitSignal 监听退出信号（拦截关闭信号）
 /*
-PS: 收到退出信号后，程序会退出.
+@param exitFunc (1) 可以为nil
+				(2)
 */
-func MonitorExitSignal() {
+func MonitorExitSignal(exitFunc func(os.Signal)) {
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
 
 	go func() {
 		sig := <-c
-		logrus.Fatalf("signal: [%s]", sig.String())
+
+		logrus.WithField("signal", sig.String()).Warn("receive a signal and this process will exit")
+		if exitFunc != nil {
+			exitFunc(sig)
+		}
+		os.Exit(0)
 	}()
 }
--- test/test1.go
+++ test/test1.go
@@ -1,29 +1,9 @@
 package main
 
-import (
-	"github.com/panjf2000/ants/v2"
-	"github.com/richelieu42/chimera/v2/src/core/ioKit"
-	"github.com/richelieu42/chimera/v2/src/log/logrusKit"
-	"github.com/sirupsen/logrus"
-	"syscall"
-)
+import "github.com/richelieu42/chimera/v2/src/core/osKit"
 
 func main() {
+	osKit.MonitorExitSignal(nil)
 
-	syscall.SIGHUP
-
-	ants.Release
-
-	logger, err := logrusKit.NewLumberjackLogger([]ioKit.LumberjackOption{
-		ioKit.WithFilePath("a.log"),
-		ioKit.WithConsole(true),
-		//ioKit.WithMaxSize(1),
-		ioKit.WithCompress(true),
-	})
-	if err != nil {
-		logrus.Fatal(err)
-	}
-	for {
-		logger.Info(666)
-	}
+	select {}
 }
