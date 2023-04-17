3cac6c90aa308c8285f99030953a7f5c727535c3
Richelieu <richelieu042@gmail.com>
2023-04-17 15:12:08 +0800

    init

--- test/test1.go
+++ test/test1.go
@@ -1,24 +1,5 @@
 package main
 
-import (
-	"github.com/richelieu42/chimera/v2/src/core/runtimeKit"
-	"github.com/richelieu42/chimera/v2/src/core/signalKit"
-	"github.com/richelieu42/chimera/v2/src/log/logrusKit"
-	"github.com/sirupsen/logrus"
-	"os"
-	"time"
-)
-
 func main() {
-	//logrus.Info(syscall.SIGHUP.String())
-	//logrus.Info(syscall.SIGINT.String())
 
-	logrus.Infof("pid: [%d]", runtimeKit.PID)
-
-	logrusKit.MustSetUp(nil)
-	signalKit.MonitorExitSignal(func(signal os.Signal) {
-		time.Sleep(time.Second * 3)
-	})
-
-	select {}
 }
--- test/test2.go
+++ test/test2.go
@@ -1,36 +1,5 @@
 package main
 
-import (
-	"github.com/gin-gonic/gin"
-	"github.com/redis/go-redis/v9"
-	"github.com/richelieu42/chimera/v2/src/confKit"
-	"github.com/richelieu42/chimera/v2/src/web/ginKit"
-	"github.com/sirupsen/logrus"
-	"net/http"
-)
-
-type config struct {
-	Gin *ginKit.Config
-}
-
 func main() {
-	c := &config{}
-	confKit.MustLoad("/Users/richelieu/GolandProjects/chimera/chimera-lib/env.yaml", c)
 
-	ginKit.MustSetUp(c.Gin, nil, func(engine *gin.Engine) error {
-		engine.Use(func(ctx *gin.Context) {
-			logrus.Info("before Next()")
-			ctx.Next()
-			logrus.Info("after Next()")
-		}, func(ctx *gin.Context) {
-			logrus.Info("before Abort()")
-			ctx.AbortWithError(http.StatusInternalServerError, redis.Nil)
-			logrus.Info("after Abort()")
-		})
-
-		engine.Any("/test", func(ctx *gin.Context) {
-			ctx.String(http.StatusOK, "ok")
-		})
-		return nil
-	})
 }
