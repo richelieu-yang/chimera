b44d7311b4b6154c510407e06aed592d2e65ca4f
Richelieu <richelieu042@gmail.com>
2023-04-18 14:09:34 +0800

    init

--- test/test1.go
+++ test/test1.go
@@ -1,13 +1,15 @@
 package main
 
 import (
+	"github.com/gin-contrib/pprof"
+	"github.com/gin-gonic/gin"
 	"github.com/sirupsen/logrus"
-	"net/http"
-	_ "net/http/pprof"
 )
 
 func main() {
-	if err := http.ListenAndServe("0.0.0.0:8080", nil); err != nil {
+	engine := gin.Default()
+	pprof.Register(engine)
+	if err := engine.Run(":8080"); err != nil {
 		logrus.Fatal(err)
 	}
 }
