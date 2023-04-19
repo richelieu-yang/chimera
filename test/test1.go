48f715d99e826b1935cca90a3f64bf04dd4d6b58
Richelieu <richelieu042@gmail.com>
2023-04-19 10:55:57 +0800

    init

--- test/test1.go
+++ test/test1.go
@@ -1,27 +1,5 @@
 package main
 
-import (
-	"github.com/gin-gonic/gin"
-	"github.com/richelieu42/chimera/v2/src/web/httpKit"
-	"github.com/sirupsen/logrus"
-	"net/http"
-)
-
 func main() {
-	engine := gin.Default()
 
-	//if err := engine.SetTrustedProxies(nil); err != nil {
-	//	logrus.Fatal(err)
-	//}
-
-	engine.Any("/test", func(ctx *gin.Context) {
-		err := httpKit.Proxy(ctx.Writer, ctx.Request, nil, "http", "192.168.60.206:8001", nil, nil)
-		if err != nil {
-			ctx.String(http.StatusInternalServerError, err.Error())
-		}
-	})
-
-	if err := engine.Run(":8000"); err != nil {
-		logrus.Fatal(err)
-	}
 }
--- test/test2.go
+++ test/test2.go
@@ -1,23 +1,5 @@
 package main
 
-import (
-	"github.com/gin-gonic/gin"
-	"github.com/sirupsen/logrus"
-	"net/http"
-)
-
 func main() {
-	engine := gin.Default()
 
-	if err := engine.SetTrustedProxies([]string{"192.168.9.254"}); err != nil {
-		logrus.Fatal(err)
-	}
-
-	engine.Any("/test", func(ctx *gin.Context) {
-		ctx.String(http.StatusOK, "this is 8001")
-	})
-
-	if err := engine.Run(":8001"); err != nil {
-		logrus.Fatal(err)
-	}
 }
