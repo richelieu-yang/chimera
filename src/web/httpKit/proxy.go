e86dc561e46ee1d891ab9c603497d128df1e08da
Richelieu <richelieu042@gmail.com>
2023-04-19 11:26:30 +0800

    init

--- src/web/httpKit/proxy.go
+++ src/web/httpKit/proxy.go
@@ -52,16 +52,15 @@ func Proxy(w http.ResponseWriter, r *http.Request, scheme, addr string, options 
 	return proxy(w, r, scheme, addr, opts.errorLogger, opts.reqUrlPath, opts.extraQuery)
 }
 
+// proxy
 /*
 @param errLogger 	可以为nil，但不建议这么干，因为错误会输出到控制台（通过 log.Printf()），不利于错误定位
 @param scheme 		"http" || "https"
 @param addr 		e.g."127.0.0.1:8888"
 @param reqUrlPath 	(1) 可以为nil（此时不修改 req.URL.Path）
-
-	(2) 非nil的话，个人感觉: 字符串的第一个字符应该是"/"
-
+					(2) 非nil的话，个人感觉: 字符串的第一个字符应该是"/"
 @param extraQuery 	可以为nil
-@return 可能是 context.Canceled（可以用==进行比较）
+@return 可能是 context.Canceled（可以用 == 进行比较）
 
 更多可参考:
 httputil.NewSingleHostReverseProxy() https://m.bilibili.com/video/BV1H64y1u7D7?buvid=Y44D4D448DC195994A5A88CED2DA982C60DF&is_story_h5=false&mid=5%2BiuUUrTqJQOdIa1r3VR0g%3D%3D&p=1&plat_id=114&share_from=ugc&share_medium=iphone&share_plat=ios&share_session_id=8B36D2C9-4DCB-4BE5-80AD-F7D49E292B5F&share_source=WEIXIN&share_tag=s_i&timestamp=1680160438&unique_k=16bK0gz&up_id=456307879
