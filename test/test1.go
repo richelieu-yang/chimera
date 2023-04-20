9cac33d6ef5c9910eeb1145d00ef03e2a51e7623
Richelieu <richelieu042@gmail.com>
2023-04-20 15:02:40 +0800

    init

--- test/test1.go
+++ test/test1.go
@@ -1,11 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"github.com/richelieu42/chimera/v2/src/web/httpClientKit"
-)
-
 func main() {
-	fmt.Println(httpClientKit.Post("https://www.moulem.com/?fr=au12"))
-	fmt.Println(httpClientKit.Upload("https://www.moulem.com/?fr=au12", nil))
+
 }
