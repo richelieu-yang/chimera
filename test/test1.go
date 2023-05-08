dc77c80e9c519a44aeccbadb1eff6293947b07c1
Richelieu <miro42@163.com>
2023-05-08 16:12:14 +0800

    init

--- test/test1.go
+++ test/test1.go
@@ -1,39 +1,5 @@
 package main
 
-import (
-	"context"
-	"fmt"
-	"github.com/richelieu42/chimera/v2/src/log/logrusKit"
-	"github.com/sirupsen/logrus"
-	clientv3 "go.etcd.io/etcd/client/v3"
-	"time"
-)
-
 func main() {
-	logrusKit.MustSetUp(nil)
-
-	v3Config := clientv3.Config{
-		Endpoints: []string{"127.0.0.1:2379"},
-
-		AutoSyncInterval:     time.Minute,
-		DialTimeout:          time.Second * 5,
-		DialKeepAliveTime:    time.Second * 5,
-		DialKeepAliveTimeout: time.Second * 5,
-		RejectOldCluster:     true,
-		PermitWithoutStream:  true,
-	}
-	client, err := clientv3.New(v3Config)
-	if err != nil {
-		logrus.Panic(err)
-	}
-	// 实例化一个用于操作etcd的KV
-	kv := clientv3.NewKV(client)
-
-	getResp, err := kv.Get(context.TODO(), "/school/class/students")
-	if err != nil {
-		logrus.Panic(err)
-	}
-	// 输出本次的Revision
-	fmt.Printf("Key is s %s \n Value is %s \n", getResp.Kvs[0].Key, getResp.Kvs[0].Value)
 
 }
--- test/test2.go
+++ test/test2.go
@@ -1,11 +1,64 @@
 package main
 
 import (
-	"fmt"
-	"github.com/richelieu42/chimera/v2/src/dataSizeKit"
-	"math"
+	"context"
+	"github.com/richelieu42/chimera/v2/src/log/logrusKit"
+	"github.com/sirupsen/logrus"
+	clientv3 "go.etcd.io/etcd/client/v3"
+	"time"
 )
 
 func main() {
-	fmt.Println(dataSizeKit.ToReadableStringWithIEC(math.MaxInt32))
+	logrusKit.MustSetUp(nil)
+
+	v3Config := clientv3.Config{
+		Endpoints: []string{"127.0.0.1:2379"},
+
+		AutoSyncInterval:     time.Minute,
+		DialTimeout:          time.Second * 5,
+		DialKeepAliveTime:    time.Second * 5,
+		DialKeepAliveTimeout: time.Second * 5,
+		RejectOldCluster:     true,
+		PermitWithoutStream:  true,
+	}
+	client, err := clientv3.New(v3Config)
+	if err != nil {
+		logrus.Fatal(err)
+	}
+	defer client.Close()
+	//kv := clientv3.NewKV(client)
+
+	leaseGrantResponse, err := client.Grant(context.TODO(), 10)
+	if err != nil {
+		logrus.Fatal(err)
+	}
+	logrus.Infof("lease id: [%d].", leaseGrantResponse.ID)
+
+	ch, err := client.KeepAlive(context.TODO(), leaseGrantResponse.ID)
+	if err != nil {
+		logrus.Fatal(err)
+	}
+	go func() {
+		defer func() {
+			logrus.Info("goroutine ends")
+		}()
+
+		for {
+			select {
+			case keepResp := <-ch:
+				if keepResp == nil {
+					// e.g. 该租约被删除了
+					logrus.Warn("租约已失效")
+					goto END
+				}
+				logrus.WithFields(logrus.Fields{
+					"ID":  keepResp.ID,
+					"TTL": keepResp.TTL,
+				}).Info("续租成功.")
+			}
+		}
+	END:
+	}()
+
+	select {}
 }
