b9252fd382714884e800f93ce0bce85552382b6a
Richelieu <miro42@163.com>
2023-05-10 16:05:46 +0800

    init

--- src/log/zapKit/logger.go
+++ src/log/zapKit/logger.go
@@ -1,42 +1,30 @@
 package zapKit
 
 import (
-	"github.com/richelieu42/chimera/v2/src/core/file/fileKit"
+	"github.com/richelieu42/chimera/v2/src/core/errorKit"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
-	"gopkg.in/natefinch/lumberjack.v2"
+	"io"
 )
 
 // NewLogger
 /*
-@param level e.g. zap.InfoLevel
+@param writer 	可以是: ioKit.NewLumberjackWriteCloser()
+@param level 	e.g. zap.InfoLevel
 */
-func NewLogger(logPath string, maxFileSize, maxFileIndex int, compress bool, level zapcore.Level) (*zap.Logger, error) {
-	// 由于调用输出时才会尝试生成日志文件（包括创建父目录），失败则会在控制台有错误输出:
-	// e.g. 2022-12-07 11:03:01.742597 +0800 CST m=+0.000902876 write error: can't make directories for new logfile: mkdir /logs: read-only file system
-	// 因此，还不如在初始化logger时先把该做的都做了.
-	if err := fileKit.MkParentDirs(logPath); err != nil {
-		return nil, err
+func NewLogger(writer io.Writer, level zapcore.Level) (*zap.Logger, error) {
+
+	if writer == nil {
+		return nil, errorKit.Simple("writer == nil")
 	}
 
 	encoder := getEncoder()
-	writeSyncer := getLogWriter(logPath, maxFileSize, maxFileIndex, compress)
-
+	writeSyncer := zapcore.AddSync(writer)
 	atomicLevel := zap.NewAtomicLevel()
 	atomicLevel.SetLevel(level)
 
 	core := zapcore.NewCore(encoder, writeSyncer, atomicLevel)
 	return zap.New(core, zap.AddCaller()), nil
-}
-
-func getLogWriter(logPath string, maxFileSize, maxFileIndex int, compress bool) zapcore.WriteSyncer {
-	lumberJackLogger := &lumberjack.Logger{
-		Filename:   logPath,
-		MaxSize:    maxFileSize,
-		MaxBackups: maxFileIndex,
-		Compress:   compress,
-	}
-	return zapcore.AddSync(lumberJackLogger)
 }
 
 func getEncoder() zapcore.Encoder {
@@ -45,3 +33,35 @@ func getEncoder() zapcore.Encoder {
 	encoderConfig.EncodeLevel = zapcore.CapitalLevelEncoder
 	return zapcore.NewConsoleEncoder(encoderConfig)
 }
+
+//// NewLogger1
+///*
+//@param level e.g. zap.InfoLevel
+//*/
+//func NewLogger1(logPath string, maxFileSize, maxFileIndex int, compress bool, level zapcore.Level) (*zap.Logger, error) {
+//	// 由于调用输出时才会尝试生成日志文件（包括创建父目录），失败则会在控制台有错误输出:
+//	// e.g. 2022-12-07 11:03:01.742597 +0800 CST m=+0.000902876 write error: can't make directories for new logfile: mkdir /logs: read-only file system
+//	// 因此，还不如在初始化logger时先把该做的都做了.
+//	if err := fileKit.MkParentDirs(logPath); err != nil {
+//		return nil, err
+//	}
+//
+//	encoder := getEncoder()
+//	writeSyncer := getLogWriter(logPath, maxFileSize, maxFileIndex, compress)
+//
+//	atomicLevel := zap.NewAtomicLevel()
+//	atomicLevel.SetLevel(level)
+//
+//	core := zapcore.NewCore(encoder, writeSyncer, atomicLevel)
+//	return zap.New(core, zap.AddCaller()), nil
+//}
+
+//func getLogWriter(logPath string, maxFileSize, maxFileIndex int, compress bool) zapcore.WriteSyncer {
+//	lumberJackLogger := &lumberjack.Logger{
+//		Filename:   logPath,
+//		MaxSize:    maxFileSize,
+//		MaxBackups: maxFileIndex,
+//		Compress:   compress,
+//	}
+//	return zapcore.AddSync(lumberJackLogger)
+//}
--- src/log/zapKit/sugaredLogger.go
+++ src/log/zapKit/sugaredLogger.go
@@ -2,13 +2,8 @@ package zapKit
 
 import (
 	"go.uber.org/zap"
-	"go.uber.org/zap/zapcore"
 )
 
-func NewSugaredLogger(logPath string, maxFileSize, maxFileIndex int, compress bool, level zapcore.Level) (*zap.SugaredLogger, error) {
-	logger, err := NewLogger(logPath, maxFileSize, maxFileIndex, compress, level)
-	if err != nil {
-		return nil, err
-	}
-	return logger.Sugar(), nil
+func NewSugaredLogger(logger *zap.Logger) *zap.SugaredLogger {
+	return logger.Sugar()
 }
