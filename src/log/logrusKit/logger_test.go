05f94dbf8358d11cedcf5934b36a93c91c689608
Richelieu <miro42@163.com>
2023-05-10 17:14:30 +0800

    init

--- src/log/logrusKit/logger_test.go
+++ src/log/logrusKit/logger_test.go
@@ -1,28 +1,24 @@
 package logrusKit
 
 import (
+	"github.com/richelieu42/chimera/v2/src/assertKit"
 	"github.com/richelieu42/chimera/v2/src/core/ioKit"
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestNewLogger(t *testing.T) {
-	///* 输出到控制台 */
-	//logger := NewLogger()
-	//logger.Info("to console")
-	//
-	///* 输出到文件（可rotate） */
-	//path := "test.log"
-	//writer, err := ioKit.NewLumberjackWriteCloser(ioKit.WithFilePath(path))
-	//assert.Nil(t, err)
-	//logger = NewLogger(WithWriter(writer))
-	//logger.Info("to rotatable file")
-	//
-	//err = assertKit.ExistAndIsFile(path)
-	//assert.Nil(t, err)
+	/* 输出到控制台 */
+	logger := NewLogger()
+	logger.Info("to console")
 
+	/* 输出到文件（可rotate） */
+	path := "test.log"
 	writer, err := ioKit.NewLumberjackWriteCloser(ioKit.WithFilePath(path))
-	/*
-		process err
-	*/
-	logger := NewLogger(WithWriter(writer))
+	assert.Nil(t, err)
+	logger = NewLogger(WithWriter(writer))
+	logger.Info("to rotatable file")
+
+	err = assertKit.ExistAndIsFile(path)
+	assert.Nil(t, err)
 }
