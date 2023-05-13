a6ff220a16f0661a4ca7e6a9c35721799b02c05e
Richelieu <miro42@163.com>
2023-05-13 20:04:12 +0800

    init

--- test/test2.go
+++ test/test2.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"context"
 	"github.com/richelieu42/chimera/v2/src/log/logrusKit"
 	"github.com/sirupsen/logrus"
 	clientv3 "go.etcd.io/etcd/client/v3"
+	"go.etcd.io/etcd/client/v3/concurrency"
+	"sync"
 	"time"
 )
 
@@ -25,31 +28,70 @@ func main() {
 		logrus.Fatal(err)
 	}
 	defer client.Close()
-	//kv := clientv3.NewKV(client)
-	//
-	//key := "/test/"
-	//
-	//// Put
-	//putOp := clientv3.OpPut(key, "ccc")
-	//opResp, err := kv.Do(context.TODO(), putOp)
-	//if err != nil {
-	//	logrus.Fatal(err)
-	//}
-	//logrus.Info("manager to put")
-	//
-	//// Get
-	//getOp := clientv3.OpGet(key)
-	//opResp, err = kv.Do(context.TODO(), getOp)
-	//if err != nil {
-	//	logrus.Fatal(err)
-	//}
-	//logrus.Infof("manager to get, value: [%s]", string(opResp.Get().Kvs[0].Value))
-	//
-	//// Delete
-	//delOp := clientv3.OpDelete(key)
-	//opResp, err = kv.Do(context.TODO(), delOp)
-	//if err != nil {
-	//	logrus.Fatal(err)
-	//}
-	//logrus.Info("manager to delete")
+
+	var wg sync.WaitGroup
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+
+		session, err := concurrency.NewSession(client)
+		if err != nil {
+			logrus.Fatal(err)
+		}
+		defer session.Close()
+		mu := concurrency.NewMutex(session, "/lock")
+
+		// 加锁
+		logrus.Info("[goroutine 0] ready to lock on")
+		if err := mu.Lock(context.TODO()); err != nil {
+			logrus.Fatal(err)
+		}
+		logrus.Info("[goroutine 0] lock on")
+
+		// 等一会
+		time.Sleep(time.Second * 3)
+
+		// 解锁
+		logrus.Info("[goroutine 0] ready to lock off")
+		if err := mu.Unlock(context.TODO()); err != nil {
+			logrus.Fatal(err)
+		}
+		logrus.Info("[goroutine 0] lock off")
+	}()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+
+		session, err := concurrency.NewSession(client)
+		if err != nil {
+			logrus.Fatal(err)
+		}
+		defer session.Close()
+		mu := concurrency.NewMutex(session, "/lock")
+
+		// 等一会，让锁被抢掉
+		time.Sleep(time.Second)
+
+		// 加锁
+		logrus.Info("[goroutine 1] ready to lock on")
+		if err := mu.Lock(context.TODO()); err != nil {
+			logrus.Fatal(err)
+		}
+		logrus.Info("[goroutine 1] lock on")
+
+		// 等一会
+		time.Sleep(time.Second * 3)
+
+		// 解锁
+		logrus.Info("[goroutine 1] ready to lock off")
+		if err := mu.Unlock(context.TODO()); err != nil {
+			logrus.Fatal(err)
+		}
+		logrus.Info("[goroutine 1] lock off")
+	}()
+
+	wg.Wait()
+	logrus.Info("======")
 }
