cf018fdf9195dc91814cee8d8fdf123b98616558
Richelieu <richelieu042@gmail.com>
2023-05-16 17:10:08 +0800

    init

--- src/core/sliceKit/sort.go
+++ src/core/sliceKit/sort.go
@@ -6,37 +6,53 @@ import (
 	"sort"
 )
 
-// Sort （简单数据类型）排序
-/*
-@param s 		可以为nil，将返回nil
-@param descArgs true: 降序
-				false: 升序（默认）
-@return 可能为nil
-
-e.g.
-[int](nil)    	 				=> nil
-[int](nil, true)				=> nil
-([]int{0, 1, 9, 8, 1}) 		 	=> [0 1 1 8 9]
-([]int{0, 1, 9, 8, 1}, true) 	=> [9 8 1 1 0]
-*/
-func Sort[T constraints.Ordered](s []T, descArgs ...bool) []T {
-	var less func(i, j int) bool
-	byDescending := GetFirstItemWithDefault(false, descArgs...)
-	if byDescending {
-		// 降序
-		less = func(i, j int) bool {
-			return s[i] > s[j]
-		}
-	} else {
-		// 升序
-		less = func(i, j int) bool {
-			return s[i] < s[j]
-		}
-	}
-
-	sort.Slice(s, less)
+// Sort （升序）排序
+func Sort[T constraints.Ordered](s []T) []T {
+	sort.Slice(s, func(i, j int) bool {
+		return s[i] < s[j]
+	})
 	return s
 }
+
+// SortByDesc （降序）排序
+func SortByDesc[T constraints.Ordered](s []T) []T {
+	sort.Slice(s, func(i, j int) bool {
+		return s[i] > s[j]
+	})
+	return s
+}
+
+//// Sort1 （简单数据类型）排序
+///*
+//@param s 		可以为nil，将返回nil
+//@param descArgs true: 降序
+//				false: 升序（默认）
+//@return 可能为nil
+//
+//e.g.
+//[int](nil)    	 				=> nil
+//[int](nil, true)				=> nil
+//([]int{0, 1, 9, 8, 1}) 		 	=> [0 1 1 8 9]
+//([]int{0, 1, 9, 8, 1}, true) 	=> [9 8 1 1 0]
+//*/
+//func Sort1[T constraints.Ordered](s []T, descArgs ...bool) []T {
+//	var less func(i, j int) bool
+//	byDescending := GetFirstItemWithDefault(false, descArgs...)
+//	if byDescending {
+//		// 降序
+//		less = func(i, j int) bool {
+//			return s[i] > s[j]
+//		}
+//	} else {
+//		// 升序
+//		less = func(i, j int) bool {
+//			return s[i] < s[j]
+//		}
+//	}
+//
+//	sort.Slice(s, less)
+//	return s
+//}
 
 // SortComplexSlice 排序复杂类型的slice实例（元素的类型可以为自定义结构体）
 /*
--- test/test1.go
+++ test/test1.go
@@ -1,5 +1,35 @@
 package main
 
-func main() {
+import (
+	"github.com/sirupsen/logrus"
+	"sync"
+	"time"
+)
 
+func main() {
+	var wg sync.WaitGroup
+
+	wg.Add(2)
+	go func() {
+		defer wg.Done()
+		time.Sleep(time.Second * 2)
+	}()
+	go func() {
+		defer wg.Done()
+		time.Sleep(time.Second * 3)
+	}()
+	wg.Wait()
+	logrus.Info("------")
+
+	wg.Add(2)
+	go func() {
+		defer wg.Done()
+		time.Sleep(time.Second * 2)
+	}()
+	go func() {
+		defer wg.Done()
+		time.Sleep(time.Second * 3)
+	}()
+	wg.Wait()
+	logrus.Info("======")
 }
