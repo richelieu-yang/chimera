4f58af395efb81a9add1a04e7199b8ea05fea6c8
Richelieu <richelieu042@gmail.com>
2023-05-16 13:33:57 +0800

    init

--- src/database/redisKit/distributed_mutex.go
+++ src/database/redisKit/distributed_mutex.go
@@ -11,10 +11,11 @@ PS:
 (1) 不可重入锁;
 (2) 更多详见"Redis分布式锁（多语言）.docx";
 (3) 写入Redis中的键，默认TTL为 8s.
+(4) 如果持有锁的时间 > Redis键的TTL，此时解锁将返回 false, error(类型: *redsync.ErrTaken; 错误内容: lock already taken, locked nodes: [0]);
 
 @param name 建议以 "mutex:" 为前缀
 
-e.g. 将TTL修改为30s
+e.g. 将TTL修改为30s（原8s）
 NewDistributedMutex("name", redsync.WithExpiry(time.Second * 30))
 */
 func (client *Client) NewDistributedMutex(name string, options ...redsync.Option) *redsync.Mutex {
--- test/test1.go
+++ test/test1.go
@@ -1,5 +1,46 @@
 package main
 
-func main() {
+import (
+	"github.com/richelieu42/chimera/v2/src/confKit"
+	"github.com/richelieu42/chimera/v2/src/database/redisKit"
+	"github.com/richelieu42/chimera/v2/src/log/logrusKit"
+	"github.com/sirupsen/logrus"
+	"time"
+)
 
+func main() {
+	path := "/Users/richelieu/GolandProjects/chimera/chimera-lib/config.yaml"
+
+	logrusKit.MustSetUp(nil)
+
+	type config struct {
+		Redis *redisKit.Config `json:"redis"`
+	}
+	c := &config{}
+	confKit.MustLoad(path, c)
+	redisKit.MustSetUp(c.Redis)
+
+	client, err := redisKit.GetClient()
+	if err != nil {
+		logrus.Fatal(err)
+	}
+
+	//go func() {
+	//
+	//}()
+
+	mu := client.NewDistributedMutex("/ccc")
+	logrus.Info("ready to lock on")
+	if err := mu.Lock(); err != nil {
+		logrus.Fatal(err)
+	}
+	logrus.Info("lock on")
+
+	time.Sleep(time.Second * 9)
+
+	ok, err := mu.Unlock()
+	logrus.WithFields(logrus.Fields{
+		"ok":    ok,
+		"error": err,
+	}).Info("lock off")
 }
