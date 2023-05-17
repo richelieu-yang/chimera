561802a8cb7c41a7823e63aab4da4d043c40d9e7
Richelieu <richelieu042@gmail.com>
2023-05-17 09:43:49 +0800

    init

--- src/randomKit/int.go
+++ src/randomKit/int.go
@@ -1,3 +1,7 @@
+// Package randomKit
+/*
+有两个标准库("math/rand"、"crypto/rand")，建议使用第一个，更加全面.
+*/
 package randomKit
 
 import "github.com/richelieu42/chimera/v2/src/core/errorKit"
@@ -6,11 +10,15 @@ import "github.com/richelieu42/chimera/v2/src/core/errorKit"
 /*
 @return 范围: [min, max)
 */
-func Int(min, max int) (int, error) {
+func Int(min, max int) (rst int, err error) {
 	if min >= max {
-		return 0, errorKit.Simple("min(%d) is greater than or equal to max(%d)", min, max)
+		err = errorKit.Simple("min(%d) is greater than or equal to max(%d)", min, max)
+		return
 	}
 
-	i := r.Intn(max - min)
-	return i + min, nil
+	// rst范围: [0, max - min)
+	rst = r.Intn(max - min)
+	// rst范围: [min, max)
+	rst += min
+	return
 }
