87fa1793631505e2cd18c01c2fe1fc7dc1ac75f0
Richelieu <richelieu042@gmail.com>
2023-05-18 21:06:15 +0800

    init

--- test/test1.go
+++ test/test1.go
@@ -3,9 +3,24 @@ package main
 import (
 	"fmt"
 	"github.com/gogf/gf/v2/container/glist"
+	"github.com/richelieu42/chimera/v2/src/jsonKit"
 )
 
 func main() {
-	l := glist.NewFrom(nil)
-	fmt.Println(l)
+	l := glist.NewFrom([]interface{}{0, 1, 2})
+
+	// 序列化
+	json, err := jsonKit.MarshalToString(l)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(json) // [0,1,2]
+
+	// 反序列化
+	l1 := &glist.List{}
+	err = jsonKit.UnmarshalFromString(json, l1)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(l1) // [0,1,2]
 }
