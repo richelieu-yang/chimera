741b2f75985099655aa790bf261e4f19c81501d1
Richelieu <richelieu042@gmail.com>
2023-05-22 14:17:32 +0800

    init

--- src/jsonKit/marshal.go
+++ src/jsonKit/marshal.go
@@ -1,45 +1,38 @@
 package jsonKit
 
-// Marshal 序列化.
+import "github.com/richelieu42/chimera/v2/src/core/strKit"
+
+// Marshal 序列化（可选配置: api && indent）.
 /*
 @param obj 可以为nil || ""
 
 e.g.
-(nil) 	=> []byte("null"), nil
-("") 	=> []byte("\"\""), nil
+	(nil) 	=> []byte("null"), nil
+	("") 	=> []byte("\"\""), nil
 */
 func Marshal(obj interface{}, options ...JsonOption) ([]byte, error) {
 	opts := loadOptions(options...)
-	return opts.api.Marshal(obj)
+
+	if strKit.IsAllEmpty(opts.prefix, opts.indent) {
+		return opts.api.Marshal(obj)
+	}
+	return opts.api.MarshalIndent(obj, opts.prefix, opts.indent)
 }
 
-func MarshalWithIndent(obj interface{}, options ...JsonOption) ([]byte, error) {
-	prefix := ""
-	// Richelieu: 与golang自带的"encoding/json"（可以用"\t"）不同，indent中不能有非空格的字符
-	//indent := "\t"
-	indent := "    "
-
-	opts := loadOptions(options...)
-	return opts.api.MarshalIndent(obj, prefix, indent)
-}
-
-// MarshalToString 序列化为字符串.
+// MarshalToString 序列化为json字符串（可选配置: api && indent）.
 /*
 @param obj 可以为nil || ""
 
 e.g.
-(nil) 	=> "null", nil
-("") 	=> "\"\"", nil
+	(nil) 	=> "null", nil
+	("") 	=> "\"\"", nil
 */
 func MarshalToString(obj interface{}, options ...JsonOption) (string, error) {
 	opts := loadOptions(options...)
-	return opts.api.MarshalToString(obj)
-}
 
-func MarshalToStringWithIndent(obj interface{}, options ...JsonOption) (string, error) {
-	data, err := MarshalWithIndent(obj, options...)
-	if err != nil {
-		return "", err
+	if strKit.IsAllEmpty(opts.prefix, opts.indent) {
+		return opts.api.MarshalToString(obj)
 	}
-	return string(data), nil
+	data, err := opts.api.MarshalIndent(obj, opts.prefix, opts.indent)
+	return string(data), err
 }
