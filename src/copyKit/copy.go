006267dadf6b83f76ac402c36e45cd2f556359b9
Richelieu <richelieu042@gmail.com>
2023-05-22 20:35:17 +0800

    init

--- src/copyKit/copy.go
+++ src/copyKit/copy.go
@@ -3,6 +3,8 @@ package copyKit
 import (
 	"github.com/gogf/gf/v2/util/gutil"
 	"github.com/jinzhu/copier"
+	"github.com/richelieu42/chimera/v2/src/core/errorKit"
+	"github.com/richelieu42/chimera/v2/src/core/interfaceKit"
 )
 
 // Copy 浅拷贝
@@ -28,12 +30,40 @@ PS:
 (2) 不使用 github.com/mohae/deepcopy: 	虽然效果一样，但不推荐使用（star少; 最后更新时间2017）
 (3) 不使用 github.com/jinzhu/copier: 	深拷贝有bug，详见"Golang.wps"
 
-@param src 可以为nil（此时将返回nil）
-@return The returned value will need to be asserted to the correct type.（需要手动断言）
+@param src 可以为nil（此时将返回: nil, nil）
 
-e.g.
+e.g. 传参为nil的情况
+	a, err := copyKit.DeepCopy[interface{}](nil)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(a)
+	fmt.Println(a == nil) // true
 
+	b, err := copyKit.DeepCopy[[]int](nil)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(b)
+	fmt.Println(b == nil) // true
+
+	c, err := copyKit.DeepCopy[map[string]interface{}](nil)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(c)
+	fmt.Println(c == nil) // true
 */
-func DeepCopy(src interface{}) interface{} {
-	return gutil.Copy(src)
+func DeepCopy[T any](src T) (dest T, err error) {
+	if interfaceKit.IsNil(src) {
+		return
+	}
+
+	obj := gutil.Copy(src)
+	var ok bool
+	if dest, ok = obj.(T); ok {
+		return
+	}
+	err = errorKit.Simple("types of src(%T) and dest(%T) are different")
+	return
 }
--- test/test1.go
+++ test/test1.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/richelieu42/chimera/v2/src/copyKit"
+)
 
+func main() {
+	a, err := copyKit.DeepCopy[interface{}](nil)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(a)
+	fmt.Println(a == nil) // true
+
+	b, err := copyKit.DeepCopy[[]int](nil)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(b)
+	fmt.Println(b == nil) // true
+
+	c, err := copyKit.DeepCopy[map[string]interface{}](nil)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(c)
+	fmt.Println(c == nil) // true
 }
