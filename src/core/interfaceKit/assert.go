ac5a7f6bde8350765cc60630f16e6b8f17605486
Richelieu <richelieu042@gmail.com>
2023-05-22 16:06:38 +0800

    init

---
+++ src/core/interfaceKit/assert.go
@@ -0,0 +1,13 @@
+package interfaceKit
+
+import (
+	"github.com/richelieu42/chimera/v2/src/core/errorKit"
+	"github.com/richelieu42/chimera/v2/src/funcKit"
+)
+
+func AssertNotNil(obj interface{}) error {
+	if obj == nil {
+		return errorKit.Simple("[%s] obj == nil", funcKit.GetFuncName(1))
+	}
+	return nil
+}
