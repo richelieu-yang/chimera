925a3d3225684d6b8bfab0ffa62d30fbd3d21b2b
Richelieu <richelieu042@gmail.com>
2023-05-22 15:12:12 +0800

    init

--- src/core/ptrKit/assert.go
+++ src/core/ptrKit/assert.go
@@ -1,14 +1,17 @@
 package ptrKit
 
-import "github.com/richelieu42/chimera/v2/src/core/errorKit"
+import (
+	"github.com/richelieu42/chimera/v2/src/core/errorKit"
+	"github.com/richelieu42/chimera/v2/src/funcKit"
+)
 
 // AssertNotNilPointer 断言传参 (1)值非nil; (2)类型为指针
-func AssertNotNilPointer(ptr interface{}, paramName string) error {
+func AssertNotNilPointer(ptr interface{}) error {
 	if ptr == nil {
-		return errorKit.SimpleWithExtraSkip(1, "[Assertion failed] param(name: %s) == nil", paramName)
+		return errorKit.SimpleWithExtraSkip(1, "[%s] ptr == nil", funcKit.GetFuncName(1))
 	}
 	if !IsPointer(ptr) {
-		return errorKit.SimpleWithExtraSkip(1, "[Assertion failed] type(%T) of param(type: %s) isn't pointer", ptr, paramName)
+		return errorKit.SimpleWithExtraSkip(1, "[%s] ptr(type: %T) isn't a pointer", funcKit.GetFuncName(1), ptr)
 	}
 	return nil
 }
