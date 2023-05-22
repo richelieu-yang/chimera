f6b008c7f206bb100247772c44467f6fafe70d01
Richelieu <richelieu042@gmail.com>
2023-05-22 13:14:43 +0800

    init

--- src/jsonKit/options.go
+++ src/jsonKit/options.go
@@ -1,1 +1,38 @@
 package jsonKit
+
+import jsoniter "github.com/json-iterator/go"
+
+type (
+	jsonOptions struct {
+		api jsoniter.API
+	}
+
+	JsonOption func(opts *jsonOptions)
+)
+
+func loadOptions(options ...JsonOption) *jsonOptions {
+	opts := &jsonOptions{
+		api: nil,
+	}
+
+	for _, option := range options {
+		option(opts)
+	}
+
+	// 容错，以防调用方瞎搞
+	if opts.api == nil {
+		opts.api = jsoniter.ConfigDefault
+	}
+
+	return opts
+}
+
+// WithApi
+/*
+@param api jsoniter.ConfigDefault || jsoniter.ConfigCompatibleWithStandardLibrary || jsoniter.ConfigFastest
+*/
+func WithApi(api jsoniter.API) JsonOption {
+	return func(opts *jsonOptions) {
+		opts.api = api
+	}
+}
