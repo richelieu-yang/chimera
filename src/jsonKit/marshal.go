a54f5b7e072ea76c3b6bff58855489b8606abe95
Richelieu <richelieu042@gmail.com>
2023-05-22 13:55:53 +0800

    init

--- src/jsonKit/marshal.go
+++ src/jsonKit/marshal.go
@@ -1,9 +1,5 @@
 package jsonKit
 
-import (
-	jsoniter "github.com/json-iterator/go"
-)
-
 // Marshal 序列化.
 /*
 @param obj 可以为nil || ""
@@ -12,58 +8,36 @@ e.g.
 (nil) 	=> []byte("null"), nil
 ("") 	=> []byte("\"\""), nil
 */
-func Marshal(obj interface{}) ([]byte, error) {
-	return jsoniter.Marshal(obj)
+func Marshal(obj interface{}, options ...JsonOption) ([]byte, error) {
+	opts := loadOptions(options...)
+	return opts.api.Marshal(obj)
+}
+
+func MarshalWithIndent(obj interface{}, options ...JsonOption) ([]byte, error) {
+	prefix := ""
+	// Richelieu: 与golang自带的"encoding/json"（可以用"\t"）不同，indent中不能有非空格的字符
+	//indent := "\t"
+	indent := "    "
+
+	opts := loadOptions(options...)
+	return opts.api.MarshalIndent(obj, prefix, indent)
 }
 
 // MarshalToString 序列化为字符串.
 /*
-PS:
-(1) 缺陷: 多次序列化相同的map实例(length >= 2)，返回值可能不同，想解决可以使用 MarshalToStringWithJsoniterApi().
-
 @param obj 可以为nil || ""
 
 e.g.
 (nil) 	=> "null", nil
 ("") 	=> "\"\"", nil
 */
-func MarshalToString(obj interface{}) (string, error) {
-	return jsoniter.MarshalToString(obj)
+func MarshalToString(obj interface{}, options ...JsonOption) (string, error) {
+	opts := loadOptions(options...)
+	return opts.api.MarshalToString(obj)
 }
 
-// MarshalWithJsoniterApi 可以自定义api
-func MarshalWithJsoniterApi(api jsoniter.API, obj interface{}) ([]byte, error) {
-	if api == nil {
-		api = jsoniter.ConfigDefault
-	}
-	return api.Marshal(obj)
-}
-
-// MarshalToStringWithJsoniterApi 可以自定义api
-/*
-@param api 可以为nil，jsoniter.ConfigDefault || jsoniter.ConfigCompatibleWithStandardLibrary || ...
-
-e.g.
-如果希望多次序列化同一map实例，返回的json字符串一直，传参api可以为 jsoniter.ConfigCompatibleWithStandardLibrary.
-*/
-func MarshalToStringWithJsoniterApi(api jsoniter.API, obj interface{}) (string, error) {
-	if api == nil {
-		api = jsoniter.ConfigDefault
-	}
-	return api.MarshalToString(obj)
-}
-
-func MarshalWithIndent(obj interface{}) ([]byte, error) {
-	prefix := ""
-	// Richelieu: 与golang自带的"encoding/json"（可以用"\t"）不同，indent中不能有非空格的字符
-	//indent := "\t"
-	indent := "    "
-
-	return jsoniter.MarshalIndent(obj, prefix, indent)
-}
-
-func MarshalToStringWithIndent(obj interface{}) (string, error) {
-	data, err := MarshalWithIndent(obj)
+func MarshalToStringWithIndent(obj interface{}, options ...JsonOption) (string, error) {
+	data, err := MarshalWithIndent(obj, options...)
 	if err != nil {
 		return "", err
 	}
--- src/jsonKit/seal.go
+++ src/jsonKit/seal.go
@@ -2,7 +2,6 @@ package jsonKit
 
 import (
 	"fmt"
-	jsoniter "github.com/json-iterator/go"
 	"github.com/richelieu42/chimera/v2/src/core/strKit"
 	"github.com/richelieu42/chimera/v2/src/msgKit"
 )
@@ -51,22 +50,18 @@ func Seal(code string, msgArgs ...interface{}) string {
 }
 
 func SealWithData(code string, data interface{}, msgArgs ...interface{}) string {
-	json, _ := SealFully(jsoniter.ConfigDefault, code, "", data, msgArgs...)
+	json, _ := SealFully(code, "", data, msgArgs...)
 	return json
 }
 
-func SealFully(api jsoniter.API, code, message string, data interface{}, msgArgs ...interface{}) (json string, err error) {
-	if api == nil {
-		api = jsoniter.ConfigDefault
-	}
-
+func SealFully(code, message string, data interface{}, msgArgs ...interface{}) (json string, err error) {
 	message = getFinalMessage(code, message, data, msgArgs...)
 	resp := &Response{Code: code, Message: message, Data: data}
 
 	if responseHook == nil {
-		return MarshalToStringWithJsoniterApi(api, resp)
+		return MarshalToString(resp)
 	}
-	return MarshalToStringWithJsoniterApi(api, responseHook(resp))
+	return MarshalToString(responseHook(resp))
 }
 
 func getFinalMessage(code, msg string, data interface{}, msgArgs ...interface{}) string {
