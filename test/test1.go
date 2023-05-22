d966f1987f6fdd8ba92bd5d99d4532f8e9258457
Richelieu <richelieu042@gmail.com>
2023-05-22 13:29:26 +0800

    init

--- test/test1.go
+++ test/test1.go
@@ -1,18 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"github.com/richelieu42/chimera/v2/src/copyKit"
-)
-
 func main() {
-	src := []string{"0", "1", "2"}
-	dest := make([]string, len(src))
-	if err := copyKit.Copy(&dest, src); err != nil {
-		panic(err)
-	}
 
-	src[0] = "a"
-	fmt.Println(src)  // [a 1 2]
-	fmt.Println(dest) // [0 1 2]
 }
