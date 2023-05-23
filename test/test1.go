73215ef3b7fbeca2c57a57080ab75d777e425b0d
Richelieu <richelieu042@gmail.com>
2023-05-23 14:24:14 +0800

    init

--- test/test1.go
+++ test/test1.go
@@ -1,7 +1,12 @@
 package main
 
-import "github.com/gogf/gf/v2/util/grand"
+import (
+	"fmt"
+	"github.com/richelieu42/chimera/v2/src/crypto/md5Kit"
+)
 
 func main() {
-	grand.N()
+	fmt.Println(md5Kit.EncryptFile("/Users/richelieu/Desktop/tasks.wps"))
+
+	fmt.Println("d9bc2286000a70c293180252479d8075" == "d9bc2286000a70c293180252479d8075")
 }
