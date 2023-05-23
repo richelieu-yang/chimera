715569712b160cffa922c3261f3256fde673602e
Richelieu <richelieu042@gmail.com>
2023-05-23 10:09:57 +0800

    init

--- test/test1.go
+++ test/test1.go
@@ -1,15 +1,15 @@
 package main
 
 import (
-	"github.com/richelieu42/chimera/v2/src/copyKit"
+	"fmt"
+	"github.com/richelieu42/chimera/v2/src/core/mapKit"
 )
 
 func main() {
-	m := map[string]interface{}{
-		"a": 0,
-	}
-	err := copyKit.Copy(&m, nil)
-	if err != nil {
-		panic(err)
-	}
+	m := mapKit.Merge[string, int](
+		map[string]int{"a": 1, "b": 2},
+		map[string]int{"a": 10},
+		map[string]int{"a": 100},
+	)
+	fmt.Println(m) // map[a:100 b:2]
 }
