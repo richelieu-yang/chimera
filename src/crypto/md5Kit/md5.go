8ea0bcc2c0aada1f9d6e3e1059da6823587ed2a1
Richelieu <richelieu042@gmail.com>
2023-05-23 14:09:34 +0800

    init

--- src/crypto/md5Kit/md5.go
+++ src/crypto/md5Kit/md5.go
@@ -1,44 +1,65 @@
 package md5Kit
 
 import (
-	"crypto/md5"
-	"encoding/hex"
+	"github.com/gogf/gf/v2/crypto/gmd5"
 	"github.com/richelieu42/chimera/v2/src/core/fileKit"
-	"io"
-	"os"
 )
 
-// GetFromFile 获取文件的md5
-func GetFromFile(path string) (string, error) {
-	data, err := fileKit.ReadFile(path)
-	if err != nil {
-		return "", err
-	}
-
-	return Get(data), nil
-}
-
-// GetFromFile1
-// Deprecated
+// Encrypt
 /*
-path为 空字符串 或者 对应文件不存在 时，会报错：open : The system cannot find the file specified.
+Deprecated: 不推荐使用此法，应该明确 传参data 的类型.
 */
-func GetFromFile1(path string) (string, error) {
-	file, err := os.Open(path)
-	if err != nil {
+func Encrypt(data interface{}) (string, error) {
+	return gmd5.Encrypt(data)
+}
+
+func EncryptFile(filePath string) (string, error) {
+	if err := fileKit.AssertExistAndIsFile(filePath); err != nil {
 		return "", err
 	}
 
-	m := md5.New()
-	_, err = io.Copy(m, file)
-	if err != nil {
-		return "", err
-	}
-	return hex.EncodeToString(m.Sum(nil)), nil
+	return gmd5.EncryptFile(filePath)
 }
 
-func Get(s []byte) string {
-	m := md5.New()
-	m.Write(s)
-	return hex.EncodeToString(m.Sum(nil))
+func EncryptBytes(data []byte) (string, error) {
+	return gmd5.EncryptBytes(data)
 }
+
+func EncryptString(data string) (string, error) {
+	return gmd5.EncryptString(data)
+}
+
+//// GetFromFile 获取文件的md5
+//func GetFromFile(path string) (string, error) {
+//	data, err := fileKit.ReadFile(path)
+//	if err != nil {
+//		return "", err
+//	}
+//
+//	return Get(data), nil
+//}
+//
+//// GetFromFile1
+//// Deprecated
+///*
+//path为 空字符串 或者 对应文件不存在 时，会报错：open : The system cannot find the file specified.
+//*/
+//func GetFromFile1(path string) (string, error) {
+//	file, err := os.Open(path)
+//	if err != nil {
+//		return "", err
+//	}
+//
+//	m := md5.New()
+//	_, err = io.Copy(m, file)
+//	if err != nil {
+//		return "", err
+//	}
+//	return hex.EncodeToString(m.Sum(nil)), nil
+//}
+//
+//func Get(s []byte) string {
+//	m := md5.New()
+//	m.Write(s)
+//	return hex.EncodeToString(m.Sum(nil))
+//}
