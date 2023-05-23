e1ea7ba4bb6ac13a316aec45e39df35fc001c291
Richelieu <richelieu042@gmail.com>
2023-05-23 09:34:43 +0800

    init

--- src/listKit/doubleLinkedList.go
+++ src/listKit/doubleLinkedList.go
@@ -2,7 +2,7 @@ package listKit
 
 import "github.com/gogf/gf/v2/container/glist"
 
-// NewDoubleLinkedList 创建（带并发安全开关的）双向链表
+// NewDoubleLinkedList 创建（带并发安全开关的）双向链表.
 /*
 PS: 返回值的使用可以参考 "GoFrame.wps".
 */
@@ -10,7 +10,7 @@ func NewDoubleLinkedList(safe ...bool) *glist.List {
 	return glist.New(safe...)
 }
 
-// NewDoubleLinkedListFrom 创建（带并发安全开关的）双向链表
+// NewDoubleLinkedListFrom 创建（带并发安全开关的）双向链表.
 /*
 PS: 返回值的使用可以参考 "GoFrame.wps".
 
