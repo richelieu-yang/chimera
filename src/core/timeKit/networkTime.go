3595a542853f18c7eeec94f2e2de98517a58621f
Richelieu <richelieu042@gmail.com>
2023-05-23 09:27:00 +0800

    init

--- src/core/timeKit/networkTime.go
+++ src/core/timeKit/networkTime.go
@@ -1,7 +1,8 @@
 package timeKit
 
 import (
-	"github.com/richelieu42/chimera/v2/src/web/httpClientKit"
+	"crypto/tls"
+	"net/http"
 	"time"
 )
 
@@ -32,9 +33,23 @@ func GetNetworkTime() (time.Time, string, error) {
 func getNetworkTimeBySource(source string) (time.Time, error) {
 	/*
 		超时时间设置的短一点，以防内网环境启动服务耗时太长.
-		PS: 内网环境，可能的情况: (1) 直接返回error；(2) 请求超时而返回error.
+		PS:
+		(1) 此处不使用 httpClientKit，原因: 避免import cycle
+		(2) 内网环境，可能的情况: (a)直接返回error; (b)请求超时而返回error.
 	*/
-	resp, err := httpClientKit.GetForResponse(source, httpClientKit.WithSafe(false), httpClientKit.WithTimeout(time.Second*6))
+	client := &http.Client{
+		Timeout: time.Second * 3,
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{
+				InsecureSkipVerify: true,
+			},
+		},
+	}
+	req, err := http.NewRequest("GET", source, nil)
+	if err != nil {
+		return time.Time{}, err
+	}
+	resp, err := client.Do(req)
 	if err != nil {
 		return time.Time{}, err
 	}
