3d189b7bcba890c247670a87982630c3c953b7d1
Richelieu <richelieu042@gmail.com>
2023-05-23 17:18:07 +0800

    init

--- src/core/mapKit/gListMap.go
+++ src/core/mapKit/gListMap.go
@@ -2,6 +2,10 @@ package mapKit
 
 import "github.com/gogf/gf/v2/container/gmap"
 
+// NewListMap
+/*
+使用场景: 当需要按输入顺序返回结果时使用ListMap.
+*/
 func NewListMap(safe ...bool) *gmap.ListMap {
 	return gmap.NewListMap(safe...)
 }
--- src/core/mapKit/gTreeMap.go
+++ src/core/mapKit/gTreeMap.go
@@ -6,6 +6,8 @@ import (
 
 // NewTreeMap
 /*
+PS: 当需要让返回结果按照自然升序排列时使用TreeMap.
+
 @param comparator e.g. gutil.ComparatorInt
 */
 func NewTreeMap(comparator func(v1, v2 interface{}) int, safe ...bool) *gmap.TreeMap {
