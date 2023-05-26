58e418dd3744feabe15af3b1e13773f88dca4675
Richelieu <richelieu042@gmail.com>
2023-05-26 16:02:51 +0800

    init

--- src/crypto/rsaKit/split.go
+++ src/crypto/rsaKit/split.go
@@ -1,14 +1,15 @@
 package rsaKit
 
-func split(buf []byte, lim int) [][]byte {
+func split(buf []byte, limit int) [][]byte {
 	var chunk []byte
-	chunks := make([][]byte, 0, len(buf)/lim+1)
-	for len(buf) >= lim {
-		chunk, buf = buf[:lim], buf[lim:]
+	chunks := make([][]byte, 0, len(buf)/limit+1)
+
+	for len(buf) >= limit {
+		chunk, buf = buf[:limit], buf[limit:]
 		chunks = append(chunks, chunk)
 	}
 	if len(buf) > 0 {
-		chunks = append(chunks, buf[:len(buf)])
+		chunks = append(chunks, buf[:])
 	}
 	return chunks
 }
