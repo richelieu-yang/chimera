4e86cc77f541c3d0ce4b30ebdb97b3e38f4afe87
Richelieu <richelieu042@gmail.com>
2023-05-26 17:12:53 +0800

    init

--- test/test1.go
+++ test/test1.go
@@ -6,9 +6,22 @@ import (
 	"crypto/x509"
 	"encoding/pem"
 	"github.com/richelieu42/chimera/v2/src/core/errorKit"
+	"github.com/richelieu42/chimera/v2/src/core/fileKit"
 	"github.com/richelieu42/chimera/v2/src/crypto/rsaKit"
-	"os"
 )
+
+func main() {
+	pri, pub, err := GenerateKeys(1024, rsaKit.PKCS8, "cyy")
+	if err != nil {
+		panic(err)
+	}
+	if err := fileKit.WriteToFile(pri, "pri.pem"); err != nil {
+		panic(err)
+	}
+	if err := fileKit.WriteToFile(pub, "pub.pem"); err != nil {
+		panic(err)
+	}
+}
 
 // GenerateKeys 生成公钥私钥
 /*
@@ -17,11 +30,11 @@ golang 生成RSA公钥和私钥文件 https://blog.csdn.net/lff1123/article/deta
 @param bits 		512 || 1024 || 2048 || 3072 || 4096
 @param keyFormat	rsaKit.PKCS1 || rsaKit.PKCS8
 */
-func GenerateKeys(bits int, keyFormat rsaKit.KeyFormat, privateKeyPassword string) error {
+func GenerateKeys(bits int, keyFormat rsaKit.KeyFormat, privateKeyPassword string) (pri []byte, pub []byte, err error) {
 	// 生成私钥文件
 	privateKey, err := rsa.GenerateKey(rand.Reader, bits)
 	if err != nil {
-		return err
+		return nil, nil, err
 	}
 	var derStream []byte
 	switch keyFormat {
@@ -30,41 +43,32 @@ func GenerateKeys(bits int, keyFormat rsaKit.KeyFormat, privateKeyPassword strin
 	case rsaKit.PKCS8:
 		derStream, err = x509.MarshalPKCS8PrivateKey(privateKey)
 		if err != nil {
-			return err
+			return nil, nil, err
 		}
 	default:
-		return errorKit.Simple("invalid keyFormat(%v)", keyFormat)
+		return nil, nil, errorKit.Simple("invalid keyFormat(%v)", keyFormat)
 	}
 	block := &pem.Block{
-		Type:  "RSA PRIVATE KEY",
+		Type:  "PRIVATE KEY",
 		Bytes: derStream,
 	}
-	file, err := os.Create("private.pem")
+	pri = pem.EncodeToMemory(block)
+	pri, err = rsaKit.EncryptPEM(pri, []byte(privateKeyPassword))
 	if err != nil {
-		return err
-	}
-	err = pem.Encode(file, block)
-	if err != nil {
-		return err
+		return nil, nil, err
 	}
 
 	// 生成公钥文件
 	publicKey := &privateKey.PublicKey
 	derPkix, err := x509.MarshalPKIXPublicKey(publicKey)
 	if err != nil {
-		return err
+		return nil, nil, err
 	}
 	block = &pem.Block{
 		Type:  "PUBLIC KEY",
 		Bytes: derPkix,
 	}
-	file, err = os.Create("public.pem")
-	if err != nil {
-		return err
-	}
-	err = pem.Encode(file, block)
-	if err != nil {
-		return err
-	}
-	return nil
+	pub = pem.EncodeToMemory(block)
+
+	return pri, pub, nil
 }
