4b9ed7f918c6857d57b1ba1aadf176900c2d9e8c
Richelieu <richelieu042@gmail.com>
2023-05-29 14:37:47 +0800

    init

---
+++ src/crypto/rsaKit/rsa_test.go
@@ -0,0 +1,43 @@
+package rsaKit
+
+import (
+	"fmt"
+	"github.com/richelieu42/chimera/v2/src/core/fileKit"
+	"testing"
+)
+
+func TestRSA(t *testing.T) {
+	options := []RsaOption{
+		WithPassword("qwdqwdqwdqwdqwdqwdqwdq"),
+		WithFormat(PKCS8),
+	}
+
+	priPath := "_pri.pem"
+	pubPath := "_pub.pem"
+	if err := GenerateKeyFiles(4096, priPath, pubPath, options...); err != nil {
+		panic(err)
+	}
+
+	pri, err := fileKit.ReadFile(priPath)
+	if err != nil {
+		panic(err)
+	}
+	pub, err := fileKit.ReadFile(pubPath)
+	if err != nil {
+		panic(err)
+	}
+
+	data := []byte("`1245`152678`169837~!@#$%^&*()_+=")
+	// 加密
+	data, err = Encrypt(data, pub)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("密文:", string(data))
+	// 解密
+	data, err = Decrypt(data, pri, options...)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("明文:", string(data))
+}
--- test/test1.go
+++ test/test1.go
@@ -1,43 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"github.com/richelieu42/chimera/v2/src/core/fileKit"
-	"github.com/richelieu42/chimera/v2/src/crypto/rsaKit"
-)
-
 func main() {
-	options := []rsaKit.RsaOption{
-		rsaKit.WithPassword(""),
-		rsaKit.WithFormat(rsaKit.PKCS1),
-	}
 
-	priPath := "pri.pem"
-	pubPath := "pub.pem"
-	if err := rsaKit.GenerateKeyFiles(4096, priPath, pubPath, options...); err != nil {
-		panic(err)
-	}
-
-	pri, err := fileKit.ReadFile(priPath)
-	if err != nil {
-		panic(err)
-	}
-	pub, err := fileKit.ReadFile(pubPath)
-	if err != nil {
-		panic(err)
-	}
-
-	data := []byte("`1245`152678`169837~!@#$%^&*()_+=")
-	// 加密
-	data, err = rsaKit.Encrypt(data, pub)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("密文:", string(data))
-	// 解密
-	data, err = rsaKit.Decrypt(data, pri, options...)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("明文:", string(data))
 }
