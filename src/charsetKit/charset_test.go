ed5ec2fea67493a07aa36374b6b616f9bc8788e4
Richelieu <richelieu042@gmail.com>
2023-06-03 10:58:54 +0800

    ~

---
+++ src/charsetKit/charset_test.go
@@ -0,0 +1,27 @@
+package charsetKit
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestConvert(t *testing.T) {
+	charset := "GBK"
+	text := "`1234567890-=~!@#$%^&*()_+<>?,./;':\"[]\\{}|强无敌群无多我饿去当前文档"
+
+	tmp, err := UTF8To(charset, text)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("GBK:", tmp)
+
+	tmp, err = ToUTF8(charset, tmp)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("UTF-8:", tmp)
+
+	if tmp != text {
+		panic("not equal")
+	}
+}
