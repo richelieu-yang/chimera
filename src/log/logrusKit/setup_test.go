46ca1914e18fa376d5f02452d5626b049855130c
Richelieu <richelieu042@gmail.com>
2023-06-03 13:32:19 +0800

    ~

--- src/log/logrusKit/setup_test.go
+++ src/log/logrusKit/setup_test.go
@@ -1,7 +1,6 @@
 package logrusKit
 
 import (
-	"github.com/gogf/gf/v2/os/gfile"
 	"github.com/sirupsen/logrus"
 	"testing"
 )
@@ -9,22 +8,11 @@ import (
 func TestMustSetUp(t *testing.T) {
 	MustSetUp(&Config{
 		Level:      "debug",
-		PrintBasic: false,
+		PrintBasic: true,
 	})
 
-	//DisableQuoteTemporarily(nil, func() {
-	//	logrus.Info("1\n2\n3\n")
-	//})
-	//logrus.Info("1\n2\n3\n")
-
-	logrus.Info("---")
-	logrus.Info(gfile.Pwd())
-	logrus.Info(gfile.SelfDir())
-	logrus.Info(gfile.SelfPath())
-	logrus.Info(gfile.MainPkgPath())
-	logrus.Info("---")
-
-	//fmt.Println(gfile.Pwd())
-	//fmt.Println(gfile.SelfDir())
-	//fmt.Println(gfile.MainPkgPath())
+	DisableQuoteTemporarily(nil, func() {
+		logrus.Info("1\n2\n3\n")
+	})
+	logrus.Info("1\n2\n3\n")
 }
