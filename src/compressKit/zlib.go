d3d424f494a416d111547e2aea09b46e7ce2a0ed
Richelieu <richelieu042@gmail.com>
2023-06-03 11:11:19 +0800

    ~

--- src/compressKit/zlib.go
+++ src/compressKit/zlib.go
@@ -1,1 +1,11 @@
 package compressKit
+
+import "github.com/gogf/gf/v2/encoding/gcompress"
+
+func Zlib(data []byte) ([]byte, error) {
+	return gcompress.Zlib(data)
+}
+
+func UnZlib(data []byte) ([]byte, error) {
+	return gcompress.UnZlib(data)
+}
