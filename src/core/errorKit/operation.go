911386ca6f186856a98f3b652d0ba08e6c03074c
Richelieu <richelieu042@gmail.com>
2023-06-03 16:54:30 +0800

    ~

--- src/core/errorKit/operation.go
+++ src/core/errorKit/operation.go
@@ -1,1 +1,23 @@
 package errorKit
+
+import "github.com/gogf/gf/v2/errors/gerror"
+
+var (
+	// Equal 错误比较
+	Equal func(err, target error) bool = gerror.Equal
+
+	// HasStack 判断错误是否带堆栈
+	HasStack func(err error) bool = gerror.HasStack
+
+	// Stack 获取堆栈信息
+	Stack func(err error) string = gerror.Stack
+
+	// Current 获取当前error
+	Current func(err error) error = gerror.Current
+
+	// Unwrap 获取层级错误的下一级错误error接口对象(当下一层级不存在时，返回nil)
+	Unwrap func(err error) error = gerror.Unwrap
+
+	// Cause 获取根错误error
+	Cause func(err error) error = gerror.Cause
+)
