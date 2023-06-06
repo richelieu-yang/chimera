8195a6e6cda9588d9f16eae2608f99e166c0c73e
Richelieu <richelieu042@gmail.com>
2023-06-06 15:21:00 +0800

    ~

--- src/mq/mqKit/clientLog.go
+++ src/mq/mqKit/clientLog.go
@@ -1,1 +1,40 @@
 package mqKit
+
+import (
+	rmq_client "github.com/apache/rocketmq-clients/golang/v5"
+	"github.com/richelieu-yang/chimera/v2/src/core/fileKit"
+	"github.com/richelieu-yang/chimera/v2/src/core/osKit"
+	"github.com/richelieu-yang/chimera/v2/src/core/pathKit"
+	"github.com/richelieu-yang/chimera/v2/src/core/strKit"
+	"os"
+)
+
+func InitClientLog(path string) error {
+	if strKit.IsBlank(path) {
+		// (1) 输出到控制台
+		if err := os.Setenv(rmq_client.ENABLE_CONSOLE_APPENDER, "true"); err != nil {
+			return err
+		}
+	} else {
+		// (2) 输出到日志文件
+		if err := fileKit.AssertNotExistOrIsFile(path); err != nil {
+			return err
+		}
+		dir := pathKit.GetParentDir(path)
+		if err := fileKit.MkDirs(dir); err != nil {
+			return err
+		}
+		name := fileKit.GetBaseName(path)
+
+		if err := osKit.SetEnvs(map[string]string{
+			rmq_client.ENABLE_CONSOLE_APPENDER: "false",
+			rmq_client.CLIENT_LOG_ROOT:         dir,
+			rmq_client.CLIENT_LOG_FILENAME:     name,
+		}); err != nil {
+			return err
+		}
+	}
+
+	rmq_client.ResetLogger()
+	return nil
+}
