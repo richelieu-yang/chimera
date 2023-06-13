8c22de007564fbd31027adb3255892a5a96e37c9
Richelieu <richelieu042@gmail.com>
2023-06-13 09:28:42 +0800

    ~

--- go.mod
+++ go.mod
@@ -52,6 +52,7 @@ require (
 	golang.org/x/exp v0.0.0-20230522175609-2e198f4a06a1
 	google.golang.org/grpc v1.55.0
 	gopkg.in/natefinch/lumberjack.v2 v2.2.1
+	gorm.io/driver/mysql v1.5.1
 	gorm.io/gorm v1.25.1
 )
 
@@ -86,6 +87,7 @@ require (
 	github.com/go-ole/go-ole v1.2.6 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/go-sql-driver/mysql v1.7.1 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
 	github.com/godbus/dbus v0.0.0-20190726142602-4481cbc300e2 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
@@ -99,6 +101,8 @@ require (
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/jmespath/go-jmespath v0.3.0 // indirect
 	github.com/klauspost/compress v1.15.15 // indirect
 	github.com/leodido/go-urn v1.2.4 // indirect
--- test/test1.go
+++ test/test1.go
@@ -1,35 +1,75 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
-	"github.com/redis/go-redis/v9"
-	"github.com/richelieu-yang/chimera/v2/src/core/errorKit"
-	"github.com/richelieu-yang/chimera/v2/src/core/strKit"
-	"github.com/richelieu-yang/chimera/v2/src/funcKit"
+	"github.com/richelieu-yang/chimera/v2/src/core/ioKit"
+	"github.com/richelieu-yang/chimera/v2/src/database/mysqlKit"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"time"
 )
 
 func main() {
-	fmt.Println(funcKit.AddEntireCaller(1, "123"))
-
-	fmt.Printf("%+v\n", a())
-	fmt.Println("=========")
-	fmt.Printf("%+v\n", b())
-	fmt.Println("=========")
-	fmt.Printf("%+v\n", c())
-}
-
-func a() error {
-	return errorKit.New("123")
-}
-
-func b() error {
-	str := "   "
-	if err := strKit.AssertNotBlank(str, "str"); err != nil {
-		return err
+	c := &mysqlKit.DsnConfig{
+		UserName: "root",
+		Password: "~Test123",
+		Host:     "127.0.0.1:3306",
+		DBName:   "ccc2",
 	}
-	return nil
-}
 
-func c() error {
-	return errorKit.Wrap(redis.Nil, "123")
+	writeCloser, err := ioKit.NewLumberjackWriteCloser("a.log")
+	if err != nil {
+		panic(err)
+	}
+	//var logger logger.Interface
+
+	logger.Discard
+
+	logger.Default
+
+	dsn := c.String()
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		Logger: logger.New(writeCloser, logger.Config{
+			SlowThreshold:             200 * time.Millisecond,
+			LogLevel:                  Warn,
+			IgnoreRecordNotFoundError: false,
+			Colorful:                  true,
+		}),
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	var sqlDB *sql.DB
+	sqlDB, err = db.DB()
+	if err != nil {
+		panic(err)
+	}
+	sqlDB.SetMaxIdleConns(10)
+	sqlDB.SetMaxOpenConns(100)
+	sqlDB.SetConnMaxLifetime(time.Hour)
+	if err := sqlDB.Ping(); err != nil {
+		panic(err)
+	}
+
+	type User struct {
+		Name     string
+		Age      uint
+		Birthday time.Time
+	}
+	user := &User{Name: "Jinzhu", Age: 18, Birthday: time.Now()}
+
+	// 可以通过Set设置附加参数，下面设置表的存储引擎为InnoDB
+	db.Set("gorm:table_options", "ENGINE=MyISAM").AutoMigrate(&User{})
+
+	// 自动建表
+	if err := db.AutoMigrate(user); err != nil {
+		panic(err)
+	}
+	// 插入数据
+	result := db.Create(user) // 通过数据的指针来创建
+	fmt.Println(result.Error)
+	fmt.Println(result.RowsAffected)
 }
