5fca8cd56ef6329dbcd8bb6f7f86663437889165
Richelieu <richelieu042@gmail.com>
2023-06-14 15:33:01 +0800

    ~

--- src/core/bytesKit/bytes.go
+++ src/core/bytesKit/bytes.go
@@ -31,43 +31,43 @@ func Equals(a, b []byte) bool {
 }
 
 // Contains 判断 subslice 子字节切片是否包含在 b 字节切片
-var Contains = bytes.Contains
+var Contains func(b, subslice []byte) bool = bytes.Contains
 
 // Count 统计 sep 字节切片在 s 字节切片中非重叠实例数
-var Count = bytes.Count
+var Count func(s, sep []byte) int = bytes.Count
 
 // HasPrefix 字节切片 s 是否以 prefix 开头
-var HasPrefix = bytes.HasPrefix
+var HasPrefix func(s, prefix []byte) bool = bytes.HasPrefix
 
 // HasSuffix 字节切片 s 是否以 suffix 结尾
-var HasSuffix = bytes.HasSuffix
+var HasSuffix func(s, suffix []byte) bool = bytes.HasSuffix
 
 // Index 查找 sep 在 s 中第一次出现的索引下标，如果没有则返回 -1
-var Index = bytes.Index
+var Index func(s, sep []byte) int = bytes.Index
 
 // LastIndex 查找 sep 在 s 中最后一次出现的索引下标，如果没有则返回 -1
-var LastIndex = bytes.LastIndex
+var LastIndex func(s, sep []byte) int = bytes.LastIndex
 
 // Split 将 sep 作为分割符，将 s 分割，返回拆分之后的字节切片
-var Split = bytes.Split
+var Split func(s, sep []byte) [][]byte = bytes.Split
 
 // SplitN 将 sep 作为分割符，将 s 分割 n 份，返回拆分之后的字节切片
-var SplitN = bytes.SplitN
+var SplitN func(s, sep []byte, n int) [][]byte = bytes.SplitN
 
 // ToLower 将字节切片所有字节全部转换为小写字母，返回该字节切片的一个副本
-var ToLower = bytes.ToLower
+var ToLower func(s []byte) []byte = bytes.ToLower
 
 // ToUpper 将字节切片所有字节全部转换为大小字母，返回该字节切片的一个副本
-var ToUpper = bytes.ToUpper
+var ToUpper func(s []byte) []byte = bytes.ToUpper
 
 // Trim 返回清除 s 中开头和结尾存在的 cutset 之后的一个子切片
-var Trim = bytes.Trim
+var Trim func(s []byte, cutset string) []byte = bytes.Trim
 
 // TrimLeft 返回清除 s 中开头存在的 cutset 之后的一个子切片
-var TrimLeft = bytes.TrimLeft
+var TrimLeft func(s []byte, cutset string) []byte = bytes.TrimLeft
 
 // TrimRight 返回清除 s 中结尾存在的 cutset 之后的一个子切片
-var TrimRight = bytes.TrimRight
+var TrimRight func(s []byte, cutset string) []byte = bytes.TrimRight
 
 // TrimSpace 返回清除 s 中开头和结尾存在的 \t\n\r 之后的一个子切片
-var TrimSpace = bytes.TrimSpace
+var TrimSpace func(s []byte) []byte = bytes.TrimSpace
