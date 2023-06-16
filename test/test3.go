2cf4d49cae6f66c45d9c9ef355b9c2956d752816
Richelieu <richelieu042@gmail.com>
2023-06-16 13:13:34 +0800

    ~

--- test/test3.go
+++ test/test3.go
@@ -1,17 +1,5 @@
 package main
 
-import (
-	"github.com/richelieu-yang/chimera/v2/src/core/ioKit"
-	"github.com/richelieu-yang/chimera/v2/src/dataSizeKit"
-)
-
 func main() {
-	writeCloser, err := ioKit.NewRotatableWriteCloser("aaa.log", dataSizeKit.EiB)
-	if err != nil {
-		panic(err)
-	}
 
-	writeCloser.Rotate()
-	writeCloser.Rotate()
-	writeCloser.Rotate()
 }
