8368a60ccb8e9f577eb39e02a44486173e66597b
Richelieu <richelieu042@gmail.com>
2023-06-16 14:20:45 +0800

    ~

--- src/confKit/load_test.go
+++ src/confKit/load_test.go
@@ -7,10 +7,16 @@ import (
 )
 
 func TestMustLoad(t *testing.T) {
+	type config1 struct {
+		Name string `json:"name,default=test"`
+	}
+
 	type config struct {
 		Time    time.Duration `json:"time"`
 		Number  int           `json:"number,range=[1:100)"`
 		Number1 int           `json:"number1,range=[1:)"`
+		//
+		Config1 config1 `json:"config1"`
 	}
 
 	c := &config{}
@@ -19,4 +25,5 @@ func TestMustLoad(t *testing.T) {
 	fmt.Printf("c.Time: %s\n", c.Time)
 	fmt.Printf("c.Number: %d\n", c.Number)
 	fmt.Printf("c.Number1: %d\n", c.Number1)
+	fmt.Printf("c.Config1.Name: %s\n", c.Config1.Name)
 }
