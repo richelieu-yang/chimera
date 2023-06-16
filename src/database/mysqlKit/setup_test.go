fd282583ecace7364ebdf2b99d4dc7d52ee56dee
Richelieu <richelieu042@gmail.com>
2023-06-16 16:51:26 +0800

    ~

--- src/database/mysqlKit/setup_test.go
+++ src/database/mysqlKit/setup_test.go
@@ -4,9 +4,22 @@ import (
 	"fmt"
 	"github.com/richelieu-yang/chimera/v2/src/confKit"
 	"github.com/richelieu-yang/chimera/v2/src/consts"
+	"github.com/richelieu-yang/chimera/v2/src/core/fileKit"
 	"github.com/richelieu-yang/chimera/v2/src/core/pathKit"
+	"github.com/sirupsen/logrus"
 	"testing"
+	"time"
 )
+
+type user struct {
+	Name     string
+	Age      uint
+	Birthday time.Time
+}
+
+func (u *user) TableName() string {
+	return "user"
+}
 
 func TestSetUp(t *testing.T) {
 	if wd, err := pathKit.ReviseWorkingDirInTestMode(consts.ProjectName); err != nil {
@@ -21,6 +34,22 @@ func TestSetUp(t *testing.T) {
 
 	c := &config{}
 	confKit.MustLoad("chimera-lib/config.yaml", c)
+	f, err := fileKit.NewFile("gorm.log")
+	if err != nil {
+		panic(err)
+	}
+	MustSetUp(c.MySQL, f)
 
-	MustSetUp(c.MySQL, nil)
+	user := &user{Name: "Jinzhu", Age: 18, Birthday: time.Now()}
+
+	// 可以通过Set设置附加参数，下面设置表的存储引擎为InnoDB
+	db.Set("gorm:table_options", "ENGINE=MyISAM") /*.AutoMigrate(&user{})*/
+	// 自动建表
+	if err := db.AutoMigrate(user); err != nil {
+		logrus.Fatal(err)
+	}
+	// 插入数据
+	result := db.Create(user) // 通过数据的指针来创建
+	fmt.Println(result.Error)
+	fmt.Println(result.RowsAffected)
 }
