536a29b1df3c0a1de205ed564718f90a9f8624c7
Richelieu <richelieu042@gmail.com>
2023-06-19 09:47:49 +0800

    ~

--- test/test3.go
+++ test/test3.go
@@ -2,38 +2,17 @@ package main
 
 import (
 	"fmt"
-	"github.com/zeromicro/go-zero/core/mapping"
+	"github.com/richelieu-yang/chimera/v2/src/core/fileKit"
+	"os"
 )
 
 func main() {
-	type inner struct {
-		Value1 int `key:"value1,range=(100:]"`
-		//Value2  int8   `key:"value2,range=[1:5]"`
-		//Value3  int16  `key:"value3,range=[1:5]"`
-		//Value4  int32  `key:"value4,range=[1:5]"`
-		//Value5  int64  `key:"value5,range=[1:5]"`
-		//Value6  uint   `key:"value6,range=[:5]"`
-		//Value8  uint8  `key:"value8,range=[1:5],string"`
-		//Value9  uint16 `key:"value9,range=[1:5],string"`
-		//Value10 uint32 `key:"value10,range=[1:5],string"`
-		//Value11 uint64 `key:"value11,range=[1:5],string"`
-	}
-	m := map[string]any{
-		"value1": 100,
-		//"value2":  int8(1),
-		//"value3":  int16(2),
-		//"value4":  int32(4),
-		//"value5":  int64(5),
-		//"value6":  uint(0),
-		//"value8":  "1",
-		//"value9":  "2",
-		//"value10": "4",
-		//"value11": "5",
-	}
-
-	var in inner
-	if err := mapping.UnmarshalKey(m, &in); err != nil {
+	f, err := fileKit.NewFile("aaaa.log")
+	if err != nil {
 		panic(err)
 	}
-	fmt.Println(666)
+	defer f.Close()
+
+	os.Stdout = f
+	fmt.Println("aaa\n")
 }
