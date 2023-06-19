73d0da5c471593cf71243c4762fe8fa117f4a382
Richelieu <richelieu042@gmail.com>
2023-06-19 10:40:21 +0800

    ~

--- src/log/logrusKit/output.go
+++ src/log/logrusKit/output.go
@@ -10,6 +10,9 @@ import (
 var defaultOutput io.Writer = os.Stdout
 
 // SetOutput 修改logrus默认的输出
+/*
+PS: 但是 logrus.New() 返回的*logrus.Logger实例的Out属性仍旧是 os.Stderr，因此建议通过 NewLogger() 创建*logrus.Logger实例.
+*/
 func SetOutput(writer io.Writer) {
 	if writer == nil {
 		return
