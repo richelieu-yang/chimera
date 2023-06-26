8b1e0ffa8a5a3bf36870bfb6d992bb4df97a2f78
Richelieu <richelieu042@gmail.com>
2023-06-26 09:58:04 +0800

    ~

--- src/core/bytesKit/bytesconv.go
+++ src/core/bytesKit/bytesconv.go
@@ -4,6 +4,8 @@ import "unsafe"
 
 // StringToBytes string => []byte
 /*
+Go 如何让在强制转换类型时不发生内存拷贝
+	https://mp.weixin.qq.com/s/wvJm0Q95PTB_YUkZq51C8Q
 PS:
 (1) 参考了 gin(v1.9.1) 中的 internal/bytesconv/bytesconv.go ，后续要及时同步更新此方法！
 (2) 必须满足的条件：对数据仅仅只有读操作！！！
@@ -19,6 +21,8 @@ func StringToBytes(s string) []byte {
 
 // BytesToString []byte => string
 /*
+Go 如何让在强制转换类型时不发生内存拷贝
+	https://mp.weixin.qq.com/s/wvJm0Q95PTB_YUkZq51C8Q
 PS:
 (1) 参考了 gin(v1.9.1) 中的 internal/bytesconv/bytesconv.go ，后续要及时同步更新此方法！
 (2) 必须满足的条件：对数据仅仅只有读操作！！！
