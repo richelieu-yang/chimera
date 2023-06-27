59cfba7792a902ab18b0a9cbbce365e7497348c3
Richelieu <richelieu042@gmail.com>
2023-06-27 10:25:47 +0800

    ~

--- src/database/mysqlKit/setup_test.go
+++ src/database/mysqlKit/setup_test.go
@@ -8,14 +8,14 @@ import (
 	"github.com/richelieu-yang/chimera/v2/src/core/fileKit"
 	"github.com/richelieu-yang/chimera/v2/src/core/pathKit"
 	"github.com/sirupsen/logrus"
+	"gorm.io/gorm"
 	"testing"
 )
 
 type User struct {
-	ID int
+	gorm.Model
 
-	Name sql.NullString `gorm:"default:null"`
-	Age  uint32
+	Name sql.NullString
 }
 
 func TestSetUp(t *testing.T) {
@@ -42,18 +42,28 @@ func TestSetUp(t *testing.T) {
 		panic(err)
 	}
 	// 可以通过Set设置附加参数，下面设置表的存储引擎为InnoDB
-	db.Set("gorm:table_options", "ENGINE=InnoDB") /*.AutoMigrate(&user{})*/
+	db.Set("gorm:table_options", "ENGINE=InnoDB")
 
-	user := &User{}
+	user := &User{
+		Name: sql.NullString{
+			String: "test",
+			Valid:  true,
+		},
+	}
 	if err := db.AutoMigrate(user); err != nil {
 		logrus.Fatal(err)
 	}
 
-	rst := db.Create(&User{
-		Age: 20,
-	})
+	rst := db.Where(User{
+		Name: sql.NullString{
+			String: "test",
+			Valid:  true,
+		},
+	}).FirstOrCreate(user)
 	if rst.Error != nil {
-		logrus.Fatal(rst.Error)
+		panic(rst.Error)
 	}
-	logrus.Infof("rst.RowsAffected: %d", rst.RowsAffected)
+	// 创建记录，rst.RowsAffected == 1
+	// 否则 rst.RowsAffected == 0
+	fmt.Printf("RowsAffected: [%d]\n", rst.RowsAffected)
 }
--- src/lockKit/locker.go
+++ src/lockKit/locker.go
@@ -2,15 +2,15 @@ package lockKit
 
 import "github.com/gogf/gf/v2/os/gmlock"
 
-// NewLocker 内存锁模块，支持按照给定键名动态生成互斥锁，并发安全并支持Try*Lock特性.
-/*
-使用场景: 需要动态创建互斥锁，或者需要维护大量动态锁的场景.
+var (
+	// NewLocker 内存锁（动态互斥锁），支持 按照给定键名 动态生成互斥锁，并发安全并支持Try*Lock特性.
+	/*
+	   使用场景: 需要动态创建互斥锁，或者需要维护大量动态锁的场景.
 
-PS:
-(1) 当维护大量动态互斥锁的场景时，如果不再使用的互斥锁对象，请手动调用 Remove() 删除掉；
-(2) 可以锁 "关键词"；
-(3) 支持 "读写锁".
-*/
-func NewLocker() *gmlock.Locker {
-	return gmlock.New()
-}
+	   PS:
+	   (1) 当维护大量动态互斥锁的场景时，如果不再使用的互斥锁对象，请手动调用 Remove() 删除掉；
+	   (2) 可以锁 "关键词"；
+	   (3) 支持 "读写锁".
+	*/
+	NewLocker func() *gmlock.Locker = gmlock.New
+)
