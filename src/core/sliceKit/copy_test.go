b03f2ea5e8fd2879f2468c3bab55ed2203d03bad
Richelieu <richelieu042@gmail.com>
2023-06-29 11:30:27 +0800

    ~

---
+++ src/core/sliceKit/copy_test.go
@@ -0,0 +1,39 @@
+package sliceKit
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestCopy(t *testing.T) {
+	m := map[string]interface{}{
+		"Id":   0,
+		"Name": "张三",
+	}
+	s := []interface{}{666, m}
+	s1 := Copy(s)
+
+	s[0] = 999
+	m["Name"] = "李四"
+
+	fmt.Println(s)  // [999 map[Id:0 Name:李四]]
+	fmt.Println(s1) // [666 map[Id:0 Name:李四]]
+}
+
+func TestDeepCopy(t *testing.T) {
+	m := map[string]interface{}{
+		"Id":   0,
+		"Name": "张三",
+	}
+	s := []interface{}{666, m}
+	s1, err := DeepCopy(s)
+	if err != nil {
+		panic(err)
+	}
+
+	s[0] = 999
+	m["Name"] = "李四"
+
+	fmt.Println(s)  // [999 map[Id:0 Name:李四]]
+	fmt.Println(s1) // [666 map[Id:0 Name:张三]]
+}
