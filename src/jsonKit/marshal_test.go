b9468fe3cec64c3d9db7baf3ef391da46017b9fe
Richelieu <richelieu042@gmail.com>
2023-06-29 09:30:32 +0800

    ~

--- src/jsonKit/marshal_test.go
+++ src/jsonKit/marshal_test.go
@@ -1,7 +1,23 @@
 package jsonKit
 
-import "testing"
+import (
+	"fmt"
+	"gorm.io/gorm"
+	"testing"
+)
 
 func TestMarshalToString(t *testing.T) {
+	type user struct {
+		gorm.Model `json:"model"`
 
+		Name string `json:"name"`
+	}
+
+	u := &user{
+		Model: gorm.Model{
+			ID: 1,
+		},
+		Name: "test",
+	}
+	fmt.Println(MarshalToString(u, WithIndent("    ")))
 }
