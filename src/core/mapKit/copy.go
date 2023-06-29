ccaa44125ef803343521636934a301e0266629d4
Richelieu <richelieu042@gmail.com>
2023-06-29 11:20:12 +0800

    ~

--- src/core/mapKit/copy.go
+++ src/core/mapKit/copy.go
@@ -1,14 +1,20 @@
 package mapKit
 
+import "github.com/richelieu-yang/chimera/v2/src/copyKit"
+
 // Copy 浅拷贝
 /*
 @param src 可以为nil
 @return 保底为空的map实例（不为nil）
 */
-func Copy[K comparable, V any](src map[K]V) map[K]V {
+func Copy[K comparable, V any](m map[K]V) map[K]V {
 	dest := map[K]V{}
-	for k, v := range src {
+	for k, v := range m {
 		dest[k] = v
 	}
 	return dest
+}
+
+func DeepCopy[K comparable, V any](m map[K]V) (map[K]V, error) {
+	return copyKit.DeepCopy(m)
 }
---
+++ src/core/mapKit/copy_test.go
@@ -0,0 +1,43 @@
+package mapKit
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestCopy(t *testing.T) {
+	m0 := map[string]interface{}{
+		"a": 1,
+	}
+	m1 := map[string]interface{}{
+		"m0": m0,
+		"b":  2,
+	}
+
+	dolly := Copy(m1)
+	m0["a"] = "a"
+	m1["b"] = "b"
+
+	fmt.Println(m1)    // map[b:b m0:map[a:a]]
+	fmt.Println(dolly) // map[b:2 m0:map[a:a]]
+}
+
+func TestDeepCopy(t *testing.T) {
+	m0 := map[string]interface{}{
+		"a": 1,
+	}
+	m1 := map[string]interface{}{
+		"m0": m0,
+		"b":  2,
+	}
+
+	dolly, err := DeepCopy(m1)
+	if err != nil {
+		panic(err)
+	}
+	m0["a"] = "a"
+	m1["b"] = "b"
+
+	fmt.Println(m1)    // map[b:b m0:map[a:a]]
+	fmt.Println(dolly) // map[b:2 m0:map[a:1]]
+}
