b24c3849cebfef89ca1e9d517cc40c806967995a
Richelieu <richelieu042@gmail.com>
2023-06-30 15:25:36 +0800

    ~

--- src/web/httpKit/proxy.go
+++ src/web/httpKit/proxy.go
@@ -52,6 +52,8 @@ func WithQueryParams(queryParams map[string]string) ProxyOption {
 
 // Proxy 代理请求（反向代理，请求转发）.
 /*
+PS: 转发请求前如果想变更请求头，可以在调用此函数前设置请求头.
+
 @param w e.g. ctx.Writer
 @param r e.g. ctx.Request
 */
