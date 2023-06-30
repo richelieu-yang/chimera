91f642c5f3760f7e59ff9a37ebeb5cc79d837af1
Richelieu <richelieu042@gmail.com>
2023-06-30 13:46:35 +0800

    ~

--- src/core/strKit/trim.go
+++ src/core/strKit/trim.go
@@ -3,7 +3,7 @@ package strKit
 import "strings"
 
 var (
-	// Trim 返回字符串，删除了s"左边和右边"的连续cutset
+	// Trim 返回字符串，删除了s"左边&&右边"的连续cutset
 	/*
 		e.g.
 		fmt.Println(strings.Trim("aaa0aaa0aaa", "a")) // "0aaa0"
@@ -28,16 +28,24 @@ var (
 	*/
 	TrimRight func(s, cutset string) string = strings.TrimRight
 
-	// TrimFunc
+	// TrimFunc 返回字符串，删除了s"左边&&右边"的连续的满足条件的rune
 	/*
 		@param f	(1) 返回true: 	删除此rune，继续判断下一个rune
 					(2) 返回false: 	不删除此rune，中断此次（左边||右边）删除
 	*/
 	TrimFunc func(s string, f func(rune) bool) string = strings.TrimFunc
 
-	// TrimLeftFunc
+	// TrimLeftFunc 返回字符串，删除了s"左边"的连续的满足条件的rune
+	/*
+		@param f	(1) 返回true: 	删除此rune，继续判断下一个rune
+					(2) 返回false: 	不删除此rune，中断此次（左边||右边）删除
+	*/
 	TrimLeftFunc func(s string, f func(rune) bool) string = strings.TrimLeftFunc
 
-	// TrimRightFunc
+	// TrimRightFunc 返回字符串，删除了s"右边"的连续的满足条件的rune
+	/*
+		@param f	(1) 返回true: 	删除此rune，继续判断下一个rune
+					(2) 返回false: 	不删除此rune，中断此次（左边||右边）删除
+	*/
 	TrimRightFunc func(s string, f func(rune) bool) string = strings.TrimRightFunc
 )
