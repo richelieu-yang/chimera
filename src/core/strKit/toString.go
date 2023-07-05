8f57eb079e1b53d465cf99cc4f78e0ee06aae75f
Richelieu <richelieu042@gmail.com>
2023-07-05 13:37:22 +0800

    ~

--- src/core/strKit/toString.go
+++ src/core/strKit/toString.go
@@ -5,24 +5,42 @@ import (
 	"strconv"
 )
 
-func IntToString(i int) string {
-	return strconv.Itoa(i)
-}
+var (
+	ToString func(i interface{}) string = cast.ToString
 
-// ToString
-/*
-e.g.
-(nil) => ""
-*/
-func ToString(obj interface{}) string {
-	return cast.ToString(obj)
-}
+	ToStringE func(i interface{}) (string, error) = cast.ToStringE
 
-// ToStringE
-/*
-e.g.
-(nil) => "", nil
-*/
-func ToStringE(obj interface{}) (string, error) {
-	return cast.ToStringE(obj)
-}
+	// IntToString int（10进制） => string
+	/*
+		PS: strconv.Itoa(i) <=> FormatInt(int64(i), 10)
+	*/
+	IntToString func(i int) string = strconv.Itoa
+
+	// IntToStringWithBase int（各种进制） => string
+	/*
+		@param base 传参i的进制数
+	*/
+	IntToStringWithBase func(i int64, base int) string = strconv.FormatInt
+)
+
+//func IntToString(i int) string {
+//	return strconv.Itoa(i)
+//}
+
+//// ToString
+///*
+//e.g.
+//(nil) => ""
+//*/
+//func ToString(obj interface{}) string {
+//	return cast.ToString(obj)
+//}
+//
+//// ToStringE
+///*
+//e.g.
+//(nil) => "", nil
+//*/
+//func ToStringE(obj interface{}) (string, error) {
+//	return cast.ToStringE(obj)
+//}
