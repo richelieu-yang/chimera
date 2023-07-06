1c0d1bc280b5fde01fa02b43c3f6598cf1a43b27
Richelieu <richelieu042@gmail.com>
2023-07-06 15:30:23 +0800

    ~

--- src/json/sonicKit/marshal.go
+++ src/json/sonicKit/marshal.go
@@ -5,5 +5,5 @@ import "github.com/bytedance/sonic"
 var (
 	Marshal func(val interface{}) ([]byte, error) = sonic.Marshal
 
-	MarshalString func(val interface{}) (string, error) = sonic.MarshalString
+	MarshalToString func(val interface{}) (string, error) = sonic.MarshalString
 )
--- src/json/sonicKit/unmarshal.go
+++ src/json/sonicKit/unmarshal.go
@@ -5,5 +5,5 @@ import "github.com/bytedance/sonic"
 var (
 	Unmarshal func(buf []byte, val interface{}) error = sonic.Unmarshal
 
-	UnmarshalString func(buf string, val interface{}) error = sonic.UnmarshalString
+	UnmarshalFromString func(buf string, val interface{}) error = sonic.UnmarshalString
 )
