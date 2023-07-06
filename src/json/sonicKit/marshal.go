1da335424ffab3787a3fe598ba4595b97c9c415d
Richelieu <richelieu042@gmail.com>
2023-07-06 15:37:26 +0800

    ~

--- src/json/sonicKit/marshal.go
+++ src/json/sonicKit/marshal.go
@@ -7,3 +7,17 @@ var (
 
 	MarshalToString func(val interface{}) (string, error) = sonic.MarshalString
 )
+
+func MarshalByAPI(api sonic.API, val interface{}) ([]byte, error) {
+	if api == nil {
+		api = sonic.ConfigDefault
+	}
+	return api.Marshal(val)
+}
+
+func MarshalToStringByAPI(api sonic.API, val interface{}) (string, error) {
+	if api == nil {
+		api = sonic.ConfigDefault
+	}
+	return api.MarshalToString(val)
+}
--- src/json/sonicKit/unmarshal.go
+++ src/json/sonicKit/unmarshal.go
@@ -7,3 +7,17 @@ var (
 
 	UnmarshalFromString func(buf string, val interface{}) error = sonic.UnmarshalString
 )
+
+func UnmarshalByAPI(api sonic.API, data []byte, v interface{}) error {
+	if api == nil {
+		api = sonic.ConfigDefault
+	}
+	return api.Unmarshal(data, v)
+}
+
+func UnmarshalFromStringByAPI(api sonic.API, str string, v interface{}) error {
+	if api == nil {
+		api = sonic.ConfigDefault
+	}
+	return api.UnmarshalFromString(str, v)
+}
