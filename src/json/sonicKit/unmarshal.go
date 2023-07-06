42b849fcf2d023816cd37ad46539ece4b4da6f34
Richelieu <richelieu042@gmail.com>
2023-07-06 15:29:33 +0800

    ~

--- src/json/sonicKit/unmarshal.go
+++ src/json/sonicKit/unmarshal.go
@@ -1,1 +1,9 @@
 package sonicKit
+
+import "github.com/bytedance/sonic"
+
+var (
+	Unmarshal func(buf []byte, val interface{}) error = sonic.Unmarshal
+
+	UnmarshalString func(buf string, val interface{}) error = sonic.UnmarshalString
+)
