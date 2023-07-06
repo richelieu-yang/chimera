6447c5abfb46cd413bf5df0dc8fdff9d58b3a125
Richelieu <richelieu042@gmail.com>
2023-07-06 16:06:20 +0800

    ~

--- test/test1.go
+++ test/test1.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/bytedance/sonic"
+	"github.com/richelieu-yang/chimera/v2/src/json/sonicKit"
+)
 
+func main() {
+	m := map[string]interface{}{
+		"a":    1,
+		"b":    2,
+		"1":    3,
+		"111":  6,
+		"1111": 6,
+		"1112": 6,
+		"d":    4,
+	}
+	for i := 0; i < 10; i++ {
+		str, _ := sonicKit.MarshalToStringByAPI(sonic.ConfigStd, m)
+		if str != `{"1":3,"111":6,"1111":6,"1112":6,"a":1,"b":2,"d":4}` {
+			panic(str)
+		}
+		fmt.Println(str)
+	}
 }
