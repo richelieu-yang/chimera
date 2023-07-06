344d5cd6ff49d51570ae9a9e8c4bea3b78538a23
Richelieu <richelieu042@gmail.com>
2023-07-06 15:54:03 +0800

    ~

---
+++ src/json/sonicKit/decoder.go
@@ -0,0 +1,11 @@
+package sonicKit
+
+import (
+	"github.com/bytedance/sonic"
+	"io"
+)
+
+// NewDecoder 解码器（from json）
+func NewDecoder(api sonic.API, reader io.Reader) sonic.Decoder {
+	return api.NewDecoder(reader)
+}
---
+++ src/json/sonicKit/encoder.go
@@ -0,0 +1,11 @@
+package sonicKit
+
+import (
+	"github.com/bytedance/sonic"
+	"io"
+)
+
+// NewEncoder 编码器（to json）
+func NewEncoder(api sonic.API, writer io.Writer) sonic.Encoder {
+	return api.NewEncoder(writer)
+}
