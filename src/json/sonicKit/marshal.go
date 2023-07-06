20b2dbef5d656bf80d04e288865ef9c7a126ffee
Richelieu <richelieu042@gmail.com>
2023-07-06 15:47:43 +0800

    ~

--- src/json/sonicKit/marshal.go
+++ src/json/sonicKit/marshal.go
@@ -3,21 +3,36 @@ package sonicKit
 import "github.com/bytedance/sonic"
 
 var (
-	Marshal func(val interface{}) ([]byte, error) = sonic.Marshal
+	Marshal func(v interface{}) ([]byte, error) = sonic.Marshal
 
-	MarshalToString func(val interface{}) (string, error) = sonic.MarshalString
+	MarshalToString func(v interface{}) (string, error) = sonic.MarshalString
 )
 
-func MarshalByAPI(api sonic.API, val interface{}) ([]byte, error) {
+func MarshalByAPI(api sonic.API, v interface{}) ([]byte, error) {
 	if api == nil {
 		api = sonic.ConfigDefault
 	}
-	return api.Marshal(val)
+	return api.Marshal(v)
 }
 
-func MarshalToStringByAPI(api sonic.API, val interface{}) (string, error) {
+func MarshalToStringByAPI(api sonic.API, v interface{}) (string, error) {
 	if api == nil {
 		api = sonic.ConfigDefault
 	}
-	return api.MarshalToString(val)
+	return api.MarshalToString(v)
+}
+
+func MarshalByAPIWithIndent(api sonic.API, v interface{}, prefix, indent string) ([]byte, error) {
+	if api == nil {
+		api = sonic.ConfigDefault
+	}
+	return api.MarshalIndent(v, prefix, indent)
+}
+
+func MarshalToStringByAPIWithIndent(api sonic.API, v interface{}, prefix, indent string) (string, error) {
+	data, err := MarshalByAPIWithIndent(api, v, prefix, indent)
+	if err != nil {
+		return "", err
+	}
+	return string(data), nil
 }
