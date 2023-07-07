bc1be03f0f449a3d75fb666330d009eb8edbf680
Richelieu <richelieu042@gmail.com>
2023-07-07 14:27:03 +0800

    ~

--- src/web/cookieKit/common.go
+++ src/web/cookieKit/common.go
@@ -9,8 +9,8 @@ import (
 /*
 PS:
 (1) 同时设置 MaxAge 和 Expires 的情况下，
-	(a) IE浏览器，忽略 MaxAge;
-	(b) 其它浏览器，忽略 Expires.
+	(a) IE浏览器，忽略 MaxAge，使用 Expires;
+	(b) 其它浏览器，忽略 Expires，使用 MaxAge.
 (2) IE浏览器只认 Expires，但这样有缺陷: 浏览器（客户端）和服务端存在时间差的情况下，Cookie可能会提前或延后过期.
 */
 func SetCookieMaxAge(cookie *http.Cookie, maxAge int) {
