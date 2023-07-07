4b10e66cb5153789ee023eb88a244f8caa6283de
Richelieu <richelieu042@gmail.com>
2023-07-07 13:25:26 +0800

    ~

--- src/web/ginKit/config.go
+++ src/web/ginKit/config.go
@@ -1,9 +1,8 @@
 package ginKit
 
 import (
-	"github.com/richelieu-yang/chimera/v2/src/core/errorKit"
 	"github.com/richelieu-yang/chimera/v2/src/core/fileKit"
-	"github.com/richelieu-yang/chimera/v2/src/core/strKit"
+	"github.com/richelieu-yang/chimera/v2/src/core/interfaceKit"
 	"github.com/richelieu-yang/chimera/v2/src/web/refererKit"
 )
 
@@ -14,15 +13,21 @@ type (
 		Colorful bool   `json:"colorful,default=true"`
 		Pprof    bool   `json:"pprof,default=false"`
 
-		Middleware *MiddlewareConfig `json:"middleware"`
-		SSL        *SslConfig        `json:"ssl"`
+		SSL        SslConfig        `json:"ssl"`
+		Middleware MiddlewareConfig `json:"middleware"`
+	}
+
+	SslConfig struct {
+		Access   bool   `json:"access,default=false"`
+		CertFile string `json:"certFile,optional"`
+		KeyFile  string `json:"keyFile,optional"`
 	}
 
 	MiddlewareConfig struct {
-		BodyLimit     int64                                `json:"bodyLimit,default=-1,range=[-1:10000]"`
+		BodyLimit     int64                                `json:"bodyLimit,default=-1,range=[-1:]"`
 		Gzip          bool                                 `json:"gzip,default=false"`
 		XFrameOptions string                               `json:"xFrameOptions,optional"`
-		Cors          *CorsConfig                          `json:"cors,optional"`
+		Cors          CorsConfig                           `json:"cors,optional"`
 		Referer       []*refererKit.RefererVerifierBuilder `json:"referer,optional"`
 	}
 
@@ -31,42 +36,21 @@ type (
 		Access  bool     `json:"access,default=false"`
 		Origins []string `json:"origins,optional"`
 	}
-
-	SslConfig struct {
-		CertFile string `json:"certFile,optional"`
-		KeyFile  string `json:"keyFile,optional"`
-		Port     int    `json:"port,default=-1,range=[-1:65535]"`
-	}
 )
 
 func (config *Config) Verify() error {
-	if config == nil {
-		return errorKit.New("config == nil")
+	if err := interfaceKit.AssertNotNil(config, "config"); err != nil {
+		return err
 	}
 
 	// ssl
 	sslConfig := config.SSL
-	if sslConfig != nil {
-		if sslConfig.Port == -1 || strKit.HasEmpty(sslConfig.CertFile, sslConfig.KeyFile) {
-			sslConfig = nil
-		} else {
-			if err := fileKit.AssertExistAndIsFile(sslConfig.CertFile); err != nil {
-				return err
-			}
-			if err := fileKit.AssertExistAndIsFile(sslConfig.KeyFile); err != nil {
-				return err
-			}
+	if sslConfig.Access {
+		if err := fileKit.AssertExistAndIsFile(sslConfig.CertFile); err != nil {
+			return err
 		}
-	}
-
-	// ports(http && https)
-	if config.Port != -1 {
-		if sslConfig != nil && config.Port == sslConfig.Port {
-			return errorKit.New("http port and https port are same(%d)", config.Port)
-		}
-	} else {
-		if sslConfig == nil {
-			return errorKit.New("both http port and https port are invalid(-1)")
+		if err := fileKit.AssertExistAndIsFile(sslConfig.KeyFile); err != nil {
+			return err
 		}
 	}
 
--- src/web/ginKit/middleware.go
+++ src/web/ginKit/middleware.go
@@ -10,14 +10,14 @@ import (
 )
 
 // attachMiddlewares 绑定一些常用的中间件.
-func attachMiddlewares(engine *gin.Engine, config *MiddlewareConfig, recoveryMiddleware gin.HandlerFunc) error {
+func attachMiddlewares(engine *gin.Engine, config MiddlewareConfig, recoveryMiddleware gin.HandlerFunc) error {
 	// gzip
 	/*
 		PS:
 		(1) 必须在 recoveryMiddleware 前面，为了万无一失还是放在最前面吧；
 		(2) gzip会使得响应头中的 Content-Length 不生效.
 	*/
-	if config != nil && config.Gzip {
+	if config.Gzip {
 		engine.Use(gzip.Gzip(gzip.BestSpeed))
 	}
 
@@ -30,64 +30,63 @@ func attachMiddlewares(engine *gin.Engine, config *MiddlewareConfig, recoveryMid
 	}
 	engine.Use(recoveryMiddleware)
 
-	if config != nil {
-		// cors
-		{
-			cc := config.Cors
-			if cc != nil && cc.Access {
-				// 配置cors
-				origins := cc.Origins
-				origins = sliceKit.RemoveEmpty(origins, true)
-				origins = sliceKit.Uniq(origins)
+	// cors
+	{
+		cc := config.Cors
+		if cc.Access {
+			// 配置cors
+			origins := cc.Origins
+			origins = sliceKit.RemoveEmpty(origins, true)
+			origins = sliceKit.Uniq(origins)
 
-				engine.Use(NewCorsMiddleware(origins))
-			} else {
-				// 不配置cors
+			engine.Use(NewCorsMiddleware(origins))
+		} else {
+			// 不配置cors
+		}
+	}
+
+	// referer（必须在cors中间件后面）
+	{
+		refererConfig := config.Referer
+		if refererConfig != nil {
+			middleware, err := refererKit.NewGinRefererMiddleware(refererConfig)
+			if err != nil {
+				return err
 			}
+			engine.Use(middleware)
 		}
+	}
 
-		// referer（必须在cors中间件后面）
-		{
-			refererConfig := config.Referer
-			if refererConfig != nil {
-				middleware, err := refererKit.NewGinRefererMiddleware(refererConfig)
-				if err != nil {
-					return err
-				}
-				engine.Use(middleware)
-			}
-		}
-
-		// bodyLimit
-		// TODO: 因为http.MaxBytesReader()，如果涉及"请求转发（代理）"，转发方不要全局配置此属性，否则会导致: 有时成功，有时代理失败（error），有时http客户端失败
-		if config.BodyLimit > 0 {
-			limit := config.BodyLimit << 20
-			engine.Use(func(ctx *gin.Context) {
-				// 参考了echo中的 middleware.BodyLimit()
-
-				// (1) Based on content length
-				if ctx.Request.ContentLength > limit {
-					ctx.AbortWithStatus(http.StatusRequestEntityTooLarge)
-					return
-				}
-
-				// (2) Based on content read
-				if ctx.Request.Body != nil {
-					ctx.Request.Body = http.MaxBytesReader(ctx.Writer, ctx.Request.Body, limit+128)
-				}
-			})
-		}
-
-		// others
+	// bodyLimit
+	// TODO: 因为http.MaxBytesReader()，如果涉及"请求转发（代理）"，转发方不要全局配置此属性，否则会导致: 有时成功，有时代理失败（error），有时http客户端失败
+	if config.BodyLimit > 0 {
+		limit := config.BodyLimit << 20
 		engine.Use(func(ctx *gin.Context) {
-			if strKit.IsNotEmpty(config.XFrameOptions) {
-				// e.g.不能被嵌入到任何iframe或frame中
-				ctx.Header("X-Frame-Options", config.XFrameOptions)
+			// 参考了echo中的 middleware.BodyLimit()
+
+			// (1) Based on content length
+			if ctx.Request.ContentLength > limit {
+				ctx.AbortWithStatus(http.StatusRequestEntityTooLarge)
+				return
 			}
-			// 解决漏洞: 未启用Web浏览器XSS保护
-			ctx.Header("X-XSS-Protection", "1;mode=block")
+
+			// (2) Based on content read
+			if ctx.Request.Body != nil {
+				ctx.Request.Body = http.MaxBytesReader(ctx.Writer, ctx.Request.Body, limit+128)
+			}
 		})
 	}
+
+	// others
+	engine.Use(func(ctx *gin.Context) {
+		if strKit.IsNotEmpty(config.XFrameOptions) {
+			// e.g.不能被嵌入到任何iframe或frame中
+			ctx.Header("X-Frame-Options", config.XFrameOptions)
+		}
+
+		// 解决漏洞: 未启用Web浏览器XSS保护
+		ctx.Header("X-XSS-Protection", "1;mode=block")
+	})
 
 	return nil
 }
