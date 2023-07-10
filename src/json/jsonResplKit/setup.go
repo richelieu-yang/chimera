3620e6b1d3c670809e5bb2971d198eadb45dad72
Richelieu <richelieu042@gmail.com>
2023-07-10 10:18:16 +0800

    ~

--- src/json/jsonResplKit/setup.go
+++ src/json/jsonResplKit/setup.go
@@ -2,7 +2,9 @@ package jsonResplKit
 
 import (
 	"github.com/richelieu-yang/chimera/v2/src/core/interfaceKit"
+	"github.com/richelieu-yang/chimera/v2/src/log/logrusKit"
 	"github.com/richelieu-yang/chimera/v2/src/msgKit"
+	"github.com/sirupsen/logrus"
 )
 
 var (
@@ -10,6 +12,13 @@ var (
 	api          API                 = nil
 	msgProcessor func(string) string = nil
 )
+
+func MustSetUp(respProvider RespProvider, msgFilePaths []string, options ...Option) {
+	if err := SetUp(respProvider, msgFilePaths, options...); err != nil {
+		logrusKit.DisableQuote(nil)
+		logrus.Fatalf("%+v", err)
+	}
+}
 
 func SetUp(respProvider RespProvider, msgFilePaths []string, options ...Option) error {
 	if err := interfaceKit.AssertNotNil(respProvider, "respProvider"); err != nil {
