87c2abbfa7888ee8f09b9c24b22b0b1a62de9769
Richelieu <richelieu042@gmail.com>
2023-07-10 15:03:58 +0800

    ~

--- src/database/etcdKit/config.go
+++ src/database/etcdKit/config.go
@@ -2,6 +2,7 @@ package etcdKit
 
 import (
 	"github.com/richelieu-yang/chimera/v2/src/core/errorKit"
+	"github.com/richelieu-yang/chimera/v2/src/core/interfaceKit"
 	"github.com/richelieu-yang/chimera/v2/src/core/sliceKit"
 )
 
@@ -14,13 +15,13 @@ type (
 )
 
 func (config *Config) Check() error {
-	if config == nil {
-		return errorKit.New("config == nil")
+	if err := interfaceKit.AssertNotNil(config, "config"); err != nil {
+		return err
 	}
 
 	config.Endpoints = sliceKit.Uniq(config.Endpoints)
 	config.Endpoints = sliceKit.RemoveEmpty(config.Endpoints, true)
-	if len(config.Endpoints) == 0 {
+	if sliceKit.IsEmpty(config.Endpoints) {
 		return errorKit.New("config.Endpoints is empty")
 	}
 
