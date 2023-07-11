4c97f30b106635141ae6b126e98074dede4ccdad
Richelieu <richelieu042@gmail.com>
2023-07-11 14:32:53 +0800

    ~

--- src/json/jsonKit/apiSonic.go
+++ src/json/jsonKit/apiSonic.go
@@ -1,3 +1,4 @@
+// 编译标签(build tag): 参考gin中的"sonic.go"
 //go:build (linux || windows || darwin) && amd64
 
 package jsonKit
--- test/test1.go
+++ test/test1.go
@@ -1,4 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"github.com/richelieu-yang/chimera/v2/src/json/jsonKit"
+)
+
 func main() {
+	fmt.Println(jsonKit.MarshalToString(1))
 }
