4775f9ed1f9f4e34524c282d698fcf5286a544ab
Richelieu <richelieu042@gmail.com>
2023-07-11 14:03:26 +0800

    ~

--- src/json/jsonKit/api.go
+++ src/json/jsonKit/api.go
@@ -2,14 +2,40 @@ package jsonKit
 
 var api API = nil
 
-type API interface {
-	Marshal(v interface{}) ([]byte, error)
+type (
+	API interface {
+		Marshal(v interface{}) ([]byte, error)
 
-	MarshalIndent(v interface{}, prefix, indent string) ([]byte, error)
+		MarshalIndent(v interface{}, prefix, indent string) ([]byte, error)
 
-	MarshalToString(v interface{}) (string, error)
+		MarshalToString(v interface{}) (string, error)
 
-	Unmarshal(data []byte, v interface{}) error
+		Unmarshal(data []byte, v interface{}) error
 
-	UnmarshalFromString(str string, v interface{}) error
+		UnmarshalFromString(str string, v interface{}) error
+	}
+)
+
+func SetAPI(a API) {
+	api = a
+}
+
+func Marshal(v interface{}) ([]byte, error) {
+	return api.Marshal(v)
+}
+
+func MarshalIndent(v interface{}, prefix, indent string) ([]byte, error) {
+	return api.MarshalIndent(v, prefix, indent)
+}
+
+func MarshalToString(v interface{}) (string, error) {
+	return api.MarshalToString(v)
+}
+
+func Unmarshal(data []byte, v interface{}) error {
+	return api.Unmarshal(data, v)
+}
+
+func UnmarshalFromString(str string, v interface{}) error {
+	return api.UnmarshalFromString(str, v)
 }
