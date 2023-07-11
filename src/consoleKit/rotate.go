5e5a3e8ac04f7a84679d08cf4a3b155455586fef
Richelieu <richelieu042@gmail.com>
2023-07-11 16:30:11 +0800

    ~

--- src/consoleKit/rotate.go
+++ src/consoleKit/rotate.go
@@ -2,8 +2,10 @@ package consoleKit
 
 import (
 	"fmt"
+	"github.com/richelieu-yang/chimera/v2/src/consts"
 	"github.com/richelieu-yang/chimera/v2/src/core/fileKit"
 	"github.com/richelieu-yang/chimera/v2/src/core/pathKit"
+	"github.com/richelieu-yang/chimera/v2/src/core/strKit"
 	"github.com/richelieu-yang/chimera/v2/src/core/timeKit"
 	"github.com/richelieu-yang/chimera/v2/src/cronKit"
 	"github.com/robfig/cron/v3"
@@ -13,13 +15,19 @@ import (
 
 // RotateOutput
 /*
+PS: 主要用于将nohup输出每天分块.
+
 @param output 	控制台输出文件（e.g.nohup.out）的路径
 @param backDir	备份文件存放的目录
 */
 func RotateOutput(output, backupDir, spec string) (*cron.Cron, error) {
 	c, _, err := cronKit.NewCronWithTask(spec, func() {
-		if err := rotate(output, backupDir); err != nil {
-			logrus.Errorf("%+v", err)
+		if backupPath, err := rotate(output, backupDir); err != nil {
+			logrus.WithError(err).Errorf("[%s] Fail to rotate.", strKit.ToUpper(consts.ProjectName))
+		} else {
+			logrus.WithFields(logrus.Fields{
+				"backupPath": backupPath,
+			}).Infof("[%s] Succeed to rotate.", strKit.ToUpper(consts.ProjectName))
 		}
 	})
 	if err != nil {
@@ -29,12 +37,12 @@ func RotateOutput(output, backupDir, spec string) (*cron.Cron, error) {
 	return c, nil
 }
 
-func rotate(output, backupDir string) error {
+func rotate(output, backupDir string) (string, error) {
 	if err := fileKit.AssertExistAndIsFile(output); err != nil {
-		return err
+		return "", err
 	}
 	if err := fileKit.MkDirs(backupDir); err != nil {
-		return err
+		return "", err
 	}
 
 	dateStr := timeKit.FormatTimeToString(time.Now().Add(-timeKit.Day), "2006-01-02")
@@ -43,11 +51,11 @@ func rotate(output, backupDir string) error {
 	target := pathKit.Join(backupDir, fmt.Sprintf("%s-%s%s", name, dateStr, ext))
 	// 复制文件
 	if err := fileKit.CopyFile(output, target); err != nil {
-		return err
+		return "", err
 	}
 	// 清空文件
 	if err := fileKit.Truncate(output, 0); err != nil {
-		return err
+		return "", err
 	}
-	return nil
+	return target, nil
 }
