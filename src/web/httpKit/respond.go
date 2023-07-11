b1619a3bffc23715f3db183e8ad47f48378db828
Richelieu <richelieu042@gmail.com>
2023-07-11 10:56:11 +0800

    ~

--- src/web/httpKit/respond.go
+++ src/web/httpKit/respond.go
@@ -14,8 +14,10 @@ import (
 const (
 	// PlainContentType 纯文本格式
 	PlainContentType = "text/plain; charset=utf-8"
+
 	// JsonContentType JSON数据格式
 	JsonContentType = "application/json; charset=utf-8"
+
 	// OctetStreamContentType 二进制流数据（如常见的文件下载）
 	/*
 		参考：https://www.runoob.com/http/http-content-type.html
