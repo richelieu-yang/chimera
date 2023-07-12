7653ce643d6a8a63bc221dda7c887059772ffab9
Richelieu <richelieu042@gmail.com>
2023-07-12 13:49:58 +0800

    ~

--- test/test3.go
+++ test/test3.go
@@ -1,28 +1,19 @@
 package main
 
 import (
+	"encoding/base64"
 	"fmt"
-	"github.com/richelieu-yang/chimera/v2/src/json/jsonResplKit"
+	"github.com/gogf/gf/v2/encoding/gbase64"
 )
 
 func main() {
-	type resp struct {
-		Code string      `json:"errorCode"`
-		Msg  string      `json:"errorMessage"`
-		Data interface{} `json:"result"`
-	}
-	var respProvider jsonResplKit.RespProvider = func(code, msg string, data interface{}) interface{} {
-		msg = "[A] " + msg
-		return &resp{
-			Code: code,
-			Msg:  msg,
-			Data: data,
-		}
-	}
 
-	jsonResplKit.MustSetUp(respProvider, []string{"chimera-lib/msg.properties"})
-	// {"errorCode":"0","errorMessage":"[A] no error","result":1} <nil>
-	fmt.Println(jsonResplKit.Seal("0", 1))
-	// {"errorCode":"42","errorMessage":"[A] hello 测试","result":null} <nil>
-	fmt.Println(jsonResplKit.Seal("42", nil, "测试"))
+	gbase64.Decode()
+
+	input := []byte("\x00\x00\x3e\x00\x00\x3f\x00")
+	fmt.Println(string(input))
+	fmt.Println(base64.StdEncoding.EncodeToString(input))                               // AAA+AAA/AA==
+	fmt.Println(base64.URLEncoding.EncodeToString(input))                               // AAA-AAA_AA==
+	fmt.Println(base64.StdEncoding.WithPadding(base64.NoPadding).EncodeToString(input)) // AAA+AAA/AA
+	fmt.Println(base64.URLEncoding.WithPadding(base64.NoPadding).EncodeToString(input)) // AAA-AAA_AA
 }
