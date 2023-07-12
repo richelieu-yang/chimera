3e818d1f7b6c68ae15f2dff9829acf7efb5f6d9e
Richelieu <richelieu042@gmail.com>
2023-07-12 14:07:11 +0800

    ~

--- test/test3.go
+++ test/test3.go
@@ -1,19 +1,10 @@
 package main
 
 import (
-	"encoding/base64"
 	"fmt"
-	"github.com/gogf/gf/v2/encoding/gbase64"
+	"github.com/richelieu-yang/chimera/v2/src/core/timeKit"
 )
 
 func main() {
-
-	gbase64.Decode()
-
-	input := []byte("\x00\x00\x3e\x00\x00\x3f\x00")
-	fmt.Println(string(input))
-	fmt.Println(base64.StdEncoding.EncodeToString(input))                               // AAA+AAA/AA==
-	fmt.Println(base64.URLEncoding.EncodeToString(input))                               // AAA-AAA_AA==
-	fmt.Println(base64.StdEncoding.WithPadding(base64.NoPadding).EncodeToString(input)) // AAA+AAA/AA
-	fmt.Println(base64.URLEncoding.WithPadding(base64.NoPadding).EncodeToString(input)) // AAA-AAA_AA
+	fmt.Println(timeKit.FormatCurrentTime(timeKit.FormatFileName))
 }
