0d4e47a6ac1dec03df9ddc3c7117200aff1365a5
Richelieu <richelieu042@gmail.com>
2023-07-13 14:03:50 +0800

    ~

--- src/crypto/rsaKit/generate_test.go
+++ src/crypto/rsaKit/generate_test.go
@@ -1,15 +1,23 @@
 package rsaKit
 
-import "testing"
+import (
+	"github.com/richelieu-yang/chimera/v2/src/consts"
+	"github.com/richelieu-yang/chimera/v2/src/core/pathKit"
+	"testing"
+)
 
 func TestGenerateKeyFiles(t *testing.T) {
+	_, err := pathKit.ReviseWorkingDirInTestMode(consts.ProjectName)
+	if err != nil {
+		panic(err)
+	}
+
 	options := []RsaOption{
 		WithFormat(PKCS1),
 		WithPassword(""),
 	}
-
-	priPath := "_pri.key"
-	pubPath := "_pub.key"
+	priPath := "_pri.pem"
+	pubPath := "_pub.pem"
 	if err := GenerateKeyFiles(2048, priPath, pubPath, options...); err != nil {
 		panic(err)
 	}
