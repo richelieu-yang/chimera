17e04992810a2b257309121c2423a826163d03ef
Richelieu <richelieu042@gmail.com>
2023-07-14 13:37:18 +0800

    ~

--- src/core/mathKit/minMax.go
+++ src/core/mathKit/minMax.go
@@ -4,7 +4,7 @@ import "golang.org/x/exp/constraints"
 
 // Max
 /*
-TODO: go1.21，使用max().
+TODO: go1.21，使用max()（区分go版本）.
 */
 func Max[T constraints.Ordered](p T, args ...T) T {
 	rst := p
@@ -19,7 +19,7 @@ func Max[T constraints.Ordered](p T, args ...T) T {
 
 // Min
 /*
-TODO: go1.21，使用min().
+TODO: go1.21，使用min()（区分go版本）.
 */
 func Min[T constraints.Ordered](p T, args ...T) T {
 	rst := p
