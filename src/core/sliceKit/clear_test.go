28a5e9868aa4ac3c0dc4651b8db165e1e6d53e78
Richelieu <richelieu042@gmail.com>
2023-07-14 13:52:47 +0800

    ~

--- src/core/sliceKit/clear_test.go
+++ src/core/sliceKit/clear_test.go
@@ -2,16 +2,26 @@ package sliceKit
 
 import (
 	"fmt"
+	"github.com/richelieu-yang/chimera/v2/src/core/runtimeKit"
 	"testing"
 )
 
 func TestClear(t *testing.T) {
-	s := make([]interface{}, 3, 6)
-	s[0] = 0
-	s[1] = 1
-	s[2] = 2
-	fmt.Println(s, len(s), cap(s))
+	type bean struct {
+		id int
+	}
+
+	fmt.Println(runtimeKit.GoVersion)
+
+	s := make([]bean, 3, 6)
+	s[0] = bean{0}
+	s[1] = bean{1}
+	s[2] = bean{2}
+	fmt.Println(s, len(s), cap(s)) // [{0} {1} {2}] 3 6
 
 	Clear(s)
-	fmt.Println(s, len(s), cap(s))
+	fmt.Println(s, len(s), cap(s)) // [{0} {0} {0}] 3 6
+
+	s[0].id = 999
+	fmt.Println(s, len(s), cap(s)) // [{999} {0} {0}] 3 6
 }
--- test/test3.go
+++ test/test3.go
@@ -1,19 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"github.com/richelieu-yang/chimera/v2/src/core/mapKit"
-	"github.com/richelieu-yang/chimera/v2/src/core/runtimeKit"
-)
-
 func main() {
-	fmt.Println(runtimeKit.GoVersion)
-	m := map[string]interface{}{
-		"1": 1,
-		"0": 0,
-	}
-	fmt.Println(m)
 
-	mapKit.Clear(m)
-	fmt.Println(m)
 }
