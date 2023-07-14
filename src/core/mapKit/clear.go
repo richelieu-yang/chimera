c6ef240b69a378fd54c92b70e3bb4f3269951d1f
Richelieu <richelieu042@gmail.com>
2023-07-14 13:40:40 +0800

    ~

--- src/core/mapKit/clear.go
+++ src/core/mapKit/clear.go
@@ -2,6 +2,10 @@
 
 package mapKit
 
+// Clear
+/*
+Deprecated: go <= 1.20的情况下，性能较差，还不如创建个空的map然后覆盖.
+*/
 func Clear[K comparable, V any](m map[K]V) {
 	for k, _ := range m {
 		delete(m, k)
--- test/test3.go
+++ test/test3.go
@@ -3,15 +3,16 @@ package main
 import (
 	"fmt"
 	"github.com/richelieu-yang/chimera/v2/src/core/mapKit"
+	"github.com/richelieu-yang/chimera/v2/src/core/runtimeKit"
 )
 
 func main() {
+	fmt.Println(runtimeKit.GoVersion)
 	m := map[string]interface{}{
 		"1": 1,
+		"0": 0,
 	}
 	fmt.Println(m)
-
-	clear()
 
 	mapKit.Clear(m)
 	fmt.Println(m)
