d7cf9279099631e713b4f917f4c57c31f381a207
Richelieu <richelieu042@gmail.com>
2023-08-01 17:22:49 +0800

    ~

--- src/core/osKit/info_windows.go
+++ src/core/osKit/info_windows.go
@@ -1,1 +1,19 @@
 package osKit
+
+import "github.com/richelieu-yang/chimera/v2/src/core/errorKit"
+
+func GetCountOfProcesses() (int, error) {
+	return 0, errorKit.New("not yet realized")
+}
+
+func GetMaxOpenFiles() (int, error) {
+	return 0, errorKit.New("not yet realized")
+}
+
+func GetMaxUserProcesses() (int, error) {
+	return 0, errorKit.New("not yet realized")
+}
+
+func GetCoreFileSize() (string, error) {
+	return "", errorKit.New("not yet realized")
+}
