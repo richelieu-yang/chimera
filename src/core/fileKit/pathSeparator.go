d95bad5476ec2751d5963d8bf4762599ac841573
Richelieu <richelieu042@gmail.com>
2023-08-01 14:34:21 +0800

    ~

--- src/core/fileKit/pathSeparator.go
+++ src/core/fileKit/pathSeparator.go
@@ -3,11 +3,13 @@ package fileKit
 import "os"
 
 const (
-	// PathSeparator 路径分隔符，Mac("/")
-	PathSeparator          = string(os.PathSeparator)
 	PathSeparatorRune rune = os.PathSeparator
 
-	// PathListSeparator 路径列表分隔符，Mac(":")
-	PathListSeparator          = string(os.PathListSeparator)
+	// PathSeparator 路径分隔符，Mac("/")
+	PathSeparator = string(os.PathSeparator)
+
 	PathListSeparatorRune rune = os.PathListSeparator
+
+	// PathListSeparator 路径列表分隔符，Mac(":")
+	PathListSeparator = string(os.PathListSeparator)
 )
