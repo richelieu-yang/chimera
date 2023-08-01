2a1100bca83dbbde2964ba0108b36dff24d80186
Richelieu <richelieu042@gmail.com>
2023-08-01 10:24:47 +0800

    ~

--- src/core/memoryKit/memory.go
+++ src/core/memoryKit/memory.go
@@ -6,61 +6,6 @@ import (
 	"github.com/shirou/gopsutil/v3/mem"
 )
 
-type (
-	MemoryStat mem.VirtualMemoryStat
-)
-
-func (stat MemoryStat) String() string {
-	return fmt.Sprintf("total: %s, available: %s, used: %s, free: %s, used percent: %.2f%%",
-		dataSizeKit.ToReadableStringWithIEC(stat.Total),
-		dataSizeKit.ToReadableStringWithIEC(stat.Available),
-		dataSizeKit.ToReadableStringWithIEC(stat.Used),
-		dataSizeKit.ToReadableStringWithIEC(stat.Free),
-		stat.UsedPercent,
-	)
-}
-
-//// GetAvailableMemoryRatio 获取可用内存的比例
-///*
-//@return e.g. (0.4909677505493164, nil)
-//
-//“比例”和“比率”的区别？
-//全班人数50人，男生30，女生20，那男生的比例就是30/50，同理女生的就是20/50，那么男女的比率是什么呢，是30/20。
-//*/
-//func GetAvailableMemoryRatio() (float64, error) {
-//	stat, err := mem.VirtualMemory()
-//	if err != nil {
-//		return 0.0, err
-//	}
-//	tmp := floatKit.Div(float64(stat.Available), float64(stat.Total))
-//	return tmp, nil
-//}
-//
-//// GetAvailableMemoryPercent
-///*
-//@return e.g. (49.09634590148926, nil)
-//*/
-//func GetAvailableMemoryPercent() (float64, error) {
-//	stat, err := mem.VirtualMemory()
-//	if err != nil {
-//		return 0.0, err
-//	}
-//	return 100 - stat.UsedPercent, nil
-//}
-//
-//// GetUsedMemoryRatio
-///*
-//@return e.g. (0.509037971496582, nil)
-//*/
-//func GetUsedMemoryRatio() (float64, error) {
-//	stat, err := mem.VirtualMemory()
-//	if err != nil {
-//		return 0.0, err
-//	}
-//	tmp := floatKit.Div(float64(stat.Used), float64(stat.Total))
-//	return tmp, nil
-//}
-
 // GetUsedMemoryPercent
 /*
 @return e.g. (50.903940200805664, nil)
