2b441d0d3417fe4d69a695c7646f6adeeaca6b59
Richelieu <richelieu042@gmail.com>
2023-08-03 13:46:23 +0800

    ~

--- test/test3.go
+++ test/test3.go
@@ -1,19 +1,5 @@
 package main
 
-import (
-	"flag"
-	"fmt"
-)
-
-var addr string
-
-func init() {
-	flag.StringVar(&addr, "addr", "default", "address")
-}
-
 func main() {
-	// 必要，否则取不到值
-	flag.Parse()
 
-	fmt.Printf("addr:[%s]\n", addr)
 }
