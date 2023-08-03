6a362523b74285ddd52525868475a335e8accfb3
Richelieu <richelieu042@gmail.com>
2023-08-03 15:57:04 +0800

    ~

--- src/core/osKit/info_windows.go
+++ src/core/osKit/info_windows.go
@@ -10,17 +10,14 @@ func GetCurrentCountOfProcessesAndThreads() (int, error) {
 	return 0, errorKit.New("not yet realized")
 }
 
-// GetThreadsMax 获取Linux的"kernel.threads-max"
 func GetThreadsMax() (int, error) {
 	return 0, errorKit.New("not yet realized")
 }
 
-// GetPidMax 获取Linux的"kernel.pid_max"
 func GetPidMax() (int, error) {
 	return 0, errorKit.New("not yet realized")
 }
 
-// GetMaxThreadCountInAProcess 获取 Linux的"vm.max_map_count"（单进程可生成的最大线程数）
 func GetMaxThreadCountInAProcess() (int, error) {
 	return 0, errorKit.New("not yet realized")
 }
