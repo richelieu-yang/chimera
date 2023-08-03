067240e9682a1ce9f2cfc24cba2f88f9b6e3e465
Richelieu <richelieu042@gmail.com>
2023-08-03 14:36:14 +0800

    ~

--- src/core/osKit/info_darwin.go
+++ src/core/osKit/info_darwin.go
@@ -4,23 +4,6 @@ import (
 	"github.com/richelieu-yang/chimera/v2/src/core/errorKit"
 )
 
-// GetThreadCount
-/*
-支持: 	Linux
-不支持:	Mac
-*/
 func GetThreadCount() (int, error) {
 	return 0, errorKit.New("not yet realized")
-
-	//str, err := cmdKit.ExecuteToString("sh", "-c", "ps -eLf | wc -l")
-	//if err != nil {
-	//	return 0, err
-	//}
-	//str = strKit.TrimSpace(str)
-	//
-	//i, err := strconv.Atoi(str)
-	//if err != nil {
-	//	return 0, err
-	//}
-	//return i, nil
 }
--- src/core/osKit/info_linux.go
+++ src/core/osKit/info_linux.go
@@ -24,3 +24,43 @@ func GetThreadCount() (int, error) {
 	}
 	return i, nil
 }
+
+// GetThreadsMax 获取"kernel.threads-max"
+/*
+命令:
+cat /proc/sys/kernel/threads-max
+sysctl kernel.threads-max
+*/
+func GetThreadsMax() (int, error) {
+	str, err := cmdKit.ExecuteToString("sh", "-c", "cat /proc/sys/kernel/threads-max")
+	if err != nil {
+		return 0, err
+	}
+	str = strKit.TrimSpace(str)
+
+	i, err := strconv.Atoi(str)
+	if err != nil {
+		return 0, err
+	}
+	return i, nil
+}
+
+// GetPidMax 获取"kernel.pid_max"
+/*
+命令:
+cat /proc/sys/kernel/pid_max
+sysctl kernel.pid_max
+*/
+func GetPidMax() (int, error) {
+	str, err := cmdKit.ExecuteToString("sh", "-c", "cat /proc/sys/kernel/pid_max")
+	if err != nil {
+		return 0, err
+	}
+	str = strKit.TrimSpace(str)
+
+	i, err := strconv.Atoi(str)
+	if err != nil {
+		return 0, err
+	}
+	return i, nil
+}
