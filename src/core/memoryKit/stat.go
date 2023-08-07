0efbdce0345e1d989a4b047395ddf23a1ffa87e5
Richelieu <richelieu042@gmail.com>
2023-08-07 13:43:31 +0800

    ~

--- src/core/memoryKit/stat.go
+++ src/core/memoryKit/stat.go
@@ -5,15 +5,15 @@ import "runtime"
 // GetMemoryStats 获取有关Go程序的实时内存统计信息.
 /*
 runtime.MemStats结构体的的字段:
-(1) Alloc
+(1) Alloc（单位: 字节）
 	Alloc is bytes of allocated heap objects.
 	分配的堆对象的字节数。
 
-(2) TotalAlloc
+(2) TotalAlloc（单位: 字节）
 	TotalAlloc is cumulative bytes allocated for heap objects.
 	堆对象分配的累积字节数。与 Alloc 不同，当对象被释放时它不会减少。
 
-(3) Sys
+(3) Sys（单位: 字节）
 	Sys is the total bytes of memory obtained from the OS.
 	从操作系统获得的内存总字节数。这测量了 Go 运行时为堆、栈和其他内部数据结构保留的虚拟地址空间。
 
