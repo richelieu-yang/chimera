bfc0bb0ad514137d0b288961a382b31decf109b3
Richelieu <richelieu042@gmail.com>
2023-08-07 13:21:13 +0800

    ~

--- test/test2.go
+++ test/test2.go
@@ -1,35 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"github.com/richelieu-yang/chimera/v2/src/cmdKit"
-	"github.com/richelieu-yang/chimera/v2/src/core/strKit"
-)
-
-// GetUlimitInfo
-/*
-e.g. macOS
-core file size          (blocks, -c) 0
-data seg size           (kbytes, -d) unlimited
-file size               (blocks, -f) unlimited
-max locked memory       (kbytes, -l) unlimited
-max memory size         (kbytes, -m) unlimited
-open files                      (-n) 10240
-pipe size            (512 bytes, -p) 1
-stack size              (kbytes, -s) 8176
-cpu time               (seconds, -t) unlimited
-max user processes              (-u) 5333
-virtual memory          (kbytes, -v) unlimited <nil>
-*/
-func GetUlimitInfo() (string, error) {
-	str, err := cmdKit.ExecuteToString("sh", "-c", "ulimit -a")
-	if err != nil {
-		return "", err
-	}
-	str = strKit.TrimSpace(str)
-	return str, nil
-}
-
 func main() {
-	fmt.Println(GetUlimitInfo())
+
 }
--- test/test3.go
+++ test/test3.go
@@ -1,10 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"github.com/richelieu-yang/chimera/v2/src/core/fileKit"
-)
-
 func main() {
-	fmt.Println(fileKit.IsHidden("/Users/richelieu/go1.21rc3"))
+
 }
