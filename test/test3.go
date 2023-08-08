042966ae4dbfccffeec182f619ea7c4f5c020912
Richelieu <richelieu042@gmail.com>
2023-08-08 17:16:57 +0800

    ~

--- test/test3.go
+++ test/test3.go
@@ -1,19 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"github.com/shirou/gopsutil/v3/disk"
-)
-
 func main() {
-	parts, err := disk.Partitions(true)
-	if err != nil {
-		panic(err)
-	}
-	diskInfo, err := disk.Usage(parts[0].Mountpoint)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(diskInfo.UsedPercent)
 
 }
