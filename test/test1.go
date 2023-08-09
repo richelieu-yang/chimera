39b82ca6d69764dadf62af6f56eb45f96e5443dd
Richelieu <richelieu042@gmail.com>
2023-08-09 09:43:14 +0800

    ~

--- test/test1.go
+++ test/test1.go
@@ -1,14 +1,18 @@
 package main
 
 import (
+	"github.com/richelieu-yang/chimera/v2/src/log/logrusKit"
 	"github.com/richelieu-yang/chimera/v2/src/statKit"
 )
 
 func main() {
-	//logrusKit.MustSetUp(nil)
-	//
-	//statKit.MustSetup("")
-	//select {}
+	logrusKit.MustSetUp(&logrusKit.Config{
+		Level:      "",
+		PrintBasic: true,
+	})
 
-	statKit.PrintStats(nil)
+	statKit.MustSetup("")
+	select {}
+
+	//statKit.PrintStats(nil)
 }
