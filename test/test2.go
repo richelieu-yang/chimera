c7a245db9767b37f8e7905384a1d76e135ff1820
Richelieu <richelieu042@gmail.com>
2023-08-10 16:08:55 +0800

    ~

--- test/test2.go
+++ test/test2.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"context"
+	"fmt"
+	"github.com/redis/go-redis/v9"
+)
 
+func main() {
+	ctx, cancel := context.WithCancelCause(context.TODO())
+
+	cancel(redis.Nil)
+	fmt.Println(ctx.Err())                       // context canceled
+	fmt.Println(ctx.Err() == context.Canceled)   // true
+	fmt.Println(context.Cause(ctx))              // redis: nil
+	fmt.Println(context.Cause(ctx) == redis.Nil) // true
 }
