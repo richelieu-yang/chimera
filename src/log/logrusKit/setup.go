b89b0cd96b55d46d646ac0e192a54182621aecc7
Richelieu <richelieu042@gmail.com>
2023-08-14 10:23:52 +0800

    ~

--- src/log/logrusKit/setup.go
+++ src/log/logrusKit/setup.go
@@ -2,10 +2,7 @@ package logrusKit
 
 import (
 	"github.com/sirupsen/logrus"
-	"sync"
 )
-
-var setupOnce sync.Once
 
 func MustSetUp(config *Config) {
 	err := SetUp(config)
@@ -19,27 +16,24 @@ func MustSetUp(config *Config) {
 /*
 @param config 可以为nil
 */
-func SetUp(config *Config) (err error) {
-	setupOnce.Do(func() {
-		if config == nil {
-			config = &Config{
-				Level:      "debug",
-				PrintBasic: false,
-			}
+func SetUp(config *Config) error {
+	if config == nil {
+		config = &Config{
+			Level:      "debug",
+			PrintBasic: false,
 		}
+	}
 
-		logrus.SetFormatter(DefaultTextFormatter)
-		logrus.SetReportCaller(true)
-		var level logrus.Level
-		level, err = ParseLevel(config.Level)
-		if err != nil {
-			return
-		}
-		logrus.SetLevel(level)
+	logrus.SetFormatter(DefaultTextFormatter)
+	logrus.SetReportCaller(true)
+	level, err := ParseLevel(config.Level)
+	if err != nil {
+		return err
+	}
+	logrus.SetLevel(level)
 
-		if config.PrintBasic {
-			PrintBasicDetails(nil)
-		}
-	})
-	return
+	if config.PrintBasic {
+		PrintBasicDetails(nil)
+	}
+	return nil
 }
