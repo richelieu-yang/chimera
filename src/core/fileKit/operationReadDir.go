06216f16da40ce5792139498237fc4347c957d29
Richelieu <richelieu042@gmail.com>
2023-08-14 16:16:23 +0800

    ~

--- src/core/fileKit/operationReadDir.go
+++ src/core/fileKit/operationReadDir.go
@@ -8,9 +8,8 @@ PS:
 (1) 不包含子目录下的文件（或目录）.
 
 @param name 目录路径
-			(1) 如果为 "" ，将返回error
-			(2) 可以为 "."
-			(3)
+			(1) 如果为"" ，将返回error
+			(2) 可以为"."
 @return 第一个返回值是按照 文件名 从小到大 排序的
 */
 var ReadDir func(name string) ([]os.DirEntry, error) = os.ReadDir
