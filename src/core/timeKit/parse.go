a6f167aa691040e105c0b1c2baf88c64ecf05ecf
Richelieu <richelieu042@gmail.com>
2023-08-15 10:04:06 +0800

    ~

--- src/core/timeKit/parse.go
+++ src/core/timeKit/parse.go
@@ -6,6 +6,11 @@ import (
 
 // Parse 类型转换: string => time.Time
 /*
+PS:
+(1) 为什么不直接使用 time.Parse()？
+	因为time.Parse使用 time.UTC 作为loc，会有时差.
+(2) 本函数使用 time.Local 作为loc.
+
 @param layout 	时间格式
 @param str 		要解析的时间字符串
 
@@ -13,15 +18,20 @@ e.g.
 (timeKit.FormatDate, "2016-08-08")
 */
 func Parse[F ~string](format F, timeStr string) (time.Time, error) {
-	return time.Parse(string(format), timeStr)
+	loc := time.Local
+	return time.ParseInLocation(string(format), timeStr, loc)
 }
 
+// ParseInLocation
+/*
+@param loc time.Local || time.UTC
+*/
 func ParseInLocation[F ~string](format F, timeStr string, loc *time.Location) (time.Time, error) {
 	return time.ParseInLocation(string(format), timeStr, loc)
 }
 
 // ParseDuration string => time.Duration
-/*-
+/*
 @param str (1) 如果为 ""，将返回error
     	   (2) e.g. "300ms"、"-1.5h"、"2h45m"
 */
--- test/test3.go
+++ test/test3.go
@@ -7,8 +7,9 @@ import (
 
 func main() {
 	t := time.Now()
-	t1 := t.Add(time.Second)
+	secs := t.Unix()
+	t1 := time.Unix(secs, 0)
 
-	fmt.Println(t.Before(t1)) // true
-	fmt.Println(t.After(t1))  // false
+	fmt.Println(t)  // 2021-07-31 13:22:25.221929 +0800 CST m=+0.004342301
+	fmt.Println(t1) // 2021-07-31 13:22:25 +0800 CST
 }
