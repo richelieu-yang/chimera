4b1cfe46415f7f7890c240f7ce11f28d0e7931b0
Richelieu <richelieu042@gmail.com>
2023-08-17 09:29:21 +0800

    ~

---
+++ src/mq/pulsarKit/info.md
@@ -0,0 +1,8 @@
+## 请求超时     
+http请求超时，用Go语言处理变得更简洁了
+    https://mp.weixin.qq.com/s/FDTpVwLYbQAj4RMAN-nJEg
+
+方法1: 通过 http.Client 结构体的 Timeout 字段.
+方法2: context.Context + http.NewRequestWithContext()
+
+
--- test/test3.go
+++ test/test3.go
@@ -1,15 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
-
 func main() {
-	t := time.Now()
-	secs := t.Unix()
-	t1 := time.Unix(secs, 0)
 
-	fmt.Println(t)  // 2021-07-31 13:22:25.221929 +0800 CST m=+0.004342301
-	fmt.Println(t1) // 2021-07-31 13:22:25 +0800 CST
 }
