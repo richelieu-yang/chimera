f5d28a562b16913fe8bf388f804488ae013f1041
Richelieu <richelieu042@gmail.com>
2023-08-22 16:01:40 +0800

    ~

---
+++ src/log/zapKit/info.md
@@ -0,0 +1,5 @@
+## 参考
+github
+    https://github.com/uber-go/zap
+Go 语言之 zap 日志库简单使用
+    https://zhuanlan.zhihu.com/p/637747131
--- test/test3.go
+++ test/test3.go
@@ -1,67 +1,22 @@
 package main
 
 import (
-	"encoding/json"
-	"reflect"
-
-	"github.com/go-kratos/kratos/v2/encoding"
-	"google.golang.org/protobuf/encoding/protojson"
-	"google.golang.org/protobuf/proto"
+	"go.uber.org/zap"
+	"time"
 )
 
-// Name is the name registered for the json codec.
-const Name = "json"
+func main() {
+	url := "https://www.baudu.com"
 
-var (
-	// MarshalOptions is a configurable JSON format marshaller.
-	MarshalOptions = protojson.MarshalOptions{
-		EmitUnpopulated: true,
-	}
-	// UnmarshalOptions is a configurable JSON format parser.
-	UnmarshalOptions = protojson.UnmarshalOptions{
-		DiscardUnknown: true,
-	}
-)
+	logger, _ := zap.NewProduction()
+	defer logger.Sync() // flushes buffer, if any
+	sugar := logger.Sugar()
 
-func init() {
-	encoding.RegisterCodec(codec{})
-}
-
-// codec is a Codec implementation with json.
-type codec struct{}
-
-func (codec) Name() string {
-	return Name
-}
-
-func (codec) Marshal(v interface{}) ([]byte, error) {
-	switch m := v.(type) {
-	case json.Marshaler:
-		return m.MarshalJSON()
-	case proto.Message:
-		return MarshalOptions.Marshal(m)
-	default:
-		return json.Marshal(m)
-	}
-}
-
-func (codec) Unmarshal(data []byte, v interface{}) error {
-	switch m := v.(type) {
-	case json.Unmarshaler:
-		return m.UnmarshalJSON(data)
-	case proto.Message:
-		return UnmarshalOptions.Unmarshal(data, m)
-	default:
-		rv := reflect.ValueOf(v)
-		for rv := rv; rv.Kind() == reflect.Ptr; {
-			if rv.IsNil() {
-				rv.Set(reflect.New(rv.Type().Elem()))
-			}
-			rv = rv.Elem()
-		}
-		if m, ok := reflect.Indirect(rv).Interface().(proto.Message); ok {
-			return UnmarshalOptions.Unmarshal(data, m)
-		}
-		return json.Unmarshal(data, m)
-	}
+	sugar.Infow("failed to fetch URL",
+		// Structured context as loosely typed key-value pairs.
+		"url", url,
+		"attempt", 3,
+		"backoff", time.Second,
+	)
+	sugar.Infof("Failed to fetch URL: %s", url)
 }
