32a154738e54b9bf80779f78d1d788741073a2a6
Richelieu <richelieu042@gmail.com>
2023-08-23 11:20:19 +0800

    ~

---
+++ src/core/mapKit/types.go
@@ -0,0 +1,18 @@
+package mapKit
+
+import "github.com/samber/lo"
+
+// MapKeys 修改map实例的 键类型（key）
+func MapKeys[K comparable, V any, R comparable](in map[K]V, iteratee func(value V, key K) R) map[R]V {
+	return lo.MapKeys(in, iteratee)
+}
+
+// MapValues 修改map实例的 值类型（value）
+func MapValues[K comparable, V any, R any](in map[K]V, iteratee func(value V, key K) R) map[K]R {
+	return lo.MapValues(in, iteratee)
+}
+
+// MapEntries 修改map实例的 键类型（key）和值类型（value）
+func MapEntries[K1 comparable, V1 any, K2 comparable, V2 any](in map[K1]V1, iteratee func(key K1, value V1) (K2, V2)) map[K2]V2 {
+	return lo.MapEntries(in, iteratee)
+}
