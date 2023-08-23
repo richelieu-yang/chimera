5a95c6d449dc81566a32f4a288d316301879739f
Richelieu <richelieu042@gmail.com>
2023-08-23 15:53:28 +0800

    ~

--- src/core/cpuKit/setup_linux.go
+++ src/core/cpuKit/setup_linux.go
@@ -1,7 +1,7 @@
 package cpuKit
 
 import (
-	// go.uber.org/automaxprocs 作用: Linux环境下，自动设置 GOMAXPROCS 的值，以便更好地利用容器的CPU资源.
+	// go.uber.org/automaxprocs作用: Linux环境下，自动设置 GOMAXPROCS 的值，以便更好地利用容器的CPU资源.
 	_ "go.uber.org/automaxprocs"
 )
 
--- test/test1.go
+++ test/test1.go
@@ -1,39 +1,5 @@
 package main
 
-import (
-	"context"
-	"fmt"
-	"github.com/richelieu-yang/chimera/v2/src/core/cpuKit"
-	"github.com/richelieu-yang/chimera/v2/src/web/httpClientKit"
-	"net/http"
-	"time"
-)
-
-func init() {
-	cpuKit.SetUp()
-}
-
 func main() {
-	ctx, _ := context.WithTimeout(context.TODO(), time.Second*2)
 
-	req, err := http.NewRequestWithContext(ctx, "GET", "http://127.0.0.1/ping", nil)
-	if err != nil {
-		fmt.Println("Error:", err)
-		return
-	}
-
-	client := http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		fmt.Println("timeout: ", httpClientKit.IsTimeoutError(err))
-		fmt.Println("error:", err)
-		return
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode == http.StatusOK {
-		fmt.Println("Request successful")
-	} else {
-		fmt.Println("Request failed with status:", resp.Status)
-	}
 }
