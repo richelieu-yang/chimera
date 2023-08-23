717a9b2f48f79d653add4d5d5f15d32897ffe824
Richelieu <richelieu042@gmail.com>
2023-08-23 09:48:04 +0800

    ~

--- test/test3.go
+++ test/test3.go
@@ -1,22 +1,5 @@
 package main
 
-import (
-	"go.uber.org/zap"
-	"time"
-)
-
 func main() {
-	url := "https://www.baudu.com"
 
-	logger, _ := zap.NewProduction()
-	defer logger.Sync() // flushes buffer, if any
-	sugar := logger.Sugar()
-
-	sugar.Infow("failed to fetch URL",
-		// Structured context as loosely typed key-value pairs.
-		"url", url,
-		"attempt", 3,
-		"backoff", time.Second,
-	)
-	sugar.Infof("Failed to fetch URL: %s", url)
 }
