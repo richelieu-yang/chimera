38e4ad04d1f4baf6b2cd98de2d3c5c05eca672a1
Richelieu <richelieu042@gmail.com>
2023-08-26 13:20:32 +0800

    ~

--- test/test1.go
+++ test/test1.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"github.com/bytedance/sonic"
+	"github.com/richelieu-yang/chimera/v2/src/log/logrusKit"
+	"github.com/sirupsen/logrus"
+)
 
+func main() {
+	logrusKit.MustSetUp(nil)
+
+	m := map[string]interface{}{
+		"0": 0,
+		"1": 1,
+	}
+	jsonStr, err := sonic.ConfigStd.MarshalToString(m)
+	if err != nil {
+		logrus.Fatal(err)
+	}
+	logrus.Infof("jsonStr: [%s].", jsonStr)
+	var m1 map[string]interface{}
+	if err := sonic.ConfigStd.UnmarshalFromString(jsonStr, &m1); err != nil {
+		logrus.Fatal(err)
+	}
 }
