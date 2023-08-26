f02336ee3c8c0a18b0d88b1fd0c4fd5e127621c4
Richelieu <richelieu042@gmail.com>
2023-08-26 14:02:16 +0800

    ~

--- src/json/jsonKit/apiSonic.go
+++ src/json/jsonKit/apiSonic.go
@@ -6,7 +6,7 @@ package jsonKit
 import "github.com/bytedance/sonic"
 
 /*
-!!!: 并非 amd64 CPU 就行了，还需要支持 avx 等.（e.g.yozo某台amd64内网机就不行）
+!!!: 并非 amd64 CPU 就行了，还需要支持 avx指令集 等.（e.g.yozo某台amd64内网机就不行）
 */
 func init() {
 	library = "bytedance/sonic"
