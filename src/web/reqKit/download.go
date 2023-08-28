a6e4bbe43ce3a490333c59067a0eb25eaddba871
Richelieu <richelieu042@gmail.com>
2023-08-28 14:31:05 +0800

    ~

--- src/web/reqKit/download.go
+++ src/web/reqKit/download.go
@@ -2,6 +2,7 @@ package reqKit
 
 import (
 	"github.com/richelieu-yang/chimera/v2/src/core/fileKit"
+	"github.com/richelieu-yang/chimera/v2/src/core/interfaceKit"
 	"github.com/richelieu-yang/chimera/v2/src/web/httpKit"
 	"io"
 )
@@ -20,15 +21,29 @@ func DownloadToFile(url, filePath string) error {
 	}
 
 	client := NewClient()
-	resp, err := client.R().SetOutputFile(filePath).Get(url)
+	_, err := client.R().SetOutputFile(filePath).Get(url)
 	if err != nil {
 		return err
 	}
 	//defer resp.Body.Close()
-	resp = resp
 	return nil
 }
 
-func DownloadToWriter(path string, writer io.Writer) {
+func DownloadToWriter(url string, writer io.Writer) error {
+	// url
+	if err := httpKit.AssertHttpUrl(url); err != nil {
+		return err
+	}
+	// writer
+	if err := interfaceKit.AssertNotNil(writer, "writer"); err != nil {
+		return err
+	}
 
+	client := NewClient()
+	_, err := client.R().SetOutput(writer).Get(url)
+	if err != nil {
+		return err
+	}
+	//defer resp.Body.Close()
+	return nil
 }
