34615381bf1f4cfc6650502451679056f6c17b9f
Richelieu <richelieu042@gmail.com>
2023-08-28 14:44:50 +0800

    ~

--- src/web/reqKit/download_test.go
+++ src/web/reqKit/download_test.go
@@ -5,7 +5,7 @@ import "testing"
 func TestDownloadToFile(t *testing.T) {
 	url := "https://www.baidu.com/img/PCtm_d9c8750bed0b3c7d089fa7d55720d6cf.png"
 
-	if err := DownloadToFile(url, "aaa.png"); err != nil {
+	if err := DownloadToFile(url, "_file.png"); err != nil {
 		panic(err)
 	}
 }
