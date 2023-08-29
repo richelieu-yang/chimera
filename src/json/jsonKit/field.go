768b85204e630f57e304fd04fdba3a88df8e6fe2
Richelieu <richelieu042@gmail.com>
2023-08-29 13:54:21 +0800

    iii

--- src/json/jsonKit/field.go
+++ src/json/jsonKit/field.go
@@ -6,12 +6,32 @@ import "github.com/tidwall/gjson"
 /*
 @param path 如果是嵌套的内部字段，可以通过'.'组合
 */
-func GetStringField(json []byte, path string) string {
-	result := gjson.GetBytes(json, path)
+func GetStringField(jsonData []byte, path string) string {
+	result := gjson.GetBytes(jsonData, path)
 	return result.String()
 }
 
-func GetStringFieldFromString(json, path string) string {
-	result := gjson.Get(json, path)
+func GetStringFieldFromString(jsonStr, path string) string {
+	result := gjson.Get(jsonStr, path)
 	return result.String()
+}
+
+func GetInt64Field(jsonData []byte, path string) int64 {
+	result := gjson.GetBytes(jsonData, path)
+	return result.Int()
+}
+
+func GetInt64FieldFromString(jsonStr, path string) int64 {
+	result := gjson.Get(jsonStr, path)
+	return result.Int()
+}
+
+func GetBoolField(jsonData []byte, path string) bool {
+	result := gjson.GetBytes(jsonData, path)
+	return result.Bool()
+}
+
+func GetBoolFieldFromString(jsonStr, path string) bool {
+	result := gjson.Get(jsonStr, path)
+	return result.Bool()
 }
---
+++ src/json/jsonKit/fieldSlice.go
@@ -0,0 +1,57 @@
+package jsonKit
+
+import "github.com/tidwall/gjson"
+
+func GetStringSliceField(jsonData []byte, path string) (s []string) {
+	result := gjson.GetBytes(jsonData, path)
+
+	for _, item := range result.Array() {
+		s = append(s, item.String())
+	}
+	return
+}
+
+func GetStringSliceFieldFromString(jsonStr, path string) (s []string) {
+	result := gjson.Get(jsonStr, path)
+
+	for _, item := range result.Array() {
+		s = append(s, item.String())
+	}
+	return
+}
+
+func GetInt64SliceField(jsonData []byte, path string) (s []int64) {
+	result := gjson.GetBytes(jsonData, path)
+
+	for _, item := range result.Array() {
+		s = append(s, item.Int())
+	}
+	return
+}
+
+func GetInt64SliceFieldFromString(jsonStr, path string) (s []int64) {
+	result := gjson.Get(jsonStr, path)
+
+	for _, item := range result.Array() {
+		s = append(s, item.Int())
+	}
+	return
+}
+
+func GetBoolSliceField(jsonData []byte, path string) (s []bool) {
+	result := gjson.GetBytes(jsonData, path)
+
+	for _, item := range result.Array() {
+		s = append(s, item.Bool())
+	}
+	return
+}
+
+func GetBoolSliceFieldFromString(jsonStr, path string) (s []bool) {
+	result := gjson.Get(jsonStr, path)
+
+	for _, item := range result.Array() {
+		s = append(s, item.Bool())
+	}
+	return
+}
