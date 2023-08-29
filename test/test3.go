118dde2bc38e758bcdf402982be57572ba991f23
Richelieu <richelieu042@gmail.com>
2023-08-29 14:39:56 +0800

    iii

--- test/test3.go
+++ test/test3.go
@@ -1,30 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"github.com/bytedance/sonic"
-	"github.com/sirupsen/logrus"
-)
-
 func main() {
-	defer func() {
-		if r := recover(); r != nil {
-			fmt.Printf("recover: %T %v", r, r)
-		}
-	}()
 
-	api := sonic.ConfigStd
-	m := map[string]interface{}{
-		"0": 3.1415926,
-		"1": 1,
-	}
-	jsonStr, err := api.MarshalToString(m)
-	if err != nil {
-		logrus.Fatal(err)
-	}
-	fmt.Println("jsonStr:", jsonStr)
-	var m1 map[string]interface{}
-	if err := api.UnmarshalFromString(jsonStr, &m1); err != nil {
-		logrus.Fatal(err)
-	}
 }
