edfcbf70d6b3babe81a6023f8bf837067bc9512c
Richelieu <richelieu042@gmail.com>
2023-08-30 13:51:46 +0800

    iii

--- src/statKit/setup_test.go
+++ src/statKit/setup_test.go
@@ -1,15 +1,19 @@
 package statKit
 
 import (
-	"github.com/gin-gonic/gin"
 	"testing"
 )
 
 func TestMustSetup(t *testing.T) {
 	MustSetup("_stat.log")
 
-	engine := gin.Default()
-	if err := engine.Run(":8888"); err != nil {
-		panic(engine)
+	/* 0 此时进程的CPU使用率很低 */
+	//engine := gin.Default()
+	//if err := engine.Run(":8888"); err != nil {
+	//	panic(engine)
+	//}
+
+	/* 1 此时进程的CPU使用率很高，维持在99%左右 */
+	for {
 	}
 }
