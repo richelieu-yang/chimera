b81f2fdda521ea7b37f37672c6150fd02dc9641f
Richelieu <richelieu042@gmail.com>
2023-09-01 14:15:10 +0800

    iii

--- src/database/redisKit/uniqueId.go
+++ src/database/redisKit/uniqueId.go
@@ -8,6 +8,9 @@ import (
 // NewUniqueId 通过Redis生成唯一id.
 /*
 PS: 返回值可以用作 分布式唯一id.
+
+@param key 	(1) 可以不存在
+			(2) 可以为""
 */
 func (client *Client) NewUniqueId(ctx context.Context, key string) (string, error) {
 	i, err := client.Incr(ctx, key)
