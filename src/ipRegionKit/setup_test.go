e1408ddc6000d5c091deff828075b9f6e8b8db28
Richelieu <richelieu042@gmail.com>
2023-09-01 10:12:36 +0800

    iii

--- src/ipRegionKit/setup_test.go
+++ src/ipRegionKit/setup_test.go
@@ -2,9 +2,7 @@ package ipRegionKit
 
 import (
 	"fmt"
-	"github.com/lionsoul2014/ip2region/binding/golang/xdb"
 	"github.com/richelieu-yang/chimera/v2/src/consts"
-	"github.com/richelieu-yang/chimera/v2/src/core/fileKit"
 	"github.com/richelieu-yang/chimera/v2/src/core/pathKit"
 	"github.com/sirupsen/logrus"
 	"testing"
@@ -17,21 +15,8 @@ func TestGetRegion(t *testing.T) {
 	}
 	logrus.Infof("wd: [%s].", wd)
 
-	/* 加载 ip2region.xdb 文件 */
 	xdbPath := "chimera-lib/ip2region.xdb"
-	if err := fileKit.AssertExistAndIsFile(xdbPath); err != nil {
-		panic(err)
-	}
-	// 缓存整个xdb数据
-	cBuff, err := xdb.LoadContentFromFile(xdbPath)
-	if err != nil {
-		panic(err)
-	}
-	tmpSearcher, err := xdb.NewWithBuffer(cBuff)
-	if err != nil {
-		panic(err)
-	}
-	searcher = tmpSearcher
+	MustSetUp(xdbPath)
 
 	//ip := "10.0.9.141"
 	ip := "218.90.174.146"
