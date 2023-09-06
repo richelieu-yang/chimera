23335edefc0ef8f356df007aaaf99c9d63c157fe
Richelieu <richelieu042@gmail.com>
2023-09-06 09:19:03 +0800

    iii

--- src/yamlKit/yaml.go
+++ src/yamlKit/yaml.go
@@ -1,6 +1,9 @@
 package yamlKit
 
-import "gopkg.in/yaml.v3"
+import (
+	"github.com/richelieu-yang/chimera/v2/src/core/fileKit"
+	"gopkg.in/yaml.v3"
+)
 
 var Marshal func(in interface{}) (out []byte, err error) = yaml.Marshal
 
@@ -11,6 +14,23 @@ var Marshal func(in interface{}) (out []byte, err error) = yaml.Marshal
 func MarshalToString(in interface{}) (string, error) {
 	data, err := Marshal(in)
 	return string(data), err
+}
+
+// MarshalToFile
+/*
+PS: 对 传参filePath 的验证和断言在 fileKit.WriteToFile 里面.
+
+@param in		建议为结构体实例指针 || map实例 || slice实例
+@param filePath (1) .yaml格式的文件
+				(2) 不存在的话，会创建一个新的文件
+				(3) 存在且是个文件的话，会 "覆盖" 掉旧的（并不会加到该文件的最后面）
+*/
+func MarshalToFile(in interface{}, filePath string) error {
+	data, err := Marshal(in)
+	if err != nil {
+		return err
+	}
+	return fileKit.WriteToFile(data, filePath)
 }
 
 // Unmarshal
