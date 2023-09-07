94d91caf516bc145e8b32a1a3ef866512909553b
Richelieu <richelieu042@gmail.com>
2023-09-07 13:37:50 +0800

    iii

--- test/test1.go
+++ test/test1.go
@@ -1,38 +1,5 @@
 package main
 
-import (
-	"github.com/richelieu-yang/chimera/v2/src/core/cpuKit"
-	"github.com/richelieu-yang/chimera/v2/src/log/logrusKit"
-)
-
-func init() {
-	cpuKit.SetUp()
-	logrusKit.MustSetUp(&logrusKit.Config{
-		Level:      "",
-		PrintBasic: true,
-	})
-}
-
 func main() {
-	//// 看下能否恢复
-	//defer func() {
-	//	if obj := recover(); obj != nil {
-	//		logrus.Infof("recover: [%T, %v].", obj, obj)
-	//	}
-	//}()
-	//
-	//m := map[string]interface{}{
-	//	"0": 3.1415926,
-	//	"1": 1,
-	//}
-	//jsonStr, err := sonic.ConfigStd.MarshalToString(m)
-	//if err != nil {
-	//	logrus.Fatal(err)
-	//}
-	//logrus.Infof("jsonStr: [%s].", jsonStr)
-	//var m1 map[string]interface{}
-	//if err := sonic.ConfigStd.UnmarshalFromString(jsonStr, &m1); err != nil {
-	//	logrus.Fatal(err)
-	//}
-	//logrus.Info("end")
+
 }
