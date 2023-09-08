a59c355cad781319192398ba930b637f48d7bebc
Richelieu <richelieu042@gmail.com>
2023-09-08 17:01:36 +0800

    iii

--- src/core/pathKit/tempDir.go
+++ src/core/pathKit/tempDir.go
@@ -8,16 +8,18 @@ import (
 
 // GetTempDir 获取系统的临时目录.
 /*
+Deprecated: 不建议向系统临时目录中放东西（服务器不一定会给权限）.
+
 e.g.
 Windows: 	"C:\Users\Lenovo\AppData\Local\Temp"
 Mac: 		"/var/folders/4_/33p_vn057msfh2nvgx6hwv_40000gn/T/"
 */
-func GetTempDir() string {
-	return os.TempDir()
-}
+var GetTempDir func() string = os.TempDir
 
 // GetUniqueTempDir 获取 本依赖 的专属临时目录.
 /*
+Deprecated: 不建议向系统临时目录中放东西（服务器不一定会给权限）.
+
 e.g. Mac
 () => "/var/folders/4_/33p_vn057msfh2nvgx6hwv_40000gn/T/$$chimera", nil
 */
