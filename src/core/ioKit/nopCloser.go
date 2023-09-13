31c58f4fd70beca5dde76f4b1f5fd639cb51c0ac
Richelieu <richelieu042@gmail.com>
2023-09-13 14:20:20 +0800

    iii

--- src/core/ioKit/nopCloser.go
+++ src/core/ioKit/nopCloser.go
@@ -10,6 +10,7 @@ PS: 返回值调用Close()将什么都不会做，直接返回nil.
 */
 var NopCloser func(reader io.Reader) io.ReadCloser = io.NopCloser
 
+// NopCloserToReadSeeker 给 io.ReadSeeker 实例加上Close()（虽然这个Close()什么都不做）.
 func NopCloserToReadSeeker(readSeeker io.ReadSeeker) io.ReadSeekCloser {
 	return &readSeekCloser{
 		readSeeker: readSeeker,
