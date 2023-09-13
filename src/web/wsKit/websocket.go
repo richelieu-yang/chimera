76b4fff982851154c20db7d4cd0a116f74031350
Richelieu <richelieu042@gmail.com>
2023-09-13 09:37:51 +0800

    iii

--- src/web/wsKit/websocket.go
+++ src/web/wsKit/websocket.go
@@ -10,10 +10,6 @@ import (
 此函数是为了避免情况: 代理（e.g.Nginx）没有设置WebSocket穿透，导致WebSocket服务收到的WebSocket请求的header有问题.
 */
 func PolyfillWebSocketRequest(r *http.Request) {
-	if !httpKit.HeaderContainsValueIgnoreCase(r.Header, "Connection", "Upgrade") {
-		httpKit.AddHeader(r.Header, "Connection", "Upgrade")
-	}
-	if !httpKit.HeaderContainsValueIgnoreCase(r.Header, "Upgrade", "websocket") {
-		httpKit.AddHeader(r.Header, "Upgrade", "websocket")
-	}
+	httpKit.AddHeaderIgnoreCaseIfMissing(r.Header, "Connection", "Upgrade")
+	httpKit.AddHeaderIgnoreCaseIfMissing(r.Header, "Upgrade", "websocket")
 }
