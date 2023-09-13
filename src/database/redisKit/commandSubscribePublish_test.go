cab7534e51fd8a4ba3e3ed4de33112c55a9ca65a
Richelieu <richelieu042@gmail.com>
2023-09-13 15:30:22 +0800

    iii

--- src/database/redisKit/commandSubscribePublish_test.go
+++ src/database/redisKit/commandSubscribePublish_test.go
@@ -52,7 +52,7 @@ func TestClient_SubscribeAndPublish(t *testing.T) {
 			for msg := range ch {
 				logrus.WithFields(logrus.Fields{
 					"channel": msg.Channel,
-					"payLoad": msg.Payload, // 过期的键（key）
+					"payLoad": msg.Payload,
 				}).Info("Receive a message.")
 			}
 			logrus.Info("Receive ends.......................")
@@ -116,51 +116,53 @@ func TestClient_SubscribeAndPublish1(t *testing.T) {
 	}
 	client = client
 
-	flag := atomicKit.NewBool()
-	id := idKit.NewULID()
+	{
+		flag := atomicKit.NewBool()
+		id := idKit.NewULID()
 
-	/* pubSub使用方法1 */
-	//go func() {
-	//	pubSub := client.Subscribe(context.TODO(), "__keyevent@0__:expired")
-	//	defer pubSub.Close()
-	//
-	//	for {
-	//		msg, err := pubSub.ReceiveMessage(context.TODO())
-	//		if err != nil {
-	//			panic(err)
-	//		}
-	//		logrus.WithFields(logrus.Fields{
-	//			"channel": msg.Channel,
-	//			"payLoad": msg.Payload, // 过期的键（key）
-	//		}).Info("Receive a message.")
-	//	}
-	//}()
-	/* pubSub使用方法2 */
-	go func() {
-		pubSub := client.Subscribe(context.TODO(), "__keyevent@0__:expired")
-		defer pubSub.Close()
+		/* pubSub使用方法1 */
+		//go func() {
+		//	pubSub := client.Subscribe(context.TODO(), "__keyevent@0__:expired")
+		//	defer pubSub.Close()
+		//
+		//	for {
+		//		msg, err := pubSub.ReceiveMessage(context.TODO())
+		//		if err != nil {
+		//			panic(err)
+		//		}
+		//		logrus.WithFields(logrus.Fields{
+		//			"channel": msg.Channel,
+		//			"payLoad": msg.Payload, // 过期的键（key）
+		//		}).Info("Receive a message.")
+		//	}
+		//}()
+		/* pubSub使用方法2 */
+		go func() {
+			pubSub := client.Subscribe(context.TODO(), "__keyevent@0__:expired")
+			defer pubSub.Close()
 
-		ch := pubSub.Channel()
-		for msg := range ch {
-			logrus.WithFields(logrus.Fields{
-				"channel": msg.Channel,
-				"payLoad": msg.Payload, // 过期的键（key）
-			}).Info("Receive a message.")
-			if msg.Payload == id {
-				flag.Cas(false, true)
+			ch := pubSub.Channel()
+			for msg := range ch {
+				logrus.WithFields(logrus.Fields{
+					"channel": msg.Channel,
+					"payLoad": msg.Payload, // 过期的键（key）
+				}).Info("Receive a message.")
+				if msg.Payload == id {
+					flag.Cas(false, true)
+				}
 			}
-		}
-	}()
+		}()
 
-	go func() {
-		_, err := client.Set(context.TODO(), id, "test", time.Second*3)
-		if err != nil {
-			panic(err)
-		}
-	}()
+		go func() {
+			_, err := client.Set(context.TODO(), id, "test", time.Second*3)
+			if err != nil {
+				panic(err)
+			}
+		}()
 
-	time.Sleep(time.Second * 5)
-	if !flag.Val() {
-		panic("value of param flag is [false]")
+		time.Sleep(time.Second * 5)
+		if !flag.Val() {
+			panic("value of param flag is [false]")
+		}
 	}
 }
