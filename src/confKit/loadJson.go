b1cd54d08f2aaada73da879803cfb6df66cdff7b
Richelieu <richelieu042@gmail.com>
2023-09-15 09:13:49 +0800

    iii

--- src/confKit/loadJson.go
+++ src/confKit/loadJson.go
@@ -1,10 +1,28 @@
 package confKit
 
-import "github.com/zeromicro/go-zero/core/conf"
+import (
+	"github.com/richelieu-yang/chimera/v2/src/log/logrusKit"
+	"github.com/sirupsen/logrus"
+	"github.com/zeromicro/go-zero/core/conf"
+)
 
 // LoadFromJsonBytes 加载 .json 格式的配置文件内容.
 var LoadFromJsonBytes func(content []byte, v any) error = conf.LoadFromJsonBytes
 
 func LoadFromJsonText(text string, v any) error {
 	return LoadFromJsonBytes([]byte(text), v)
+}
+
+func MustLoadFromJsonBytes(content []byte, v any) {
+	if err := LoadFromJsonBytes(content, v); err != nil {
+		logrusKit.DisableQuote(nil)
+		logrus.Fatalf("%+v", err)
+	}
+}
+
+func MustLoadFromJsonText(text string, v any) {
+	if err := LoadFromJsonText(text, v); err != nil {
+		logrusKit.DisableQuote(nil)
+		logrus.Fatalf("%+v", err)
+	}
 }
