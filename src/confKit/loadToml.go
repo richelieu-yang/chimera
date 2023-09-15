28d42e46b8d460fb5cdd07ebde7bb4150345e537
Richelieu <richelieu042@gmail.com>
2023-09-15 09:14:40 +0800

    iii

--- src/confKit/loadToml.go
+++ src/confKit/loadToml.go
@@ -1,10 +1,28 @@
 package confKit
 
-import "github.com/zeromicro/go-zero/core/conf"
+import (
+	"github.com/richelieu-yang/chimera/v2/src/log/logrusKit"
+	"github.com/sirupsen/logrus"
+	"github.com/zeromicro/go-zero/core/conf"
+)
 
 // LoadFromTomlBytes 加载 .toml 格式的配置文件内容.
 var LoadFromTomlBytes func(content []byte, v any) error = conf.LoadFromTomlBytes
 
 func LoadFromTomlText(text string, v any) error {
 	return LoadFromTomlBytes([]byte(text), v)
+}
+
+func MustLoadFromTomlBytes(content []byte, v any) {
+	if err := LoadFromTomlBytes(content, v); err != nil {
+		logrusKit.DisableQuote(nil)
+		logrus.Fatalf("%+v", err)
+	}
+}
+
+func MustLoadFromTomlText(text string, v any) {
+	if err := LoadFromTomlText(text, v); err != nil {
+		logrusKit.DisableQuote(nil)
+		logrus.Fatalf("%+v", err)
+	}
 }
--- src/confKit/loadYaml.go
+++ src/confKit/loadYaml.go
@@ -1,10 +1,28 @@
 package confKit
 
-import "github.com/zeromicro/go-zero/core/conf"
+import (
+	"github.com/richelieu-yang/chimera/v2/src/log/logrusKit"
+	"github.com/sirupsen/logrus"
+	"github.com/zeromicro/go-zero/core/conf"
+)
 
 // LoadFromYamlBytes 加载 .yaml 格式的配置文件内容.
 var LoadFromYamlBytes func(content []byte, v any) error = conf.LoadFromYamlBytes
 
 func LoadFromYamlText(text string, v any) error {
 	return LoadFromYamlBytes([]byte(text), v)
+}
+
+func MustLoadFromYamlBytes(content []byte, v any) {
+	if err := LoadFromYamlBytes(content, v); err != nil {
+		logrusKit.DisableQuote(nil)
+		logrus.Fatalf("%+v", err)
+	}
+}
+
+func MustLoadFromYamlText(text string, v any) {
+	if err := LoadFromYamlText(text, v); err != nil {
+		logrusKit.DisableQuote(nil)
+		logrus.Fatalf("%+v", err)
+	}
 }
