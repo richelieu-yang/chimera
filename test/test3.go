c9da3c48935fe53e1c993f3bbb82ce5c3166a526
Richelieu <richelieu042@gmail.com>
2023-09-18 13:21:19 +0800

    iii

--- test/test3.go
+++ test/test3.go
@@ -1,13 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"go.opentelemetry.io/otel"
-)
-
 func main() {
-	tp := otel.GetTracerProvider()
-	fmt.Printf("%T\n", tp)
 
-	//fmt.Println(otel.GetTracerProvider() != nil)
 }
