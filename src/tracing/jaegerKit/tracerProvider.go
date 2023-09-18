9238b0a69b6872820b77fa1193a8b6e6e6d75790
Richelieu <richelieu042@gmail.com>
2023-09-18 09:30:02 +0800

    iii

---
+++ src/tracing/jaegerKit/tracerProvider.go
@@ -0,0 +1,39 @@
+package jaegerKit
+
+import (
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/exporters/jaeger"
+	"go.opentelemetry.io/otel/sdk/resource"
+	"go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.17.0"
+)
+
+// NewTracerProvider
+/*
+@param url 连接的jaeger服务（collector || agent）
+
+e.g. url
+	"http://localhost:14268/api/traces"
+*/
+func NewTracerProvider(url, service, environment string, id int64) (*trace.TracerProvider, error) {
+	/*
+		创建jaeger provider.
+		PS: 可以直接连collector，也可以连agent
+	*/
+	exporter, err := jaeger.New(jaeger.WithCollectorEndpoint(jaeger.WithEndpoint(url)))
+	if err != nil {
+		return nil, err
+	}
+	tp := trace.NewTracerProvider(
+		// Always be sure to batch in production.
+		trace.WithBatcher(exporter),
+		// Record information about this application in a Resource.
+		trace.WithResource(resource.NewWithAttributes(
+			semconv.SchemaURL,
+			semconv.ServiceNameKey.String(service),
+			attribute.String("environment", environment),
+			attribute.Int64("ID", id),
+		)),
+	)
+	return tp, nil
+}
--- test/test3.go
+++ test/test3.go
@@ -1,36 +1,13 @@
 package main
 
 import (
-	"go.opentelemetry.io/otel/attribute"
-	"go.opentelemetry.io/otel/exporters/jaeger"
-	"go.opentelemetry.io/otel/sdk/resource"
-	"go.opentelemetry.io/otel/sdk/trace"
-	semconv "go.opentelemetry.io/otel/semconv/v1.17.0"
+	"fmt"
+	"go.opentelemetry.io/otel"
 )
 
-// NewTracerProvider
-/*
-@param url 连接的jaeger服务 e.g."http://localhost:14268/api/traces"
-*/
-func NewTracerProvider(url, service, environment string, id int64) (*trace.TracerProvider, error) {
-	/*
-		创建jaeger provider.
-		PS: 可以直接连collector，也可以连agent
-	*/
-	exporter, err := jaeger.New(jaeger.WithCollectorEndpoint(jaeger.WithEndpoint(url)))
-	if err != nil {
-		return nil, err
-	}
-	tp := trace.NewTracerProvider(
-		// Always be sure to batch in production.
-		trace.WithBatcher(exporter),
-		// Record information about this application in a Resource.
-		trace.WithResource(resource.NewWithAttributes(
-			semconv.SchemaURL,
-			semconv.ServiceNameKey.String(service),
-			attribute.String("environment", environment),
-			attribute.Int64("ID", id),
-		)),
-	)
-	return tp, nil
+func main() {
+	tp := otel.GetTracerProvider()
+	fmt.Printf("%T\n", tp)
+
+	//fmt.Println(otel.GetTracerProvider() != nil)
 }
