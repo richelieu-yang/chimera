b23a2f8d798f421db3772e26ad4d2e7d6351dab6
Richelieu <richelieu042@gmail.com>
2023-09-19 16:05:03 +0800

    iii

--- go.mod
+++ go.mod
@@ -69,6 +69,7 @@ require (
 	gopkg.in/yaml.v3 v3.0.1
 	gorm.io/driver/mysql v1.5.1
 	gorm.io/gorm v1.25.4
+	sigs.k8s.io/yaml v1.3.0
 )
 
 require (
--- test/test2.go
+++ test/test2.go
@@ -2,11 +2,49 @@ package main
 
 import (
 	"fmt"
-	"github.com/richelieu-yang/chimera/v2/src/core/fileKit"
+	"os"
+	"sigs.k8s.io/yaml"
 )
 
 func main() {
-	path := "/Users/richelieu/Downloads/111.txt"
-	fmt.Println(fileKit.IsReadable(path))
-	fmt.Println(fileKit.IsWritable(path))
+	// 读取yaml文件
+	file, err := os.Open("example.yaml")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	var data interface{}
+	err = yaml.NewDecoder(file).Decode(&data)
+	if err != nil {
+		panic(err)
+	}
+
+	// 将数据转换为yaml字符串
+	yamlStr, err := yaml.Marshal(data)
+	if err != nil {
+		panic(err)
+	}
+
+	// 将yaml字符串转换为map
+	var result map[string]interface{}
+	err = yaml.Unmarshal(yamlStr, &result)
+	if err != nil {
+		panic(err)
+	}
+
+	// 输出转换后的map
+	fmt.Println(result)
+
+	// 将map转换为yaml文件
+	file, err = os.Create("example.yaml")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	err = yaml.NewEncoder(file).Encode(result)
+	if err != nil {
+		panic(err)
+	}
 }
