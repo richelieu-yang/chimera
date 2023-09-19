dbd32a64f92e03e75b7a33b63171928310a1a5bc
Richelieu <richelieu042@gmail.com>
2023-09-19 14:38:00 +0800

    iii

--- src/core/signalKit/exitSignals.go
+++ src/core/signalKit/exitSignals.go
@@ -8,23 +8,25 @@ import (
 )
 
 var (
-	// exitSignals 会将程序退出的所有能捕捉的信号
-	/*
-		参考: https://mp.weixin.qq.com/s/ATiAnX0PrqkBVnSFBTiMGQ
-	*/
-	exitSignals = []os.Signal{
-		syscall.SIGHUP,
-		syscall.SIGUSR1,
-		syscall.SIGUSR2,
-		syscall.SIGINT,
-		// e.g.kill ${pid} || kill -15 ${pid}
-		syscall.SIGTERM,
-		syscall.SIGTSTP,
-		syscall.SIGQUIT,
+	exitSignals = []os.Signal{syscall.SIGUSR1, syscall.SIGUSR2, syscall.SIGTERM}
 
-		// 无条件结束程序（不能被捕获、阻塞或忽略）
-		syscall.SIGSTOP,
-		// 停止进程（不能被捕获、阻塞或忽略）	e.g.kill -9 ${pid}
-		syscall.SIGKILL,
-	}
+	//// exitSignals 会将程序退出的所有能捕捉的信号
+	///*
+	//	参考: https://mp.weixin.qq.com/s/ATiAnX0PrqkBVnSFBTiMGQ
+	//*/
+	//exitSignals = []os.Signal{
+	//	syscall.SIGHUP,
+	//	syscall.SIGUSR1,
+	//	syscall.SIGUSR2,
+	//	syscall.SIGINT,
+	//	// e.g.kill ${pid} || kill -15 ${pid}
+	//	syscall.SIGTERM,
+	//	syscall.SIGTSTP,
+	//	syscall.SIGQUIT,
+	//
+	//	// 无条件结束程序（不能被捕获、阻塞或忽略）
+	//	syscall.SIGSTOP,
+	//	// 停止进程（不能被捕获、阻塞或忽略）	e.g.kill -9 ${pid}
+	//	syscall.SIGKILL,
+	//}
 )
--- src/core/signalKit/exitSignals_windows.go
+++ src/core/signalKit/exitSignals_windows.go
@@ -6,19 +6,21 @@ import (
 )
 
 var (
-	// exitSignals 会将程序退出的所有能捕捉的信号
-	/*
-		参考: https://mp.weixin.qq.com/s/ATiAnX0PrqkBVnSFBTiMGQ
-	*/
-	exitSignals = []os.Signal{
-		syscall.SIGHUP,
-		//syscall.SIGUSR1,
-		//syscall.SIGUSR2,
-		syscall.SIGINT,
-		syscall.SIGTERM,
-		//syscall.SIGTSTP,
-		syscall.SIGQUIT,
-		//syscall.SIGSTOP,
-		syscall.SIGKILL,
-	}
+	exitSignals = []os.Signal{syscall.SIGTERM}
+
+	//// exitSignals 会将程序退出的所有能捕捉的信号
+	///*
+	//	参考: https://mp.weixin.qq.com/s/ATiAnX0PrqkBVnSFBTiMGQ
+	//*/
+	//exitSignals = []os.Signal{
+	//	syscall.SIGHUP,
+	//	//syscall.SIGUSR1,
+	//	//syscall.SIGUSR2,
+	//	syscall.SIGINT,
+	//	syscall.SIGTERM,
+	//	//syscall.SIGTSTP,
+	//	syscall.SIGQUIT,
+	//	//syscall.SIGSTOP,
+	//	syscall.SIGKILL,
+	//}
 )
