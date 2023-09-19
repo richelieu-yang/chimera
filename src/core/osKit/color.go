b46c01c034eef6e38ac94deaeedaa769358ea016
Richelieu <richelieu042@gmail.com>
2023-09-19 15:27:28 +0800

    iii

--- src/core/osKit/color.go
+++ src/core/osKit/color.go
@@ -1,1 +1,11 @@
 package osKit
+
+import "github.com/dablelv/cyan/os"
+
+var (
+	IsSupportColor func() bool = os.IsSupportColor
+
+	IsSupport256Color func() bool = os.IsSupport256Color
+
+	IsSupportTrueColor func() bool = os.IsSupportTrueColor
+)
