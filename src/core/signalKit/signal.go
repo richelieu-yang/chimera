92ed5ee48a9b702052fd12ac62b62d30d58754bb
Richelieu <richelieu042@gmail.com>
2023-09-19 09:48:57 +0800

    iii

--- src/core/signalKit/signal.go
+++ src/core/signalKit/signal.go
@@ -12,8 +12,9 @@ var monitorOnce sync.Once
 // MonitorExitSignal 监听退出信号（拦截关闭信号）.
 /*
 PS:
-(1) 无法拦截部分信号（e.g. syscall.SIGSTOP、syscall.SIGKILL）；
-(2) 可以通过 logrus.RegisterExitHandler() 在程序退出前"毁尸灭迹"（在里面你甚至可以 time.Sleep）.
+(1) 无法拦截部分信号（e.g. syscall.SIGSTOP、syscall.SIGKILL）;
+(2) 可以通过 logrus.RegisterExitHandler() 在程序退出前"毁尸灭迹"（在里面你甚至可以 time.Sleep）;
+(3) 此函数对 主动调用os.Exit() 无效.
 */
 func MonitorExitSignal() {
 	monitorOnce.Do(func() {
