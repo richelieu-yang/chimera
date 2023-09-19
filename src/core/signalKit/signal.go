81f7a9896baa12caeeac5472b744f8072a5e7844
Richelieu <richelieu042@gmail.com>
2023-09-19 14:32:12 +0800

    iii

--- src/core/signalKit/signal.go
+++ src/core/signalKit/signal.go
@@ -12,6 +12,8 @@ var monitorOnce sync.Once
 
 // MonitorExitSignal 监听退出信号（拦截关闭信号）.
 /*
+可以参考 go-zero 中的 "proc/signals.go".
+
 PS:
 (1) 无法拦截部分信号（e.g. syscall.SIGSTOP、syscall.SIGKILL）;
 (2) 可以通过 logrus.RegisterExitHandler() 在程序退出前"毁尸灭迹"（在里面你甚至可以 time.Sleep）;
