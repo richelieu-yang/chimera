441124f5dd3dc4a6a4ad90358b3aed3bb716d6ae
Richelieu <richelieu042@gmail.com>
2023-09-21 10:52:31 +0800

    iii

--- test/test3.go
+++ test/test3.go
@@ -6,14 +6,15 @@ import (
 )
 
 func main() {
-	fmt.Println(validateKit.Required(nil)) // Key: '' Error:Field validation for '' failed on the 'required' tag
+	if err := validateKit.Field("3.14", "numeric"); err != nil {
+		fmt.Println(err.Error())
+	} else {
+		fmt.Println("ok")
+	}
 
-	fmt.Println(validateKit.Required(""))    // Key: '' Error:Field validation for '' failed on the 'required' tag
-	fmt.Println(validateKit.Required("aaa")) // <nil>
-
-	fmt.Println(validateKit.Required(0)) // Key: '' Error:Field validation for '' failed on the 'required' tag
-	fmt.Println(validateKit.Required(1)) // <nil>
-
-	fmt.Println(validateKit.Required(false)) // Key: '' Error:Field validation for '' failed on the 'required' tag
-	fmt.Println(validateKit.Required(true))  // <nil>
+	//if err := validateKit.Field(0, "required,min=-1,max=100"); err != nil {
+	//	fmt.Println(err.Error()) // Key: '' Error:Field validation for '' failed on the 'required' tag
+	//} else {
+	//	fmt.Println("ok")
+	//}
 }
