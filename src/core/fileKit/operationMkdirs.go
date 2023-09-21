c219b2705b159ba6b33621c24dd0769588944617
Richelieu <richelieu042@gmail.com>
2023-09-21 15:50:54 +0800

    iii

--- src/core/fileKit/operationMkdirs.go
+++ src/core/fileKit/operationMkdirs.go
@@ -21,7 +21,7 @@ PS:
 (1) 如果目录已经存在，将返回nil；
 (2) 如果 传参dirPath 对应的是个已存在的文件，将返回error（"mkdir {xxx}: not a directory"）.
 
-@param perm		e.g. os.ModePerm
+@param perm		一般都是用077权限，e.g. os.ModePerm
 @param dirPaths	目录路径s（相对路径 || 绝对路径）
 
 e.g.
@@ -36,7 +36,7 @@ e.g.1 Mac
 */
 func MkDirsWithPerm(perm os.FileMode, dirPaths ...string) error {
 	for _, dirPath := range dirPaths {
-		// os.MkdirAll() 的第一个传参
+		// os.MkdirAll() 的第一个传参:
 		// (1) 如果为""会返回error(mkdir : no such file or directory)
 		// (2) 如果为多个空格，返回的error为nil（并不会创建目录）
 		if strKit.IsEmpty(dirPath) {
@@ -44,7 +44,7 @@ func MkDirsWithPerm(perm os.FileMode, dirPaths ...string) error {
 		}
 
 		if err := os.MkdirAll(dirPath, perm); err != nil {
-			err = errorKit.Wrap(err, `fail with dirPath(%s) and perm(%d)`, dirPath, perm)
+			err = errorKit.Wrap(err, `fail with dirPath(%s) and perm(%s)`, dirPath, perm)
 			return err
 		}
 	}
--- test/test3.go
+++ test/test3.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/richelieu-yang/chimera/v2/src/core/errorKit"
+	"os"
+)
 
+func main() {
+	//fmt.Printf("%s %v\n", os.ModePerm, os.ModePerm)
+
+	var perm os.FileMode = 0666
+	//fmt.Printf("%s %v\n", perm, perm)
+
+	err := errorKit.New(`fail with  perm(%s)`, perm)
+	fmt.Printf(err.Error())
 }
