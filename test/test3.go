be6fe311c642cd4f3c35f46f32770a9e9564a64a
Richelieu <richelieu042@gmail.com>
2023-09-25 17:14:04 +0800

    iii

--- test/test3.go
+++ test/test3.go
@@ -1,15 +1,5 @@
 package main
 
-import (
-	"github.com/richelieu-yang/chimera/v2/src/component/nacosKit"
-)
-
 func main() {
-	config := nacosKit.Config{
-		NamespaceId: "",
-		//Addresses:   []string{"http://localhost:8848/nacos"},
-		Addresses: nil,
-	}
 
-	nacosKit.MustSetUp(config)
 }
