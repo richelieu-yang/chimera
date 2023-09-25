26ebafe3dcf4fc93548cdb01ca0af9ea02034fdb
Richelieu <richelieu042@gmail.com>
2023-09-25 13:30:49 +0800

    iii

--- src/json/jsonKit/api.go
+++ src/json/jsonKit/api.go
@@ -3,7 +3,6 @@ package jsonKit
 import (
 	"github.com/richelieu-yang/chimera/v2/src/compareKit"
 	"github.com/richelieu-yang/chimera/v2/src/consts"
-	"github.com/richelieu-yang/chimera/v2/src/log/logrusKit"
 	"github.com/sirupsen/logrus"
 )
 
@@ -43,7 +42,6 @@ func GetStdApi() API {
 
 func testAPI() {
 	api := defaultApi
-
 	m := map[string]interface{}{
 		"0": 3.1415926,
 		"1": 1,
@@ -55,17 +53,17 @@ qwdqwd
 496465~·《》？L:"{}|,./l;'[]\/*-的确
 `,
 	}
+
 	jsonData, err := api.Marshal(m)
 	if err != nil {
-		logrus.WithError(err).Fatal("[%s, JSON] Fail to marshal!!!", consts.ProjectUpperName)
+		logrus.WithError(err).Fatalf("[%s, JSON] Fail to marshal!!!", consts.ProjectUpperName)
 	}
 	var m1 map[string]interface{}
 	if err := api.Unmarshal(jsonData, &m1); err != nil {
-		logrus.WithError(err).Fatal("[%s, JSON] Fail to unmarshal!!!", consts.ProjectUpperName)
+		logrus.WithError(err).Fatalf("[%s, JSON] Fail to unmarshal!!!", consts.ProjectUpperName)
 	}
 	if compareKit.Equal(m, m1) {
-		logrusKit.DisableQuote(nil)
 		diff := compareKit.Diff(m, m1)
-		logrus.Fatalf("[%s, JSON] diff between m and m1:\n%s\n", consts.ProjectUpperName, diff)
+		logrus.WithField("diff", diff).Fatalf("[%s, JSON] m and m1 are different.", consts.ProjectUpperName)
 	}
 }
--- test/test3.go
+++ test/test3.go
@@ -1,5 +1,12 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/richelieu-yang/chimera/v2/src/json/jsonKit"
+)
 
+func main() {
+	fmt.Println(jsonKit.MarshalIndentToString(map[string]interface{}{
+		"a": true,
+	}, "", "    "))
 }
