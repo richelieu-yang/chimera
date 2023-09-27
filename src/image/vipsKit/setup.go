e4a7978c8ef92ada5936667b9ea45b7675b1bca3
Richelieu <richelieu042@gmail.com>
2023-09-27 17:23:02 +0800

    iii

--- src/image/vipsKit/setup.go
+++ src/image/vipsKit/setup.go
@@ -10,10 +10,10 @@ import (
 !!!:
 (1) macOS需要设置环境变量（否则govips may not compile）: export CGO_CFLAGS_ALLOW="-Xpreprocessor"
 (2) 必要条件: CGO_ENABLED=1
+(3) 涉及到C，交叉编译只能搞 当前所在环境 的.
 
 PS:
 (1) vips 8.14.5支持的格式: webp、heif、pdf、png、tiff、svg、jp2k、gif、jpeg、magick.
-
 
 @param config 可以为nil（使用默认配置: concurrency=1 cache_max_files=0 cache_max_mem=52428800 cache_max=100）
 */
