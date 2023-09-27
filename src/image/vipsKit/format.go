d37d3236b1952cb06045a3af28e0fa6c86558879
Richelieu <richelieu042@gmail.com>
2023-09-27 16:43:23 +0800

    iii

--- src/image/vipsKit/format.go
+++ src/image/vipsKit/format.go
@@ -2,25 +2,29 @@ package vipsKit
 
 import (
 	"github.com/davidbyttow/govips/v2/vips"
+	"github.com/richelieu-yang/chimera/v2/src/core/fileKit"
 	"os"
 )
 
-func ToWebp(src, target string, params *vips.WebpExportParams) error {
-	image, err := vips.NewImageFromFile(src)
+func ToWebp(src, dest string, exportParams *vips.WebpExportParams) error {
+	if err := fileKit.AssertNotExistOrIsFile(dest, true); err != nil {
+		return err
+	}
+
+	// 默认值
+	if exportParams == nil {
+		exportParams = vips.NewWebpExportParams()
+		exportParams.Quality = 100
+	}
+
+	imageRef, err := Read(src, nil)
 	if err != nil {
 		return err
 	}
 
-	// Rotate the picture upright and reset EXIF orientation tag
-	err = image.AutoRotate()
+	imageData, _, err := imageRef.ExportWebp(exportParams)
 	if err != nil {
 		return err
 	}
-
-	exportParams := vips.NewWebpExportParams()
-	webpData, _, err := image.ExportWebp(exportParams)
-	if err != nil {
-		return err
-	}
-	return os.WriteFile(target, webpData, 0644)
+	return os.WriteFile(dest, imageData, 0644)
 }
---
+++ src/image/vipsKit/read.go
@@ -0,0 +1,29 @@
+package vipsKit
+
+import (
+	"github.com/davidbyttow/govips/v2/vips"
+	"github.com/richelieu-yang/chimera/v2/src/core/fileKit"
+)
+
+// Read
+/*
+@param params 可以为nil
+*/
+func Read(path string, params *vips.ImportParams) (*vips.ImageRef, error) {
+	if err := fileKit.AssertExistAndIsFile(path); err != nil {
+		return nil, err
+	}
+
+	ref, err := vips.LoadImageFromFile(path, params)
+	if err != nil {
+		return nil, err
+	}
+
+	// Rotate the picture upright and reset EXIF orientation tag
+	err = ref.AutoRotate()
+	if err != nil {
+		return nil, err
+	}
+
+	return ref, nil
+}
