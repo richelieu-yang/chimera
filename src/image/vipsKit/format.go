ebf70ae4cc1bd61cbda5a1ba0b3f04c399e98823
Richelieu <richelieu042@gmail.com>
2023-09-27 17:00:29 +0800

    iii

--- src/image/vipsKit/format.go
+++ src/image/vipsKit/format.go
@@ -6,6 +6,52 @@ import (
 	"os"
 )
 
+func ToJpeg(src, dest string, exportParams *vips.JpegExportParams) error {
+	if err := fileKit.AssertNotExistOrIsFile(dest); err != nil {
+		return err
+	}
+
+	// 默认值
+	if exportParams == nil {
+		exportParams = vips.NewJpegExportParams()
+		exportParams.Quality = 100
+	}
+
+	imageRef, err := Read(src, nil)
+	if err != nil {
+		return err
+	}
+
+	imageData, _, err := imageRef.ExportJpeg(exportParams)
+	if err != nil {
+		return err
+	}
+	return os.WriteFile(dest, imageData, 0644)
+}
+
+func ToPng(src, dest string, exportParams *vips.PngExportParams) error {
+	if err := fileKit.AssertNotExistOrIsFile(dest); err != nil {
+		return err
+	}
+
+	// 默认值
+	if exportParams == nil {
+		exportParams = vips.NewPngExportParams()
+		exportParams.Quality = 100
+	}
+
+	imageRef, err := Read(src, nil)
+	if err != nil {
+		return err
+	}
+
+	imageData, _, err := imageRef.ExportPng(exportParams)
+	if err != nil {
+		return err
+	}
+	return os.WriteFile(dest, imageData, 0644)
+}
+
 func ToWebp(src, dest string, exportParams *vips.WebpExportParams) error {
 	if err := fileKit.AssertNotExistOrIsFile(dest); err != nil {
 		return err
@@ -23,6 +69,98 @@ func ToWebp(src, dest string, exportParams *vips.WebpExportParams) error {
 	}
 
 	imageData, _, err := imageRef.ExportWebp(exportParams)
+	if err != nil {
+		return err
+	}
+	return os.WriteFile(dest, imageData, 0644)
+}
+
+func ToTiff(src, dest string, exportParams *vips.TiffExportParams) error {
+	if err := fileKit.AssertNotExistOrIsFile(dest); err != nil {
+		return err
+	}
+
+	// 默认值
+	if exportParams == nil {
+		exportParams = vips.NewTiffExportParams()
+		exportParams.Quality = 100
+	}
+
+	imageRef, err := Read(src, nil)
+	if err != nil {
+		return err
+	}
+
+	imageData, _, err := imageRef.ExportTiff(exportParams)
+	if err != nil {
+		return err
+	}
+	return os.WriteFile(dest, imageData, 0644)
+}
+
+func ToGif(src, dest string, exportParams *vips.GifExportParams) error {
+	if err := fileKit.AssertNotExistOrIsFile(dest); err != nil {
+		return err
+	}
+
+	// 默认值
+	if exportParams == nil {
+		exportParams = vips.NewGifExportParams()
+		exportParams.Quality = 100
+	}
+
+	imageRef, err := Read(src, nil)
+	if err != nil {
+		return err
+	}
+
+	imageData, _, err := imageRef.ExportGIF(exportParams)
+	if err != nil {
+		return err
+	}
+	return os.WriteFile(dest, imageData, 0644)
+}
+
+func ToAvif(src, dest string, exportParams *vips.AvifExportParams) error {
+	if err := fileKit.AssertNotExistOrIsFile(dest); err != nil {
+		return err
+	}
+
+	// 默认值
+	if exportParams == nil {
+		exportParams = vips.NewAvifExportParams()
+		exportParams.Quality = 100
+	}
+
+	imageRef, err := Read(src, nil)
+	if err != nil {
+		return err
+	}
+
+	imageData, _, err := imageRef.ExportAvif(exportParams)
+	if err != nil {
+		return err
+	}
+	return os.WriteFile(dest, imageData, 0644)
+}
+
+func ToHeif(src, dest string, exportParams *vips.HeifExportParams) error {
+	if err := fileKit.AssertNotExistOrIsFile(dest); err != nil {
+		return err
+	}
+
+	// 默认值
+	if exportParams == nil {
+		exportParams = vips.NewHeifExportParams()
+		exportParams.Quality = 100
+	}
+
+	imageRef, err := Read(src, nil)
+	if err != nil {
+		return err
+	}
+
+	imageData, _, err := imageRef.ExportHeif(exportParams)
 	if err != nil {
 		return err
 	}
--- test/test3.go
+++ test/test3.go
@@ -1,35 +1,16 @@
 package main
 
 import (
-	"github.com/davidbyttow/govips/v2/vips"
-	"github.com/sirupsen/logrus"
-	"os"
+	"github.com/richelieu-yang/chimera/v2/src/image/vipsKit"
 )
 
 func main() {
-	vips.Startup(nil)
-	defer vips.Shutdown()
+	vipsKit.SetUp(nil)
 
-	logrus.Info("666")
-
-	image1, err := vips.NewImageFromFile("iShot_2023-09-27_14.57.56.png")
-	if err != nil {
+	if err := vipsKit.ToWebp("iShot_2023-09-27_14.57.56.png", "1.webp", nil); err != nil {
 		panic(err)
 	}
-
-	// Rotate the picture upright and reset EXIF orientation tag
-	err = image1.AutoRotate()
-	if err != nil {
-		panic(err)
-	}
-
-	exportParams := vips.NewWebpExportParams()
-	image1bytes, _, err := image1.ExportWebp(exportParams)
-	if err != nil {
-		panic(err)
-	}
-	err = os.WriteFile("output.webp", image1bytes, 0644)
-	if err != nil {
+	if err := vipsKit.ToWebp("iShot_2023-09-27_14.57.56.png", "2.webp", nil); err != nil {
 		panic(err)
 	}
 }
