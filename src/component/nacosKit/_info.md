0f09f30b723f92b6a9e4988a6e6bfe1f39ecadb1
Richelieu <richelieu042@gmail.com>
2023-09-28 13:27:43 +0800

    iii

--- src/component/nacosKit/_info.md
+++ src/component/nacosKit/_info.md
@@ -1,5 +1,10 @@
 ## 参考
 "Nacos2.x - Golang.wps"
 
-## 获取配置
+## naming
+#### weight 字段
+值范围: [0.0, 10000.0]
+
+## config
+#### 获取配置
 * 某一namespace，DataId 和 Group 对应的配置不存在的情况，将返回 ("", nil)
--- src/component/nacosKit/setup_test.go
+++ src/component/nacosKit/setup_test.go
@@ -1,13 +1,14 @@
 package nacosKit
 
 import (
+	"fmt"
 	"github.com/nacos-group/nacos-sdk-go/v2/vo"
 	"testing"
 )
 
 func TestMustSetUp(t *testing.T) {
 	config := &Config{
-		NamespaceId: "",
+		NamespaceId: "6393fde0-464c-43e1-9c4e-60f4f372a74f",
 		Addresses:   []string{"http://localhost:8848/nacos"},
 	}
 
@@ -24,9 +25,24 @@ func TestMustSetUp(t *testing.T) {
 	}
 	defer configClient.CloseClient()
 
-	configClient.GetConfig(vo.ConfigParam{
-		DataId: "common",
-		//Group:  "",
+	ok, err := namingClient.UpdateInstance(vo.UpdateInstanceParam{
+		Ip:   "127.0.0.1",
+		Port: 80,
+		/*
+			0: 			注册（或更新）成功，但实例不健康（即使 Healthy 为 true）
+			12345.67:	注册（或更新）失败，error: retry 3 times request failed!: request return error code 500
+		*/
+		Weight:      10000,
+		Enable:      true,
+		Healthy:     true,
+		Metadata:    nil,
+		ClusterName: "",
+		ServiceName: "ws",
+		GroupName:   "wo3",
+		Ephemeral:   false,
 	})
-
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("ok", ok)
 }
