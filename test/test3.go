53c49f887c04ac8cf3cc77ad596e3872d5936d58
Richelieu <richelieu042@gmail.com>
2023-10-10 09:40:30 +0800

    iii

--- test/test3.go
+++ test/test3.go
@@ -1,18 +1,5 @@
 package main
 
-import (
-	"github.com/richelieu-yang/chimera/v2/src/log/logrusKit"
-)
-
 func main() {
-	logger0 := logrusKit.NewLogger()
-	logger1 := logrusKit.NewLogger()
 
-	logger0.Info("0")
-	logger1.Info("1")
-
-	logrusKit.DisableQuote(logger1)
-
-	logger0.Info("0")
-	logger1.Info("1")
 }
