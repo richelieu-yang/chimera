80298795eb1cf4f553fc4633e0f06505c2ff1f23
Richelieu <richelieu042@gmail.com>
2023-10-10 15:34:01 +0800

    iii

--- src/component/db/redisKit/stream.go
+++ src/component/db/redisKit/stream.go
@@ -7,8 +7,14 @@ import (
 
 // XAdd [生产者] 添加消息到末尾（如果指定的队列不存在，则创建一个队列）.
 /*
+语法: XADD key ID field value [field value ...]
+key:			队列名称，如果不存在就创建
+ID:				消息 id，我们使用 * 表示由 redis 生成，可以自定义，但是要自己保证递增性。
+field value:	记录
+
 @param a 	(1) 必需字段: Stream、Values
 			(2) Stream字段对应: Redis中的key（stream类型）
+			(3) 可选的ID字段，为 ""（默认） 则由Redis生成
 @return 	id: 消息的id
 */
 func (client *Client) XAdd(ctx context.Context, a *redis.XAddArgs) (id string, err error) {
