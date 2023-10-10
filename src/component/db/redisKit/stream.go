8568a0c86d2292535dc4f21d5cebb7d44fff8237
Richelieu <richelieu042@gmail.com>
2023-10-10 13:49:01 +0800

    iii

--- src/component/db/redisKit/stream.go
+++ src/component/db/redisKit/stream.go
@@ -5,7 +5,7 @@ import (
 	"github.com/redis/go-redis/v9"
 )
 
-// XAdd 添加消息到末尾.
+// XAdd （生产者）添加消息到末尾.
 func (client *Client) XAdd(ctx context.Context, a *redis.XAddArgs) (string, error) {
 	cmd := client.universalClient.XAdd(ctx, a)
 	return cmd.Result()
@@ -14,6 +14,12 @@ func (client *Client) XAdd(ctx context.Context, a *redis.XAddArgs) (string, erro
 // XDel 删除消息.
 func (client *Client) XDel(ctx context.Context, stream string, ids ...string) (int64, error) {
 	cmd := client.universalClient.XDel(ctx, stream, ids...)
+	return cmd.Result()
+}
+
+// XGroupCreate （消费者）
+func (client *Client) XGroupCreate(ctx context.Context, stream, group, start string) (string, error) {
+	cmd := client.universalClient.XGroupCreate(ctx, stream, group, start)
 	return cmd.Result()
 }
 
@@ -27,12 +33,8 @@ func (client *Client) XReadStreams(ctx context.Context, streams ...string) ([]re
 	return cmd.Result()
 }
 
+// XReadGroup （消费者）
 func (client *Client) XReadGroup(ctx context.Context, a *redis.XReadGroupArgs) ([]redis.XStream, error) {
 	cmd := client.universalClient.XReadGroup(ctx, a)
-	return cmd.Result()
-}
-
-func (client *Client) XGroupCreate(ctx context.Context, stream, group, start string) (string, error) {
-	cmd := client.universalClient.XGroupCreate(ctx, stream, group, start)
 	return cmd.Result()
 }
