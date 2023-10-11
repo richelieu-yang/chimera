af2451636e226fa6bc985bc7883cacc0d7da5c76
Richelieu <richelieu042@gmail.com>
2023-10-11 15:27:24 +0800

    iii

--- src/log/logrusKit/_info.md
+++ src/log/logrusKit/_info.md
@@ -8,3 +8,20 @@ Logrus也提供了一些有用的特性，如：
 * 可以使用钩子（Hooks）来在每次写入日志时执行一些操作，如发送邮件、写入数据库等；
 * 可以使用条目（Entry）来记录带有字段的日志信息，或者使用WithFields、WithTime、WithError等方法来创建带有字段的条目；
 * 可以使用日志级别函数（如Info、Warn、Error等）来记录不同级别的日志信息，或者使用Log或Print等方法来记录默认级别的日志信息。
+
+## exit handler
+e.g.
+package main
+
+import (
+"github.com/sirupsen/logrus"
+)
+
+func main() {
+// 程序正常退出不会触发 logrus.RegisterExitHandle() 注册的exit handlers，因此加上此defer语句.
+defer logrus.Fatal("Exit normally.")
+
+	logrus.RegisterExitHandler(func() {
+		logrus.Info(111)
+	})
+}
--- test/test3.go
+++ test/test3.go
@@ -1,47 +1,14 @@
 package main
 
 import (
-	"fmt"
-	"runtime/metrics"
+	"github.com/sirupsen/logrus"
 )
 
 func main() {
-	descs := metrics.All()
-	samples := make([]metrics.Sample, len(descs))
-	for i := range samples {
-		samples[i].Name = descs[i].Name
-	}
+	// 程序正常退出不会触发 logrus.RegisterExitHandle() 注册的exit handlers，因此加上此defer语句
+	defer logrus.Fatal("Exit normally.")
 
-	metrics.Read(samples)
-
-	for _, sample := range samples {
-		name, value := sample.Name, sample.Value
-
-		switch value.Kind() {
-		case metrics.KindUint64:
-			fmt.Printf("%s: %d\n", name, value.Uint64())
-		case metrics.KindFloat64:
-			fmt.Printf("%s: %f\n", name, value.Float64())
-		case metrics.KindFloat64Histogram:
-			fmt.Printf("%s: %f\n", name, medianBucket(value.Float64Histogram()))
-		case metrics.KindBad:
-			continue
-		}
-	}
-}
-
-func medianBucket(h *metrics.Float64Histogram) float64 {
-	total := uint64(0)
-	for _, count := range h.Counts {
-		total += count
-	}
-	thresh := total / 2
-	total = 0
-	for i, count := range h.Counts {
-		total += count
-		if total >= thresh {
-			return h.Buckets[i]
-		}
-	}
-	panic("should not happen")
+	logrus.RegisterExitHandler(func() {
+		logrus.Info(111)
+	})
 }
