5ff9b1c815a8272fd2a59936ec0218775c6613c8
Richelieu <richelieu042@gmail.com>
2023-10-13 14:18:15 +0800

    iii

--- src/charsetKit/charset.go
+++ src/charsetKit/charset.go
@@ -4,7 +4,7 @@ import "github.com/gogf/gf/v2/encoding/gcharset"
 
 // Convert 转换字符串的编码（字符集的编码）
 /*
-支持的字符集（charset）: "UTF-8"、"GBK"、"Big5"等，更多详见: https://goframe.org/pages/viewpage.action?pageId=1114178.
+支持的字符集（charset）: UTF-8、GBK、Big5、ISO-* 等，更多详见: https://goframe.org/pages/viewpage.action?pageId=1114178.
 */
 var Convert func(dstCharset string, srcCharset string, src string) (dst string, err error) = gcharset.Convert
 
--- test/test3.go
+++ test/test3.go
@@ -1,23 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"github.com/google/wire"
 )
 
 func main() {
 	wire.NewSet()
 
-	fmt.Println("c", test()) // c 2
-}
-
-func test() (rst int) {
-	defer fmt.Println("a", rst) // a 0
-	defer func() {
-		fmt.Println("b", rst) // b 2
-	}()
-
-	rst = 1
-	rst = 2
-	return
 }
