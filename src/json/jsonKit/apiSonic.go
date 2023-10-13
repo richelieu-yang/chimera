7bd6db3a446d4fdcc1e1b32f66a676824daa792c
Richelieu <richelieu042@gmail.com>
2023-10-13 09:32:33 +0800

    iii

--- src/json/jsonKit/apiSonic.go
+++ src/json/jsonKit/apiSonic.go
@@ -20,7 +20,7 @@ func init() {
 	stdApi = sonic.ConfigStd
 
 	if !cpuKit.HasFeature(cpuid.AVX) {
-		logrus.Fatalf("[%s, JSON] AVX isn't supported by CPU!!!", consts.UpperProjectName)
+		logrus.Fatalf("[%s, JSON, SONIC] AVX isn't supported by CPU!!!", consts.UpperProjectName)
 		return
 	}
 
