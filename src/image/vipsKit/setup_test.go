a52952d1238790d02c9904406853aa18bc248d0b
Richelieu <richelieu042@gmail.com>
2023-10-16 11:11:05 +0800

    iii

--- src/image/vipsKit/setup_test.go
+++ src/image/vipsKit/setup_test.go
@@ -7,7 +7,7 @@ import (
 func TestSetUp(t *testing.T) {
 	SetUp(nil)
 
-	if err := ToHeif("", "test.heic", nil); err != nil {
+	if err := ToHeif("/Users/richelieu/Downloads/iShot_2023-10-16_11.07.52.png", "test.jpg", nil); err != nil {
 		panic(err)
 	}
 }
