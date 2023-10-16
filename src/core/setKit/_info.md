159720a172e7722cd3142f8ea9faf940a8eb9cbe
Richelieu <richelieu042@gmail.com>
2023-10-16 09:27:48 +0800

    iii

--- src/core/setKit/_info.md
+++ src/core/setKit/_info.md
@@ -1,2 +1,6 @@
 集合类型-gset
     https://goframe.org/pages/viewpage.action?pageId=1114348
+
+## Set
+* 无序
+* 不允许重复.
--- src/core/setKit/gset.go
+++ src/core/setKit/gset.go
@@ -1,1 +1,17 @@
 package setKit
+
+import "github.com/gogf/gf/v2/container/gset"
+
+// NewGset
+/*
+@param safe 是否并发安全？
+*/
+var NewGset func(safe ...bool) *gset.Set = gset.NewSet
+
+// NewGsetFrom
+/*
+@param items 	(1) 切片实例（元素类型不限）
+				(2) 可以为nil
+@param safe 	是否并发安全？
+*/
+var NewGsetFrom func(items interface{}, safe ...bool) *gset.Set = gset.NewFrom
