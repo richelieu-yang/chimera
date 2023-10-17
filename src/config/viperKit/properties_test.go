fa323c15690b9182b7c93a40a24d5b3d12da9d01
Richelieu <richelieu042@gmail.com>
2023-10-17 17:04:15 +0800

    iii

--- src/config/viperKit/properties_test.go
+++ src/config/viperKit/properties_test.go
@@ -3,23 +3,29 @@ package viperKit
 import (
 	"fmt"
 	"github.com/richelieu-yang/chimera/v2/src/json/jsonKit"
-	"github.com/spf13/viper"
 	"testing"
 )
 
 // TestLoadProperties 读取"_test.properties".
 func TestLoadProperties(t *testing.T) {
-	viper.SetConfigName("_test") // 配置文件名称（没有扩展名）
-	viper.AddConfigPath(".")     // 配置文件路径（可以设置多个）
-	err := viper.ReadInConfig()  // 读取配置数据
-	if err != nil {              // 处理读取配置文件的错误
+	var m map[string]interface{}
+
+	//viper.SetConfigName("_test") // 配置文件名称（没有扩展名）
+	//viper.AddConfigPath(".")     // 配置文件路径（可以设置多个）
+	//err := viper.ReadInConfig()  // 读取配置数据
+	//if err != nil {              // 处理读取配置文件的错误
+	//	panic(err)
+	//}
+	//if err := viper.Unmarshal(&m); err != nil {
+	//	panic(err)
+	//}
+
+	path := "_test.properties"
+	_, err := ReadFileAs(path, nil, &m)
+	if err != nil {
 		panic(err)
 	}
 
-	var m map[string]interface{}
-	if err := viper.Unmarshal(&m); err != nil {
-		panic(err)
-	}
 	str, _ := jsonKit.MarshalIndentToString(m, "", "    ")
 	fmt.Println(str)
 }
