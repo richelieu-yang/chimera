cfcf17a5fc15127030276051539c8d8714821866
Richelieu <richelieu042@gmail.com>
2023-10-17 17:08:59 +0800

    iii

--- src/config/viperKit/properties_test.go
+++ src/config/viperKit/properties_test.go
@@ -8,24 +8,20 @@ import (
 
 // TestLoadProperties 读取"_test.properties".
 func TestLoadProperties(t *testing.T) {
-	var m map[string]interface{}
-
-	//viper.SetConfigName("_test") // 配置文件名称（没有扩展名）
-	//viper.AddConfigPath(".")     // 配置文件路径（可以设置多个）
-	//err := viper.ReadInConfig()  // 读取配置数据
-	//if err != nil {              // 处理读取配置文件的错误
-	//	panic(err)
-	//}
-	//if err := viper.Unmarshal(&m); err != nil {
-	//	panic(err)
-	//}
+	type redis struct {
+		Cluster []string `json:"cluster"`
+	}
+	type config struct {
+		Redis redis `json:"redis"`
+	}
 
 	path := "_test.properties"
-	_, err := ReadFileAs(path, nil, &m)
+	c := &config{}
+	_, err := ReadFileAs(path, nil, c)
 	if err != nil {
 		panic(err)
 	}
 
-	str, _ := jsonKit.MarshalIndentToString(m, "", "    ")
+	str, _ := jsonKit.MarshalIndentToString(c, "", "    ")
 	fmt.Println(str)
 }
