d84c463d047ab131fd8616c8bb947541d35355a1
Richelieu <richelieu042@gmail.com>
2023-10-18 09:28:13 +0800

    iii

--- src/component/nacosKit/config.go
+++ src/component/nacosKit/config.go
@@ -2,11 +2,9 @@ package nacosKit
 
 type (
 	Config struct {
-		/* client */
 		// NamespaceId 命名空间的id，配置 "" 和 "public" 效果一样（都是使用保留空间public）
 		NamespaceId string `json:"namespaceId" yaml:"namespaceId"`
 
-		/* server */
 		Addrs []string `json:"addrs" yaml:"addrs" validate:"required,dive,required"`
 	}
 )
