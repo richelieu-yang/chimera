e4dd8ecf774fd458683b4c3bb30b0bac7162bfc3
Richelieu <richelieu042@gmail.com>
2023-10-18 09:45:01 +0800

    iii

--- src/component/nacosKit/config.go
+++ src/component/nacosKit/config.go
@@ -5,6 +5,6 @@ type (
 		// NamespaceId 命名空间的id，配置 "" 和 "public" 效果一样（都是使用保留空间public）
 		NamespaceId string `json:"namespaceId" yaml:"namespaceId"`
 
-		Addrs []string `json:"addrs" yaml:"addrs" validate:"required,dive,required"`
+		Addrs []string `json:"addrs" yaml:"addrs" validate:"required,dive,http_url"`
 	}
 )
