280114685b31913926b4620d26b028405a876c73
Richelieu <richelieu042@gmail.com>
2023-10-18 15:06:48 +0800

    Revert "iii"
    This reverts commit 089a30c9

--- src/core/sliceKit/loop.go
+++ src/core/sliceKit/loop.go
@@ -3,7 +3,7 @@ package sliceKit
 import "github.com/samber/lo"
 import lop "github.com/samber/lo/parallel"
 
-// ForEach 遍历
+// ForEach 遍历（内部是for range，遍历前会拷贝一份s然后遍历它）
 /*
 @param s		可以为nil
 @param iteratee	不能为nil
@@ -12,7 +12,7 @@ func ForEach[T any](s []T, iteratee func(item T, index int)) {
 	lo.ForEach(s, iteratee)
 }
 
-// ForEachInParallel 遍历
+// ForEachInParallel 遍历（内部是for range + goroutine，遍历前会拷贝一份s然后遍历它）
 /*
 !!!: 多协程，并发.
 
--- test/test3.go
+++ test/test3.go
@@ -1,5 +1,11 @@
 package main
 
-func main() {
+import "github.com/richelieu-yang/chimera/v2/src/core/sliceKit"
 
+func main() {
+	s := []int{0, 1, 2}
+
+	sliceKit.ForEach(s, func(item int, index int) {
+
+	})
 }
