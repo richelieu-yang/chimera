c2518a21dfca326a2bd03bcec97c3b76a8c83308
Richelieu <richelieu042@gmail.com>
2023-10-18 15:08:03 +0800

    dev

--- src/core/sliceKit/loop.go
+++ src/core/sliceKit/loop.go
@@ -7,6 +7,15 @@ import lop "github.com/samber/lo/parallel"
 /*
 @param s		可以为nil
 @param iteratee	不能为nil
+
+e.g.
+	s := []int{0, 1, 2}
+	fmt.Println(s) // [0 1 2]
+
+	sliceKit.ForEach(s, func(item int, index int) {
+		s[index] = item + 1
+	})
+	fmt.Println(s) // [1 2 3]
 */
 func ForEach[T any](s []T, iteratee func(item T, index int)) {
 	lo.ForEach(s, iteratee)
--- test/test3.go
+++ test/test3.go
@@ -1,11 +1,16 @@
 package main
 
-import "github.com/richelieu-yang/chimera/v2/src/core/sliceKit"
+import (
+	"fmt"
+	"github.com/richelieu-yang/chimera/v2/src/core/sliceKit"
+)
 
 func main() {
 	s := []int{0, 1, 2}
+	fmt.Println(s) // [0 1 2]
 
 	sliceKit.ForEach(s, func(item int, index int) {
-
+		s[index] = item + 1
 	})
+	fmt.Println(s) // [1 2 3]
 }
