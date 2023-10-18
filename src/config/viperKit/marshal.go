4e6707e593a70698985bb32ae91091a3db08449a
Richelieu <richelieu042@gmail.com>
2023-10-18 11:28:01 +0800

    iii

--- src/config/viperKit/marshal.go
+++ src/config/viperKit/marshal.go
@@ -1,12 +1,20 @@
 package viperKit
 
 import (
+	"github.com/richelieu-yang/chimera/v2/src/core/fileKit"
 	"github.com/richelieu-yang/chimera/v2/src/core/interfaceKit"
 	"github.com/richelieu-yang/chimera/v2/src/json/jsonKit"
 )
 
+// MarshalToFile
+/*
+@param obj 如果为nil，将返回error
+*/
 func MarshalToFile(obj interface{}, target string) error {
 	if err := interfaceKit.AssertNotNil(obj, "obj"); err != nil {
+		return err
+	}
+	if err := fileKit.AssertNotExistOrIsFile(target); err != nil {
 		return err
 	}
 
@@ -14,7 +22,9 @@ func MarshalToFile(obj interface{}, target string) error {
 	if err != nil {
 		return err
 	}
-	Unmarshal(data, "json")
-
-	return nil
+	v, err := Read(data, "json", nil)
+	if err != nil {
+		return err
+	}
+	return v.WriteConfigAs(target)
 }
