631215df6bc470dd67ac5b4fe9043a268fdf0835
Richelieu <richelieu042@gmail.com>
2023-10-19 13:30:33 +0800

    dev

--- test/test3.go
+++ test/test3.go
@@ -2,24 +2,10 @@ package main
 
 import (
 	"fmt"
-	"runtime/debug"
-	"time"
+	"github.com/richelieu-yang/chimera/v2/src/config/viperKit"
 )
 
 func main() {
-	defer func() {
-		if obj := recover(); obj != nil {
-			fmt.Println(obj)
-			//debug.PrintStack()
-			debug.Stack()
-		}
-	}()
-
-	go func() {
-		time.Sleep(time.Second * 3)
-		panic(1)
-	}()
-
-	for {
-	}
+	extname := ".JSON"
+	fmt.Println(viperKit.PolyfillExtName(extname)) // "json"
 }
