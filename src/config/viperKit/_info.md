afd1cec8e6274caa0136eba66dd869db6a412d43
Richelieu <richelieu042@gmail.com>
2023-10-19 15:32:10 +0800

    dev

--- src/config/viperKit/_info.md
+++ src/config/viperKit/_info.md
@@ -5,3 +5,10 @@ var SupportedExts = []string{"json", "toml", "yaml", "yml", "properties", "props
 ## TODO: 默认值
 Viper不支持通过结构体标签（tag）来设置默认值.  
 ![_img.png](_img.png)  
+
+e.g. 反例，设置默认值失败
+type config struct {  
+    A int `json:"a,default=1"`  
+    B int `json:"b,default=2"`  
+    C int `json:"c"`  
+}
--- test/test3.go
+++ test/test3.go
@@ -3,9 +3,21 @@ package main
 import (
 	"fmt"
 	"github.com/richelieu-yang/chimera/v2/src/config/viperKit"
+	"github.com/richelieu-yang/chimera/v2/src/json/jsonKit"
 )
 
+type config struct {
+	A int `json:"a,default=1"`
+	B int `json:"b,default=2"`
+	C int `json:"c"`
+}
+
 func main() {
-	extname := ".JSON"
-	fmt.Println(viperKit.PolyfillContentType(extname)) // "json"
+	c := &config{}
+	_, err := viperKit.Unmarshal([]byte("{}"), "json", nil, c)
+	if err != nil {
+		panic(err)
+	}
+	str, _ := jsonKit.MarshalIndentToString(c, "", "    ")
+	fmt.Println(str)
 }
