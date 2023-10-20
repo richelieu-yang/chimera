e9022ac4c3c108986c88796bbe8467e68fd32c1d
Richelieu <richelieu042@gmail.com>
2023-10-20 14:14:06 +0800

    dev

--- src/validateKit/validate_test.go
+++ src/validateKit/validate_test.go
@@ -6,8 +6,8 @@ import (
 )
 
 func TestValidateIPv4(t *testing.T) {
-	fmt.Println(IPv4(""))           // Key: '' Error:Field validation for '' failed on the 'ipv4' tag
+	fmt.Println(IPv4(""))           // Key: '' Error:Var validation for '' failed on the 'ipv4' tag
 	fmt.Println(IPv4("127.0.0.1"))  // <nil>
 	fmt.Println(IPv4("10.0.9.141")) // <nil>
-	fmt.Println(IPv4("10.0.9141"))  // Key: '' Error:Field validation for '' failed on the 'ipv4' tag
+	fmt.Println(IPv4("10.0.9141"))  // Key: '' Error:Var validation for '' failed on the 'ipv4' tag
 }
