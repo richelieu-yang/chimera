001ff7e3067f6a3e186c223deb639b3778148c9e
Richelieu <richelieu042@gmail.com>
2023-10-20 10:11:58 +0800

    dev

--- test/test2.go
+++ test/test2.go
@@ -1,12 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"github.com/richelieu-yang/chimera/v2/src/reflectKit"
-	"reflect"
-)
-
 func main() {
-	v := reflectKit.ValueOf(111)
-	fmt.Println(v.Kind() == reflect.Int) // true
+
 }
