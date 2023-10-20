88f182cbab25734285ed8d050e99ac7a3f1f22f5
Richelieu <richelieu042@gmail.com>
2023-10-20 09:59:54 +0800

    dev

--- src/reflectKit/value.go
+++ src/reflectKit/value.go
@@ -4,9 +4,31 @@ import "reflect"
 
 // ValueOf
 /*
+@param i 可以为nil
+
 e.g.
 	v := reflectKit.ValueOf(111)
 	fmt.Println(v.Kind() == reflect.Int)	// true
+
+e.g.1
+	{
+		var obj interface{} = nil
+		v := reflectKit.ValueOf(obj)
+		k := v.Kind()
+		fmt.Println(k)                    // invalid
+		fmt.Println(k == reflect.Invalid) // true
+	}
+
+	{
+		type bean struct {
+		}
+
+		var obj *bean = nil
+		v := reflectKit.ValueOf(obj)
+		k := v.Kind()
+		fmt.Println(k)                    // ptr
+		fmt.Println(k == reflect.Invalid) // false
+	}
 */
 var ValueOf func(i any) reflect.Value = reflect.ValueOf
 
