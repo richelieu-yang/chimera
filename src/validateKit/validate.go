85f2912d20533217de669e554cbe9aae41d1d0f1
Richelieu <richelieu042@gmail.com>
2023-10-20 10:17:29 +0800

    dev

--- src/validateKit/validate.go
+++ src/validateKit/validate.go
@@ -4,6 +4,8 @@ import (
 	"github.com/go-playground/validator/v10"
 	"github.com/richelieu-yang/chimera/v2/src/core/sliceKit"
 	"github.com/richelieu-yang/chimera/v2/src/core/strKit"
+	"github.com/richelieu-yang/chimera/v2/src/netKit"
+	"reflect"
 )
 
 // New
@@ -22,7 +24,25 @@ func New(tagNameArgs ...string) *validator.Validate {
 		v.SetTagName(tagName)
 	}
 
+	if err := registerDefaultValidation(v); err != nil {
+		panic(err)
+	}
+
 	return v
+}
+
+// registerDefaultValidation 注册默认的验证器(s).
+func registerDefaultValidation(v *validator.Validate) error {
+	tag := "port"
+	err := v.RegisterValidation(tag, func(fl validator.FieldLevel) bool {
+		var field reflect.Value = fl.Field()
+		return netKit.IsValidPort(field)
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 // Struct 验证结构体.
