e7ee2553fe35b37d0b39cfdbbde6897bcf328cdf
Richelieu <richelieu042@gmail.com>
2023-10-26 14:43:06 +0800

    dev

--- src/dependencyInjection/wireKit/provider.go
+++ src/dependencyInjection/wireKit/provider.go
@@ -7,7 +7,8 @@ var (
 	/*
 		PS:
 		(1) 如果经常同时使用多个提供者函数，这非常有用;
-		(2) 可以将其他提供者函数集添加到提供者函数集中.
+		(2) 实践中， 一组业务相关的 provider 时常被放在一起组织成 ProviderSet，以方便维护与切换;
+		(3) 可以将其他提供者函数集添加到提供者函数集中.
 	*/
 	NewSet func(...interface{}) wire.ProviderSet = wire.NewSet
 )
