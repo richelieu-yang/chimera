b7f1da33b62d75605944fc95602daed5ef736d21
Richelieu <richelieu042@gmail.com>
2023-10-27 09:40:53 +0800

    dev

--- src/idKit/xid.go
+++ src/idKit/xid.go
@@ -5,12 +5,11 @@ import (
 	"time"
 )
 
-// NewXid
+// NewXid Xid是一个全局唯一的ID生成器，它使用Mongo Object ID算法来生成全局唯一的ID.
 /*
-Xid是一个全局唯一的ID生成器，它使用Mongo Object ID算法来生成全局唯一的ID.
-
 PS:
-(1) 一个进程（Process）内，生成的id不会重复.
+(1) 单进程内生成唯一id，推荐使用 xid.
+(2) 一个进程（Process）内，生成的id不会重复.
 
 @return 长度(len())固定为20	e.g.  "ckic7hfnl531vbl645n0"
 							e.g.1 "ckth51co47mgs2kacmk0"
