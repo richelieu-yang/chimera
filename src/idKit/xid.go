5a18c433f1a8bccb3f4305ecd19bd6744f2c11a9
Richelieu <richelieu042@gmail.com>
2023-10-27 09:30:45 +0800

    dev

--- src/idKit/xid.go
+++ src/idKit/xid.go
@@ -12,7 +12,8 @@ Xid是一个全局唯一的ID生成器，它使用Mongo Object ID算法来生成
 PS:
 (1) 一个进程（Process）内，生成的id不会重复.
 
-@return 长度(len())固定为20.	e.g."ckic7hfnl531vbl645n0"
+@return 长度(len())固定为20	e.g.  "ckic7hfnl531vbl645n0"
+							e.g.1 "ckth51co47mgs2kacmk0"
 */
 func NewXid() string {
 	return xid.New().String()
--- test/test3.go
+++ test/test3.go
@@ -1,5 +1,10 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/richelieu-yang/chimera/v2/src/idKit"
+)
 
+func main() {
+	fmt.Println(idKit.NewXid())
 }
