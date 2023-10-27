2bc8ac7a14f53992fa432cfcfb5f59f37272dd25
Richelieu <richelieu042@gmail.com>
2023-10-27 11:05:08 +0800

    dev

--- src/component/web/push/pushKit/types.go
+++ src/component/web/push/pushKit/types.go
@@ -2,59 +2,55 @@ package pushKit
 
 import (
 	"github.com/richelieu-yang/chimera/v2/src/mutexKit"
-	"net/http"
 )
 
-type Listener interface {
-	OnFailure(w http.ResponseWriter, r *http.Request, error string)
+type (
+	Channel interface {
+		// Push 推送（二进制）消息给客户端.
+		Push(messageType MessageType, data []byte) error
 
-	OnHandshake(w http.ResponseWriter, r *http.Request, channel Channel)
+		// Close 后端主动关闭通道.
+		Close() error
+	}
 
-	// OnMessage 收到 客户端 发来的消息.
-	/*
-		PS: 仅适用于WebSocket连接，因为SSE连接是单工的.
-	*/
-	OnMessage(channel Channel, messageType int, data []byte)
+	BaseChannel struct {
+		Id    string
+		Bsid  string
+		User  string
+		Group string
 
-	OnClose(channel Channel, code int, text string)
-}
+		RWMutex mutexKit.RWMutex
 
-type Channel interface {
-	// Push 推送（二进制）消息给客户端.
-	Push(messageType MessageType, data []byte) error
-
-	// Close 后端主动关闭通道.
-	Close() error
-}
-
-type BaseChannel struct {
-	Id    string
-	Bsid  string
-	User  string
-	Group string
-
-	RWMutex mutexKit.RWMutex
-
-	Data   interface{}
-	Closed bool
-}
+		Data   interface{}
+		Closed bool
+	}
+)
 
 func (channel *BaseChannel) IsClosed() (rst bool) {
+	// 读锁
 	channel.RWMutex.RLockFunc(func() {
 		rst = channel.Closed
 	})
 	return
 }
 
-func (channel *BaseChannel) SetClosed() {
+// SetClosed
+/*
+@return true: 	设置成功
+		false:	设置失败（因为已经被设置关闭）
+*/
+func (channel *BaseChannel) SetClosed() (flag bool) {
 	if channel.Closed {
 		return
 	}
 
+	// 写锁
 	channel.RWMutex.LockFunc(func() {
 		if channel.Closed {
 			return
 		}
 		channel.Closed = true
+		flag = true
 	})
+	return
 }
