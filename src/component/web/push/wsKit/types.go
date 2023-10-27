9e372f598421570d0b26c6f69614e9c4215543dc
Richelieu <richelieu042@gmail.com>
2023-10-27 10:01:40 +0800

    dev

--- src/component/web/push/wsKit/types.go
+++ src/component/web/push/wsKit/types.go
@@ -2,6 +2,7 @@ package wsKit
 
 import (
 	"github.com/gorilla/websocket"
+	"github.com/richelieu-yang/chimera/v2/src/core/timeKit"
 	"github.com/richelieu-yang/chimera/v2/src/idKit"
 	"net/http"
 	"time"
@@ -9,31 +10,44 @@ import (
 
 type (
 	Handler struct {
+		// upgrader 是并发安全的
 		upgrader *websocket.Upgrader
 
 		idGenerator func() (string, error)
 	}
 )
 
-func NewHandler(handshakeTimeout time.Duration, checkOrigin func(r *http.Request) bool, idGenerator func() (string, error)) {
+// NewHandler
+/*
+@param handshakeTimeout	默认3s
+@param checkOrigin		可以为nil（允许所有）
+@param idGenerator		可以为nil（使用xid）
+*/
+func NewHandler(handshakeTimeout time.Duration, checkOrigin func(r *http.Request) bool, idGenerator func() (string, error)) *Handler {
+	handshakeTimeout = timeKit.ToDefaultDurationIfInvalid(handshakeTimeout, time.Second*3)
 	if checkOrigin == nil {
 		checkOrigin = func(r *http.Request) bool {
 			// 允许跨域
 			return true
 		}
 	}
+	// upgrader 并发安全的
+	upgrader := &websocket.Upgrader{
+		HandshakeTimeout: handshakeTimeout,
+		CheckOrigin: func(r *http.Request) bool {
+			// 允许跨域
+			return true
+		},
+	}
+
 	if idGenerator == nil {
 		idGenerator = func() (string, error) {
 			return idKit.NewXid(), nil
 		}
 	}
 
-	// upgrader 并发安全的
-	var upgrader = websocket.Upgrader{
-		HandshakeTimeout: time.Second * 3,
-		CheckOrigin: func(r *http.Request) bool {
-			// 允许跨域
-			return true
-		},
+	return &Handler{
+		upgrader:    upgrader,
+		idGenerator: idGenerator,
 	}
 }
--- src/core/timeKit/duration.go
+++ src/core/timeKit/duration.go
@@ -2,10 +2,10 @@ package timeKit
 
 import "time"
 
-// ToDefaultIfInvalid 如果t的值无效，将返回默认值def；否则返回t.
-func ToDefaultIfInvalid(t, def time.Duration) time.Duration {
-	if t <= 0 {
+// ToDefaultDurationIfInvalid 如果d的值无效，将返回默认值def；否则返回d.
+func ToDefaultDurationIfInvalid(d, def time.Duration) time.Duration {
+	if d <= 0 {
 		return def
 	}
-	return t
+	return d
 }
