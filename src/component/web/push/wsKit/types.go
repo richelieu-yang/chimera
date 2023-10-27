bb2ab17edb3c8a8f476758786bdeb125e6110aef
Richelieu <richelieu042@gmail.com>
2023-10-27 10:10:56 +0800

    dev

--- src/component/web/push/wsKit/types.go
+++ src/component/web/push/wsKit/types.go
@@ -2,6 +2,8 @@ package wsKit
 
 import (
 	"github.com/gorilla/websocket"
+	"github.com/richelieu-yang/chimera/v2/src/component/web/push/pushKit"
+	"github.com/richelieu-yang/chimera/v2/src/core/interfaceKit"
 	"github.com/richelieu-yang/chimera/v2/src/core/timeKit"
 	"github.com/richelieu-yang/chimera/v2/src/idKit"
 	"net/http"
@@ -9,21 +11,28 @@ import (
 )
 
 type (
-	Handler struct {
+	Processor struct {
 		// upgrader 是并发安全的
 		upgrader *websocket.Upgrader
 
 		idGenerator func() (string, error)
+
+		listener pushKit.Listener
 	}
 )
 
-// NewHandler
+func (p *Processor) Handle(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// NewProcessor
 /*
 @param handshakeTimeout	默认3s
 @param checkOrigin		可以为nil（允许所有）
 @param idGenerator		可以为nil（使用xid）
+@param listener			不能为nil
 */
-func NewHandler(handshakeTimeout time.Duration, checkOrigin func(r *http.Request) bool, idGenerator func() (string, error)) *Handler {
+func NewProcessor(handshakeTimeout time.Duration, checkOrigin func(r *http.Request) bool, idGenerator func() (string, error), listener pushKit.Listener) (*Processor, error) {
 	handshakeTimeout = timeKit.ToDefaultDurationIfInvalid(handshakeTimeout, time.Second*3)
 	if checkOrigin == nil {
 		checkOrigin = func(r *http.Request) bool {
@@ -31,7 +40,6 @@ func NewHandler(handshakeTimeout time.Duration, checkOrigin func(r *http.Request
 			return true
 		}
 	}
-	// upgrader 并发安全的
 	upgrader := &websocket.Upgrader{
 		HandshakeTimeout: handshakeTimeout,
 		CheckOrigin: func(r *http.Request) bool {
@@ -46,8 +54,13 @@ func NewHandler(handshakeTimeout time.Duration, checkOrigin func(r *http.Request
 		}
 	}
 
-	return &Handler{
+	if err := interfaceKit.AssertNotNil(listener, "listener"); err != nil {
+		return nil, err
+	}
+
+	return &Processor{
 		upgrader:    upgrader,
 		idGenerator: idGenerator,
-	}
+		listener:    listener,
+	}, nil
 }
