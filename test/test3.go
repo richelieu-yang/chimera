ede382d40397716b9f40bb9f7190c524cfc109ec
Richelieu <richelieu042@gmail.com>
2023-10-30 09:56:53 +0800

    dev

--- test/test3.go
+++ test/test3.go
@@ -1,14 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"github.com/richelieu-yang/chimera/v2/src/mutexKit"
-)
-
 func main() {
-	m := mutexKit.NewRWMutex()
 
-	m.LockFunc(func() {
-		fmt.Println(666)
-	})
 }
