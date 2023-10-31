f642d4bd4b02bad9c6726c28f022b998f8f25fa9
Richelieu <richelieu042@gmail.com>
2023-10-31 15:38:58 +0800

    dev

--- src/component/web/push/sseKit/sse.go
+++ src/component/web/push/sseKit/sse.go
@@ -2,6 +2,7 @@ package sseKit
 
 import (
 	"github.com/richelieu-yang/chimera/v2/src/component/web/push/pushKit"
+	"github.com/richelieu-yang/chimera/v2/src/core/errorKit"
 	"github.com/richelieu-yang/chimera/v2/src/idKit"
 )
 
@@ -10,7 +11,7 @@ import (
 @param idGenerator	可以为nil（将使用xid）
 @param listener		不能为nil
 */
-func NewProcessor(idGenerator func() (string, error), listener pushKit.Listener, messageType messageType) (pushKit.Processor, error) {
+func NewProcessor(idGenerator func() (string, error), listener pushKit.Listener, msgType messageType) (pushKit.Processor, error) {
 	if idGenerator == nil {
 		idGenerator = func() (string, error) {
 			return idKit.NewXid(), nil
@@ -20,11 +21,18 @@ func NewProcessor(idGenerator func() (string, error), listener pushKit.Listener,
 	if err != nil {
 		return nil, err
 	}
+	switch msgType {
+	case MessageTypeRaw:
+	case MessageTypeEncode:
+	case MessageTypeBase64:
+	default:
+		return nil, errorKit.New("invalid message type(%d)", msgType)
+	}
 
 	processor := &SseProcessor{
 		idGenerator: idGenerator,
 		listeners:   listeners,
-		msgType:     messageType,
+		msgType:     msgType,
 	}
 	return processor, nil
 }
--- src/component/web/push/sseKit/sse_test.go
+++ src/component/web/push/sseKit/sse_test.go
@@ -2,18 +2,42 @@ package sseKit
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/richelieu-yang/chimera/v2/src/component/web/push/pushKit"
 	"github.com/richelieu-yang/chimera/v2/src/log/logrusKit"
-	"github.com/sirupsen/logrus"
+	"net/http"
 	"testing"
 )
 
+type listener struct {
+	pushKit.Listener
+}
+
+func (l *listener) OnFailure(w http.ResponseWriter, r *http.Request, failureInfo string) {
+
+}
+
+func (l *listener) OnHandshake(w http.ResponseWriter, r *http.Request, channel pushKit.Channel) {
+
+}
+
+func (l *listener) OnMessage(channel pushKit.Channel, messageType int, data []byte) {
+
+}
+
+func (l *listener) OnClose(channel pushKit.Channel, closeInfo string) {
+
+}
+
 func TestNewProcessor(t *testing.T) {
 	logrusKit.MustSetUp(nil)
-	processor, err := NewProcessor()
+	processor, err := NewProcessor(nil, &listener{}, MessageTypeRaw)
+	if err != nil {
+		panic(err)
+	}
 
 	engine := gin.Default()
-
+	engine.GET("/sse", processor.ProcessWithGin)
 	if err := engine.Run(":80"); err != nil {
-		logrus.Fatal(err)
+		panic(err)
 	}
 }
