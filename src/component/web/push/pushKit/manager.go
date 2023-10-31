e20dda5ccb49ae91e7dbf86378709cc62ac3b215
Richelieu <richelieu042@gmail.com>
2023-10-31 17:27:09 +0800

    dev

--- src/component/web/push/pushKit/manager.go
+++ src/component/web/push/pushKit/manager.go
@@ -1,1 +1,32 @@
 package pushKit
+
+import (
+	"github.com/richelieu-yang/chimera/v2/src/core/mapKit"
+	"github.com/richelieu-yang/chimera/v2/src/core/setKit"
+)
+
+var (
+	// allMap
+	/*
+		key: id（一对一）
+	*/
+	allMap = mapKit.NewMapWithLock[string, Channel]()
+
+	// groupMap
+	/*
+		key: group（一对多）
+	*/
+	groupMap = mapKit.NewMapWithLock[string, setKit.SetWithLock[Channel]]()
+
+	// userMap
+	/*
+		key: user（一对多）
+	*/
+	userMap = mapKit.NewMapWithLock[string, setKit.SetWithLock[Channel]]()
+
+	// bsidMap
+	/*
+		key: bsid（一对一）
+	*/
+	bsidMap = mapKit.NewMapWithLock[string, Channel]()
+)
