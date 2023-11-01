5cb7ebef363851466f016b804758743c8a3a465e
Richelieu <richelieu042@gmail.com>
2023-11-01 10:26:50 +0800

    dev

--- src/compareKit/compare.go
+++ src/compareKit/compare.go
@@ -1,19 +1,53 @@
 package compareKit
 
-import "github.com/google/go-cmp/cmp"
+import (
+	"github.com/google/go-cmp/cmp"
+	"reflect"
+)
 
 // Equal 是否相等？
 /*
 !!!:
 (1) 如果传参结构体（或其内部的结构体）实现了 "(T) Equal(T) bool" 或者 "(T) Equal(I) bool"，
-	方法的receiver必须为 "值类型"，这样的话，无论比较 结构体实例 还是 结构体实例指针 都可以.
+	且方法的receiver必须为 "值类型"，这样的话，无论比较 结构体实例 还是 结构体实例指针 都可以.
+(2) 如果传参x、y都是 非nil的结构体实例指针，比较的是它们的内容是否相等.
 
 PS:
 (1) 如果传参分别为 结构体实例 和 结构体实例指针，将返回false.
 (2) 传参x、y值都为nil的情况下，	(a) 类型不同，将返回 false;
 							(b) 类型相同，将返回 true.
+
+e.g.
+	type person struct {
+		Name string
+		Age  int
+	}
+
+	p1 := &person{"Alice", 18}
+	p2 := &person{"Bob", 20}
+	p3 := &person{"Alice", 18}
+
+	fmt.Println(compareKit.Equal(p1, p2)) // false
+	fmt.Println(compareKit.Equal(p1, p3)) // true
 */
 var Equal func(x, y interface{}, opts ...cmp.Option) bool = cmp.Equal
+
+// Equal1 比较2个结构体实例指针的内容是否相等.
+/*
+e.g.
+	type person struct {
+		Name string
+		Age  int
+	}
+
+	p1 := &person{"Alice", 18}
+	p2 := &person{"Bob", 20}
+	p3 := &person{"Alice", 18}
+
+	fmt.Println(reflect.DeepEqual(p1, p2)) // false
+	fmt.Println(reflect.DeepEqual(p1, p3)) // true
+*/
+var Equal1 func(x, y any) bool = reflect.DeepEqual
 
 // Diff 获取差异.
 /*
--- test/test2.go
+++ test/test2.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import "fmt"
 
+type Person struct {
+	Name string
+	Age  int
+}
+
+func main() {
+	p1 := &Person{"Alice", 18}
+	p2 := &Person{"Bob", 20}
+	p3 := &Person{"Alice", 18}
+
+	// 比较结构体实例指针的内存地址
+	if p1 == p2 {
+		fmt.Println("p1 equals to p2")
+	}
+
+	// 比较结构体实例指针是否指向同一个实例
+	if p1 == p3 {
+		fmt.Println("p1 equals to p3")
+	}
 }
