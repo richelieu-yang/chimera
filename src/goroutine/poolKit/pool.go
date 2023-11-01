1c435e70d91a69ad83b90b2e87323e260c6578e7
Richelieu <richelieu042@gmail.com>
2023-11-01 14:31:34 +0800

    dev

--- src/goroutine/poolKit/pool.go
+++ src/goroutine/poolKit/pool.go
@@ -7,21 +7,27 @@ import (
 // NewPool 创建 *ants.Pool 实例.
 /*
 PS:
-(1) 通过 "func (p *Pool) Submit(task func()) error" 执行任务，任务无参无返回值.
+(1) 通过 Pool.Submit() 执行任务，任务无参无返回值.
 (2) 默认情况下（即不传options）:
 	(a) (Nonblocking: false) 阻塞模式
 	(b) (MaxBlockingTasks: 0) 阻塞模式下，最多因为调用Pool.Submit()而阻塞的goroutine数量 不设限制.
+(3) 可以通过 ants.WithLogger() 指定日志输出（默认输出到控制台）.
 
-@param options
+@param size (1) cap
+			(2) 并不是传了多少就立即创建多少协程.
 */
 var NewPool func(size int, options ...ants.Option) (*ants.Pool, error) = ants.NewPool
 
 // NewPoolWithFunc 创建 *ants.PoolWithFunc 实例.
 /*
 PS:
-(1) 通过 "func (p *PoolWithFunc) Invoke(args interface{}) error" 执行任务，任务有1个参数（interface{}类型）无返回值.
+(1) 通过 Pool.Submit() 执行任务，任务无参无返回值.
 (2) 默认情况下（即不传options）:
 	(a) (Nonblocking: false) 阻塞模式
 	(b) (MaxBlockingTasks: 0) 阻塞模式下，最多因为调用Pool.Submit()而阻塞的goroutine数量 不设限制.
+(3) 可以通过 ants.WithLogger() 指定日志输出（默认输出到控制台）.
+
+@param size (1) cap
+			(2) 并不是传了多少就立即创建多少协程.
 */
 var NewPoolWithFunc func(size int, pf func(interface{}), options ...ants.Option) (*ants.PoolWithFunc, error) = ants.NewPoolWithFunc
--- test/test1.go
+++ test/test1.go
@@ -1,28 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"github.com/richelieu-yang/chimera/v2/src/reflectKit"
-	"reflect"
-)
-
 func main() {
-	{
-		var obj interface{} = nil
-		v := reflectKit.ValueOf(obj)
-		k := v.Kind()
-		fmt.Println(k)                    // invalid
-		fmt.Println(k == reflect.Invalid) // true
-	}
 
-	{
-		type bean struct {
-		}
-
-		var obj *bean = nil
-		v := reflectKit.ValueOf(obj)
-		k := v.Kind()
-		fmt.Println(k)                    // ptr
-		fmt.Println(k == reflect.Invalid) // false
-	}
 }
