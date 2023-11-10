d50c83320597d4747bc3b0c0142b039448e0a76c
Richelieu <richelieu042@gmail.com>
2023-11-10 08:51:46 +0800

    dev

--- src/core/setKit/_info.md
+++ src/core/setKit/_info.md
@@ -4,3 +4,8 @@
 ## Set
 * 无序
 * 不允许重复.
+
+
+## mapset.Set 接口
+* Cardinality() int  元素个数
+* Each(func(T) bool) 遍历（虽然其内部使用了for range，但在使用时无需使用 同名变量覆盖v:=v）
--- test/test3.go
+++ test/test3.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/richelieu-yang/chimera/v2/src/core/setKit"
+	"time"
+)
 
+func main() {
+	set := setKit.NewSet(false, 0, 1, 2, 3)
+	set.Each(func(i int) bool {
+		go func() {
+			fmt.Println("a", i)
+		}()
+		return false
+	})
+
+	s := []int{0, 1, 2, 3}
+	for _, ele := range s {
+		go func() {
+			fmt.Println("b", ele)
+		}()
+	}
+
+	time.Sleep(time.Second)
 }
