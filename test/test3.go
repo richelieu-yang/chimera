955b162add04cdaed610fa486ef864cce43cec9c
Richelieu <richelieu042@gmail.com>
2023-11-10 10:07:02 +0800

    dev

--- test/test3.go
+++ test/test3.go
@@ -1,26 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"github.com/richelieu-yang/chimera/v2/src/core/setKit"
-	"time"
-)
-
 func main() {
-	set := setKit.NewSet(false, 0, 1, 2, 3)
-	set.Each(func(i int) bool {
-		go func() {
-			fmt.Println("a", i)
-		}()
-		return false
-	})
-	time.Sleep(time.Second)
 
-	s := []int{0, 1, 2, 3}
-	for _, ele := range s {
-		go func() {
-			fmt.Println("b", ele)
-		}()
-	}
-	time.Sleep(time.Second)
 }
