5d390dca81eb5fd8c53036e3ca0c091bde70e4a8
Richelieu <richelieu042@gmail.com>
2023-11-13 16:57:56 +0800

    dev

--- test/test3.go
+++ test/test3.go
@@ -12,25 +12,10 @@ func main() {
 	if err := ginKit.DefaultNoRoute(engine); err != nil {
 		panic(err)
 	}
-
-	//fs := resources.AssetFile()
-	////engine.StaticFileFS("/404", "_resources/html/404.min.html", fs)
-	//engine.NoRoute(func(ctx *gin.Context) {
-	//	f, err := fs.Open("_resources/html/404.min.html")
-	//	if err != nil {
-	//		panic(err)
-	//	}
-	//	defer f.Close()
-	//
-	//	data, err := ioKit.ReadFromReader(f)
-	//	if err != nil {
-	//		panic(err)
-	//	}
-	//	ctx.Data(http.StatusNotFound, "text/html; charset=utf-8", data)
-	//
-	//	//ctx.FileFromFS("_resources/html/404.min.html", fs)
-	//	//ctx.Status(404)
-	//})
+	engine.Any("/test", func(ctx *gin.Context) {
+		ctx.String(200, "ok")
+		ctx.Status(500)
+	})
 
 	if err := engine.Run(":80"); err != nil {
 		panic(err)
