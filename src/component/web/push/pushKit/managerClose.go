ea49862ca588f75de6ebca8d0f7040105462a88c
Richelieu <richelieu042@gmail.com>
2023-11-13 09:56:17 +0800

    dev

--- src/component/web/push/pushKit/managerClose.go
+++ src/component/web/push/pushKit/managerClose.go
@@ -1,22 +1,63 @@
 package pushKit
 
 import (
-	"github.com/richelieu-yang/chimera/v2/src/core/errorKit"
 	"github.com/richelieu-yang/chimera/v2/src/core/strKit"
 )
 
+// CloseById
+/*
+PS: 解绑（unbind）后续由 inner handler 处理.
+*/
 func CloseById(id string, reason string) (err error) {
 	reason = strKit.EmptyToDefault(reason, "no reason")
 
-	var channel Channel
+	/* 写锁 */
 	idMap.LockFunc(func() {
-		channel = idMap.Map[id]
+		channel := idMap.Map[id]
 		if channel == nil {
-			err = errorKit.New("No channel for id(%s)", id)
+			return
+		}
+		err = channel.Close(reason)
+	})
+	return
+}
+
+// CloseByBsid
+/*
+PS: 解绑（unbind）后续由 inner handler 处理.
+*/
+func CloseByBsid(bsid string, reason string) (err error) {
+	reason = strKit.EmptyToDefault(reason, "no reason")
+
+	/* 写锁 */
+	bsidMap.LockFunc(func() {
+		channel := bsidMap.Map[bsid]
+		if channel == nil {
 			return
 		}
 		err = channel.Close(reason)
 		// 解绑（unbind）后续由 inner handler 处理
 	})
 	return
+}
+
+// CloseByUser
+/*
+PS: 解绑（unbind）后续由 inner handler 处理.
+*/
+func CloseByUser(user string, reason string) {
+	reason = strKit.EmptyToDefault(reason, "no reason")
+
+	/* 写锁 */
+	userMap.LockFunc(func() {
+		userSet := userMap.Map[user]
+
+		/* 写锁 */
+		userSet.LockFunc(func() {
+			userSet.Set.Each(func(channel Channel) bool {
+				_ = channel.Close(reason)
+				return false
+			})
+		})
+	})
 }
