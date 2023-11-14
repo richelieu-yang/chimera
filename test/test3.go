53249eaa72b396b784e74642e7f9b40be19afbb7
Richelieu <richelieu042@gmail.com>
2023-11-14 14:12:27 +0800

    dev

--- test/test3.go
+++ test/test3.go
@@ -1,23 +1,5 @@
 package main
 
-import (
-	"github.com/gin-gonic/gin"
-	"github.com/richelieu-yang/chimera/v2/src/component/web/ginKit"
-)
-
 func main() {
-	engine := gin.Default()
 
-	ginKit.DefaultFavicon(engine)
-	if err := ginKit.DefaultNoRoute(engine); err != nil {
-		panic(err)
-	}
-	engine.Any("/test", func(ctx *gin.Context) {
-		ctx.String(200, "ok")
-		ctx.Status(500)
-	})
-
-	if err := engine.Run(":80"); err != nil {
-		panic(err)
-	}
 }
