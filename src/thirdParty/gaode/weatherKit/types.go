dd437f54dec983335fdcca1df2271700b9453058
Richelieu <richelieu042@gmail.com>
2023-11-15 16:31:14 +0800

    dev

--- src/thirdParty/gaode/weatherKit/types.go
+++ src/thirdParty/gaode/weatherKit/types.go
@@ -1,6 +1,59 @@
 package weatherKit
 
 type (
-	Weather struct {
+	GaodeResponse struct {
+		// Status 值为0或1，1：成功；0：失败
+		Status   string `json:"status"`
+		InfoCode string `json:"infocode"`
+		Info     string `json:"info"`
+
+		Lives     []*Live     `json:"lives"`
+		Forecasts []*Forecast `json:"forecasts"`
+	}
+
+	Live struct {
+		// Province 省份名
+		Province string `json:"province"`
+
+		// City 城市名
+		City string `json:"city"`
+
+		// Adcode 区域编码
+		Adcode string `json:"adcode"`
+
+		// Weather 天气现象（汉字描述）
+		Weather string `json:"weather"`
+
+		// Temperature 实时气温，单位：摄氏度
+		Temperature string `json:"temperature"`
+
+		// WindPower 风力级别，单位：级
+		WindPower string `json:"windpower"`
+
+		// Humidity 空气湿度
+		Humidity string `json:"humidity"`
+
+		// ReportTime 数据发布的时间
+		ReportTime string `json:"reporttime"`
+	}
+
+	Forecast struct {
+		// Date 日期
+		Date string `json:"date"`
+
+		// Week 星期几
+		Week string `json:"week"`
+
+		// DayWeather 白天天气现象
+		DayWeather string `json:"dayweather"`
+
+		// NightWeather 晚上天气现象
+		NightWeather string `json:"nightweather"`
+
+		// DayTemp 白天温度
+		DayTemp string `json:"daytemp"`
+
+		// NightTemp 晚上温度
+		NightTemp string `json:"nighttemp"`
 	}
 )
--- src/thirdParty/gaode/weatherKit/weather.go
+++ src/thirdParty/gaode/weatherKit/weather.go
@@ -1,25 +1,23 @@
 package weatherKit
 
-// GetTodayWeather 获取当天的天气.
-/*
-@param city 城市编码
-*/
-func GetTodayWeather(city string) {
+import "github.com/richelieu-yang/chimera/v2/src/component/web/reqKit"
 
-}
+const (
+	Url = "https://restapi.amap.com/v3/weather/weatherInfo"
+)
 
 // GetLiveWeather 获取"实况"天气.
 /*
 @param city 城市编码
 */
-func GetLiveWeather(city string) {
-
+func GetLiveWeather(city string) (*Live, error) {
+	reqKit.GetDefaultClient()
 }
 
 // GetForecastWeather 获取"预报"天气.
 /*
 @param city 城市编码
 */
-func GetForecastWeather(city string) {
+func GetForecastWeather(city string) ([]*Forecast, error) {
 
 }
