6351da53589639012cfac39538f0805240042463
Richelieu <richelieu042@gmail.com>
2023-11-15 16:32:31 +0800

    dev

--- test/test3.go
+++ test/test3.go
@@ -1,5 +1,10 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/richelieu-yang/chimera/v2/src/component/web/reqKit"
+)
 
+func main() {
+	fmt.Println(reqKit.Get("https://req.cool/zh/docs/prologue/quickstart/", nil))
 }
