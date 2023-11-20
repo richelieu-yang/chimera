76f0aec3a7a4e74d19465935b90724038cb4591b
Richelieu <richelieu042@gmail.com>
2023-11-20 09:32:59 +0800

    dev

--- src/component/web/reqKit/client.go
+++ src/component/web/reqKit/client.go
@@ -20,7 +20,9 @@ var defaultClient = NewClient(3)
 重用Client https://req.cool/zh/docs/tutorial/best-practices/#%e9%87%8d%e7%94%a8-client
 	不要每次发请求都创建 Client，造成不必要的开销，通常可以复用同一 Client 发所有请求.
 
-!!!: 不修改返回值的话，可以调用此方法；否则调用 NewClient.
+!!!:
+(1) 不修改返回值的话，可以调用此方法；否则调用 NewClient;
+(2) 最大重试次数 == 3.
 */
 func GetDefaultClient() *Client {
 	return defaultClient
