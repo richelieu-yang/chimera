14c80c30df9982890e5e654b7731a9d7634bc9e0
Richelieu <richelieu042@gmail.com>
2023-11-20 10:04:22 +0800

    dev

--- test/test2.go
+++ test/test2.go
@@ -1,10 +1,4 @@
 package main
 
-import (
-	"fmt"
-	"github.com/richelieu-yang/chimera/v2/src/component/web/push/pushKit"
-)
-
 func main() {
-	fmt.Println(pushKit.GetStatistics())
 }
--- test/test3.go
+++ test/test3.go
@@ -4,13 +4,38 @@ import (
 	"crypto/tls"
 	"fmt"
 	"github.com/eatmoreapple/openwechat"
+	"github.com/richelieu-yang/chimera/v2/src/core/fileKit"
+	"github.com/richelieu-yang/chimera/v2/src/core/ioKit"
+	"github.com/richelieu-yang/chimera/v2/src/core/timeKit"
 	"github.com/richelieu-yang/chimera/v2/src/cronKit"
+	"github.com/richelieu-yang/chimera/v2/src/log/logrusKit"
 	"github.com/richelieu-yang/chimera/v2/src/thirdParty/gaodeKit"
 	"github.com/sirupsen/logrus"
 	"net/http"
+	"os"
 )
 
 var client *gaodeKit.Client
+var logger *logrus.Logger
+
+func init() {
+	logrusKit.MustSetUp(nil)
+	logrusKit.DisableQuote(nil)
+
+	var err error
+	client, err = gaodeKit.NewClient("b15c36bf1df4c272e92f3f1875a127f1")
+	if err != nil {
+		logrus.Fatal(err)
+	}
+
+	path := fmt.Sprintf("%s.log", timeKit.FormatCurrent(timeKit.FormatFileName))
+	file, err := fileKit.Create(path)
+	if err != nil {
+		logrus.Fatal(err)
+	}
+	out := ioKit.MultiWriter(file, os.Stdout)
+	logger = logrusKit.NewLogger(logrusKit.WithDisableQuote(true), logrusKit.WithOutput(out))
+}
 
 func main() {
 	var err error
@@ -29,15 +54,12 @@ func main() {
 
 	// 注册消息处理函数
 	bot.MessageHandler = func(msg *openwechat.Message) {
-
-		msg.IsSendBySelf()
-
 		if msg.IsText() && msg.Content == "ping" {
 			_, err := msg.ReplyText("pong")
 			if err != nil {
-				logrus.WithError(err).Error("Fail to pong")
+				logger.WithError(err).Error("Fail to pong")
 			} else {
-				logrus.Info("Manager to pong")
+				logger.Info("Manager to pong")
 			}
 		}
 	}
@@ -46,33 +68,30 @@ func main() {
 
 	// 登陆
 	if err := bot.Login(); err != nil {
-		fmt.Println(err)
-		return
+		logrus.Fatal(err)
 	}
 
 	// 获取登陆的用户
 	self, err := bot.GetCurrentUser()
 	if err != nil {
-		fmt.Println(err)
-		return
+		logrus.Fatal(err)
 	}
 
 	// 获取所有的好友
 	friends, err := self.Friends()
 	if err != nil {
-		fmt.Println(err)
-		return
+		logrus.Fatal(err)
 	}
 	fmt.Println(friends)
 
 	results := friends.SearchByRemarkName(1, "狗蛋妈")
 	if results.Count() == 0 {
-		logrus.Fatal("results.Count() == 0")
+		logrus.Fatal("Fail to get friend")
 	}
 	wife := results.First()
 
 	go func() {
-		send(wife)
+		start(wife)
 	}()
 
 	if err := bot.Block(); err != nil {
@@ -81,20 +100,25 @@ func main() {
 }
 
 // 发送问候
-func send(f *openwechat.Friend) {
+func start(f *openwechat.Friend) {
 	cron := cronKit.NewCron()
 
-	_, err := cron.AddFunc("0 30 6 * * *", greet(f, "宝贝，早安！该起床了。"))
+	_, err := cron.AddFunc("0 30 6 * * *", task(f, "宝贝，早安！该起床了。"))
 	if err != nil {
 		logrus.Fatal(err)
 	}
 
-	_, err = cron.AddFunc("0 30 11 * * *", greet(f, "宝贝，午安！"))
+	_, err = cron.AddFunc("0 30 11 * * *", task(f, "宝贝，午安！"))
 	if err != nil {
 		logrus.Fatal(err)
 	}
 
-	_, err = cron.AddFunc("0 30 21 * * *", greet(f, "宝贝，晚安！该困告了。"))
+	_, err = cron.AddFunc("0 30 17 * * *", task(f, "宝贝，我下班喽。"))
+	if err != nil {
+		logrus.Fatal(err)
+	}
+
+	_, err = cron.AddFunc("0 30 21 * * *", task(f, "宝贝，晚安！该困告了。"))
 	if err != nil {
 		logrus.Fatal(err)
 	}
@@ -102,14 +126,46 @@ func send(f *openwechat.Friend) {
 	cron.Start()
 }
 
-func greet(f *openwechat.Friend, text string) func() {
+func task(f *openwechat.Friend, greetText string) func() {
 	return func() {
-		_, err := f.SendText(text)
+		// greet
+		logger.Infof("greetText: %s", greetText)
+		_, err := f.SendText(greetText)
 		if err != nil {
-			logrus.WithError(err).WithField("text", text).Error("Fail to greet.")
+			logger.WithError(err).Error("Fail to greet.")
 		} else {
-			logrus.Info("Manager to greet.")
+			logger.Info("Manager to greet.")
 		}
 
+		// weather
+		weather := getWeather()
+		logger.Infof("weather: %s", weather)
+		_, err = f.SendText(weather)
+		if err != nil {
+			logger.WithError(err).Error("Fail to greet.")
+		} else {
+			logger.Info("Manager to send weather.")
+		}
 	}
+}
+
+func getWeather() string {
+	city := "320200"
+	live, err := client.GetLive(city)
+	if err != nil {
+		panic(err)
+	}
+	cast, err := client.GetTodayCast(city)
+	if err != nil {
+		panic(err)
+	}
+
+	text := fmt.Sprintf("您好，现在是[%s]，[%s %s]目前气温：%s，白天气温：%s，夜晚气温：%s.",
+		live.ReportTime,
+		live.Province,
+		live.City,
+		fmt.Sprintf("%s°C(%s)", live.Temperature, live.Weather),
+		fmt.Sprintf("%s°C(%s)", cast.DayTemp, cast.DayWeather),
+		fmt.Sprintf("%s°C(%s)", cast.NightTemp, cast.NightWeather))
+	return text
 }
