071d84df051f1f302f85a8f0e7682737b28372b4
Richelieu <richelieu042@gmail.com>
2023-11-21 13:30:45 +0800

    dev

--- src/component/web/httpKit/contentType.go
+++ src/component/web/httpKit/contentType.go
@@ -1,19 +1,19 @@
 package httpKit
 
 const (
-	// PlainContentType 纯文本格式
+	// PlainContentType 纯文本格式.
 	PlainContentType = "text/plain; charset=utf-8"
 
-	// JsonContentType JSON数据格式
+	// JsonContentType JSON数据格式.
 	JsonContentType = "application/json; charset=utf-8"
 
-	// XmlContentType XML数据格式
+	// XmlContentType XML数据格式.
 	XmlContentType = "text/xml; charset=utf-8"
 
 	// FormUrlencodedContentType 适用于 POST 请求.
 	FormUrlencodedContentType = "application/x-www-form-urlencoded; charset=utf-8"
 
-	// OctetStreamContentType 二进制流数据（如常见的文件下载）
+	// OctetStreamContentType 二进制流数据（如常见的文件下载）.
 	/*
 		参考：https://www.runoob.com/http/http-content-type.html
 	*/
