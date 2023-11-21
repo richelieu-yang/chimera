2325700859214ae0d9324f15459b4a3df4026124
Richelieu <richelieu042@gmail.com>
2023-11-21 15:34:31 +0800

    dev

--- src/core/timeKit/interval_test.go
+++ src/core/timeKit/interval_test.go
@@ -7,17 +7,17 @@ import (
 )
 
 func TestSetInterval(t *testing.T) {
-	ticker := SetInterval(func(t time.Time) {
-		logrus.Info(t.String())
+	i := SetInterval(func(t time.Time) {
+		logrus.Info(t)
 	}, time.Second*3)
 
 	logrus.Info("sleep starts")
 	time.Sleep(time.Second * 10)
 	logrus.Info("sleep ends")
 
-	ClearInterval(ticker)
-	ClearInterval(ticker)
-	ClearInterval(ticker)
+	i.Stop()
+	i.Stop()
+	i.Stop()
 
 	logrus.Info("sleep starts")
 	time.Sleep(time.Second * 10)
