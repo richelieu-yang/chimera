1f676d46f5bc2b353aa0264812e4248c832054e3
Richelieu <richelieu042@gmail.com>
2023-11-22 10:37:51 +0800

    dev

--- test/test3.go
+++ test/test3.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"github.com/richelieu-yang/chimera/v2/src/cronKit"
+	"github.com/richelieu-yang/chimera/v2/src/log/logrusKit"
+	"github.com/sirupsen/logrus"
+)
 
+func main() {
+	logrusKit.MustSetUp(nil)
+
+	c := cronKit.NewCron()
+	_, err := c.AddFunc("0 0/2 * * * ?", func() {
+		logrus.Info("-")
+	})
+	if err != nil {
+		panic(err)
+	}
+	logrus.Info("-")
+	c.Run()
 }
