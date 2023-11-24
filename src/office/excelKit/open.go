bcf357630273205b2a0f85970932c93abb6e4b30
Richelieu <richelieu042@gmail.com>
2023-11-24 14:47:04 +0800

    dev

--- src/office/excelKit/open.go
+++ src/office/excelKit/open.go
@@ -1,15 +1,26 @@
 package excelKit
 
 import (
+	"github.com/richelieu-yang/chimera/v2/src/core/fileKit"
 	"github.com/xuri/excelize/v2"
 	"io"
 )
 
 // OpenFile 打开本地文件.
-var OpenFile func(filename string, opts ...excelize.Options) (*excelize.File, error) = excelize.OpenFile
+/*
+PS: 可能会返回 excelize.ErrWorkbookFileFormat，原因: 文件格式不支持.
+*/
+func OpenFile(path string, opts ...excelize.Options) (*excelize.File, error) {
+	if err := fileKit.AssertExistAndIsFile(path); err != nil {
+		return nil, err
+	}
+	return excelize.OpenFile(path, opts...)
+}
 
 // OpenReader 打开数据流.
 /*
+PS: 可能会返回 excelize.ErrWorkbookFileFormat，原因: 文件格式不支持.
+
 @params r 数据流（包括: 远程文件）
 */
 var OpenReader func(r io.Reader, opts ...excelize.Options) (*excelize.File, error) = excelize.OpenReader
