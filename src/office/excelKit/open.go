9f923b23636eec0ca207f3ecfd6514e5959197b5
Richelieu <richelieu042@gmail.com>
2023-11-24 14:33:36 +0800

    dev

--- src/office/excelKit/open.go
+++ src/office/excelKit/open.go
@@ -5,6 +5,11 @@ import (
 	"io"
 )
 
+// OpenFile 打开本地文件.
 var OpenFile func(filename string, opts ...excelize.Options) (*excelize.File, error) = excelize.OpenFile
 
+// OpenReader 打开数据流.
+/*
+@params r 数据流（包括: 远程文件）
+*/
 var OpenReader func(r io.Reader, opts ...excelize.Options) (*excelize.File, error) = excelize.OpenReader
