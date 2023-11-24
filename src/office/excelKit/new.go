0ea74f21146566f968acd4144a2db054ca2eeca2
Richelieu <richelieu042@gmail.com>
2023-11-24 14:32:21 +0800

    dev

--- src/office/excelKit/new.go
+++ src/office/excelKit/new.go
@@ -5,25 +5,27 @@ import (
 	"github.com/xuri/excelize/v2"
 )
 
+// NewFile
+/*
+PS: 返回的 *excelize.File实例，不再使用时应当调用 File.Close().
+*/
 var NewFile func(opts ...excelize.Options) *excelize.File = excelize.NewFile
 
 // NewFileWithPath 新建1个空白的Excel文件（其内只有1个空白的工作表，表名为"Sheet1"）
 /*
+PS: 返回的 *excelize.File实例，不再使用时应当调用 File.Close().
+
 @param path 文件的路径（如果文件已经存在，会覆盖它）
 */
-func NewFileWithPath(path string, opts ...excelize.Options) (f *excelize.File, err error) {
-	if err = fileKit.MkParentDirs(path); err != nil {
-		return
-	}
-
-	f = NewFile()
-	defer func(f *excelize.File) {
-		if err != nil {
-			_ = f.Close()
-		}
-	}(f)
-	if err = f.SaveAs(path, opts...); err != nil {
+func NewFileWithPath(path string, opts ...excelize.Options) (*excelize.File, error) {
+	if err := fileKit.MkParentDirs(path); err != nil {
 		return nil, err
 	}
-	return
+
+	f := NewFile()
+	if err := f.SaveAs(path, opts...); err != nil {
+		_ = f.Close()
+		return nil, err
+	}
+	return f, nil
 }
