cc33371e715d43738bbc7fd0c2850077f21a63dd
Richelieu <richelieu042@gmail.com>
2023-11-30 17:16:55 +0800

    dev

--- src/core/envKit/get.go
+++ src/core/envKit/get.go
@@ -1,1 +1,19 @@
 package envKit
+
+import (
+	"os"
+)
+
+var (
+	GetEnv func(key string) string = os.Getenv
+)
+
+// GetEnvWithDefault （带默认值地）获取环境变量.
+func GetEnvWithDefault(key string, def string) (rst string) {
+	rst = GetEnv(key)
+
+	if rst == "" {
+		rst = def
+	}
+	return
+}
