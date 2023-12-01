8f16b189366d6dd8dc35ec771191a092e3a7fa97
Richelieu <richelieu042@gmail.com>
2023-12-01 13:49:08 +0800

    dev

--- test/test3.go
+++ test/test3.go
@@ -3,8 +3,19 @@ package main
 import (
 	"fmt"
 	"github.com/apache/rocketmq-clients/golang/v5/pkg/utils"
+	"runtime"
+	"runtime/debug"
 )
 
 func main() {
 	fmt.Println(string(utils.GetMacAddress()))
+
+	debug.PrintStack()
+
+}
+
+func DumpStacks() string {
+	buf := make([]byte, 16384)
+	buf = buf[:runtime.Stack(buf, true)]
+	return string(buf)
 }
