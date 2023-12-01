740bc9f24a72c8beb065daf35205de23021cb085
Richelieu <richelieu042@gmail.com>
2023-12-01 09:31:12 +0800

    dev

--- src/component/web/push/pushKit/typeListener.go
+++ src/component/web/push/pushKit/typeListener.go
@@ -1,8 +1,22 @@
 package pushKit
 
 import (
+	"github.com/richelieu-yang/chimera/v2/src/core/interfaceKit"
 	"net/http"
 )
+
+// NewListeners
+/*
+PS: 本方法仅供本项目使用，严禁外部调用.
+*/
+func NewListeners(listener Listener, sseFlag bool) (Listeners, error) {
+	if err := interfaceKit.AssertNotNil(listener, "listener"); err != nil {
+		return nil, err
+	}
+
+	inner := &innerListener{}
+	return []Listener{inner, listener}, nil
+}
 
 type Listener interface {
 	OnFailure(w http.ResponseWriter, r *http.Request, failureInfo string)
