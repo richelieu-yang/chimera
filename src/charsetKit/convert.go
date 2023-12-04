0e4ab56260688f2b0f1530277fa63fd977879755
Richelieu <richelieu042@gmail.com>
2023-12-04 17:29:17 +0800

    dev

--- src/charsetKit/convert.go
+++ src/charsetKit/convert.go
@@ -13,3 +13,41 @@ var (
 
 	UTF8To func(dstCharset string, src string) (dst string, err error) = gcharset.UTF8To
 )
+
+//import (
+//	"bytes"
+//	"golang.org/x/text/encoding/simplifiedchinese"
+//	"golang.org/x/text/transform"
+//	"io"
+//)
+//
+//// Utf8ToGbkString UTF-8 => GBK
+//func Utf8ToGbkString(str string) (string, int, error) {
+//	return transform.ToDsnString(simplifiedchinese.GBK.NewEncoder(), str)
+//}
+//
+//// GbkToUtf8String GBK => UTF-8
+//func GbkToUtf8String(str string) (string, int, error) {
+//	return transform.ToDsnString(simplifiedchinese.GBK.NewDecoder(), str)
+//}
+//
+//// Utf8ToGbk UTF-8 => GBK
+//func Utf8ToGbk(s []byte) ([]byte, error) {
+//	reader := transform.NewReader(bytes.NewReader(s), simplifiedchinese.GBK.NewEncoder())
+//	d, e := io.ReadAll(reader)
+//	if e != nil {
+//		return nil, e
+//	}
+//	return d, nil
+//}
+//
+//// GbkToUtf8 GBK => UTF-8
+//func GbkToUtf8(s []byte) ([]byte, error) {
+//	//第二个参数为“transform.Transformer”接口，simplifiedchinese.GBK.NewDecoder()包含了该接口
+//	reader := transform.NewReader(bytes.NewReader(s), simplifiedchinese.GBK.NewDecoder())
+//	d, e := io.ReadAll(reader)
+//	if e != nil {
+//		return nil, e
+//	}
+//	return d, nil
+//}
--- src/charsetKit/encoding.go
+++ src/charsetKit/encoding.go
@@ -8,44 +8,6 @@ import (
 	"unicode/utf8"
 )
 
-//import (
-//	"bytes"
-//	"golang.org/x/text/encoding/simplifiedchinese"
-//	"golang.org/x/text/transform"
-//	"io"
-//)
-//
-//// Utf8ToGbkString UTF-8 => GBK
-//func Utf8ToGbkString(str string) (string, int, error) {
-//	return transform.ToDsnString(simplifiedchinese.GBK.NewEncoder(), str)
-//}
-//
-//// GbkToUtf8String GBK => UTF-8
-//func GbkToUtf8String(str string) (string, int, error) {
-//	return transform.ToDsnString(simplifiedchinese.GBK.NewDecoder(), str)
-//}
-//
-//// Utf8ToGbk UTF-8 => GBK
-//func Utf8ToGbk(s []byte) ([]byte, error) {
-//	reader := transform.NewReader(bytes.NewReader(s), simplifiedchinese.GBK.NewEncoder())
-//	d, e := io.ReadAll(reader)
-//	if e != nil {
-//		return nil, e
-//	}
-//	return d, nil
-//}
-//
-//// GbkToUtf8 GBK => UTF-8
-//func GbkToUtf8(s []byte) ([]byte, error) {
-//	//第二个参数为“transform.Transformer”接口，simplifiedchinese.GBK.NewDecoder()包含了该接口
-//	reader := transform.NewReader(bytes.NewReader(s), simplifiedchinese.GBK.NewDecoder())
-//	d, e := io.ReadAll(reader)
-//	if e != nil {
-//		return nil, e
-//	}
-//	return d, nil
-//}
-
 // DetermineEncoding 编码推断.
 /*
 Deprecated: 不一定准（检测GBK文本有问题，推断出来的是 "windows-1252"）.
