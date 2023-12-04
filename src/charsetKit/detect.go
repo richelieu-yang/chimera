a9c4394cdfb910a9bad379cfea34cf758e5895b5
Richelieu <richelieu042@gmail.com>
2023-12-04 16:24:06 +0800

    dev

--- src/charsetKit/detect.go
+++ src/charsetKit/detect.go
@@ -6,10 +6,13 @@ import (
 	"golang.org/x/text/encoding"
 )
 
-// DetermineEncoding 编码推断
+// DetermineEncoding 编码推断.
+/*
+PS: 检测GBK文本有问题，推断出来的是"windows-1252".
+*/
 var DetermineEncoding func(content []byte, contentType string) (e encoding.Encoding, name string, certain bool) = charset.DetermineEncoding
 
-// Detect 检测文本的编码
+// Detect 检测文本的编码.
 func Detect(data []byte) (charset string, err error) {
 	var r *chardet.Result
 	r, err = chardet.NewTextDetector().DetectBest(data)
