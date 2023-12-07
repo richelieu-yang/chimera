d291442f77e511251ff9ad4cf098efa687cf186f
Richelieu <richelieu042@gmail.com>
2023-12-07 15:21:23 +0800

    dev

--- src/micro/raftKit/raft.go
+++ src/micro/raftKit/raft.go
@@ -2,4 +2,19 @@ package raftKit
 
 import "github.com/hashicorp/raft"
 
-var NewRaft func(conf *raft.Config, fsm raft.FSM, logs raft.LogStore, stable raft.StableStore, snaps raft.SnapshotStore, trans raft.Transport) (*raft.Raft, error) = raft.NewRaft
+var (
+	// NewRaft
+	NewRaft func(conf *raft.Config, fsm raft.FSM, logs raft.LogStore, stable raft.StableStore, snaps raft.SnapshotStore, trans raft.Transport) (*raft.Raft, error) = raft.NewRaft
+
+	// RecoverCluster
+	RecoverCluster func(conf *raft.Config, fsm raft.FSM, logs raft.LogStore, stable raft.StableStore, snaps raft.SnapshotStore, trans raft.Transport, configuration raft.Configuration) error = raft.RecoverCluster
+
+	// BootstrapCluster
+	BootstrapCluster func(conf *raft.Config, logs raft.LogStore, stable raft.StableStore, snaps raft.SnapshotStore, trans raft.Transport, configuration raft.Configuration) error = raft.BootstrapCluster
+
+	// GetConfiguration
+	GetConfiguration func(conf *raft.Config, fsm raft.FSM, logs raft.LogStore, stable raft.StableStore, snaps raft.SnapshotStore, trans raft.Transport) (raft.Configuration, error) = raft.GetConfiguration
+
+	// HasExistingState
+	HasExistingState func(logs raft.LogStore, stable raft.StableStore, snaps raft.SnapshotStore) (bool, error) = raft.HasExistingState
+)
