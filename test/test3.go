540cc4853d8c788ce6c235630fbe7d2b9252e696
Richelieu <richelieu042@gmail.com>
2023-12-07 10:03:58 +0800

    dev

--- test/test3.go
+++ test/test3.go
@@ -1,14 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"github.com/richelieu-yang/chimera/v2/src/core/ioKit"
-)
-
 func main() {
-	rw := ioKit.NewReadWriter(nil)
-	//fmt.Println(rw.WriteString("0\n"))
-	//fmt.Println(rw.WriteString("1\n"))
-	str := rw.String()
-	fmt.Println(str)
+
 }
