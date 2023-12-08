436d9a72f98574b2f3c02aaa2f876d9f31cadc5a
Richelieu <richelieu042@gmail.com>
2023-12-08 10:20:35 +0800

    dev

--- go.mod
+++ go.mod
@@ -88,6 +88,7 @@ require (
 	golang.org/x/exp v0.0.0-20231206192017-f3f8817b8deb
 	golang.org/x/image v0.14.0
 	golang.org/x/net v0.19.0
+	golang.org/x/sync v0.5.0
 	golang.org/x/text v0.14.0
 	google.golang.org/grpc v1.59.0
 	google.golang.org/protobuf v1.31.0
@@ -240,7 +241,6 @@ require (
 	golang.org/x/mobile v0.0.0-20230531173138-3c911d8e3eda // indirect
 	golang.org/x/mod v0.14.0 // indirect
 	golang.org/x/oauth2 v0.15.0 // indirect
-	golang.org/x/sync v0.5.0 // indirect
 	golang.org/x/sys v0.15.0 // indirect
 	golang.org/x/term v0.15.0 // indirect
 	golang.org/x/time v0.5.0 // indirect
--- test/test3.go
+++ test/test3.go
@@ -1,5 +1,46 @@
 package main
 
-func main() {
+import (
+	"context"
+	"github.com/richelieu-yang/chimera/v2/src/log/logrusKit"
+	"github.com/sirupsen/logrus"
+	"golang.org/x/sync/semaphore"
+	"sync"
+	"time"
+)
 
+func init() {
+	logrusKit.MustSetUp(nil)
+}
+
+func main() {
+	concurrency := 3
+	sem := semaphore.NewWeighted(int64(concurrency))
+	var wg sync.WaitGroup
+
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func(id int) {
+			defer wg.Done()
+			if err := worker(id, sem); err != nil {
+				logrus.Errorf("Error: %v\n", err)
+			}
+		}(i)
+	}
+
+	wg.Wait()
+}
+
+func worker(id int, sem *semaphore.Weighted) error {
+	if err := sem.Acquire(context.TODO(), 1); err != nil {
+		return err
+	}
+	defer sem.Release(1)
+
+	// 执行工作任务
+	logrus.Infof("Worker %d: Working...\n", id)
+
+	time.Sleep(time.Second)
+
+	return nil
 }
