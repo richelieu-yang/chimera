d547d045f3a8e285fbe14ae08dcfdce17b0b3205
Richelieu <richelieu042@gmail.com>
2023-12-14 16:46:51 +0800

    dev

--- src/micro/raft/raftKit/store.go
+++ src/micro/raft/raftKit/store.go
@@ -20,11 +20,13 @@ var (
 	PS:
 	(1) hashicorp提供了一个 raft-boltdb 来实现底层存储，它是一个嵌入式的数据库，能够 持久化 存储数据，我们直接用它来实现 LogStore 和 StableStore.
 	(2) 实现了接口: raft.LogStore、raft.StableStore
+
+	@param filePath string 存储路径（建议以".bolt"为文件后缀）
 */
-func NewBoltStore(path string) (*raftboltdb.BoltStore, error) {
-	if err := fileKit.AssertNotExistOrIsFile(path); err != nil {
+func NewBoltStore(filePath string) (*raftboltdb.BoltStore, error) {
+	if err := fileKit.AssertNotExistOrIsFile(filePath); err != nil {
 		return nil, err
 	}
 
-	return raftboltdb.NewBoltStore(path)
+	return raftboltdb.NewBoltStore(filePath)
 }
