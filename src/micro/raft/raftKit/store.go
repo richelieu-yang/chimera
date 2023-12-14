c8c2b5c25809a2ea0f1f9c6b692673d50551951e
Richelieu <richelieu042@gmail.com>
2023-12-14 15:40:36 +0800

    dev

--- src/micro/raft/raftKit/store.go
+++ src/micro/raft/raftKit/store.go
@@ -1,9 +1,23 @@
 package raftKit
 
 import (
+	"github.com/hashicorp/raft"
 	"github.com/hashicorp/raft-boltdb"
 )
 
 var (
+	// NewBoltStore
+	/*
+		PS:
+		(1) hashicorp提供了一个 raft-boltdb 来实现底层存储，它是一个嵌入式的数据库，能够持久化存储数据，我们直接用它来实现 LogStore 和 StableStore.
+		(2) 实现了接口: raft.LogStore、raft.StableStore
+	*/
 	NewBoltStore func(path string) (*raftboltdb.BoltStore, error) = raftboltdb.NewBoltStore
+
+	// NewInmemStore
+	/*
+		PS:
+		(1) 实现了接口: raft.LogStore、raft.StableStore
+	*/
+	NewInmemStore func() *raft.InmemStore = raft.NewInmemStore
 )
---
+++ src/micro/raft/raftKit/storeSnapshot.go
@@ -0,0 +1,33 @@
+package raftKit
+
+import (
+	"github.com/hashicorp/raft"
+	"io"
+)
+
+var (
+	// NewDiscardSnapshotStore
+	/*
+		PS:
+		(1) DiscardSnapshotStore: 不存储，忽略快照，相当于/dev/null（一般用于测试）
+		(2) 实现了接口: raft.SnapshotStore
+
+	*/
+	NewDiscardSnapshotStore func() *raft.DiscardSnapshotStore = raft.NewDiscardSnapshotStore
+
+	// NewFileSnapshotStore
+	/*
+		PS:
+		(1) FileSnapshotStore: 文件持久化存储
+		(2) 实现了接口: raft.SnapshotStore
+	*/
+	NewFileSnapshotStore func(base string, retain int, logOutput io.Writer) (*raft.FileSnapshotStore, error) = raft.NewFileSnapshotStore
+
+	// NewInmemSnapshotStore
+	/*
+		PS:
+		(1) InmemSnapshotStore: 内存存储，不持久化，重启程序会丢失
+		(2) 实现了接口: raft.SnapshotStore
+	*/
+	NewInmemSnapshotStore func() *raft.InmemSnapshotStore = raft.NewInmemSnapshotStore
+)
