0b0f7597aa36ea134abf8193a18988e356fcb4b0
Richelieu <richelieu042@gmail.com>
2023-12-14 16:45:08 +0800

    dev

--- src/micro/raft/raftKit/store.go
+++ src/micro/raft/raftKit/store.go
@@ -3,17 +3,10 @@ package raftKit
 import (
 	"github.com/hashicorp/raft"
 	"github.com/hashicorp/raft-boltdb"
+	"github.com/richelieu-yang/chimera/v2/src/file/fileKit"
 )
 
 var (
-	// NewBoltStore
-	/*
-		PS:
-		(1) hashicorp提供了一个 raft-boltdb 来实现底层存储，它是一个嵌入式的数据库，能够 持久化 存储数据，我们直接用它来实现 LogStore 和 StableStore.
-		(2) 实现了接口: raft.LogStore、raft.StableStore
-	*/
-	NewBoltStore func(path string) (*raftboltdb.BoltStore, error) = raftboltdb.NewBoltStore
-
 	// NewInmemStore
 	/*
 		PS:
@@ -21,3 +14,17 @@ var (
 	*/
 	NewInmemStore func() *raft.InmemStore = raft.NewInmemStore
 )
+
+// NewBoltStore
+/*
+	PS:
+	(1) hashicorp提供了一个 raft-boltdb 来实现底层存储，它是一个嵌入式的数据库，能够 持久化 存储数据，我们直接用它来实现 LogStore 和 StableStore.
+	(2) 实现了接口: raft.LogStore、raft.StableStore
+*/
+func NewBoltStore(path string) (*raftboltdb.BoltStore, error) {
+	if err := fileKit.AssertNotExistOrIsFile(path); err != nil {
+		return nil, err
+	}
+
+	return raftboltdb.NewBoltStore(path)
+}
