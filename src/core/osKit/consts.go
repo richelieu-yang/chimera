6a1933e893354947c807cbb204affe18b258be08
Richelieu <richelieu042@gmail.com>
2023-12-15 13:28:04 +0800

    dev

--- src/core/osKit/consts.go
+++ src/core/osKit/consts.go
@@ -5,15 +5,17 @@ import "os"
 const (
 	// PathSeparator 路径分隔符（string类型）
 	/*
-		e.g. Mac
-		"/"
+		e.g.
+		"/"（Mac）
+		"\"（Windows）
 	*/
 	PathSeparator = string(os.PathSeparator)
 
 	// PathListSeparator 路径列表分隔符（string类型）
 	/*
 		e.g. Mac
-		":"
+		":"（Mac）
+		";"（Windows）
 	*/
 	PathListSeparator = string(os.PathListSeparator)
 )
