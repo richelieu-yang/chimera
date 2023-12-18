0c447c39dae7f6e827101acc59411a7882312b57
Richelieu <richelieu042@gmail.com>
2023-12-18 14:08:15 +0800

    dev

--- src/component/mq/rocketmq5Kit/setup.go
+++ src/component/mq/rocketmq5Kit/setup.go
@@ -1,10 +1,9 @@
 package rocketmq5Kit
 
 import (
-	"github.com/apache/rocketmq-clients/golang/v5/credentials"
 	"github.com/richelieu-yang/chimera/v2/src/core/errorKit"
-	"github.com/richelieu-yang/chimera/v2/src/core/sliceKit"
 	"github.com/richelieu-yang/chimera/v2/src/log/logrusKit"
+	"github.com/richelieu-yang/chimera/v2/src/validateKit"
 	"github.com/sirupsen/logrus"
 )
 
@@ -33,7 +32,7 @@ func SetUp(c *Config, clientLogPath string, verifyConfig *VerifyConfig) (err err
 		}
 	}()
 
-	if err = check(c); err != nil {
+	if err = validateKit.Struct(c); err != nil {
 		return
 	}
 
@@ -50,28 +49,4 @@ func SetUp(c *Config, clientLogPath string, verifyConfig *VerifyConfig) (err err
 	}
 
 	return
-}
-
-func check(c *Config) error {
-	if c == nil {
-		return errorKit.New("c == nil")
-	}
-
-	// Endpoints
-	s := sliceKit.RemoveEmpty(c.Endpoints, true)
-	if err := sliceKit.AssertNotEmpty(s, "c.Endpoints"); err != nil {
-		return err
-	}
-	c.Endpoints = s
-
-	// Credentials
-	if c.Credentials == nil {
-		c.Credentials = &credentials.SessionCredentials{
-			AccessKey:     "",
-			AccessSecret:  "",
-			SecurityToken: "",
-		}
-	}
-
-	return nil
 }
