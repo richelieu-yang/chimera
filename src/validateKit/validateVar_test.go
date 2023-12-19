3b3250ab7602ad9ba6f7b6da903f2b447550e197
Richelieu <richelieu042@gmail.com>
2023-12-19 13:49:26 +0800

    dev

--- src/validateKit/validateVar_test.go
+++ src/validateKit/validateVar_test.go
@@ -6,6 +6,11 @@ import (
 	"time"
 )
 
+func TestVar(t *testing.T) {
+	fmt.Println(Var("", "required"))  // Key: '' Error:Field validation for '' failed on the 'required' tag
+	fmt.Println(Var("$", "required")) // <nil>
+}
+
 func TestVarWithValue(t *testing.T) {
 	// ltcsfield: Less Than Another Relative Field
 	fmt.Println(VarWithValue(time.Hour, time.Hour-time.Minute, "ltcsfield")) // Key: '' Error:Field validation for '' failed on the 'ltcsfield' tag
