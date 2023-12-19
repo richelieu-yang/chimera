f76fed7c3f9c2ac0c9dfafe52554ed1c11a2d96b
Richelieu <richelieu042@gmail.com>
2023-12-19 13:04:38 +0800

    dev

--- src/validateKit/types.go
+++ src/validateKit/types.go
@@ -1,8 +1,9 @@
 package validateKit
 
 type (
-	// Validatable 参考: "github.com/ardielle/ardielle-go/rdl"中的 rdl.Validatable
+	// Validatable Deprecated: 纯粹用 github.com/go-playground/validator/v10 吧.
 	/*
+		参考: "github.com/ardielle/ardielle-go/rdl"中的 rdl.Validatable
 		!!!: 如果结构体实现了 Validatable 接口，方法体内部不能调用 validateKit.Struct，以免发生递归死循环（但可以调用 validateKit.New）.
 	*/
 	Validatable interface {
