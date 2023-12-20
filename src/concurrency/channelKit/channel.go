b47e8aa64823447d9eded4ee29de10fd16ceea8d
Richelieu <richelieu042@gmail.com>
2023-12-20 15:21:33 +0800

    dev

--- src/concurrency/channelKit/channel.go
+++ src/concurrency/channelKit/channel.go
@@ -3,6 +3,9 @@ package channelKit
 import "github.com/duke-git/lancet/v2/concurrency"
 
 // NewChannel 返回一个Channel指针实例
+/*
+TODO: 后续研究下.
+*/
 func NewChannel[T any]() *concurrency.Channel[T] {
 	return concurrency.NewChannel[T]()
 }
