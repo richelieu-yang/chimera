acffbe674fc9548390614141245c208d20359d6e
Richelieu <richelieu042@gmail.com>
2023-12-20 09:31:24 +0800

    dev

---
+++ src/micro/oauth2Kit/_info.md
@@ -0,0 +1,5 @@
+## 参考
+go-oauth2/oauth2(3.1k Star): 
+    https://github.com/go-oauth2/oauth2
+
+
--- src/micro/oauth2Kit/store.go
+++ src/micro/oauth2Kit/store.go
@@ -1,1 +1,8 @@
 package oauth2Kit
+
+import (
+	"github.com/go-oauth2/oauth2/v4"
+	"github.com/go-oauth2/oauth2/v4/store"
+)
+
+var NewMemoryTokenStore func() (oauth2.TokenStore, error) = store.NewMemoryTokenStore
