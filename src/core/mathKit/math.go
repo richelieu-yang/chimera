23b81d9f0b9d9ef2a91d286cbc891f60ebc77bc9
Richelieu <richelieu042@gmail.com>
2023-12-20 16:48:52 +0800

    dev

--- src/core/mathKit/math.go
+++ src/core/mathKit/math.go
@@ -3,6 +3,7 @@ package mathKit
 import (
 	"github.com/duke-git/lancet/v2/mathutil"
 	"github.com/samber/lo"
+	"github.com/shopspring/decimal"
 	"golang.org/x/exp/constraints"
 )
 
@@ -27,6 +28,25 @@ var (
 // Average 计算平均数（可能需要对结果调用RoundToFloat方法四舍五入）
 func Average[T constraints.Integer | constraints.Float](numbers ...T) T {
 	return mathutil.Average[T](numbers...)
+}
+
+// Percent 计算百分比，保留 places 位小数
+/*
+@param places 保留的小数位
+@return (val * 100 / total)
+
+e.g.
+	fmt.Println(mathKit.Percent(1, 3, 3)) // 33.333
+*/
+func Percent(val, total float64, places int32) float64 {
+	//return mathutil.Percent(val, total, n)
+
+	if val == 0 || total == 0 {
+		return 0
+	}
+	d := decimal.NewFromFloat(val).Mul(decimal.NewFromInt32(100)).Div(decimal.NewFromFloat(total))
+	f, _ := d.Round(places).Float64()
+	return f
 }
 
 // Clamp clamps number within the inclusive lower and upper bounds.
--- test/test3/test3.go
+++ test/test3/test3.go
@@ -1,5 +1,10 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/richelieu-yang/chimera/v2/src/core/mathKit"
+)
 
+func main() {
+	fmt.Println(mathKit.Percent(1, 3, 3)) // 33.333
 }
