8d4d4d0b9c232b4a2f4cb98872d3f70f2b4a34ca
Richelieu <richelieu042@gmail.com>
2023-12-21 11:19:48 +0800

    dev

--- test/test3/test3.go
+++ test/test3/test3.go
@@ -1,12 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"github.com/richelieu-yang/chimera/v2/src/core/mathKit"
-)
-
 func main() {
-	fmt.Println(mathKit.Factorial(0)) // 1
-	fmt.Println(mathKit.Factorial(1)) // 1
-	fmt.Println(mathKit.Factorial(5)) // 120（=1*2*3*4*5）
+
 }
