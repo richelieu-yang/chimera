5721e9d13dfd2a91536a255cd8425b69a4c9af42
Richelieu <richelieu042@gmail.com>
2023-12-25 10:03:56 +0800

    dev

--- src/component/database/remote/redis/redisKit/error.go
+++ src/component/database/remote/redis/redisKit/error.go
@@ -2,20 +2,8 @@ package redisKit
 
 import (
 	"github.com/richelieu-yang/chimera/v2/src/core/errorKit"
-	"github.com/richelieu-yang/chimera/v2/src/core/strKit"
 )
 
 var (
 	NotSetupError = errorKit.New("Haven’t been set up correctly")
 )
-
-// IsConsumerGroupNameAlreadyExistError
-/*
-TODO: 涉及 github.com/redis/go-redis/v9 源码， 后续看有没有好的解决方法.
-
-PS: 与 XGroupCreateMkStream 搭配使用.
-*/
-func IsConsumerGroupNameAlreadyExistError(err error) bool {
-	err = errorKit.Cause(err)
-	return strKit.ContainsIgnoreCase(err.Error(), "Consumer Group name already exists")
-}
---
+++ src/component/database/remote/redis/redisKit/streamError.go
@@ -0,0 +1,17 @@
+package redisKit
+
+import (
+	"github.com/richelieu-yang/chimera/v2/src/core/errorKit"
+	"github.com/richelieu-yang/chimera/v2/src/core/strKit"
+)
+
+// IsConsumerGroupNameAlreadyExistError
+/*
+TODO: 涉及 github.com/redis/go-redis/v9 源码， 后续看有没有好的解决方法.
+
+PS: 与 XGroupCreateMkStream 搭配使用.
+*/
+func IsConsumerGroupNameAlreadyExistError(err error) bool {
+	err = errorKit.Cause(err)
+	return strKit.ContainsIgnoreCase(err.Error(), "Consumer Group name already exists")
+}
