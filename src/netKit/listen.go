b47b9c3b4c3323f4bebaf136c7b24869be84e9a3
Richelieu <richelieu042@gmail.com>
2023-12-27 15:20:23 +0800

    dev

--- src/netKit/listen.go
+++ src/netKit/listen.go
@@ -5,11 +5,15 @@ import "net"
 var (
 	// Listen 监听指定的网络地址.
 	/*
-		PS:
-		(1) 网络地址未被占用，返回的error == nil;
-		(2)	网络地址已被占用，返回的error != nil.
+		可能在以下几种情况下返回error：
+		(1) 传参 network 不是有效值;
+		(2) 传参 address 格式不正确;
+		(3) 你试图监听的端口已经被其他进程占用; e.g. listen tcp 127.0.0.1:12345: bind: address already in use
+		(4) 系统资源（如文件描述符）不足;
+		(5) 如果你试图监听的端口号小于1024，而运行程序的用户没有足够的权限（通常需要 root 权限），函数将返回错误;
+		(6) 如果网络出现问题（例如，网络接口不可用），函数将返回错误.
 
-		@param network 网络类型，如: tcp、tcp4、tcp6、unix、unixgram、unixpacket...
+		@param network 网络类型，如: tcp、tcp4、tcp6、unix、unixpacket...
 		@param address 监听的地址，格式为 ip:port，如果不指定 port，将由系统自动分配一个端口
 	*/
 	Listen func(network, address string) (net.Listener, error) = net.Listen
--- src/netKit/listen_test.go
+++ src/netKit/listen_test.go
@@ -1,11 +1,28 @@
 package netKit
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestListen(t *testing.T) {
-	l, err := Listen("tcp", "127.0.0.1:6379")
-	if err != nil {
-		panic(err)
+	addrress := "127.0.0.1:12345"
+
+	{
+		l, err := Listen("tcp", addrress)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer l.Close()
 	}
-	defer l.Close()
+	{
+		l, err := Listen("tcp", addrress)
+		if err != nil {
+			// 你试图监听的端口已经被其他进程占用
+			fmt.Println(err) // listen tcp 127.0.0.1:12345: bind: address already in use
+			return
+		}
+		defer l.Close()
+	}
 }
