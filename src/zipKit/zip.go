c0b39d71ada51361de7659227999f46146ea08b2
Richelieu <richelieu042@gmail.com>
2023-12-28 16:36:10 +0800

    dev

--- src/zipKit/zip.go
+++ src/zipKit/zip.go
@@ -3,11 +3,18 @@ package zipKit
 import "github.com/duke-git/lancet/v2/fileutil"
 
 var (
+	// Zip zip压缩文件.
+	/*
+		@param path 可以是 文件 || 目录
+	*/
 	Zip func(path string, destPath string) error = fileutil.Zip
 
+	// UnZip zip解压缩文件并保存在目录中.
 	UnZip func(zipFile string, destPath string) error = fileutil.UnZip
 
+	// ZipAppendEntry 通过将单个文件或目录追加到现有的zip文件.
 	ZipAppendEntry func(fpath string, destPath string) error = fileutil.ZipAppendEntry
 
+	// IsZipFile 判断文件是否是zip压缩文件.
 	IsZipFile func(filepath string) bool = fileutil.IsZipFile
 )
