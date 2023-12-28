2eae71545d3754764ff76728733098210c708b80
Richelieu <richelieu042@gmail.com>
2023-12-28 10:13:39 +0800

    dev

--- test/test3/test3.go
+++ test/test3/test3.go
@@ -1,10 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"github.com/richelieu-yang/chimera/v2/src/validateKit"
-)
-
 func main() {
-	fmt.Println(validateKit.Var("127.0.0.255", "omitempty,hostname|ipv4"))
+
 }
