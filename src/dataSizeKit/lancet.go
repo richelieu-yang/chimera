36bdb39fdadb06ca5887ca9a4a0b790de41ab23b
Richelieu <richelieu042@gmail.com>
2023-12-29 11:23:55 +0800

    dev

--- src/dataSizeKit/lancet.go
+++ src/dataSizeKit/lancet.go
@@ -10,11 +10,14 @@ var (
 		   PS:
 		   (1) 采用SI标准（国际单位制；https://blog.csdn.net/bioitee/article/details/120797739）.
 		   (2) 1KB == 1000
-		   (3) 采用此标准: Mac的访达...
+		   (3) 采用此标准: Mac的访达（Finder）...
 
 			@param precision 指定小数点后的位数，默认为4
 	*/
 	ToReadableSiString func(size float64, precision ...int) string = formatter.DecimalBytes
+
+	// ParseSiString SI标准(base 1000)
+	ParseSiString func(size string) (uint64, error) = formatter.ParseDecimalBytes
 
 	// ToReadableIecString IEC标准(base 1024)
 	/*
@@ -27,13 +30,6 @@ var (
 	*/
 	ToReadableIecString func(size float64, precision ...int) string = formatter.BinaryBytes
 
-	// ParseSiString SI标准(base 1000)
-	ParseSiString func(size string) (uint64, error) = formatter.ParseDecimalBytes
-
 	// ParseIecString IEC标准(base 1024)
 	ParseIecString func(size string) (uint64, error) = formatter.ParseBinaryBytes
 )
-
-func A() {
-
-}
--- src/dataSizeKit/lancet_test.go
+++ src/dataSizeKit/lancet_test.go
@@ -16,3 +16,15 @@ func TestToReadableIecString(t *testing.T) {
 	fmt.Println(result3) // 1.1774MiB
 	fmt.Println(result4) // 1.18MiB
 }
+
+func TestParseIecString(t *testing.T) {
+	result1, _ := ParseIecString("12")
+	result2, _ := ParseIecString("12ki")
+	result3, _ := ParseIecString("12 KiB")
+	result4, _ := ParseIecString("12.2 kib")
+
+	fmt.Println(result1) // 12
+	fmt.Println(result2) // 12288
+	fmt.Println(result3) // 12288
+	fmt.Println(result4) // 12492
+}
