caf2ec207ab383c0d072f75dd4e80d2f9186d5d9
Richelieu <richelieu042@gmail.com>
2023-12-29 16:05:05 +0800

    dev

--- src/file/fileKit/operationWrite.go
+++ src/file/fileKit/operationWrite.go
@@ -1,6 +1,9 @@
 package fileKit
 
-import "os"
+import (
+	"github.com/duke-git/lancet/v2/fileutil"
+	"os"
+)
 
 // WriteToFile 将数据（字节流）写到文件中.
 /*
@@ -8,7 +11,7 @@ import "os"
 				(1) 不存在的话，会创建一个新的文件;
 				(2) 存在且是个文件的话，会 "覆盖" 掉旧的（并不会加到该文件的最后面）.
 */
-func WriteToFile(data []byte, filePath string, perm os.FileMode) error {
+func WriteToFile(filePath string, data []byte, perm os.FileMode) error {
 	if err := AssertNotExistOrIsFile(filePath); err != nil {
 		return err
 	}
@@ -18,8 +21,12 @@ func WriteToFile(data []byte, filePath string, perm os.FileMode) error {
 
 // WriteStringToFile 将数据（字符串）写到文件中.
 /*
-@param filePath 目标文件的路径（不存在的话，会创建一个新的文件；存在且是个文件的话，会覆盖掉旧的（并不会加到该文件的最后面））
+@param filePath 目标文件的路径（不存在的话，会创建一个新的文件；存在且是个文件的话，根据传参append决定）
 */
-func WriteStringToFile(str, filePath string, perm os.FileMode) error {
-	return WriteToFile([]byte(str), filePath, perm)
+func WriteStringToFile(filePath string, content string, append bool) error {
+	if err := AssertNotExistOrIsFile(filePath); err != nil {
+		return err
+	}
+
+	return fileutil.WriteStringToFile(filePath, content, append)
 }
