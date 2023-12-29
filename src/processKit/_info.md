f37a236150cd411ec79ab8048e8dd84392892cbb
Richelieu <richelieu042@gmail.com>
2023-12-29 15:17:35 +0800

    dev

---
+++ src/processKit/_info.md
@@ -0,0 +1,5 @@
+## GoFrame的 gproc（进程管理）
+进程管理-gproc
+    https://goframe.org/pages/viewpage.action?pageId=1114202
+
+
--- test/test3/test3.go
+++ test/test3/test3.go
@@ -1,11 +1,7 @@
 package main
 
-import "github.com/gogf/gf/v2/os/gtimer"
-
 func main() {
 	//fileutil.Zip("/Users/richelieu/Documents/ino/WebOffice", "a.zip")
 	//zipKit.Zip("b.zip", "/Users/richelieu/Documents/ino/WebOffice")
 
-	gtimer.SetTimeout()
-	gtimer.SetInterval()
 }
