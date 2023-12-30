241c68c28f9055505fcc9e882bc8f8c181a9e1ea
Richelieu <richelieu042@gmail.com>
2023-12-30 14:44:18 +0800

    dev

--- src/micro/rateLimitKit/gin_middleware.go
+++ src/micro/rateLimitKit/gin_middleware.go
@@ -1,1 +1,21 @@
 package rateLimitKit
+
+import (
+	"github.com/gin-gonic/gin"
+	"golang.org/x/time/rate"
+	"net/http"
+)
+
+// NewRateLimitMiddleware Gin的限流器，用于限流.
+func NewRateLimitMiddleware(r rate.Limit, b int) gin.HandlerFunc {
+	limiter := NewLimiter(r, b)
+
+	return func(ctx *gin.Context) {
+		if !limiter.Allow() {
+			ctx.String(http.StatusForbidden, "Exceed rate limit.")
+			ctx.Abort()
+			return
+		}
+		ctx.Next()
+	}
+}
