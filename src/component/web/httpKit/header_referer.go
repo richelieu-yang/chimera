464c35f7b08ba478756a7f5ec781a2affa254ef6
Richelieu <richelieu042@gmail.com>
2024-01-02 16:52:38 +0800

    dev

--- src/component/web/httpKit/header_referer.go
+++ src/component/web/httpKit/header_referer.go
@@ -2,6 +2,12 @@ package httpKit
 
 import "net/http"
 
+// GetReferer
+/*
+PS:
+(1) 涉及 Referer验证;
+(2) 参考: notes/Web（漏洞等）/Web.wps
+*/
 func GetReferer(r *http.Request) string {
 	return r.Referer()
 }
