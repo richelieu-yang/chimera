8ec72269cb7dc4863022ac90dc66b4bc10d9d64d
Richelieu <richelieu042@gmail.com>
2024-01-02 17:15:32 +0800

    dev

--- test/test3/test3.go
+++ test/test3/test3.go
@@ -1,5 +1,10 @@
 package main
 
-func main() {
+import "github.com/richelieu-yang/chimera/v2/src/log/logrusKit"
 
+func main() {
+	logrusKit.MustSetUp(&logrusKit.Config{
+		Level:      "",
+		PrintBasic: true,
+	})
 }
