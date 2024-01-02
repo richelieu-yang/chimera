222b07c6fc40a32ca6ac28fd890202c167f8e091
Richelieu <richelieu042@gmail.com>
2024-01-02 09:37:26 +0800

    dev

--- src/component/web/ginKit/config.go
+++ src/component/web/ginKit/config.go
@@ -27,16 +27,24 @@ type (
 	}
 
 	MiddlewareConfig struct {
-		BodyLimit     int64      `json:"bodyLimit" yaml:"bodyLimit"`
-		Gzip          bool       `json:"gzip" yaml:"gzip"`
-		XFrameOptions string     `json:"xFrameOptions" yaml:"xFrameOptions" validate:"omitempty,lowercase,oneof=deny sameorigin|startswith=allow-from "`
-		Cors          CorsConfig `json:"cors" yaml:"cors"`
+		BodyLimit     int64  `json:"bodyLimit" yaml:"bodyLimit"`
+		Gzip          bool   `json:"gzip" yaml:"gzip"`
+		XFrameOptions string `json:"xFrameOptions" yaml:"xFrameOptions" validate:"omitempty,lowercase,oneof=deny sameorigin|startswith=allow-from "`
+
+		Cors CorsConfig `json:"cors" yaml:"cors"`
 		//Referer       []*refererKit.RefererVerifierBuilder `json:"referer" yaml:"referer"`
+		RateLimiter *RateLimiterConfig
 	}
 
 	// CorsConfig cors（跨源资源共享）的配置
 	CorsConfig struct {
 		Access  bool     `json:"access" yaml:"access"`
 		Origins []string `json:"origins" yaml:"origins" validate:"unique,dive,required"`
+	}
+
+	// RateLimiterConfig 限流器的配置（令牌桶算法）
+	RateLimiterConfig struct {
+		Limit int `json:"limit" yaml:"limit"`
+		Burst int `json:"burst" yaml:"burst"`
 	}
 )
