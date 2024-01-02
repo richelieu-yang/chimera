d9203f72d20d0ceff20efb5c79b1cc8d89f0b392
Richelieu <richelieu042@gmail.com>
2024-01-02 16:11:49 +0800

    dev

--- src/reflectKit/_info.md
+++ src/reflectKit/_info.md
@@ -1,4 +1,5 @@
 ## TODOs
-Go语言反射编程指南
-    https://mp.weixin.qq.com/s/9OjvKLEei9HjmGcsA4fq7w
+详解Go反射并实战一例 https://mp.weixin.qq.com/s/Lfl8fevtGLkvQgFjB2XvIw
+Go语言反射编程指南 https://mp.weixin.qq.com/s/9OjvKLEei9HjmGcsA4fq7w
+
 
--- test/test3/test3.go
+++ test/test3/test3.go
@@ -1,29 +1,5 @@
 package main
 
-import (
-	"context"
-	"fmt"
-	"github.com/richelieu-yang/chimera/v2/src/cmd/cmdKit"
-	"os"
-	"strconv"
-)
-
 func main() {
-	var pid int
-	args := os.Args[1:]
-	if len(args) > 0 {
-		var err error
-		pid, err = strconv.Atoi(args[0])
-		if err != nil {
-			panic(err)
-		}
-	} else {
-		pid = 12345
-	}
-	fmt.Println("pid", pid)
 
-	cmd := fmt.Sprintf(`top -n 1 -b -p %d  | grep %d | awk -F " " '{print $6}'`, pid, pid)
-	fmt.Println("cmd", cmd)
-
-	fmt.Println(cmdKit.ExecuteToString(context.TODO(), "sh", "-c", cmd))
 }
