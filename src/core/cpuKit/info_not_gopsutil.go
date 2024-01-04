51a84f00ad2d62cb53edeeb306bf90d670c63cbc
Richelieu <richelieu042@gmail.com>
2024-01-04 16:39:30 +0800

    dev

--- src/core/cpuKit/info_not_gopsutil.go
+++ src/core/cpuKit/info_not_gopsutil.go
@@ -3,9 +3,36 @@
 package cpuKit
 
 import (
+	"github.com/richelieu-yang/chimera/v2/src/core/errorKit"
 	"github.com/sirupsen/logrus"
+	"os"
 )
 
 func printBasicDetails(logger *logrus.Logger) {
 
+}
+
+// GetUsagePercent CPU使用率
+/*
+PS: 耗时约1s.
+
+e.g.
+() => 12.701612903175233
+*/
+func GetUsagePercent() (float64, error) {
+	return 0, errorKit.New("Not supported on this platform")
+}
+
+// GetCurrentProcessUsagePercent 获取 当前进程 的CPU使用百分比.
+/*
+PS: 类似Linux命令: top -p ${pid}
+*/
+func GetCurrentProcessUsagePercent() (float64, error) {
+	var pid = int32(os.Getpid())
+	return GetProcessUsagePercent(pid)
+}
+
+// GetProcessUsagePercent 获取 指定进程 的CPU使用百分比.
+func GetProcessUsagePercent(pid int32) (float64, error) {
+	return 0, errorKit.New("Not supported on this platform")
 }
