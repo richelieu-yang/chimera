fa6a0b2979388dd61adeb9358946a159b5c1722e
Richelieu <richelieu042@gmail.com>
2024-01-04 13:28:10 +0800

    dev

--- src/crypto/aes/aesCbcPkcs7Kit/entire_test.go
+++ src/crypto/aes/aesCbcPkcs7Kit/entire_test.go
@@ -14,13 +14,13 @@ func TestBase64(t *testing.T) {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(cipherText)
+	fmt.Println(cipherText) // 8IGfmJx9UzhZAbdS2Jqqh5YlwcjISpDdm429JVKnK13/bafYcNNnRux4OolbmKxS
 
 	plainTextData, err := DecryptFromBase64(cipherText, []byte(key), []byte(iv))
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(string(plainTextData))
+	fmt.Println(string(plainTextData)) // test测试~！@#￥%……&*（）——+-=
 
 	if text != string(plainTextData) {
 		panic("not equal")
@@ -36,13 +36,13 @@ func TestHex(t *testing.T) {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(cipherText)
+	fmt.Println(cipherText) // f0819f989c7d53385901b752d89aaa879625c1c8c84a90dd9b8dbd2552a72b5dff6da7d870d36746ec783a895b98ac52
 
 	plainTextData, err := DecryptFromHex(cipherText, []byte(key), []byte(iv))
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(string(plainTextData))
+	fmt.Println(string(plainTextData)) // test测试~！@#￥%……&*（）——+-=
 
 	if text != string(plainTextData) {
 		panic("not equal")
