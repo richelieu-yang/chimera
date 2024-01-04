54bb8591710a97c3a03f2330e633fad8ad5e29e3
Richelieu <richelieu042@gmail.com>
2024-01-04 09:35:10 +0800

    dev

--- src/crypto/hexKit/hex.go
+++ src/crypto/hexKit/hex.go
@@ -2,19 +2,27 @@ package hexKit
 
 import "encoding/hex"
 
+// Encode
+/*
+参考: hex.EncodeToString
+*/
 func Encode(src []byte) []byte {
 	dst := make([]byte, hex.EncodedLen(len(src)))
 	hex.Encode(dst, src)
 	return dst
 }
 
-func EncodeToString(src []byte) string {
-	return hex.EncodeToString(src)
-}
-
+// Decode
+/*
+参考: hex.DecodeString.
+*/
 func Decode(src []byte) ([]byte, error) {
 	n, err := hex.Decode(src, src)
 	return src[:n], err
+}
+
+func EncodeToString(src []byte) string {
+	return hex.EncodeToString(src)
 }
 
 func DecodeString(s string) ([]byte, error) {
--- src/crypto/hexKit/hex_test.go
+++ src/crypto/hexKit/hex_test.go
@@ -9,13 +9,31 @@ func TestEncodeAndDecode(t *testing.T) {
 	text := "test测试~!@#$%^&*()"
 
 	cipherData := Encode([]byte(text))
-	fmt.Println("cipherTest:", string(cipherData))
+	fmt.Println("cipherText:", string(cipherData)) // cipherText: 74657374e6b58be8af957e21402324255e262a2829
 
-	plainData, err := Decode(cipherData)
+	plainData, err := Decode(cipherData) // plainText: test测试~!@#$%^&*()
 	if err != nil {
 		panic(err)
 	}
 	fmt.Println("plainText:", string(plainData))
+
+	if text != string(plainData) {
+		panic("not equal")
+	}
+	fmt.Println("equal")
+}
+
+func TestEncodeToStringAndDecodeToString(t *testing.T) {
+	text := "test测试~!@#$%^&*()"
+
+	cipherText := EncodeToString([]byte(text))
+	fmt.Println("cipherText:", cipherText) // cipherText: 74657374e6b58be8af957e21402324255e262a2829
+
+	plainData, err := DecodeString(cipherText)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("plainText:", string(plainData)) // plainText: test测试~!@#$%^&*()
 
 	if text != string(plainData) {
 		panic("not equal")
