8e0197abe6403b8d0a95f2365630a9901ba8c84f
Richelieu <richelieu042@gmail.com>
2024-01-05 13:43:26 +0800

    dev

--- src/component/web/proxyKit/proxy.go
+++ src/component/web/proxyKit/proxy.go
@@ -8,9 +8,10 @@ import (
 /*
 PS: 转发请求前如果想变更请求头(Request Header)，可以在调用此函数前设置请求头.
 
-@param w		e.g.ctx.Writer
-@param r 		e.g.ctx.Request
-@param options 	可以不传
+@param w			e.g.ctx.Writer
+@param r 			e.g.ctx.Request
+@param targetAddr	e.g."127.0.0.1:12345"
+@param options 		可以不传
 */
 func Proxy(w http.ResponseWriter, r *http.Request, targetAddr string, options ...ProxyOption) error {
 	opts := loadOptions(options...)
