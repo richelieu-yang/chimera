ee4373c454e2df366f07b783c02733eaf1019052
Richelieu <richelieu042@gmail.com>
2024-01-05 13:54:33 +0800

    dev

--- src/log/logrusKit/config.go
+++ src/log/logrusKit/config.go
@@ -4,13 +4,14 @@ type (
 	Config struct {
 		Level string `json:"level" yaml:"level"`
 
-		PrintBasic bool `json:"printBasic" yaml:"printBasic"`
-
 		// DisableQuote
 		/*
 			true: 	禁用 日志内容的双引号.
 			false: 	允许 日志内容的双引号.
 		*/
 		DisableQuote bool `json:"disableQuote" yaml:"disableQuote"`
+
+		// PrintBasic 是否打印基本信息？
+		PrintBasic bool `json:"printBasic" yaml:"printBasic"`
 	}
 )
