c01e65fe055a8002a73289f7051f37d046d8b322
Richelieu <richelieu042@gmail.com>
2024-01-05 16:25:27 +0800

    dev

--- test/test3/test3.go
+++ test/test3/test3.go
@@ -1,14 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"github.com/richelieu-yang/chimera/v2/src/ocr/gosseractKit"
-)
-
 func main() {
-	text, err := gosseractKit.GertText("")
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(text)
+
 }
