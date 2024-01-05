0a2221d2382f1142e023ed93e4f9be7ceadff9c9
Richelieu <richelieu042@gmail.com>
2024-01-05 14:27:43 +0800

    dev

---
+++ src/log/logrusInitKit/init.go
@@ -0,0 +1,14 @@
+package logrusInitKit
+
+import "github.com/richelieu-yang/chimera/v2/src/log/logrusKit"
+
+/*
+初始化 logrus 相关的配置.
+
+PS:
+(1) 可以在 main() 所在的.go文件中，先通过 _ 导入此包，进行初始化配置；
+(2) 后续（比如读取完配置文件）可以重新调用 logrusKit.MustSetUp().
+*/
+func init() {
+	logrusKit.MustSetUp(nil)
+}
