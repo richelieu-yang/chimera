ce6325e97ce3d806da3d2c19bd01cf595d0b41d6
Richelieu <richelieu042@gmail.com>
2024-01-11 14:32:41 +0800

    dev

--- src/component/web/ginKit/setup_option.go
+++ src/component/web/ginKit/setup_option.go
@@ -4,7 +4,13 @@ import "github.com/gin-gonic/gin"
 
 type (
 	ginOptions struct {
-		// ServiceInfo 当前服务的信息（可以为""） (1) e.g."Agent-127.0.0.1:12345" (2) 涉及: 限流器
+		// ServiceInfo 当前服务的信息（可以为""）
+		/*
+			e.g."Agent-127.0.0.1:12345"
+
+			涉及: 	(a) 限流器
+					(b) RespondError、RespondPackage、RespondPackageOrError
+		*/
 		ServiceInfo string
 
 		// RecoveryMiddleware panic恢复中间件（可以为nil，将采用默认值: gin.Recovery()）
