63b8f3bdef8c7d3ea29786ce99a663f396b40103
Richelieu <richelieu042@gmail.com>
2024-01-12 13:31:27 +0800

    dev

--- src/compress/zstdKit/zstd.go
+++ src/compress/zstdKit/zstd.go
@@ -24,19 +24,3 @@ func Decompress(compressed []byte) ([]byte, error) {
 
 	return reader.DecodeAll(compressed, nil)
 }
-
-//func CompressWriter(writer io.Writer) ([]byte, error) {
-//	// 创建一个编码器
-//	enc, err := zstd.NewWriter(w)
-//	if err != nil {
-//		// 处理错误
-//	}
-//	defer enc.Close()
-//
-//	// 从一个 io.Reader 中读取数据并压缩到编码器
-//	_, err = io.Copy(enc, r)
-//	if err != nil {
-//		// 处理错误
-//	}
-//
-//}
