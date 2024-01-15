2d59b8b9b35b2ba99d92bda684c5cf39694a2ddf
Richelieu <richelieu042@gmail.com>
2024-01-15 14:13:44 +0800

    dev

--- test/test3/test3.go
+++ test/test3/test3.go
@@ -1,15 +1,5 @@
 package main
 
-import (
-	"encoding/base64"
-	"fmt"
-	"unsafe"
-)
-
 func main() {
-	enc := base64.StdEncoding
-	fmt.Println(unsafe.Pointer(enc))
-	enc1 := enc.WithPadding(base64.NoPadding)
-	fmt.Println(unsafe.Pointer(enc1))
 
 }
