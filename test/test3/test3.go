0fabc7347fa925def9f98b44adac441e91763848
Richelieu <richelieu042@gmail.com>
2024-01-15 08:51:19 +0800

    dev

--- test/test3/test3.go
+++ test/test3/test3.go
@@ -1,27 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"github.com/richelieu-yang/chimera/v2/src/config/confKit"
-	"github.com/richelieu-yang/chimera/v2/src/serialize/json/jsonKit"
-)
-
 func main() {
-	confKit.LoadFromJsonText()
 
-	m := map[interface{}]interface{}{
-		"0": 3.1415926,
-	}
-
-	data, err := jsonKit.Marshal(m)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(string(data))
-
-	m1 := map[interface{}]interface{}{}
-	if err := jsonKit.Unmarshal(data, &m1); err != nil {
-		panic(err) // panic: unsupported map key type: interface {}
-	}
-	fmt.Println(m1)
 }
