509a4c16d9abb2f4425ef8f1bc060bca4b5a0cc3
Richelieu <richelieu042@gmail.com>
2024-01-16 10:51:50 +0800

    dev

--- src/component/database/remote/sql/gormKit/logger_test.go
+++ src/component/database/remote/sql/gormKit/logger_test.go
@@ -3,28 +3,21 @@ package gormKit
 import (
 	"context"
 	"gorm.io/gorm/logger"
-	"log"
 	"os"
 	"testing"
 	"time"
 )
 
 func TestNewLogger(t *testing.T) {
-	writer := log.New(os.Stdout, "\r\n", log.Ldate|log.Ltime|log.Lmicroseconds)
-
-	// 参考: logger.Default
-	l := logger.New(writer, logger.Config{
-		// 慢SQL阈值
-		SlowThreshold: 200 * time.Millisecond,
-		// 日志级别
-		LogLevel: logger.Info,
-		// 忽略 logger.ErrRecordNotFound（记录未找到错误） ？
+	l := NewLogger(&LogConfig{
+		Output:                    os.Stdout,
+		SlowThreshold:             200 * time.Millisecond,
+		LogLevel:                  logger.Info,
+		Colorful:                  true,
 		IgnoreRecordNotFoundError: false,
-		// 彩色打印？
-		Colorful: true,
 	})
 
-	//l.Trace(context.TODO(), "")
+	//l.Trace(context.Background(),)
 	l.Info(context.TODO(), "info")
 	l.Warn(context.TODO(), "warn")
 	l.Error(context.TODO(), "error")
--- test/test3/test3.go
+++ test/test3/test3.go
@@ -1,31 +1,5 @@
 package main
 
-import (
-	"context"
-	"gorm.io/gorm/logger"
-	"log"
-	"os"
-	"time"
-)
-
 func main() {
-	//writer := log.New(os.Stdout, "\r\n", log.Ldate|log.Ltime|log.Lmicroseconds)
-	writer := log.New(os.Stdout, "", log.LstdFlags|log.Lmicroseconds)
 
-	// 参考: logger.Default
-	l := logger.New(writer, logger.Config{
-		// 慢SQL阈值
-		SlowThreshold: 200 * time.Millisecond,
-		// 日志级别
-		LogLevel: logger.Info,
-		// 忽略 logger.ErrRecordNotFound（记录未找到错误） ？
-		IgnoreRecordNotFoundError: false,
-		// 彩色打印？
-		Colorful: true,
-	})
-
-	//l.Trace(context.TODO(), "")
-	l.Info(context.TODO(), "info")
-	l.Warn(context.TODO(), "warn")
-	l.Error(context.TODO(), "error")
 }
