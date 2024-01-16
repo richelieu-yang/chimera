ee0a77f7b5df7f2bb1b318f00d443c5708e3e9f4
Richelieu <richelieu042@gmail.com>
2024-01-16 11:00:54 +0800

    dev

--- src/core/signalKit/monitor.go
+++ src/core/signalKit/monitor.go
@@ -1,13 +1,9 @@
 package signalKit
 
 import (
-	"github.com/sirupsen/logrus"
 	"os"
 	"os/signal"
-	"sync"
 )
-
-var monitorOnce sync.Once
 
 // MonitorExitSignal 监听退出信号（拦截关闭信号）.
 /*
@@ -18,14 +14,14 @@ PS:
 (2) 可以通过 logrus.RegisterExitHandler() 在程序退出前"毁尸灭迹"（在里面你甚至可以 time.Sleep）;
 (3) 此函数对 主动调用os.Exit() 无效.
 */
-func MonitorExitSignal() {
-	monitorOnce.Do(func() {
-		ch := make(chan os.Signal, 1)
-		signal.Notify(ch, exitSignals...)
+func MonitorExitSignal(callback func(sig os.Signal)) {
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, exitSignals...)
 
-		go func() {
-			sig := <-ch
-			logrus.WithField("signal", sig.String()).Fatal("Service receives an exit signal.")
-		}()
-	})
+	go func() {
+		sig := <-ch
+
+		callback(sig)
+		//logrus.WithField("signal", sig.String()).Fatal("Service receives an exit signal.")
+	}()
 }
