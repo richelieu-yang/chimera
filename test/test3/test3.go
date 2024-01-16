a1cc2ab293bd0167e598937c4e6510c9f77890fd
Richelieu <richelieu042@gmail.com>
2024-01-16 14:13:27 +0800

    dev

--- test/test3/test3.go
+++ test/test3/test3.go
@@ -1,22 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"github.com/richelieu-yang/chimera/v2/src/core/signalKit"
 	_ "github.com/richelieu-yang/chimera/v2/src/log/logrusInitKit"
-	"github.com/richelieu-yang/chimera/v2/src/processKit"
-	"os"
 )
 
 func main() {
-	signalKit.MonitorExitSignals(func(sig os.Signal) {
-		fmt.Println("0", sig.String())
-	})
-	signalKit.MonitorExitSignals(func(sig os.Signal) {
-		fmt.Println("1", sig.String())
-	})
 
-	fmt.Println("pid:", processKit.PID)
-
-	select {}
 }
