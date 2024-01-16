380e25ff181c5a33417a69fc3a1303dee2f95c6b
Richelieu <richelieu042@gmail.com>
2024-01-16 13:40:47 +0800

    dev

--- test/test3/test3.go
+++ test/test3/test3.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/richelieu-yang/chimera/v2/src/core/signalKit"
+	_ "github.com/richelieu-yang/chimera/v2/src/log/logrusInitKit"
 	"github.com/richelieu-yang/chimera/v2/src/processKit"
 	"os"
 )
