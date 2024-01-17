8047a3cf6f1145dd29a9c7d9eca68926780957f8
Richelieu <richelieu042@gmail.com>
2024-01-17 13:15:30 +0800

    dev

--- test/test3/test3.go
+++ test/test3/test3.go
@@ -1,13 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"github.com/richelieu-yang/chimera/v2/src/core/sliceKit"
 	_ "github.com/richelieu-yang/chimera/v2/src/log/logrusInitKit"
 )
 
 func main() {
-	var s []int = nil
-	fmt.Println(sliceKit.InterceptAfter(s, 0)) // [1]
-	fmt.Println(sliceKit.InterceptAfter(s, 1)) // []
+
 }
