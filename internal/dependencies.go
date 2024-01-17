d4f328b7e8e4a871e7a73970af3df7b08211d110
Richelieu <richelieu042@gmail.com>
2024-01-17 13:19:56 +0800

    dev

--- internal/dependencies.go
+++ internal/dependencies.go
@@ -19,7 +19,7 @@ func init() {
 		TODO: 看后续 github.com/apache/pulsar-client-go v0.11.1 会不会更新.
 		Richelieu: 为了引用最新的 dvsekhvalnov/jose2go 库（v1.6.0），v1.5.0有脆弱性漏洞，
 	*/
-	_ = jose.A128CBC_HS256
+	_ = jose.DEF
 
 	{
 		// golang.org/x/arch
