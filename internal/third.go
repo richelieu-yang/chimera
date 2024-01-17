ef69e2a7ee2d63e00a75124e40dda78029ffb116
Richelieu <richelieu042@gmail.com>
2024-01-17 15:42:30 +0800

    dev

--- internal/third.go
+++ internal/third.go
@@ -5,12 +5,16 @@ import (
 	"golang.org/x/arch/x86/x86asm"
 	"golang.org/x/crypto/cast5"
 	"golang.org/x/exp/slog"
+	"golang.org/x/image/tiff"
 	"golang.org/x/mobile/bind"
 	"golang.org/x/mod/modfile"
+	"golang.org/x/net/proxy"
 	"golang.org/x/oauth2/jws"
 	"golang.org/x/sync/errgroup"
 	"golang.org/x/sys/execabs"
 	"golang.org/x/term"
+	"golang.org/x/text/currency"
+	"golang.org/x/time/rate"
 	"golang.org/x/tools/blog"
 )
 
@@ -22,25 +26,19 @@ func init() {
 	_ = jose.DEF
 
 	{
-		// golang.org/x/arch
 		var _ x86asm.Arg
-		// golang.org/x/crypto
 		var _ *cast5.Cipher
-		// golang.org/x/mobile
 		var _ *bind.Generator
-		// golang.org/x/mod
 		var _ *modfile.Comment
-		// golang.org/x/oauth2
 		var _ jws.Signer
-		// golang.org/x/sync
 		var _ *errgroup.Group
-		// golang.org/x/sys
 		var _ *execabs.Error
-		// golang.org/x/term
 		var _ *term.Terminal
-		// golang.org/x/tools
 		var _ *blog.Doc
-		// golang.org/x/exp
 		var _ slog.Handler
+		var _ *tiff.Options
+		var _ proxy.Dialer
+		var _ *currency.Amount
+		var _ *rate.Limit
 	}
 }
