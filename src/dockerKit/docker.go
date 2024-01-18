d83b6651fbe536f5db9726fe805494bff1487332
Richelieu <richelieu042@gmail.com>
2024-01-18 11:11:32 +0800

    dev

--- src/dockerKit/docker.go
+++ src/dockerKit/docker.go
@@ -1,8 +1,4 @@
 package dockerKit
 
-import (
-	"github.com/shirou/gopsutil/v3/docker"
-)
-
-// GetDockerIdList returns a list of DockerID.
-var GetDockerIdList func() ([]string, error) = docker.GetDockerIDList
+//// GetDockerIdList returns a list of DockerID.
+//var GetDockerIdList func() ([]string, error) = docker.GetDockerIDList
