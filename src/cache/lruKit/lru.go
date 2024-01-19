f368f7ffcd15039c937e9e2302376f795a8dd9ee
Richelieu <richelieu042@gmail.com>
2024-01-19 13:22:33 +0800

    dev

--- src/cache/lruKit/lru.go
+++ src/cache/lruKit/lru.go
@@ -12,6 +12,11 @@ func NewCache[K comparable, V any](size int) (*lru.Cache[K, V], error) {
 }
 
 // NewExpirableCache 缓存（带TTL）.
+/*
+@param size (1) 缓存大小（max keys）
+			(2) 0: unlimited size
+@param ttl	Providing 0 TTL turns expiring off.
+*/
 func NewExpirableCache[K comparable, V any](size int, onEvict expirable.EvictCallback[K, V], ttl time.Duration) *expirable.LRU[K, V] {
 	return expirable.NewLRU[K, V](size, onEvict, ttl)
 }
