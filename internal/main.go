9b714cf2024ea56004ea898d0cfa731de8035ec0
Richelieu <richelieu042@gmail.com>
2024-01-19 16:56:32 +0800

    dev

--- internal/main.go
+++ internal/main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"github.com/gin-contrib/gzip"
 	"github.com/gin-gonic/gin"
 	_ "github.com/richelieu-yang/chimera/v2/src/log/logrusInitKit"
 )
 
 func main() {
 	engine := gin.Default()
+
+	engine.Use(gzip.Gzip(gzip.BestSpeed))
 
 	engine.Any("/", func(ctx *gin.Context) {
 		ctx.String(200, "Don't communicate by sharing memory, share memory by communicating.Don't communicate by sharing memory, share memory by communicating.Don't communicate by sharing memory, share memory by communicating.")
--- src/component/web/ginKit/middleware_gzip.go
+++ src/component/web/ginKit/middleware_gzip.go
@@ -5,6 +5,11 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// NewFastGzipMiddleware
+/*
+PS: 涉及多个服务（请求转发）的场景下，(1) 最外层的务使用gzip压缩;
+								(2) 内层的服务不使用gzip压缩.
+*/
 func NewFastGzipMiddleware() gin.HandlerFunc {
 	return gzip.Gzip(gzip.BestSpeed)
 }
