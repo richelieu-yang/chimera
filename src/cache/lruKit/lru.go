83b43d29c4c51e7125943159524be51c099b5e62
Richelieu <richelieu042@gmail.com>
2024-01-19 13:16:26 +0800

    dev

---
+++ src/cache/lruKit/lru.go
@@ -0,0 +1,17 @@
+package lruKit
+
+import (
+	lru "github.com/hashicorp/golang-lru/v2"
+	"github.com/hashicorp/golang-lru/v2/expirable"
+	"time"
+)
+
+// NewCache 缓存（不带TTL）.
+func NewCache[K comparable, V any](size int) (*lru.Cache[K, V], error) {
+	return lru.New[K, V](size)
+}
+
+// NewExpirableCache 缓存（带TTL）.
+func NewExpirableCache[K comparable, V any](size int, onEvict expirable.EvictCallback[K, V], ttl time.Duration) *expirable.LRU[K, V] {
+	return expirable.NewLRU[K, V](size, onEvict, ttl)
+}
