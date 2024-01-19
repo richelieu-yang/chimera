a82b92f3c4f11d5c1fdaa0f94794795447dd270e
Richelieu <richelieu042@gmail.com>
2024-01-19 13:37:41 +0800

    dev

--- src/cache/lruKit/lru_test.go
+++ src/cache/lruKit/lru_test.go
@@ -3,6 +3,7 @@ package lruKit
 import (
 	"fmt"
 	"testing"
+	"time"
 )
 
 func TestNewCache(t *testing.T) {
@@ -11,4 +12,32 @@ func TestNewCache(t *testing.T) {
 		l.Add(i, nil)
 	}
 	fmt.Println(l.Len()) // 8
+}
+
+func TestNewExpirableCache(t *testing.T) {
+	// make cache with 10ms TTL and 5 max keys
+	cache := NewExpirableCache[string, string](5, nil, time.Millisecond*10)
+
+	// set value under key1.
+	cache.Add("key1", "val1")
+
+	// get value under key1
+	r, ok := cache.Get("key1")
+
+	// check for OK value
+	if ok {
+		fmt.Printf("value before expiration is found: %v, value: %q, length: %d\n", ok, r, cache.Len())
+	}
+
+	// wait for cache to expire
+	time.Sleep(time.Millisecond * 12)
+
+	// get value under key1 after key expiration
+	r, ok = cache.Get("key1")
+	fmt.Printf("value after expiration is found: %v, value: %q, length: %d\n", ok, r, cache.Len())
+
+	// set value under key2, would evict old entry because it is already expired.
+	cache.Add("key2", "val2")
+
+	fmt.Printf("Cache len: %d\n", cache.Len())
 }
