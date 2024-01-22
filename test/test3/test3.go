34e08a7bd616b7390c07bdb1054c29b9a6e7af39
Richelieu <richelieu042@gmail.com>
2024-01-22 15:21:53 +0800

    dev

--- test/test3/test3.go
+++ test/test3/test3.go
@@ -1,43 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"github.com/gin-gonic/gin"
-	"github.com/richelieu-yang/chimera/v2/src/component/web/ginKit"
-	"net/http"
-)
-
 func main() {
-	engine := gin.Default()
 
-	// 设置NoRoute处理器
-	engine.NoRoute(func(ctx *gin.Context) {
-		ctx.JSON(404, gin.H{"code": "PAGE_NOT_FOUND", "message": "页面不存在"})
-	})
-
-	// 设置NoMethod处理器
-	engine.HandleMethodNotAllowed = true
-	engine.NoMethod(func(ctx *gin.Context) {
-		var allowed []string
-		route := ctx.Request.URL.Path
-		routeInfoSlice := engine.Routes()
-		for _, routeInfo := range routeInfoSlice {
-			if routeInfo.Path == route {
-				allowed = append(allowed, routeInfo.Method)
-				ginKit.AddResponseHeader(ctx, "Allow", routeInfo.Method)
-			}
-		}
-
-		text := fmt.Sprintf("Method(%s) isn't allowed for route(%s) and allowed methods is %s.", ctx.Request.Method, ctx.Request.URL.Path, allowed)
-		ctx.String(http.StatusMethodNotAllowed, text)
-	})
-
-	// 其他路由和方法
-	engine.POST("/ping", func(ctx *gin.Context) {
-		ctx.String(200, "pong")
-	})
-
-	if err := engine.Run(":80"); err != nil {
-		panic(err)
-	}
 }
