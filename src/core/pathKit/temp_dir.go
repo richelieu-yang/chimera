ef5a3d4bc5f041754de35069b0ad250e2e9a75e4
Richelieu <richelieu042@gmail.com>
2024-01-23 15:26:58 +0800

    dev

--- src/core/pathKit/temp_dir.go
+++ src/core/pathKit/temp_dir.go
@@ -4,38 +4,28 @@ import (
 	"github.com/richelieu-yang/chimera/v2/src/consts"
 	"github.com/richelieu-yang/chimera/v2/src/core/strKit"
 	"github.com/richelieu-yang/chimera/v2/src/file/fileKit"
+	"github.com/richelieu-yang/chimera/v2/src/idKit"
 	"os"
 )
 
 var tempDir string
 
-// GetTempDir 获取系统的临时目录.
+// GetOsTempDir 获取系统的临时目录.
 /*
-Deprecated: 不建议向系统临时目录中放东西（服务器不一定会给权限）.
+PS: 不建议向系统临时目录中放东西（服务器不一定会给权限）.
 
 e.g.
 	Windows	"C:\Users\Lenovo\AppData\Local\Temp"
 	Mac		"/var/folders/4_/33p_vn057msfh2nvgx6hwv_40000gn/T/"
 */
-var GetTempDir func() string = os.TempDir
+var GetOsTempDir func() string = os.TempDir
 
-// GetExclusiveTempDir 获取 本依赖 的专属临时目录.
-/*
-	PS: 不建议向系统临时目录中放东西（服务器不一定会给权限）.
-
-	e.g. Mac
-		() => "/var/folders/4_/33p_vn057msfh2nvgx6hwv_40000gn/T/$$chimera", nil
-*/
-func GetExclusiveTempDir() (string, error) {
+// GetTempDir 获取 项目 的临时目录.
+func GetTempDir() string {
 	if strKit.IsNotEmpty(tempDir) {
-		return tempDir, nil
+		return tempDir
 	}
-
-	dir := Join(GetTempDir(), "$$"+consts.ProjectName)
-	if err := fileKit.MkDirs(dir); err != nil {
-		return "", err
-	}
-	return dir, nil
+	return GetOsTempDir()
 }
 
 func SetTempDir(dirPath string) error {
@@ -45,4 +35,19 @@ func SetTempDir(dirPath string) error {
 
 	tempDir = dirPath
 	return nil
+}
+
+// GetExclusiveTempDir 获取 本依赖 的专属临时目录.
+/*
+	PS: 不建议向系统临时目录中放东西（服务器不一定会给权限）.
+
+	e.g. macOS
+	() => "/var/folders/4_/33p_vn057msfh2nvgx6hwv_40000gn/T/$$chimera/cmnmk1pus0n0snnatg40 <nil>", nil
+*/
+func GetExclusiveTempDir() (string, error) {
+	dir := Join(GetTempDir(), "$$"+consts.ProjectName, idKit.NewXid())
+	if err := fileKit.MkDirs(dir); err != nil {
+		return "", err
+	}
+	return dir, nil
 }
--- test/test3/test3.go
+++ test/test3/test3.go
@@ -1,5 +1,10 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/richelieu-yang/chimera/v2/src/core/pathKit"
+)
 
+func main() {
+	fmt.Println(pathKit.GetExclusiveTempDir())
 }
