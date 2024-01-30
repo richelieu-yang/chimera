8c8bdf9075af266f24430f441a287226560b89d4
Richelieu <richelieu042@gmail.com>
2024-01-30 13:11:42 +0800

    dev

--- src/component/web/ginKit/engine.go
+++ src/component/web/ginKit/engine.go
@@ -4,15 +4,11 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// NewEngine
-/*
-@param recovery 可以为nil，将采用默认值 gin.Recovery()
-*/
 func NewEngine() *gin.Engine {
 	engine := gin.New()
 
 	// 默认: []string{"X-Forwarded-For", "X-Real-IP"}
-	engine.RemoteIPHeaders = []string{"X-Real-IP", "Client-IP", "X-Forwarded-For"}
+	engine.RemoteIPHeaders = []string{"X-Forwarded-For", "X-Real-IP", "Client-IP"}
 
 	// 默认: true
 	engine.RedirectTrailingSlash = true
