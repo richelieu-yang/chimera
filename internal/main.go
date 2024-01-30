047b55d89b6d46e903a9d593c152ecfdfefbd8d9
Richelieu <richelieu042@gmail.com>
2024-01-30 09:16:01 +0800

    dev

--- internal/main.go
+++ internal/main.go
@@ -3,9 +3,7 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/richelieu-yang/chimera/v2/src/component/web/ginKit"
-	"github.com/richelieu-yang/chimera/v2/src/core/ioKit"
 	_ "github.com/richelieu-yang/chimera/v2/src/log/logrusInitKit"
-	"github.com/sirupsen/logrus"
 )
 
 func main() {
@@ -13,14 +11,8 @@ func main() {
 
 	engine.Use(ginKit.NewCorsMiddleware(nil))
 
-	engine.POST("/test", func(ctx *gin.Context) {
-		data, _ := ioKit.ReadFromReader(ctx.Request.Body)
-		logrus.Infof("data: [%s]", data)
-
+	engine.Any("/test", func(ctx *gin.Context) {
 		ctx.String(200, "hello world")
-	})
-	engine.NoRoute(func(ctx *gin.Context) {
-		ctx.String(404, "no route")
 	})
 
 	if err := engine.Run(":80"); err != nil {
--- src/component/web/ginKit/setup_test.go
+++ src/component/web/ginKit/setup_test.go
@@ -2,10 +2,11 @@ package ginKit
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/richelieu-yang/chimera/v2/src/component/web/proxyKit"
+	"github.com/richelieu-yang/chimera/v2/src/component/web/cookieKit"
 	"github.com/richelieu-yang/chimera/v2/src/config/viperKit"
 	"github.com/richelieu-yang/chimera/v2/src/consts"
 	"github.com/richelieu-yang/chimera/v2/src/core/pathKit"
+	"github.com/richelieu-yang/chimera/v2/src/idKit"
 	"github.com/richelieu-yang/chimera/v2/src/log/logrusKit"
 	"github.com/sirupsen/logrus"
 	"net/http"
@@ -46,12 +47,17 @@ func TestMustSetUp(t *testing.T) {
 		//})
 
 		BindHandlersToRoute(engine, "/*path", []string{http.MethodGet, http.MethodPost}, func(ctx *gin.Context) {
-			if err := proxyKit.Proxy(ctx.Writer, ctx.Request, "127.0.0.1:8888", proxyKit.WithPolyfillHeaders(true)); err != nil {
-				ctx.String(500, err.Error())
-				return
-			}
-			// 转发成功
-			return
+			//if err := proxyKit.Proxy(ctx.Writer, ctx.Request, "127.0.0.1:8888", proxyKit.WithPolyfillHeaders(true)); err != nil {
+			//	ctx.String(500, err.Error())
+			//	return
+			//}
+			//// 转发成功
+			//return
+
+			c := cookieKit.NewCookie("test", idKit.NewXid(), cookieKit.WithSecure(true), cookieKit.WithSameSite(http.SameSiteNoneMode))
+			//c := cookieKit.NewCookie("test", "2")
+			cookieKit.SetCookie(ctx.Writer, c)
+			ctx.String(200, "ok")
 
 			//keys := []string{
 			//	"Host",
