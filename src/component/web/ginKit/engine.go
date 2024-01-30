24359f04f858f1f4815f6028d7c6522e51e5134f
Richelieu <richelieu042@gmail.com>
2024-01-30 11:24:43 +0800

    dev

--- src/component/web/ginKit/engine.go
+++ src/component/web/ginKit/engine.go
@@ -11,9 +11,10 @@ import (
 func NewEngine() *gin.Engine {
 	engine := gin.New()
 
-	// 默认为: []string{"X-Forwarded-For", "X-Real-IP"}
+	// 默认: []string{"X-Forwarded-For", "X-Real-IP"}
 	engine.RemoteIPHeaders = []string{"X-Real-IP", "Client-IP", "X-Forwarded-For"}
 
+	// 默认: true
 	engine.RedirectTrailingSlash = true
 
 	return engine
