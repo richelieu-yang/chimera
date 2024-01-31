a2b096f0f011a8190d0734282fe5babebf3a935a
Richelieu <richelieu042@gmail.com>
2024-01-31 15:32:59 +0800

    dev

--- test/test3/test3.go
+++ test/test3/test3.go
@@ -1,15 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"github.com/richelieu-yang/chimera/v2/src/core/mathKit"
-	"math"
-	"net"
-)
-
 func main() {
-	net.ParseIP()
 
-	fmt.Println(mathKit.Round(math.NaN(), 2))
-	fmt.Println(mathKit.Round(-math.NaN(), 2))
 }
