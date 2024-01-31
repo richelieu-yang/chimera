51778e02434db5facc486aec01dab536f6e566e3
Richelieu <richelieu042@gmail.com>
2024-01-31 13:21:16 +0800

    dev

--- src/atomic/atomicKit/atomic_test.go
+++ src/atomic/atomicKit/atomic_test.go
@@ -13,9 +13,14 @@ func TestNewInt32(t *testing.T) {
 	fmt.Println(i.Load()) // 2
 	fmt.Println("======")
 
+	/* CompareAndSwap */
 	fmt.Println(i.CompareAndSwap(2, 3)) // true
 	fmt.Println(i.Load())               // 3
-
 	fmt.Println(i.CompareAndSwap(2, 3)) // false
 	fmt.Println(i.Load())               // 3
+	fmt.Println("======")
+
+	/* Store */
+	i.Store(666)
+	fmt.Println(i.Load()) // 666
 }
