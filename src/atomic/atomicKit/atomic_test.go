b1b04daa6a9b3752cdb12d42923cde32887855cf
Richelieu <richelieu042@gmail.com>
2024-01-31 13:50:44 +0800

    dev

--- src/atomic/atomicKit/atomic_test.go
+++ src/atomic/atomicKit/atomic_test.go
@@ -8,8 +8,8 @@ import (
 func TestNewInt32(t *testing.T) {
 	i := NewInt32(0)
 
-	i.Add(1)
-	i.Add(1)
+	fmt.Println(i.Add(1)) // 1
+	fmt.Println(i.Add(1)) // 2
 	fmt.Println(i.Load()) // 2
 	fmt.Println("======")
 
--- src/component/web/sessionKit/redis_store_test.go
+++ src/component/web/sessionKit/redis_store_test.go
@@ -5,7 +5,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/gorilla/sessions"
 	"github.com/redis/go-redis/v9"
-	"github.com/richelieu-yang/chimera/v2/src/atomic/gtypeKit"
+	"github.com/richelieu-yang/chimera/v2/src/atomic/atomicKit"
 	"github.com/richelieu-yang/chimera/v2/src/core/intKit"
 	"github.com/richelieu-yang/chimera/v2/src/idKit"
 	"github.com/richelieu-yang/chimera/v2/src/time/timeKit"
@@ -15,7 +15,7 @@ import (
 	"testing"
 )
 
-var i = gtypeKit.NewInt()
+var i = atomicKit.NewInt64(0)
 
 // TestRedisStore
 /*
