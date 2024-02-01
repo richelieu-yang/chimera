789d30cb2b43fe091cd019240070410658c5b273
Richelieu <richelieu042@gmail.com>
2024-02-01 16:46:51 +0800

    dev

--- src/component/tracing/otelKit/tracer_provider_grpc.go
+++ src/component/tracing/otelKit/tracer_provider_grpc.go
@@ -3,28 +3,28 @@ package otelKit
 import (
 	"context"
 	"github.com/richelieu-yang/chimera/v2/src/core/strKit"
-	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
 	"go.opentelemetry.io/otel/sdk/trace"
 )
 
-// NewHttpTracerProvider 使用otlptracehttp.
+// NewGrpcTracerProvider 使用otlptracegrpc.
 /*
 PS: 此方法第二个返回值为nil的情况下，建议立即调用 otel.SetTracerProvider.
 
-@param endpoint 	可以为""，将采用默认值: "localhost:4318"
+@param endpoint 	可以为""，将采用默认值: "localhost:4317"
 @param serviceName 	服务名
 @param attributeMap	可以为nil
-@param opts 		额外配置（不要涉及 otlptracehttp.WithEndpoint，因为在此处配了也没用）
+@param opts 		额外配置（不要涉及 otlptracegrpc.WithEndpoint，因为在此处配了也没用）
 */
-func NewHttpTracerProvider(endpoint, serviceName string, attributeMap map[string]string, opts ...otlptracehttp.Option) (*trace.TracerProvider, error) {
+func NewGrpcTracerProvider(endpoint, serviceName string, attributeMap map[string]string, opts ...otlptracegrpc.Option) (*trace.TracerProvider, error) {
 	// 默认: 使用非安全协议（http）
-	opts = append([]otlptracehttp.Option{otlptracehttp.WithInsecure()}, opts...)
+	opts = append([]otlptracegrpc.Option{otlptracegrpc.WithInsecure()}, opts...)
 	// 放在最后面（优先级最高）
 	if strKit.IsNotEmpty(endpoint) {
-		opts = append(opts, otlptracehttp.WithEndpoint(endpoint))
+		opts = append(opts, otlptracegrpc.WithEndpoint(endpoint))
 	}
 	// 创建 exporter 实例
-	exporter, err := otlptracehttp.New(context.TODO(), opts...)
+	exporter, err := otlptracegrpc.New(context.TODO(), opts...)
 	if err != nil {
 		return nil, err
 	}
