b5d502763a0b4b9aa10dcaa32c8c14059bae5e94
Richelieu <richelieu042@gmail.com>
2024-02-01 16:43:51 +0800

    dev

--- src/component/tracing/otelKit/tracer_provider_http.go
+++ src/component/tracing/otelKit/tracer_provider_http.go
@@ -2,6 +2,7 @@ package otelKit
 
 import (
 	"context"
+	"github.com/richelieu-yang/chimera/v2/src/core/strKit"
 	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp"
 	"go.opentelemetry.io/otel/sdk/trace"
 )
@@ -10,16 +11,20 @@ import (
 /*
 PS: 此方法第二个返回值为nil的情况下，建议立即调用 otel.SetTracerProvider.
 
-@param endpoint 	e.g."localhost:4318"
+@param endpoint 	(1) 可以为""，将采用默认值: "localhost:4318"
+					(2) e.g."localhost:4318"
 @param serviceName 	服务名
-@param attributeMap	可以为nil？
-@param opts 		额外配置（不建议涉及 otlptracehttp.WithEndpoint，因为在此处配了也没用）
+@param attributeMap	可以为nil
+@param opts 		额外配置（不要涉及 otlptracehttp.WithEndpoint，因为在此处配了也没用）
 */
 func NewHttpTracerProvider(endpoint, serviceName string, attributeMap map[string]string, opts ...otlptracehttp.Option) (*trace.TracerProvider, error) {
 	// 默认: 使用非安全协议（http）
 	opts = append([]otlptracehttp.Option{otlptracehttp.WithInsecure()}, opts...)
 	// 放在最后面（优先级最高）
-	opts = append(opts, otlptracehttp.WithEndpoint(endpoint))
+	if strKit.IsNotEmpty(endpoint) {
+		opts = append(opts, otlptracehttp.WithEndpoint(endpoint))
+	}
+	// 创建 exporter 实例
 	exporter, err := otlptracehttp.New(context.TODO(), opts...)
 	if err != nil {
 		return nil, err
