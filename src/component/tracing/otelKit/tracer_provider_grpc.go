70ac8bd754569a4900959873f2bef290e81d831e
Richelieu <richelieu042@gmail.com>
2024-02-05 10:11:11 +0800

    dev

--- src/component/tracing/otelKit/tracer_provider_grpc.go
+++ src/component/tracing/otelKit/tracer_provider_grpc.go
@@ -38,11 +38,29 @@ func NewGrpcTracerProvider(endpoint, serviceName string, attributeMap map[string
 	}
 
 	tp := trace.NewTracerProvider(
-		// Always be sure to batch in production.
+		/*
+			Always be sure to batch in production.
+
+			trace.WithBatcher(): 	是一个配置选项，用于设置 Span 的导出方式。
+									当使用 trace.WithBatcher() 时，Span 是通过通道异步按批发送的。
+									默认情况下，每隔一段时间（默认批处理5s超时触发）发送，或者当达到最大批处理长度（默认512）时，会把要批处理的 Span 发送出去。
+		*/
 		trace.WithBatcher(exporter),
-		// Record information about this application in a Resource.
+		/*
+			Record information about this application in a Resource.
+
+			trace.WithResource(): 	是一个配置选项，用于设置 TracerProvider 的资源信息。
+									资源是一组描述 TracerProvider 的属性，例如服务名称、环境、地理位置等。这些属性可以帮助你更好地理解和过滤跟踪数据1。
+		*/
 		trace.WithResource(res),
+		/*
+			trace.WithSampler():	是一个配置选项，用于设置采样器的行为，采样器决定了哪些跟踪信息应该被记录和导出.
+			trace.AlwaysSample():	创建了一个采样器，它会选择所有的跟踪进行采样。
+									这意味着每一个创建的 Span 都会被记录和可能被导出。这对于调试非常有用，因为你可以看到所有的跟踪信息。
+									然而，在生产环境中，这可能会产生大量的数据，因此可能需要使用更复杂的采样策略。
+		*/
 		trace.WithSampler(trace.AlwaysSample()),
 	)
+
 	return tp, nil
 }
