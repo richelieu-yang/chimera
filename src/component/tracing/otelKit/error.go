d8d67f5b1b1453e848d9fdd98b3281493d25f917
Richelieu <richelieu042@gmail.com>
2024-02-05 11:26:11 +0800

    dev

--- src/component/tracing/otelKit/error.go
+++ src/component/tracing/otelKit/error.go
@@ -4,4 +4,6 @@ import "github.com/richelieu-yang/chimera/v2/src/core/errorKit"
 
 var (
 	NotOtelRequestError = errorKit.New("not otel request")
+
+	NotSetupError = errorKit.New("Haven’t been set up correctly")
 )
