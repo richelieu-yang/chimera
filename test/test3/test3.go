9b34461353785e93ed32f9945468e2e38532e13f
Richelieu <richelieu042@gmail.com>
2024-02-07 16:30:50 +0800

    dev

--- test/test3/test3.go
+++ test/test3/test3.go
@@ -1,19 +1,13 @@
 package main
 
-import (
-	"fmt"
-	"go.opentelemetry.io/otel"
-	"go.opentelemetry.io/otel/trace"
-)
-
 func main() {
-	/* (1) 初始获取到的值不为nil */
-	tp := otel.GetTracerProvider()
-	fmt.Println(tp)        // &{<nil> {0 0} map[] <nil>}
-	fmt.Println(tp != nil) // true
-
-	/* (2) 设置为nil后，再次获取得到nil */
-	var tp1 trace.TracerProvider = nil
-	otel.SetTracerProvider(tp1)
-	fmt.Println(otel.GetTracerProvider() == nil) // true
+	///* (1) 初始获取到的值不为nil */
+	//tp := otel.GetTracerProvider()
+	//fmt.Println(tp)        // &{<nil> {0 0} map[] <nil>}
+	//fmt.Println(tp != nil) // true
+	//
+	///* (2) 设置为nil后，再次获取得到nil */
+	//var tp1 trace.TracerProvider = nil
+	//otel.SetTracerProvider(tp1)
+	//fmt.Println(otel.GetTracerProvider() == nil) // true
 }
