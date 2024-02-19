2ad27878d4bbc947a6466149e3adf12f4f83ce8a
Richelieu <richelieu042@gmail.com>
2024-02-19 15:18:39 +0800

    dev

---
+++ src/component/database/remote/redis/redisKit/hyper_log_log.go
@@ -0,0 +1,18 @@
+package redisKit
+
+import "context"
+
+func (client *Client) PFAdd(ctx context.Context, key string, els ...interface{}) (int64, error) {
+	cmd := client.universalClient.PFAdd(ctx, key, els...)
+	return cmd.Result()
+}
+
+func (client *Client) PFCount(ctx context.Context, keys ...string) (int64, error) {
+	cmd := client.universalClient.PFCount(ctx, keys...)
+	return cmd.Result()
+}
+
+func (client *Client) PFMerge(ctx context.Context, dest string, keys ...string) (string, error) {
+	cmd := client.universalClient.PFMerge(ctx, dest, keys...)
+	return cmd.Result()
+}
