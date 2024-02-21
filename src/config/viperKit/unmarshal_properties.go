72f6140f09ec7f09c2eddf4351ccc50cc9601c68
Richelieu <richelieu042@gmail.com>
2024-02-21 15:34:54 +0800

    dev

--- src/config/viperKit/unmarshal_properties.go
+++ src/config/viperKit/unmarshal_properties.go
@@ -1,6 +1,6 @@
 package viperKit
 
-func UnmarshalProperties(content []byte, configType string, defaultMap map[string]interface{}, ptr interface{}) error {
+func UnmarshalProperties(content []byte, ptr interface{}) error {
 	_, err := Unmarshal(content, "properties", nil, ptr)
 	return err
 }
