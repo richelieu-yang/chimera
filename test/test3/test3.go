20ee578849b290de872fa7d18d4bfcfa00b94d81
Richelieu <richelieu042@gmail.com>
2024-02-21 14:38:48 +0800

    dev

--- test/test3/test3.go
+++ test/test3/test3.go
@@ -2,17 +2,25 @@ package main
 
 import (
 	"fmt"
-	"github.com/richelieu-yang/chimera/v3/src/versionKit"
+	"github.com/nicksnyder/go-i18n/v2/i18n"
+	"golang.org/x/text/language"
 )
 
 func main() {
-	v, err := versionKit.NewVersion("1.0.0-alpha+001")
-	if err != nil {
-		panic(err)
-	}
-	/*
-		v.pre == "alpha"
-		v.metadata == "001"
-	*/
-	fmt.Println(v)
+	// 初始化翻译包，通常会从文件加载资源
+	bundle := i18n.NewBundle(language.English)
+	bundle.MustParseMessageFileBytes([]byte(`hello = Hello, world!`), "en-US")
+	bundle.MustParseMessageFileBytes([]byte(`hello = Bonjour le monde!`), "fr-FR")
+
+	// 创建一个本地化器（localizer），用于获取指定语言的翻译
+	localizer := i18n.NewLocalizer(bundle, "")
+
+	// 设置用户偏好语言
+	//tag, _ := language.Parse("fr-FR") // 假设用户选择法语
+
+	// 获取翻译并打印
+	msg := localizer.MustLocalize(&i18n.LocalizeConfig{
+		MessageID: "hello",
+	})
+	fmt.Println(msg) // 输出：Bonjour le monde!
 }
