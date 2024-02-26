42a2b2d64b06cc2fd7041632a7f6f308456e2e41
Richelieu <richelieu042@gmail.com>
2024-02-26 15:02:50 +0800

    dev

--- src/i18nKit/setup_test.go
+++ src/i18nKit/setup_test.go
@@ -13,4 +13,6 @@ func TestMustSetUp(t *testing.T) {
 	fmt.Println(GetMessage("0", ""))   // no error <nil>
 	fmt.Println(GetMessage("0", "zh")) // 无错误 <nil>
 	fmt.Println(GetMessage("0", "en")) // no error <nil>
+
+	fmt.Println(GetMessage("1", "en", "zh")) //  message "1" not found in language "en"
 }
