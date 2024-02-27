3d970891308f57cd1af8701bf430b141c1ea2013
Richelieu <richelieu042@gmail.com>
2024-02-27 11:24:54 +0800

    dev

---
+++ src/i18n/i18nPackKit/setup_test.go
@@ -0,0 +1,15 @@
+package i18nPackKit
+
+import (
+	"fmt"
+	"golang.org/x/text/language"
+	"testing"
+)
+
+func TestMustSetUp(t *testing.T) {
+	MustSetUp(language.English, []string{"_test.en.properties", "_test.zh.properties"}, nil)
+
+	fmt.Println(Seal(nil, "0", nil))            // {"code":"0","message":"no error"} <nil>
+	fmt.Println(Seal([]string{"en"}, "0", nil)) // {"code":"0","message":"no error"} <nil>
+	fmt.Println(Seal([]string{"zh"}, "0", nil)) // {"code":"0","message":"无错误"} <nil>
+}
