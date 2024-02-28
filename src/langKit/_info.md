342e6a722ad26a76e14b78aa87300254f8021aeb
Richelieu <richelieu042@gmail.com>
2024-02-28 16:39:08 +0800

    dev

--- src/langKit/_info.md
+++ src/langKit/_info.md
@@ -1,5 +1,10 @@
 ## 参考
 - [Golang结合OpenCC实现高效的中文简体繁体互转](https://pylist.com/topic/191.html)
 
+## opencc
+#### 安装命令
+brew install opencc 
 
+#### json文件
+![_jsons.png](_jsons.png)
 
--- test/test3.go
+++ test/test3.go
@@ -5,15 +5,12 @@ import (
 	"github.com/stevenyao/go-opencc"
 )
 
-const (
-	input      = "中国鼠标软件打印机后台湾，参数错误: %s"
-	config_s2t = "s2t.json"
-)
-
 func main() {
-	fmt.Println("Test Converter class:")
-	c := opencc.NewConverter(config_s2t)
-	defer c.Close()
-	output := c.Convert(input)
-	fmt.Println(output)
+	path := "./s2t.json"
+
+	converter := opencc.NewConverter(path)
+	defer converter.Close()
+
+	output := converter.Convert("中国鼠标软件打印机后台湾，参数错误: %s")
+	fmt.Println("繁体: ", output)
 }
