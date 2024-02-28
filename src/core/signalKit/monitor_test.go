478f90757c4880c60ff543dbdf39f781f91d65bb
Richelieu <richelieu042@gmail.com>
2024-02-28 16:42:28 +0800

    dev

--- src/core/signalKit/monitor_test.go
+++ src/core/signalKit/monitor_test.go
@@ -3,16 +3,20 @@ package signalKit
 import (
 	"fmt"
 	"github.com/richelieu-yang/chimera/v3/src/processKit"
+	"github.com/sirupsen/logrus"
 	"os"
 	"testing"
+	"time"
 )
 
 func TestMonitorExitSignals(t *testing.T) {
 	MonitorExitSignals(func(sig os.Signal) {
-		fmt.Println("0", sig.String())
+		logrus.Info("0", sig.String())
+		time.Sleep(time.Second * 3)
 	})
 	MonitorExitSignals(func(sig os.Signal) {
-		fmt.Println("1", sig.String())
+		logrus.Info("1", sig.String())
+		time.Sleep(time.Second * 3)
 	})
 
 	fmt.Println(processKit.PID)
