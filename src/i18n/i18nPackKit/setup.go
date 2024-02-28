b87156a2ac381756220dfca568b009452270f2dd
Richelieu <richelieu042@gmail.com>
2024-02-28 09:53:12 +0800

    dev

--- src/i18n/i18nPackKit/setup.go
+++ src/i18n/i18nPackKit/setup.go
@@ -5,8 +5,6 @@ import (
 	"github.com/nicksnyder/go-i18n/v2/i18n"
 	"github.com/richelieu-yang/chimera/v3/src/core/errorKit"
 	"github.com/richelieu-yang/chimera/v3/src/i18n/i18nKit"
-	"github.com/richelieu-yang/chimera/v3/src/log/logrusKit"
-	"github.com/sirupsen/logrus"
 	"golang.org/x/text/language"
 )
 
@@ -27,24 +25,9 @@ var (
 	}
 )
 
-func MustSetUp(defaultLanguage language.Tag, messageFilePaths []string, maker BeanMaker) {
-	if err := SetUp(defaultLanguage, messageFilePaths, maker); err != nil {
-		logrusKit.DisableQuote(nil)
-		logrus.Fatalf("%+v", err)
-	}
-}
-
-func SetUp(defaultLanguage language.Tag, messageFilePaths []string, maker BeanMaker) (err error) {
-	defer func() {
-		if err != nil {
-			innerBundle = nil
-			innerBeanMaker = nil
-		}
-	}()
-
-	innerBundle, err = i18nKit.NewBundle(defaultLanguage)
+func SetUp(defaultLanguage language.Tag, maker BeanMaker) {
+	innerBundle = i18nKit.NewBundle(defaultLanguage)
 	innerBeanMaker = maker
-	return
 }
 
 func getMaker() BeanMaker {
--- src/i18n/i18nPackKit/setup_test.go
+++ src/i18n/i18nPackKit/setup_test.go
@@ -7,12 +7,36 @@ import (
 )
 
 func TestMustSetUp(t *testing.T) {
-	MustSetUp(language.Chinese, []string{
-		"_test.en.properties",
-		"_test.zh.properties",
-	}, nil)
+	SetUp(language.English, nil)
 
-	fmt.Println(Seal(nil, "0", nil))            // {"code":"0","message":"无错误"} <nil>
-	fmt.Println(Seal([]string{"en"}, "0", nil)) // {"code":"0","message":"no error"} <nil>
-	fmt.Println(Seal([]string{"zh"}, "0", nil)) // {"code":"0","message":"无错误"} <nil>
+	/* 英文 */
+	_, err := LoadMessageFile("_test.en.properties")
+	if err != nil {
+		panic(err)
+	}
+	/* 中文（简体） */
+	{
+		mf, err := LoadMessageFile("_test.zh-Hans.properties")
+		if err != nil {
+			panic(err)
+		}
+		if err := Associate(mf, "zh", "zh-CN"); err != nil {
+			panic(err)
+		}
+	}
+	/* 中文（繁体） */
+	{
+		mf, err := LoadMessageFile("_test.zh-Hant.properties")
+		if err != nil {
+			panic(err)
+		}
+		if err := Associate(mf, "zh-HK", "zh-TW", "zh-MO", "zh-SG"); err != nil {
+			panic(err)
+		}
+	}
+
+	fmt.Println(Seal(nil, "0", nil))               // {"code":"0","message":"No error."} <nil>
+	fmt.Println(Seal([]string{"en"}, "0", nil))    // {"code":"0","message":"No error."} <nil>
+	fmt.Println(Seal([]string{"zh"}, "0", nil))    // {"code":"0","message":"无错误。"} <nil>
+	fmt.Println(Seal([]string{"zh-SG"}, "0", nil)) // {"code":"0","message":"無錯誤。"} <nil>
 }
