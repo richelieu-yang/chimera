e7a5653ceb67edcbc89a2b50203436e19a5e5a44
Richelieu <richelieu042@gmail.com>
2024-02-29 09:55:55 +0800

    dev

--- src/core/ioKit/nopCloser.go
+++ src/core/ioKit/nopCloser.go
@@ -16,35 +16,3 @@ func NopCloserToReadSeeker(readSeeker io.ReadSeeker) io.ReadSeekCloser {
 		readSeeker: readSeeker,
 	}
 }
-
-//type nopCloserToWriter struct {
-//	io.Writer
-//}
-//
-//func (nopCloserToWriter) Close() error {
-//	return nil
-//}
-//
-//type nopCloserToReadSeeker struct {
-//	io.ReadSeeker
-//}
-//
-//func (nopCloserToReadSeeker) Close() error {
-//	return nil
-//}
-//
-//// NopCloserToWriter
-///*
-//PS: 返回值调用Close()将什么都不会做，直接返回nil.
-//*/
-//func NopCloserToWriter(writer io.Writer) io.WriteCloser {
-//	return &nopCloserToWriter{
-//		writer,
-//	}
-//}
-//
-//func NopCloserToReadSeeker(readSeeker io.ReadSeeker) io.ReadSeekCloser {
-//	return &nopCloserToReadSeeker{
-//		ReadSeeker: readSeeker,
-//	}
-//}
