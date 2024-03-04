46aa4bed7bc4b6617db1185c5d723d763b1574a5
Richelieu <richelieu042@gmail.com>
2024-03-04 09:56:06 +0800

    dev

--- test/test3.go
+++ test/test3.go
@@ -1,18 +1,9 @@
 package main
 
 import (
-	"fmt"
 	_ "github.com/richelieu-yang/chimera/v3/src/log/logrusInitKit"
-	"os/user"
 )
 
 func main() {
-	u, err := user.Current()
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(u.Uid)
-	fmt.Println(u.Gid)
-	fmt.Println(u.Name)
-	fmt.Println(u.Username)
+
 }
