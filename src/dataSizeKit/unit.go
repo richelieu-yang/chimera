1c1d466e3a631a2ea5f34e652b479d0091e62067
Richelieu <richelieu042@gmail.com>
2024-03-08 16:19:05 +0800

    dev

--- src/dataSizeKit/unit.go
+++ src/dataSizeKit/unit.go
@@ -5,15 +5,23 @@ import (
 )
 
 const (
-	B   uint64 = 1
+	B uint64 = 1
+
 	KiB uint64 = 1 << 10
+
 	MiB uint64 = 1 << 20
+
 	GiB uint64 = 1 << 30
+
 	TiB uint64 = 1 << 40
+
 	PiB uint64 = 1 << 50
+
 	EiB uint64 = 1 << 60
-	//ZB  uint64 = 1 << 70
-	//YB  uint64 = 1 << 80
+
+	//ZiB  uint64 = 1 << 70
+
+	//YiB  uint64 = 1 << 80
 )
 
 // ByteToKiB
