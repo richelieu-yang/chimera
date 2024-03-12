9fbbbe731092f8f844395f236b062d4824878fa8
Richelieu <richelieu042@gmail.com>
2024-03-12 09:55:58 +0800

    dev

--- test/test3.go
+++ test/test3.go
@@ -1,9 +1,11 @@
 package main
 
-import (
-	_ "github.com/richelieu-yang/chimera/v3/src/log/logrusInitKit"
-)
+import "fmt"
 
 func main() {
-	postgresql
+	// 支持遍历整数
+	for i := range 3 {
+		fmt.Println(i)
+	}
+	fmt.Println("go1.22 has lift-off!")
 }
