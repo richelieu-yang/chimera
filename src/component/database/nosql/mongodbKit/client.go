eacf506eefd9040b4e88fb9fbfb9a3ac0a1aa15e
Richelieu <richelieu042@gmail.com>
2024-03-12 11:25:29 +0800

    dev

--- src/component/database/nosql/mongodbKit/client.go
+++ src/component/database/nosql/mongodbKit/client.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+	"time"
 )
 
 // NewClient
@@ -11,5 +13,18 @@ import (
 @param uri e.g."mongodb://localhost:27017"
 */
 func NewClient(ctx context.Context, uri string) (*mongo.Client, error) {
-	return mongo.Connect(ctx, options.Client().ApplyURI(uri))
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(uri))
+	if err != nil {
+		return nil, err
+	}
+
+	/* ping */
+	ctx1, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
+	if err := client.Ping(ctx1, readpref.Primary()); err != nil {
+		_ = client.Disconnect(context.TODO())
+		return nil, err
+	}
+
+	return client, nil
 }
