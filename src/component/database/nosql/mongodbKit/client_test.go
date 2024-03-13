868a8bd231b951988ea5af5dc7c6e19b266f8bfb
Richelieu <richelieu042@gmail.com>
2024-03-13 10:01:59 +0800

    dev

---
+++ src/component/database/nosql/mongodbKit/client_test.go
@@ -0,0 +1,17 @@
+package mongodbKit
+
+import (
+	"context"
+	"fmt"
+	"testing"
+)
+
+func TestNewClientSimply(t *testing.T) {
+	client, err := NewClientSimply(context.TODO(), "mongodb://localhost:27017")
+	if err != nil {
+		panic(err)
+	}
+	defer client.Disconnect(nil)
+
+	fmt.Print("ok")
+}
