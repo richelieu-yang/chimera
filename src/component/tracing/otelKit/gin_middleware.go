a81173169794cb4c520f0b62fd9e4636add9289d
Richelieu <richelieu042@gmail.com>
2024-03-16 13:19:45 +0800

    dev

--- src/component/tracing/otelKit/gin_middleware.go
+++ src/component/tracing/otelKit/gin_middleware.go
@@ -7,11 +7,8 @@ import "github.com/gin-gonic/gin"
 PS: 需要先 SetUp || MustSetUp !!!
 */
 func NewGinMiddleware() (gin.HandlerFunc, error) {
-	if err := check(); err != nil {
-		return nil, err
-	}
-
 	return func(ctx *gin.Context) {
+		// TODO:
 
 	}, nil
 }
--- test/test3.go
+++ test/test3.go
@@ -1,40 +1,13 @@
 package main
 
 import (
-	"context"
-	"github.com/richelieu-yang/chimera/v3/src/component/database/nosql/mongodbKit"
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/mongo/options"
+	"fmt"
+	"go.opentelemetry.io/otel"
 )
 
-type User struct {
-	Name string `bson:"name"`
-	Age  int    `bson:"age"`
-}
-
 func main() {
-	client, err := mongodbKit.NewClientSimply(context.TODO(), "mongodb://localhost:27017")
-	if err != nil {
-		panic(err)
-	}
-	defer client.Disconnect(nil)
+	tp := otel.GetTracerProvider()
 
-	db := client.Database("db_a")
-	collection := db.Collection("collection_b")
-	{
-		filter := bson.D{
-			{Key: "name", Value: "Richelieu"},
-		}
-		update := bson.D{
-			{Key: "$inc", Value: bson.D{
-				{Key: "age", Value: 1},
-			}},
-		}
-		opts := options.Update().SetComment("Test comment.")
-
-		_, err := collection.UpdateOne(context.TODO(), filter, update, opts)
-		if err != nil {
-			panic(err)
-		}
-	}
+	fmt.Println(tp != nil)        // true
+	fmt.Printf("%T %v\n", tp, tp) // *global.tracerProvider &{<nil> {0 0} map[] <nil>}
 }
