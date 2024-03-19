9f6c73fae5627ed264c8b9fd13530b54e14443f9
Richelieu <richelieu042@gmail.com>
2024-03-19 09:37:23 +0800

    dev

--- src/component/consulKit/client.go
+++ src/component/consulKit/client.go
@@ -1,1 +1,19 @@
 package consulKit
+
+import (
+	"github.com/hashicorp/consul/api"
+	"github.com/richelieu-yang/chimera/v3/src/core/interfaceKit"
+)
+
+// NewClient
+/*
+@param config 	(1) 不能为 nil;
+				(2) Address 默认为: "127.0.0.1:8500"，Scheme 默认为: "http";
+				(3) 一般情况下，配置下 Address 即可.
+*/
+func NewClient(config *api.Config) (*api.Client, error) {
+	if err := interfaceKit.AssertNotNil(config, "config"); err != nil {
+		return nil, err
+	}
+	return api.NewClient(config)
+}
--- src/component/consulKit/setup.go
+++ src/component/consulKit/setup.go
@@ -1,12 +1,1 @@
 package consulKit
-
-import (
-	"github.com/hashicorp/consul/api"
-)
-
-func NewClient() (*api.Client, error) {
-	cfg := api.DefaultConfig()
-	cfg.Address = ""
-
-	return api.NewClient(cfg)
-}
