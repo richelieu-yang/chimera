3c1abd73299e2f6d12c1c0a275dd59533d30f2d2
Richelieu <richelieu042@gmail.com>
2024-03-19 11:09:05 +0800

    dev

---
+++ src/component/consulKit/_info.md
@@ -0,0 +1,6 @@
+## 参考
+- notes/micro（微服务）/注册中心/Consul/Consul.wps
+- notes/micro（微服务）/注册中心/Consul/Consul - Golang.wps
+- [Gin框架接入Consul实现服务注册与发现](https://mp.weixin.qq.com/s/XywGt0qLm71jE_L4TAJRaQ)
+
+
--- src/component/consulKit/setup.go
+++ src/component/consulKit/setup.go
@@ -2,8 +2,13 @@ package consulKit
 
 import (
 	"github.com/hashicorp/consul/api"
+	"github.com/richelieu-yang/chimera/v3/src/core/errorKit"
 	"github.com/richelieu-yang/chimera/v3/src/log/logrusKit"
 	"github.com/sirupsen/logrus"
+)
+
+var (
+	NotSetupError = errorKit.New("Haven’t been set up correctly")
 )
 
 var innerClient *api.Client
@@ -18,4 +23,27 @@ func MustSetUp(config *api.Config) {
 func SetUp(config *api.Config) (err error) {
 	innerClient, err = NewClient(config)
 	return
+}
+
+// Register 服务注册.
+func Register(service *api.AgentServiceRegistration) error {
+	if innerClient == nil {
+		return NotSetupError
+	}
+
+	return innerClient.Agent().ServiceRegister(service)
+}
+
+// RegisterOpts 服务注册.
+func RegisterOpts(service *api.AgentServiceRegistration, opts api.ServiceRegisterOpts) error {
+	if innerClient == nil {
+		return NotSetupError
+	}
+
+	return innerClient.Agent().ServiceRegisterOpts(service, opts)
+}
+
+// Discover 服务发现.
+func Discover() {
+
 }
