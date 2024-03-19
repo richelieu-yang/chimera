e00163dc1899c461e0e7ec657f3d414462ae54a0
Richelieu <richelieu042@gmail.com>
2024-03-19 10:09:45 +0800

    dev

--- src/component/consulKit/setup.go
+++ src/component/consulKit/setup.go
@@ -1,1 +1,21 @@
 package consulKit
+
+import (
+	"github.com/hashicorp/consul/api"
+	"github.com/richelieu-yang/chimera/v3/src/log/logrusKit"
+	"github.com/sirupsen/logrus"
+)
+
+var innerClient *api.Client
+
+func MustSetUp(config *api.Config) {
+	if err := SetUp(config); err != nil {
+		logrusKit.DisableQuote(nil)
+		logrus.Fatalf("%+v", err)
+	}
+}
+
+func SetUp(config *api.Config) (err error) {
+	innerClient, err = NewClient(config)
+	return
+}
