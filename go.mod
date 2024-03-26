c5b9b61d0074e141368dcd366ab14a3a92be0d2a
Richelieu <richelieu042@gmail.com>
2024-03-26 09:02:42 +0800

    dev

--- go.mod
+++ go.mod
@@ -107,7 +107,7 @@ require (
 	go.uber.org/zap v1.27.0
 	golang.org/x/arch v0.7.0
 	golang.org/x/crypto v0.21.0
-	golang.org/x/exp v0.0.0-20240318143956-a85f2c67cd81
+	golang.org/x/exp v0.0.0-20240325151524-a685a6edb6d8
 	golang.org/x/image v0.15.0
 	golang.org/x/mobile v0.0.0-20240320162201-c76e57eead38
 	golang.org/x/mod v0.16.0
--- test/test3.go
+++ test/test3.go
@@ -1,36 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"github.com/richelieu-yang/chimera/v3/src/file/fileKit"
 	_ "github.com/richelieu-yang/chimera/v3/src/log/logrusInitKit"
-	"io/fs"
-	"path/filepath"
-	"regexp"
 )
 
 func main() {
-	re := regexp.MustCompile("\\[(\\d+)[vV]*(\\d*)\\]")
 
-	err := filepath.WalkDir("/Users/richelieu/Downloads/魔都精兵的奴隶", func(path string, d fs.DirEntry, err error) error {
-		if err != nil {
-			return err
-		}
-		if !fileKit.IsFile(path) {
-			return nil
-		}
-
-		name := fileKit.GetName(path)
-		ext := fileKit.GetExt(path)
-		s := re.FindAllStringSubmatch(name, -1)
-		newName := s[0][1] + ext
-
-		//fileKit.Rename()
-
-		fmt.Println(newName)
-		return nil
-	})
-	if err != nil {
-		panic(err)
-	}
 }
