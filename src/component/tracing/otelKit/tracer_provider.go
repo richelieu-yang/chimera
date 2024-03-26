8eb23c63552c309e7308e8eeaa48f2b77b43431d
Richelieu <richelieu042@gmail.com>
2024-03-26 14:01:49 +0800

    dev

--- src/component/tracing/otelKit/tracer_provider.go
+++ src/component/tracing/otelKit/tracer_provider.go
@@ -3,11 +3,20 @@ package otelKit
 import (
 	"context"
 	"github.com/sirupsen/logrus"
-	"go.opentelemetry.io/otel/sdk/trace"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	"go.opentelemetry.io/otel/trace"
+	"go.opentelemetry.io/otel/trace/noop"
+
 	"time"
 )
 
-func ShutdownTracerProvider(tp *trace.TracerProvider, timeout time.Duration) {
+// NewNoopTracerProvider returns a TracerProvider that does not record any telemetry.
+func NewNoopTracerProvider() trace.TracerProvider {
+	//return trace.NewNoopTracerProvider()
+	return noop.NewTracerProvider()
+}
+
+func ShutdownTracerProvider(tp *sdktrace.TracerProvider, timeout time.Duration) {
 	if tp == nil {
 		return
 	}
--- test/test3.go
+++ test/test3.go
@@ -2,8 +2,9 @@ package main
 
 import (
 	_ "github.com/richelieu-yang/chimera/v3/src/log/logrusInitKit"
+	"go.opentelemetry.io/otel/trace/noop"
 )
 
 func main() {
-
+	tp := noop.NewTracerProvider()
 }
