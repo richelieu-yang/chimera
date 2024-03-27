74f4eab43631813438f2b4b0ff3d812348a33e66
Richelieu <richelieu042@gmail.com>
2024-03-27 13:33:03 +0800

    dev

--- src/component/tracing/otelKit/gin_middleware.go
+++ src/component/tracing/otelKit/gin_middleware.go
@@ -1,11 +1,7 @@
 package otelKit
 
 import (
-	"context"
 	"github.com/gin-gonic/gin"
-	"go.opentelemetry.io/otel"
-	"go.opentelemetry.io/otel/attribute"
-	"go.opentelemetry.io/otel/trace"
 )
 
 // NewOuterGinMiddleware TODO: 适用于: 外层的服务
@@ -14,11 +10,11 @@ PS: 需要先 SetUp || MustSetUp !!!
 */
 func NewOuterGinMiddleware() (gin.HandlerFunc, error) {
 	return func(ctx *gin.Context) {
-		tracer := otel.Tracer("", trace.WithInstrumentationAttributes(attribute.String("111", "111")))
-		spanCtx, span := tracer.Start(context.TODO(), "main", trace.WithAttributes(attribute.String("222", "222")))
-		defer span.End()
-
-		ctx.Next()
+		//tracer := otel.Tracer("", trace.WithInstrumentationAttributes(attribute.String("111", "111")))
+		//spanCtx, span := tracer.Start(context.TODO(), "main", trace.WithAttributes(attribute.String("222", "222")))
+		//defer span.End()
+		//
+		//ctx.Next()
 	}, nil
 }
 
